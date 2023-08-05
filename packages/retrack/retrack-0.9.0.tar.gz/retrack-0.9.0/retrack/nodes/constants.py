2849aa47fc743d5ad92cbd6cf672adb26bafdedc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:57 +0000

    Add package retrack/0.9.0/retrack-0.9.0.tar.gz

---
+++ packages/retrack/retrack-0.9.0.tar.gz/retrack-0.9.0/retrack/nodes/constants.py
@@ -0,0 +1,99 @@
+import typing
+
+import pydantic
+
+from retrack.nodes.base import (
+    BaseNode,
+    InputConnectionModel,
+    NodeKind,
+    NodeMemoryType,
+    OutputConnectionModel,
+)
+
+#######################################################
+# Constant Metadata Models
+#######################################################
+
+
+class ConstantMetadataModel(pydantic.BaseModel):
+    value: str
+
+
+class ListMetadataModel(pydantic.BaseModel):
+    value: typing.List[str]
+
+
+class BoolMetadataModel(pydantic.BaseModel):
+    value: typing.Optional[bool] = pydantic.Field(False, alias="value")
+
+    @pydantic.validator("value")
+    def validate_value(cls, value):
+        if value is None:
+            return False
+
+        value = str(value).lower()
+
+        if value in ["true", "1", "yes", "y", "t"]:
+            return True
+
+        return False
+
+
+#######################################################
+# Constant Inputs and Outputs
+#######################################################
+
+
+class ConstantInputsModel(pydantic.BaseModel):
+    input_void: typing.Optional[InputConnectionModel] = None
+
+
+class ConstantOutputsModel(pydantic.BaseModel):
+    output_value: OutputConnectionModel
+
+
+class ListOutputsModel(pydantic.BaseModel):
+    output_list: OutputConnectionModel
+
+
+class BoolOutputsModel(pydantic.BaseModel):
+    output_bool: OutputConnectionModel
+
+
+#######################################################
+# Constant Nodes
+#######################################################
+
+
+class BaseConstant(BaseNode):
+    inputs: typing.Optional[ConstantInputsModel] = None
+
+    def kind(self) -> NodeKind:
+        return NodeKind.CONSTANT
+
+
+class Constant(BaseConstant):
+    data: ConstantMetadataModel
+    outputs: ConstantOutputsModel
+
+    def run(self, **kwargs) -> typing.Dict[str, typing.Any]:
+        return {"output_value": self.data.value}
+
+
+class List(BaseConstant):
+    data: ListMetadataModel
+    outputs: ListOutputsModel
+
+    def run(self, **kwargs) -> typing.Dict[str, typing.Any]:
+        return {}  # {"output_list": self.data.value}
+
+    def memory_type(self) -> NodeMemoryType:
+        return NodeMemoryType.CONSTANT
+
+
+class Bool(BaseConstant):
+    data: BoolMetadataModel = BoolMetadataModel(value=False)
+    outputs: BoolOutputsModel
+
+    def run(self, **kwargs) -> typing.Dict[str, typing.Any]:
+        return {"output_bool": self.data.value}
