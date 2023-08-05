d18944d04dac461e9bf025e0728a7a6cfbbbb870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:49 +0000

    Add package imgo/2.5.7/imgo-2.5.7.tar.gz

---
+++ packages/imgo/imgo-2.5.7.tar.gz/imgo-2.5.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="imgo",
+    version="2.5.7",
+    author="celerygemini",
+    description="Image Dataset Management Toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/celerygemini/imgo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
