b7d5417b07a39d120db98502f14ece284da23cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:22 +0000

    Add package yid-langchain-extensions/0.4.12/yid_langchain_extensions-0.4.12-py3-none-any.whl

---
+++ packages/yid-langchain-extensions/yid_langchain_extensions-0.4.12-py3-none-any.whl/yid_langchain_extensions/tools/utils.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from langchain import PromptTemplate
+from langchain.tools import BaseTool
+
+TOOLS_PROMPT = """AVAILABLE ACTIONS:
+------
+Now you have access to following actions in order to solve the task:
+{tools}"""
+
+
+def format_tools(tools: List[BaseTool]) -> str:
+    tools_string = "\n".join(
+        [f"> {tool.name}: {tool.description}" for tool in tools]
+    )
+    return PromptTemplate.from_template(TOOLS_PROMPT).format_prompt(
+        tools=tools_string
+    ).to_string()
+
+
+def format_tool_names(tools: List[BaseTool]) -> str:
+    return ", ".join([tool.name for tool in tools])
