5b778c4c273d90e98444414fea8a4e66444f4609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:35 +0000

    Add package litie/0.2.0/litie-0.2.0-py3-none-any.whl

---
+++ packages/litie/litie-0.2.0-py3-none-any.whl/litie/datasets/__init__.py
@@ -0,0 +1,4 @@
+from .ee import AutoEventExtractionDataModule
+from .ner import AutoNerDataModule
+from .re import AutoReDataModule
+from .uie import UieDataModule
---
+++ packages/litie/litie-0.2.0-py3-none-any.whl/litie/nn/__init__.py
@@ -0,0 +1,4 @@
+from .ee import AutoEventExtractionTaskModel, AutoEventExtractionTaskModelConfig
+from .ner import AutoNerTaskModel, AutoNerTaskModelConfig
+from .re import AutoReTaskModelConfig, AutoReTaskModel
+from .uie import UIEModel
