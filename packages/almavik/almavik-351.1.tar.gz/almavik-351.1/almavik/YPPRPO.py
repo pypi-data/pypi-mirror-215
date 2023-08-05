5286f0ef6b3a1d7bffd15d96f1134a0b479c1754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:29 +0000

    Add package almavik/351.1/almavik-351.1.tar.gz

---
+++ packages/almavik/almavik-351.1.tar.gz/almavik-351.1/almavik/YPPRPO.py
@@ -0,0 +1,146 @@
+import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QApplication,
+    QPushButton,
+    QVBoxLayout,
+    QHBoxLayout,
+    QWidget,
+    QLabel,
+    QTableWidgetItem,
+    QTableWidget,
+    QSlider,
+)
+from .widgets import LineCanvas, ImageCanvas, Table
+from .detectorDrop import detectorDrop
+
+counter = 0
+
+class Window(QWidget):
+    def __init__(self, det):
+        """
+        Initialize the main window of the application.
+        
+        Args:
+            det (detectorDrop): The detectorDrop object containing the data.
+        """
+        self.det = det
+        super().__init__()
+        self.setWindowTitle("Center of mass of the drop")
+
+        layout = QVBoxLayout()
+        line_layout = QHBoxLayout()
+        buttons_layout = QHBoxLayout()
+
+        self.slider_label = QLabel("0")
+        self.slider_label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.slider_label)
+
+        button1 = QPushButton("Previous Image")
+        button2 = QPushButton("Next Image")
+        imgCanvas = ImageCanvas(det_=det)
+        lineCanvas = LineCanvas(det_=det, current_image_idx=imgCanvas.current_image_idx)
+
+        def slider_value_changed():
+            table.show_row(imgCanvas.current_image_idx)
+            lineCanvas.current_image = slider.value()
+            lineCanvas.draw_line()
+
+        slider = QSlider()
+        slider.setOrientation(Qt.Horizontal)
+        slider.setRange(0, 400)
+        slider.setValue(0)
+        slider.setTickInterval(1)
+        slider.setTickPosition(QSlider.TicksBelow)
+        slider.valueChanged.connect(imgCanvas.update_image_from_slider)
+        slider.valueChanged.connect(slider_value_changed)
+
+        def button3_clicked():
+            """
+            Toggle the contour mode of the images.
+            """
+            global counter
+            counter += 1
+            if counter % 2 == 1:
+                imgCanvas.contour_or_not = 1
+                button3.setText("Disable Drop Contour Mode")
+            else:
+                imgCanvas.contour_or_not = 0
+                button3.setText("Enable Drop Contour Mode")
+
+        def button1_clicked():
+            """
+            Load and display the previous image.
+            """
+            imgCanvas.draw_previous_image()
+            lineCanvas.current_image = imgCanvas.current_image_idx
+            table.show_row(imgCanvas.current_image_idx)
+            slider.setValue(imgCanvas.current_image_idx)
+            lineCanvas.draw_line()
+
+        def button2_clicked():
+            """
+            Load and display the next image.
+            """
+            imgCanvas.draw_next_image()
+            lineCanvas.current_image = imgCanvas.current_image_idx
+            table.show_row(imgCanvas.current_image_idx)
+            slider.setValue(imgCanvas.current_image_idx)
+            lineCanvas.draw_line()
+
+        lineCanvas.draw_line()
+        table = Table(imgCanvas.current_image_idx, det)
+        table.show()
+
+        button3 = QPushButton("Enable Drop Contour Mode")
+        button3.clicked.connect(button3_clicked)
+        button1.clicked.connect(button1_clicked)
+        button2.clicked.connect(button2_clicked)
+
+        label = QLabel(str(slider.value()))
+        slider.valueChanged.connect(lambda value: label.setText(str(value)))
+
+        line_slider_layout = QtWidgets.QVBoxLayout()
+        line_slider_layout.addWidget(lineCanvas)
+        line_slider_layout.addWidget(slider)
+        img_layout = QtWidgets.QVBoxLayout()
+        img_layout.addWidget(imgCanvas)
+        img_layout.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+        img_table_layout = QtWidgets.QHBoxLayout()
+        img_table_layout.addStretch()
+        table.setFixedWidth(475)
+        img_table_layout.addWidget(table)
+        img_table_layout.addStretch()
+        img_layout.addLayout(img_table_layout)
+
+        line_layout.addLayout(line_slider_layout)
+        line_layout.addLayout(img_layout)
+        layout.addLayout(line_layout)
+
+        line_layout.addLayout(img_table_layout)
+        layout.addLayout(line_layout)
+
+        buttons_layout.addWidget(button1)
+        buttons_layout.addWidget(button2)
+        layout.addLayout(buttons_layout)
+        layout.addWidget(button3)
+
+        self.setLayout(layout)
+
+import pkg_resources
+
+file_path = pkg_resources.resource_filename("almavik", "exp1")
+
+#file_path = r"C:\Users\User\anaconda3\Lib\site-packages\almavik\exp1"
+
+
+def main(folder_path = file_path):
+    det = detectorDrop(folder_path)
+    app = QApplication(sys.argv)
+    window = Window(det)
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
---
+++ packages/almavik/almavik-351.1.tar.gz/almavik-351.1/almavik/widgets.py
@@ -0,0 +1,134 @@
+import os
+import csv
+
+import numpy as np
+import matplotlib.pyplot as plt
+from PyQt5.QtWidgets import QWidget, QTableWidget, QTableWidgetItem, QHBoxLayout
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
+
+
+class MplCanvas(FigureCanvasQTAgg):
+    def __init__(self, parent=None, width=7, height=7, dpi=100):
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        super(MplCanvas, self).__init__(self.fig)
+        self.axes = self.fig.add_subplot(1, 1, 1)
+        self.fig.tight_layout()
+
+    def update_canvas(self):
+        self.fig.canvas.draw()
+
+
+class LineCanvas(MplCanvas):
+    def __init__(self, det_=None, current_image_idx=0, parent=None, width=7, height=7, dpi=100):
+        super().__init__()
+        self.line = None
+        self.data = [list(row) for row in det_.center_mass]
+        self.current_image=current_image_idx
+
+    def draw_line(self):
+        self.axes.clear()
+
+        start_idx = max(self.current_image - 5, 0)
+        end_idx = min(self.current_image + 5, len(self.data))
+
+        data_to_plot = self.data[start_idx:end_idx]
+        x_data = [row[1] for row in data_to_plot]
+        y_data = [row[2] for row in data_to_plot]
+
+        self.axes.plot(x_data, y_data, color='black', linestyle='solid', marker='o')
+
+        current_point = data_to_plot[self.current_image - start_idx]
+        self.axes.plot(current_point[1], current_point[2], color='red', linestyle='solid', marker='o')
+
+        self.axes.set_xlim(min(x_data)-10, max(x_data)+10)
+        self.axes.set_ylim(min(y_data)-10, max(y_data)+10)
+
+        self.update_canvas()
+
+
+class ImageCanvas(MplCanvas):
+    def __init__(self, det_=None, parent=None, width=7, height=7, dpi=100):
+        super().__init__()
+        self.img = None
+        self.axes.axis("off")
+        self.current_image_idx = 0
+        self.contour_or_not=0
+        self.img_without_contour = det_.img
+        self.img_with_contour = det_.img_contour
+
+        self.image_paths = [f"Image {i}" for i in range(len(self.img_without_contour))]
+        self.load_image()
+
+    def load_image(self):
+        if self.contour_or_not == 0:
+            image = self.img_without_contour[self.current_image_idx]
+        else:
+            image = self.img_with_contour[self.current_image_idx]
+
+        if self.img is None:
+            self.img = self.axes.imshow(image, cmap="gray")
+        else:
+            self.img.set_data(image)
+            self.axes.draw_artist(self.img)
+            self.fig.canvas.blit(self.axes.bbox)
+
+    def draw_next_image(self):
+        if self.current_image_idx < len(self.image_paths) - 1:
+            self.current_image_idx += 1
+            self.load_image()
+
+    def draw_previous_image(self):
+        if self.current_image_idx > 0:
+            self.current_image_idx -= 1
+            self.load_image()
+
+    def update_image_from_slider(self, value):
+        value = value % 400
+        max_slider_value = len(self.img_without_contour)-1
+        image_index = int(value * len(self.image_paths) / max_slider_value)
+        self.current_image_idx = image_index
+        self.load_image()
+
+
+class Table(QWidget):
+    def __init__(self, current_image_idx, det):
+        super().__init__()
+
+        self.data = [list(row) for row in det.center_mass]
+        self.table = QTableWidget()
+        self.table.setColumnCount(len(self.data[0]))
+        self.table.setRowCount(3)
+        self.table.setVerticalHeaderLabels(["Previous", "Now", "Next"])
+        self.table.setHorizontalHeaderLabels(["Number", "x", "y"])
+
+        for i in range(len(self.data)):
+            for j in range(len(self.data[i])):
+                item = QTableWidgetItem(str(self.data[i][j]))
+                self.table.setItem(i, j, item)
+
+        layout = QHBoxLayout()
+        layout.addWidget(self.table)
+
+        self.setLayout(layout)
+        self.index = 0
+        self.show_row(current_image_idx)
+
+    def show_row(self, current_image_idx):
+        self.index = current_image_idx
+        row_indices = self.numbers_of_rows()
+        for row_idx, data_idx in enumerate(row_indices):
+            for col_idx in range(len(self.data[data_idx])):
+                item = QTableWidgetItem(str(self.data[data_idx][col_idx]))
+                self.table.setItem(row_idx, col_idx, item)
+
+    def numbers_of_rows(self):
+        row_indices = []
+        if self.index == 0:
+            row_indices.extend([0, 1, 2])
+        else:
+            if self.index == len(self.data) - 1:
+                row_indices.extend([self.index - 2, self.index - 1, self.index])
+            else:
+                row_indices.extend([self.index - 1, self.index, self.index + 1])
+        return row_indices
