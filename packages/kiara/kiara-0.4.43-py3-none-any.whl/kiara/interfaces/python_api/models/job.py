e37f58a82fc0510361f630e2a17e586bf5411b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:18 +0000

    Add package kiara/0.4.43/kiara-0.4.43-py3-none-any.whl

---
+++ packages/kiara/kiara-0.4.43-py3-none-any.whl/kiara/interfaces/python_api/models/job.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, Dict, List, Mapping, Union
+
+from pydantic import BaseModel, Field
+
+from kiara.exceptions import KiaraException
+from kiara.utils.cli import terminal_print
+from kiara.utils.files import get_data_from_file
+from kiara.utils.string_vars import replace_var_names_in_obj
+
+if TYPE_CHECKING:
+    from kiara.interfaces.python_api import KiaraAPI
+    from kiara.models.module.operation import Operation
+    from kiara.models.values.value import ValueMap
+
+
+class JobDesc(BaseModel):
+    """An object describing a compute job with both raw or referenced inputs."""
+
+    @classmethod
+    def create_from_file(cls, path: Union[str, Path]):
+
+        if isinstance(path, str):
+            path = Path(path)
+
+        if not path.is_file():
+            raise KiaraException(
+                f"Can't load job description, invalid file path: '{path}'"
+            )
+
+        data = get_data_from_file(path)
+
+        if not isinstance(data, Mapping):
+            raise KiaraException(f"Invalid job description in file: '{path}'")
+
+        if "operation" not in data.keys():
+            raise KiaraException(
+                f"Invalid job description in file: '{path}', missing 'operation' key"
+            )
+
+        repl_dict: Dict[str, Any] = {"this_dir": path.parent.as_posix()}
+        job_data = replace_var_names_in_obj(data, repl_dict=repl_dict)
+        job_data["job_alias"] = path.stem
+        return cls(**job_data)
+
+    job_alias: str = Field(description="The alias for the job.")
+    operation: str = Field(description="The operation id or module type.")
+    module_config: Union[Mapping[str, Any], None] = Field(
+        default=None, description="The configuration for the module."
+    )
+    inputs: Dict[str, Any] = Field(
+        description="The inputs for the job.", default_factory=dict
+    )
+
+    def get_operation(self, kiara_api: "KiaraAPI") -> "Operation":
+
+        if not self.module_config:
+            operation = kiara_api.get_operation(self.operation, allow_external=True)
+        else:
+            data = {
+                "module_type": self.operation,
+                "module_config": self.module_config,
+            }
+            operation = kiara_api.get_operation(operation=data, allow_external=False)
+
+        return operation
+
+
+class JobTest(object):
+    def __init__(
+        self,
+        kiara_api: "KiaraAPI",
+        job_desc: JobDesc,
+        tests: Union[Mapping[str, Mapping[str, Any]], None] = None,
+    ):
+
+        self._kiara_api: KiaraAPI = kiara_api
+        self._job_desc = job_desc
+        if tests is None:
+            tests = {}
+        self._tests = tests
+
+    def run_tests(self):
+
+        print(f"Running tests for job '{self._job_desc.job_alias}'...")  # noqa
+
+        result = self.run_job()
+        self.check_result(result)
+
+    def run_job(self) -> "ValueMap":
+
+        print(f"Running checks for job '{self._job_desc.job_alias}'...")  # noqa
+        try:
+            result = self._kiara_api.run_job(operation=self._job_desc)
+        except Exception as e:
+            exc = KiaraException(f"Failed to run job '{self._job_desc.job_alias}': {e}")
+            terminal_print(exc)
+            raise e
+        return result
+
+    def check_result(self, result: "ValueMap"):
+
+        try:
+
+            import inspect
+
+            from kiara.api import Value
+
+            for test_name, test in self._tests.items():
+
+                if not callable(test):
+                    tokens = test_name.split("::")
+                    value = result.get_value_obj(tokens[0])
+
+                    if len(tokens) > 1:
+                        data_to_test = self._kiara_api.query_value(
+                            value, "::".join(tokens[1:])
+                        )
+                    else:
+                        data_to_test = value
+
+                    if isinstance(data_to_test, Value):
+                        data_to_test = data_to_test.data
+
+                    if test != data_to_test:
+                        raise AssertionError(
+                            f"Test pattern '{test_name}' for job '{self._job_desc.job_alias}' failed: {data_to_test} (result) != {test} (expected)"
+                        )
+
+                else:
+
+                    args = inspect.signature(test)
+                    arg_values: List[Any] = []
+
+                    for arg_name in args.parameters.keys():
+                        if arg_name == "kiara_api":
+                            arg_values.append(self._kiara_api)
+                        elif arg_name == "outputs":
+                            arg_values.append(result)
+                        elif arg_name in result.field_names:
+                            arg_values.append(result.get_value_obj(arg_name))
+                        else:
+                            raise KiaraException(
+                                f"Invalid test function: '{test_name}', argument '{arg_name}' not available in result. Available arguments: {result.field_names} or 'outputs' for all outputs."
+                            )
+                    test(*arg_values)
+        except Exception as e:
+            exc = KiaraException(
+                f"Failed to run test '{test}' for job '{self._job_desc.job_alias}': {e}"
+            )
+            terminal_print(exc)
+            raise e
+        return result
---
+++ packages/kiara/kiara-0.4.43-py3-none-any.whl/kiara/modules/included_core_modules/filesystem.py
@@ -0,0 +1,306 @@
+# -*- coding: utf-8 -*-
+
+#  Copyright (c) 2021, University of Luxembourg / DHARPA project
+#  Copyright (c) 2021, Markus Binsteiner
+#
+#  Mozilla Public License, version 2.0 (see LICENSE or https://www.mozilla.org/en-US/MPL/2.0/)
+import os
+import shutil
+from typing import Any, List, Mapping, Type, Union
+
+import orjson
+from pydantic import Field
+
+from kiara.api import KiaraModuleConfig
+from kiara.exceptions import KiaraProcessingException
+from kiara.models.filesystem import FileBundle, FileModel, FolderImportConfig
+from kiara.models.values.value import SerializedData, ValueMap
+from kiara.modules import (
+    DEFAULT_NO_IDEMPOTENT_MODULE_CHARACTERISTICS,
+    KiaraModule,
+    ModuleCharacteristics,
+    ValueMapSchema,
+)
+from kiara.modules.included_core_modules.export_as import DataExportModule
+from kiara.modules.included_core_modules.serialization import DeserializeValueModule
+
+
+class ImportLocalFileModule(KiaraModule):
+
+    """Import a file from the local filesystem."""
+
+    _module_type_name = "import.local.file"
+
+    def create_inputs_schema(
+        self,
+    ) -> ValueMapSchema:
+
+        return {"path": {"type": "string", "doc": "The local path to the file."}}
+
+    def create_outputs_schema(
+        self,
+    ) -> ValueMapSchema:
+
+        return {"file": {"type": "file", "doc": "The loaded files."}}
+
+    def _retrieve_module_characteristics(self) -> ModuleCharacteristics:
+        return ModuleCharacteristics(is_idempotent=False)
+
+    def process(self, inputs: ValueMap, outputs: ValueMap):
+
+        path = inputs.get_value_data("path")
+
+        file = FileModel.load_file(source=path)
+        outputs.set_value("file", file)
+
+
+class DeserializeFileModule(DeserializeValueModule):
+
+    """Deserialize data to a 'file' value instance."""
+
+    _module_type_name = "deserialize.file"
+
+    @classmethod
+    def retrieve_supported_target_profiles(cls) -> Mapping[str, Type]:
+        return {"python_object": FileModel}
+
+    @classmethod
+    def retrieve_serialized_value_type(cls) -> str:
+        return "file"
+
+    @classmethod
+    def retrieve_supported_serialization_profile(cls) -> str:
+        return "copy"
+
+    def to__python_object(self, data: SerializedData, **config: Any):
+
+        keys = list(data.get_keys())
+        keys.remove("__file_metadata__")
+        assert len(keys) == 1
+
+        file_metadata_chunks = data.get_serialized_data("__file_metadata__")
+        assert file_metadata_chunks.get_number_of_chunks() == 1
+        file_metadata_json = list(file_metadata_chunks.get_chunks(as_files=False))
+        assert len(file_metadata_json) == 1
+        file_metadata = orjson.loads(file_metadata_json[0])
+
+        chunks = data.get_serialized_data(keys[0])
+        assert chunks.get_number_of_chunks() == 1
+
+        files = list(chunks.get_chunks(as_files=True, symlink_ok=True))
+        assert len(files) == 1
+
+        file: str = files[0]  # type: ignore
+
+        fm = FileModel.load_file(
+            source=file,
+            file_name=file_metadata["file_name"],
+        )
+        return fm
+
+
+class ImportFileBundleConfig(KiaraModuleConfig):
+
+    include_file_types: Union[None, List[str]] = Field(
+        description="File types to include.", default=None
+    )
+    exclude_file_types: Union[None, List[str]] = Field(
+        description="File types to include.", default=None
+    )
+
+
+class ImportLocalFileBundleModule(KiaraModule):
+
+    """Import a folder (file_bundle) from the local filesystem."""
+
+    _module_type_name = "import.local.file_bundle"
+    _config_cls = ImportFileBundleConfig
+
+    def create_inputs_schema(
+        self,
+    ) -> ValueMapSchema:
+
+        return {
+            "path": {"type": "string", "doc": "The local path of the folder to import."}
+        }
+
+    def create_outputs_schema(
+        self,
+    ) -> ValueMapSchema:
+
+        return {
+            "file_bundle": {"type": "file_bundle", "doc": "The imported file bundle."}
+        }
+
+    def _retrieve_module_characteristics(self) -> ModuleCharacteristics:
+        return DEFAULT_NO_IDEMPOTENT_MODULE_CHARACTERISTICS
+
+    def process(self, inputs: ValueMap, outputs: ValueMap):
+
+        path = inputs.get_value_data("path")
+
+        include = self.get_config_value("include_file_types")
+        exclude = self.get_config_value("exclude_file_types")
+
+        config = FolderImportConfig(include_files=include, exclude_files=exclude)
+
+        file_bundle = FileBundle.import_folder(source=path, import_config=config)
+        outputs.set_value("file_bundle", file_bundle)
+
+
+class DeserializeFileBundleModule(DeserializeValueModule):
+
+    """Deserialize data to a 'file' value instance."""
+
+    _module_type_name = "deserialize.file_bundle"
+
+    @classmethod
+    def retrieve_supported_target_profiles(cls) -> Mapping[str, Type]:
+        return {"python_object": FileBundle}
+
+    @classmethod
+    def retrieve_serialized_value_type(cls) -> str:
+        return "file_bundle"
+
+    @classmethod
+    def retrieve_supported_serialization_profile(cls) -> str:
+        return "copy"
+
+    def to__python_object(self, data: SerializedData, **config: Any):
+
+        keys = list(data.get_keys())
+        keys.remove("__file_metadata__")
+
+        file_metadata_chunks = data.get_serialized_data("__file_metadata__")
+        assert file_metadata_chunks.get_number_of_chunks() == 1
+        file_metadata_json = list(file_metadata_chunks.get_chunks(as_files=False))
+        assert len(file_metadata_json) == 1
+        metadata = orjson.loads(file_metadata_json[0])
+        file_metadata = metadata["included_files"]
+        bundle_name = metadata["bundle_name"]
+        # bundle_import_time = metadata["import_time"]
+        sum_size = metadata["size"]
+        number_of_files = metadata["number_of_files"]
+
+        included_files = {}
+        for rel_path in keys:
+
+            chunks = data.get_serialized_data(rel_path)
+            assert chunks.get_number_of_chunks() == 1
+
+            files = list(chunks.get_chunks(as_files=True, symlink_ok=True))
+            assert len(files) == 1
+
+            file: str = files[0]  # type: ignore
+            file_name = file_metadata[rel_path]["file_name"]
+            # import_time = file_metadata[rel_path]["import_time"]
+            fm = FileModel.load_file(source=file, file_name=file_name)
+            included_files[rel_path] = fm
+
+        fb = FileBundle(
+            included_files=included_files,
+            bundle_name=bundle_name,
+            # import_time=bundle_import_time,
+            number_of_files=number_of_files,
+            size=sum_size,
+        )
+        return fb
+
+
+class ExportFileModule(DataExportModule):
+
+    """Export files."""
+
+    _module_type_name = "export.file"
+
+    def export__file__as__file(self, value: FileModel, base_path: str, name: str):
+
+        target_path = os.path.join(base_path, value.file_name)
+
+        shutil.copy2(value.path, target_path)
+
+        return {"files": target_path}
+
+
+class PickFileFromFileBundleModule(KiaraModule):
+
+    """Pick a single file from a file_bundle value."""
+
+    _module_type_name = "file_bundle.pick.file"
+
+    def create_inputs_schema(
+        self,
+    ) -> ValueMapSchema:
+
+        return {
+            "file_bundle": {"type": "file_bundle", "doc": "The file bundle."},
+            "path": {"type": "string", "doc": "The relative path of the file to pick."},
+        }
+
+    def create_outputs_schema(
+        self,
+    ) -> ValueMapSchema:
+
+        return {"file": {"type": "file", "doc": "The file."}}
+
+    def process(self, inputs: ValueMap, outputs: ValueMap):
+
+        file_bundle: FileBundle = inputs.get_value_data("file_bundle")
+        path: str = inputs.get_value_data("path")
+
+        if path not in file_bundle.included_files.keys():
+            raise KiaraProcessingException(
+                f"Can't pick file '{path}' from file bundle: file not available."
+            )
+
+        file: FileModel = file_bundle.included_files[path]
+
+        outputs.set_value("file", file)
+
+
+class PickSubBundle(KiaraModule):
+
+    """Pick a sub-folder from a file_bundle, resulting in a new file_bundle."""
+
+    _module_type_name = "file_bundle.pick.sub_folder"
+
+    def create_inputs_schema(
+        self,
+    ) -> ValueMapSchema:
+
+        return {
+            "file_bundle": {"type": "file_bundle", "doc": "The file bundle."},
+            "sub_path": {
+                "type": "string",
+                "doc": "The relative path of the sub-folder to pick.",
+            },
+        }
+
+    def create_outputs_schema(self) -> ValueMapSchema:
+        return {
+            "file_bundle": {
+                "type": "file_bundle",
+                "doc": "The picked (sub-)file_bundle.",
+            }
+        }
+
+    def process(self, inputs: ValueMap, outputs: ValueMap):
+
+        file_bundle: FileBundle = inputs.get_value_data("file_bundle")
+        sub_path: str = inputs.get_value_data("sub_path")
+
+        result = {}
+        for path, file in file_bundle.included_files.items():
+            if path.startswith(sub_path):
+                result[path] = file
+
+        if not result:
+            raise KiaraProcessingException(
+                f"Can't pick sub-folder '{sub_path}' from file bundle: no matches."
+            )
+
+        new_file_bundle: FileBundle = FileBundle.create_from_file_models(
+            result, bundle_name=f"{file_bundle.bundle_name}_{sub_path}"
+        )
+
+        outputs.set_value("file_bundle", new_file_bundle)
