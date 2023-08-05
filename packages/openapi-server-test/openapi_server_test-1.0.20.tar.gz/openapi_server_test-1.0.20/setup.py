5612ae9e2221b8673c44ef05125b77db8ede5248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:34 +0000

    Add package openapi-server-test/1.0.20/openapi_server_test-1.0.20.tar.gz

---
+++ packages/openapi-server-test/openapi_server_test-1.0.20.tar.gz/openapi_server_test-1.0.20/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+api_version = "1.0.20"
+
+# api_version_file = "openapi_version"
+# try:
+#     with open(api_version_file, 'r') as f:
+#         val = f.readline().split()
+#         api_version = ".".join(val)
+# except OSError:
+#     print('openapi version file not found')
+
+# print(api_version)
+
+setuptools.setup(
+    name="openapi_server_test",
+    version=api_version,
+    author="BD Data Sys IE CDN",
+    description="CDN DMS Open API",
+    url="https://code.byted.org/savanna/dingman_api_server",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    install_requires = [
+        "gunicorn",
+        "gevent",
+        "flask",
+        "flask-limiter",
+        "flask-cors",
+        "grpcio==1.47.0",
+        "grpcio-tools==1.47.0",
+        "protobuf==3.20.1",
+        "requests",
+        "ipaddress",
+        "retry",
+        "swagger-ui-bundle>=0.0.2",
+        "python_dateutil>=2.6.0",
+        "connexion[swagger-ui]>=2.6.0; python_version>='3.6'",
+        "connexion[swagger-ui]<=2.3.0; python_version=='3.5' or python_version=='3.4'",
+        "werkzeug == 0.16.1; python_version=='3.5' or python_version=='3.4'",
+        "PyJWT"
+    ]
+)
