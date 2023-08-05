c12a16f1de21a6052d53077de43c81ee65c19012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:57 +0000

    Add package cellworld/0.0.188/cellworld-0.0.188.tar.gz

---
+++ packages/cellworld/cellworld-0.0.188.tar.gz/cellworld-0.0.188/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='cellworld',description='Maciver Lab computational biology research package',author='german espinosa',author_email='germanespinosa@gmail.com',packages=['cellworld'],install_requires=['numpy', 'matplotlib', 'json-cpp', 'tcp-messages', 'networkx', 'cv'],license='MIT',version='0.0.188',zip_safe=False)
