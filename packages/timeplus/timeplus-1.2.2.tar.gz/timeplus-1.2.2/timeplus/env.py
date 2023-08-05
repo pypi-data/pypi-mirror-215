e84a1702ec1aa2d8f54f19ab3ba1287fffe6593d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:44 +0000

    Add package timeplus/1.2.2/timeplus-1.2.2.tar.gz

---
+++ packages/timeplus/timeplus-1.2.2.tar.gz/timeplus-1.2.2/timeplus/env.py
@@ -0,0 +1,32 @@
+import swagger_client
+
+
+class Environment:
+    def __init__(self):
+        """Constructor"""
+        self._configuration = swagger_client.Configuration()
+        self._workspace = None
+        self._address = None
+
+    def apikey(self, key):
+        self._configuration.api_key["X-Api-Key"] = key
+        return self
+
+    def workspace(self, name):
+        self._workspace = name
+        if not self._address:
+            self._configuration.host = f"https//us.timeplus.cloud/{self._workspace}/api"
+        else:
+            self._configuration.host = f"{self._address}/{self._workspace}/api"
+        return self
+
+    def address(self, url):
+        self._address = url
+        if self._workspace:
+            self._configuration.host = f"{self._address}/{self._workspace}/api"
+        else:
+            self._configuration.host = f"{self._address}/api"
+        return self
+
+    def _conf(self):
+        return self._configuration
