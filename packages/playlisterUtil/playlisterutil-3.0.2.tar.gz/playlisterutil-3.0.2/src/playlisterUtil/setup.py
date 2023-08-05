49570ab9ac7f259ab4baf1735b8a21b733128ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:38 +0000

    Add package playlisterUtil/3.0.2/playlisterutil-3.0.2.tar.gz

---
+++ packages/playlisterUtil/playlisterutil-3.0.2.tar.gz/playlisterutil-3.0.2/src/playlisterUtil/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='playlisterUtil',
+    version='3.0.2',
+    packages=find_packages(),
+    install_requires=[
+        'pymongo==4.3.3'
+    ],
+    author='Your Name',
+    author_email='your@email.com',
+    description='A description of your module',
+)
