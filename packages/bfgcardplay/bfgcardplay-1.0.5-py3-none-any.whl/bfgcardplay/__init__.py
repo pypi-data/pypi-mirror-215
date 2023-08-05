ad8af516f2b0cf95b4348d86c362b044464a3e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:56 +0000

    Add package bfgcardplay/1.0.5/bfgcardplay-1.0.5-py3-none-any.whl

---
+++ packages/bfgcardplay/bfgcardplay-1.0.5-py3-none-any.whl/bfgcardplay/__init__.py
@@ -0,0 +1,6 @@
+"""Expose packages classes."""
+
+from bfgcardplay.source.opening_lead_suit import *
+from bfgcardplay.source.opening_lead_card import *
+from bfgcardplay.source.opening_lead import *
+from bfgcardplay.source.suggested_card import *
