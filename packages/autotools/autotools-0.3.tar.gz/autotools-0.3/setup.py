ec18e72180562ba632f7cbb312eae7e1ee160333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:21 +0000

    Add package autotools/0.3/autotools-0.3.tar.gz

---
+++ packages/autotools/autotools-0.3.tar.gz/autotools-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='autotools',
+      version='0.3',
+      description='many autools by automatic',
+      author='automatic',
+      author_email='automatick07@gmail.com',
+      requires=["openai", "gtts", "playsound", "SpeechRecognition"],
+      license='OpenSource',
+      packages=['autotools'],
+      install_requires=[],
+      zip_safe=False)
