df5d21da92defea9cd5eff735864ee9f91fe3cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:33 +0000

    Add package cgcomponents/2.0.4/cgcomponents-2.0.4.tar.gz

---
+++ packages/cgcomponents/cgcomponents-2.0.4.tar.gz/cgcomponents-2.0.4/README.md
@@ -0,0 +1,19 @@
+# CG Components
+
+## Getting Started
+#### Dependencies
+You need Python 3.7 or later to use **cgcomponents**.
+```
+pip install setuptools
+pip install wheel
+pip install twine
+```
+#### Installation
+Clone this repo to your local machine using:
+```
+git clone https://github.com/ramirooliveiracg/cgcomponents.git
+```
+## Features
+- File structure for PyPI packages
+- Setup with package informations
+- License example
---
+++ packages/cgcomponents/cgcomponents-2.0.4.tar.gz/cgcomponents-2.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="cgcomponents",
+    version="2.0.4",
+    description="Components para scrapping da CG.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ramirooliveiracg/cgcomponents",
+    author="Gabriel Dalacorte e Rogerio Filho",
+    author_email="gnunes.servico@gmail.com, crofilho2@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["cgcomponents"],
+    include_package_data=True,
+    install_requires=["requests", "Unidecode"],
+)
