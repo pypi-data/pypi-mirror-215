d0de339d35b2c58ffaf1a69e2833cf90bb63520d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:38 +0000

    Add package custom-diffusion/0.1.9/custom_diffusion-0.1.9.tar.gz

---
+++ packages/custom-diffusion/custom_diffusion-0.1.9.tar.gz/custom_diffusion-0.1.9/README.md
@@ -0,0 +1,75 @@
+<div align="center">
+<h2>
+     Custom Diffusion: Creating Video from Frame Using Diffusion
+</h2>
+<div>
+    <a href="https://pepy.tech/project/custom_diffusion"><img src="https://pepy.tech/badge/custom_diffusion" alt="downloads"></a>
+    <a href="https://badge.fury.io/py/custom_diffusion"><img src="https://badge.fury.io/py/custom_diffusion.svg" alt="pypi version"></a>
+    <a href="https://huggingface.co/spaces/ArtGAN/Stable-Diffusion-ControlNet-WebUI"><img src="https://huggingface.co/datasets/huggingface/badges/raw/main/open-in-hf-spaces-sm.svg" alt="HuggingFace Spaces"></a>
+</div>
+</div>
+
+
+### Installation
+```bash
+pip install custom_diffusion
+```
+
+### Usage
+```python
+
+# Importing the required libraries
+
+from custom_diffusion.utils.data_utils import load_images_from_folder
+from custom_diffusion import StableDiffusionControlNetGenerator
+from custom_diffusion.utils.video_utils import convert_images_to_video
+from custom_diffusion.demo import video_pipeline
+
+# Creating a video from a video file
+frames_path = video_pipeline(
+    video_path="test.mp4",
+    output_path="output.mp4",
+    start_time=0,
+    end_time=5,
+    frame_rate=1,
+)
+
+# Creating a video from a folder of images
+images_list = load_images_from_folder(frames_path)
+
+prompt = "a anime boy"
+negative_prompt = "bad"
+
+list_prompt = [prompt] * len(images_list)
+list_negative_prompt = [negative_prompt] * len(images_list)
+
+# Generating images from a list of images
+generator = StableDiffusionControlNetGenerator()
+
+generated_image_list = generator.generate_image(
+    stable_model_path="andite/anything-v4.0",
+    controlnet_model_path="lllyasviel/control_v11p_sd15_canny",
+    scheduler_name="EulerAncestralDiscrete",
+    images_list=images_list,
+    prompt=list_prompt,
+    negative_prompt=list_negative_prompt,
+    height=512,
+    width=512,
+    guess_mode=False,
+    num_images_per_prompt=1,
+    num_inference_steps=30,
+    guidance_scale=7.0,
+    controlnet_conditioning_scale=1.0,
+    generator_seed=0,
+    preprocess_type="Canny",
+    resize_type="center_crop_and_resize",
+    crop_size=512,
+)
+
+# Converting the generated images to a video
+frame2video = convert_images_to_video(
+    image_list=generated_image_list,
+    output_file="output.mp4",
+    frame_rate=5,
+)
+```
---
+++ packages/custom-diffusion/custom_diffusion-0.1.9.tar.gz/custom_diffusion-0.1.9/custom_diffusion/utils/data_utils.py
@@ -0,0 +1,76 @@
+def image_grid(imgs, rows, cols):
+    """
+    This function takes a list of images and creates a grid of images from them.
+
+    Args:
+    imgs (list): List of images to be used in the grid.
+    rows (int): Number of rows in the grid.
+
+    Returns:
+    grid (Image): The grid of images.
+    """
+    from PIL import Image
+
+    w, h = imgs[0].size
+    grid = Image.new("RGB", size=(cols * w, rows * h))
+
+    for i, img in enumerate(imgs):
+        grid.paste(img, box=(i % cols * w, i // cols * h))
+    return grid
+
+
+def load_images_from_folder(folder, pil_image=True):
+    """
+    Loads all .jpg and .png images in a specified folder and returns them as a list of PIL.Image objects.
+
+    Parameters:
+    folder (str): The path to the folder containing the images.
+
+    Returns:
+    list: A list of PIL.Image objects.
+
+    """
+    import os
+
+    from PIL import Image
+
+    images = []
+    for filename in os.listdir(folder):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            if pil_image:
+                img = Image.open(os.path.join(folder, filename))
+            else:
+                img = os.path.join(folder, filename)
+            if img is not None:
+                images.append(img)
+    return images
+
+
+def center_crop_and_resize(image, crop_size, height, width):
+    """
+    Crops the center of an image based on the input crop size and then resizes the image.
+
+    Parameters:
+    image_path (str): The path to the image to be processed.
+    crop_size (int): The size of the square crop. The function will crop a square with this side length from the center of the image.
+    height (int): The height of the resized image.
+    width (int): The width of the resized image.
+
+    Returns:
+    PIL.Image: The cropped and resized image.
+
+    """
+    # Calculate the center and the crop box coordinates
+    width, height = image.size
+    left = (width - crop_size) / 2
+    upper = (height - crop_size) / 2
+    right = (width + crop_size) / 2
+    lower = (height + crop_size) / 2
+
+    # Crop the image using the calculated coordinates
+    cropped_img = image.crop((left, upper, right, lower))
+
+    # Resize the cropped image to the specified size
+    resized_img = cropped_img.resize((height, width))
+
+    return resized_img
