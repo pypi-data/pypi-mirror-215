5e5515a965b74c35002321791d77c562336109e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:26 +0000

    Add package dbl-discoverx/0.0.1/dbl_discoverx-0.0.1.tar.gz

---
+++ packages/dbl-discoverx/dbl_discoverx-0.0.1.tar.gz/dbl_discoverx-0.0.1/discoverx/dx.py
@@ -0,0 +1,380 @@
+from functools import wraps
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as func
+from typing import List, Optional, Union
+from discoverx import logging
+from discoverx.msql import Msql
+from discoverx.rules import Rules, Rule
+from discoverx.scanner import Scanner
+from discoverx.classification import Classifier
+
+
+class DX:
+    """DiscoverX scans and searches your lakehouse
+
+    DiscoverX scans your data for patterns which have been pre-defined
+    as rules. You can either use standard rules which come with
+    DiscoverX or define and add custom rules.
+    Attributes:
+        custom_rules (List[Rule], Optional): Custom rules which will be
+            used to detect columns with corresponding patterns in your
+            data
+        classification_threshold (float, optional):
+            The threshold which will associate a column with a specific
+            rule and classify accordingly. The minimum and maximum
+            threshold values which can be specified are 0 and 1
+            respectively. The former corresponds to none of the records
+            for that column conforming to the given rule while the
+            latter means that all records conform.
+        spark (SparkSession, optional): The SparkSession which will be
+            used to scan your data. Defaults to None.
+        locale (str, optional): The two-letter country code which will be
+            used to determine the localized scanning rules.
+            Defaults to None.
+    """
+
+    def __init__(
+        self,
+        custom_rules: Optional[List[Rule]] = None,
+        classification_threshold: float = 0.95,
+        spark: Optional[SparkSession] = None,
+        locale: str = None,
+        classification_table_name: str = "_discoverx.classification.classes",
+    ):
+
+        if spark is None:
+            spark = SparkSession.getActiveSession()
+        self.spark = spark
+        self.logger = logging.Logging()
+
+        self.rules = Rules(custom_rules=custom_rules, locale=locale)
+
+        self.classification_threshold = self._validate_classification_threshold(classification_threshold)
+        self.classification_table_name = classification_table_name
+
+        self.uc_enabled = self.spark.conf.get("spark.databricks.unityCatalog.enabled", "false")
+
+        self.scanner: Optional[Scanner] = None
+        self.classifier: Optional[Classifier] = None
+
+        self.intro()
+
+    def intro(self):
+        # TODO: Decide on how to do the introduction
+        intro_text = """
+        <h1>Hi there, I'm DiscoverX.</h1>
+
+        <p>
+          I'm here to help you discover data in your lakehouse.<br />
+          You can scan your lakehouse by using
+        </p>
+        <pre><code>dx.scan()</code></pre>
+        <p>
+          For more detailed instructions, check out the <a href="https://github.com/databrickslabs/discoverx">readme</a> or use
+        </p>
+        <pre><code>help(DX)</code></pre>
+        """
+
+        missing_uc_text = """
+        <h1 style="color: red">Uch! DiscoverX needs Unity Catalog to be enabled</h1>
+
+        <p>
+          Please make sure you have Unity Catalog enabled, and that you are running a Cluster that supports Unity Catalog.
+        </p>
+        """
+
+        if self.uc_enabled == "true":
+            self.logger.friendlyHTML(intro_text)
+        else:
+            self.logger.friendlyHTML(missing_uc_text)
+
+    def display_rules(self):
+        """Displays the available rules in a friendly HTML format"""
+        text = self.rules.get_rules_info()
+        self.logger.friendlyHTML(text)
+
+    def scan(
+        self,
+        from_tables="*.*.*",
+        rules="*",
+        sample_size=10000,
+        what_if: bool = False,
+    ):
+        """Scans the lakehouse for columns matching the given rules
+
+        Args:
+            from_tables (str, optional): The tables to be scanned in format "catalog.schema.table", use "*" as a wildcard. Defaults to "*.*.*".
+            rules (str, optional): The rule names to be used to scan the lakehouse, use "*" as a wildcard. Defaults to "*".
+            sample_size (int, optional): The number of rows to be scanned per table. Defaults to 10000.
+            what_if (bool, optional): Whether to run the scan in what-if mode and print the SQL commands instead of executing them. Defaults to False.
+        """
+        catalogs, schemas, tables = Msql.validate_from_components(from_tables)
+
+        self.scanner = Scanner(
+            self.spark,
+            self.rules,
+            catalogs=catalogs,
+            schemas=schemas,
+            tables=tables,
+            rule_filter=rules,
+            sample_size=sample_size,
+            what_if=what_if,
+        )
+
+        self.scanner.scan()
+        self._classify(self.classification_threshold)
+
+    def scan_result(self):
+        """Returns the scan results as a pandas DataFrame
+
+        Raises:
+            Exception: If the scan has not been run
+        """
+        if self.scanner is None:
+            raise Exception("You first need to scan your lakehouse using Scanner.scan()")
+        if self.scanner.scan_result is None:
+            raise Exception("Your scan did not finish successfully. Please consider rerunning Scanner.scan()")
+
+        return self.scanner.scan_result.df
+
+    def _classify(self, classification_threshold: float):
+        """Classifies the columns in the lakehouse
+
+        Args:
+            classification_threshold (float): The frequency threshold (0 to 1) above which a column will be classified
+
+        Raises:
+            Exception: If the scan has not been run"""
+
+        if self.scanner is None:
+            raise Exception("You first need to scan your lakehouse using Scanner.scan()")
+        if self.scanner.scan_result is None:
+            raise Exception("Your scan did not finish successfully. Please consider rerunning Scanner.scan()")
+
+        self.classifier = Classifier(
+            classification_threshold,
+            self.scanner,
+            self.spark,
+            self.classification_table_name,
+        )
+
+        self.logger.friendlyHTML(self.classifier.summary_html)
+
+    def save(self):
+        """Publishes the classification results to the lakehouse
+
+        Raises:
+            Exception: If the classification has not been run
+
+        """
+
+        # save classes
+        self.classifier.save()
+
+    def search(self, search_term: str, from_tables: str = "*.*.*", by_class: Optional[str] = None):
+        """Searches your lakehouse for columns matching the given search term
+
+        Args:
+            search_term (str): The search term to be used to search for columns.
+            from_tables (str, optional): The tables to be searched in format "catalog.schema.table", use "*" as a wildcard. Defaults to "*.*.*".
+            by_class (str, optional): The class to be used to search for columns. Defaults to None.
+
+        Raises:
+            ValueError: If search_term is not provided
+            ValueError: If the search_term type is not valid
+            ValueError: If the by_class type is not valid
+
+        Returns:
+            DataFrame: A dataframe containing the results of the search
+        """
+
+        Msql.validate_from_components(from_tables)
+
+        if search_term is None:
+            raise ValueError("search_term has not been provided.")
+
+        if not isinstance(search_term, str):
+            raise ValueError(f"The search_term type {type(search_term)} is not valid. Please use a string type.")
+
+        if by_class is None:
+            # Trying to infer the class by the search term
+            self.logger.friendly(
+                "You did not provide any class to be searched."
+                "We will try to auto-detect matching rules for the given search term"
+            )
+            search_matching_rules = self.rules.match_search_term(search_term)
+            if len(search_matching_rules) == 0:
+                raise ValueError(
+                    f"Could not infer any class for the given search term. Please specify the by_class parameter."
+                )
+            elif len(search_matching_rules) > 1:
+                raise ValueError(
+                    f"Multiple classes {search_matching_rules} match the given search term ({search_term}). Please specify the class to search in with the by_class parameter."
+                )
+            else:
+                by_class = search_matching_rules[0]
+            self.logger.friendly(f"Discoverx will search your lakehouse using the class {by_class}")
+        elif isinstance(by_class, str):
+            search_matching_rules = [by_class]
+        else:
+            raise ValueError(f"The provided by_class {by_class} must be of string type.")
+
+        sql_filter = f"[{search_matching_rules[0]}] = '{search_term}'"
+        select_statement = (
+            "named_struct("
+            + ", ".join(
+                [
+                    f"'{rule_name}', named_struct('column', '[{rule_name}]', 'value', [{rule_name}])"
+                    for rule_name in search_matching_rules
+                ]
+            )
+            + ") AS search_result"
+        )
+
+        if search_term is None:
+            where_statement = ""
+        else:
+            where_statement = f"WHERE {sql_filter}"
+
+        return self._msql(
+            f"SELECT {select_statement}, to_json(struct(*)) AS row_content FROM {from_tables} {where_statement}"
+        )
+
+    def select_by_classes(self, from_tables: str = "*.*.*", by_classes: Optional[Union[List[str], str]] = None):
+        """Selects all columns in the lakehouse from tables that match ALL the given classes
+
+        Args:
+            from_tables (str, optional): The tables to be selected in format "catalog.schema.table", use "*" as a wildcard. Defaults to "*.*.*".
+            by_classes (Union[List[str], str], optional): The classes to be used to search for columns. Defaults to None.
+
+        Raises:
+            ValueError: If the by_classes type is not valid
+
+        Returns:
+            DataFrame: A dataframe containing the UNION ALL results of the select"""
+
+        Msql.validate_from_components(from_tables)
+
+        if isinstance(by_classes, str):
+            by_classes = [by_classes]
+        elif isinstance(by_classes, list) and all(isinstance(elem, str) for elem in by_classes):
+            by_classes = by_classes
+        else:
+            raise ValueError(
+                f"The provided by_classes {by_classes} have the wrong type. Please provide"
+                f" either a str or List[str]."
+            )
+
+        from_statement = (
+            "named_struct("
+            + ", ".join(
+                [
+                    f"'{class_name}', named_struct('column', '[{class_name}]', 'value', [{class_name}])"
+                    for class_name in by_classes
+                ]
+            )
+            + ") AS classified_columns"
+        )
+
+        return self._msql(f"SELECT {from_statement}, to_json(struct(*)) AS row_content FROM {from_tables}")
+
+    def delete_by_class(
+        self,
+        from_tables="*.*.*",
+        by_class: str = None,
+        values: Optional[Union[List[str], str]] = None,
+        yes_i_am_sure: bool = False,
+    ):
+        """Deletes all rows in the lakehouse that match any of the provided values in a column classified with the given class
+
+        Args:
+            from_tables (str, optional): The tables to delete from in format "catalog.schema.table", use "*" as a wildcard. Defaults to "*.*.*".
+            by_class (str, optional): The class to be used to search for columns. Defaults to None.
+            values (Union[List[str], str], optional): The values to be deleted. Defaults to None.
+            yes_i_am_sure (bool, optional): Whether you are sure that you want to delete the data. If False prints the SQL statements instead of executing them. Defaults to False.
+
+        Raises:
+            ValueError: If the from_tables is not valid
+            ValueError: If the by_class is not valid
+            ValueError: If the values is not valid
+        """
+
+        Msql.validate_from_components(from_tables)
+
+        if (by_class is None) or (not isinstance(by_class, str)):
+            raise ValueError(f"Please provide a class to identify the columns to be matched on the provided values.")
+
+        if values is None:
+            raise ValueError(
+                f"Please specify the values to be deleted. You can either provide a list of values or a single value."
+            )
+        elif isinstance(values, str):
+            value_string = f"'{values}'"
+        elif isinstance(values, list) and all(isinstance(elem, str) for elem in values):
+            value_string = "'" + "', '".join(values) + "'"
+        else:
+            raise ValueError(
+                f"The provided values {values} have the wrong type. Please provide" f" either a str or List[str]."
+            )
+
+        if not yes_i_am_sure:
+            self.logger.friendly(
+                f"Please confirm that you want to delete the following values from the table {from_tables} using the class {by_class}: {values}"
+            )
+            self.logger.friendly(
+                f"If you are sure, please run the same command again but set the parameter yes_i_am_sure to True."
+            )
+
+        return self._msql(
+            f"DELETE FROM {from_tables} WHERE [{by_class}] IN ({value_string})", what_if=(not yes_i_am_sure)
+        )
+
+    def _msql(self, msql: str, what_if: bool = False):
+
+        self.logger.debug(f"Executing sql template: {msql}")
+
+        msql_builder = Msql(msql)
+
+        # check if classification is available
+        # Check for more specific exception
+        try:
+            classification_result_pdf = (
+                self.spark.sql(f"SELECT * FROM {self.classification_table_name}")
+                .filter(func.col("current") == True)
+                .select(
+                    func.col("table_catalog").alias("catalog"),
+                    func.col("table_schema").alias("schema"),
+                    func.col("table_name").alias("table"),
+                    func.col("column_name").alias("column"),
+                    "class_name",
+                )
+                .toPandas()
+            )
+        except Exception:
+            raise Exception("Classification is not available")
+
+        sql_rows = msql_builder.build(classification_result_pdf)
+
+        if what_if:
+            self.logger.friendly(f"SQL that would be executed:")
+
+            for sql_row in sql_rows:
+                self.logger.friendly(sql_row.sql)
+
+            return None
+        else:
+            self.logger.debug(f"Executing SQL:\n{sql_rows}")
+            return msql_builder.execute_sql_rows(sql_rows, self.spark)
+
+    def _validate_classification_threshold(self, threshold) -> float:
+        """Validate that threshold is in interval [0,1]
+        Args:
+            threshold (float): The threshold value to be validated
+        Returns:
+            float: The validated threshold value
+        """
+        if (threshold < 0) or (threshold > 1):
+            error_msg = f"classification_threshold has to be in interval [0,1]. Given value is {threshold}"
+            self.logger.error(error_msg)
+            raise ValueError(error_msg)
+        return threshold
---
+++ packages/dbl-discoverx/dbl_discoverx-0.0.1.tar.gz/dbl_discoverx-0.0.1/discoverx/scanner.py
@@ -0,0 +1,274 @@
+from dataclasses import dataclass
+import pandas as pd
+import concurrent.futures
+from pyspark.sql import SparkSession
+from typing import Optional, List, Set
+
+from discoverx.common.helper import strip_margin, format_regex
+from discoverx import logging
+from discoverx.rules import Rules, RuleTypes
+
+logger = logging.Logging()
+
+
+@dataclass
+class ColumnInfo:
+    name: str
+    data_type: str
+    partition_index: int
+    classes: list[str]
+
+
+@dataclass
+class TableInfo:
+    catalog: Optional[str]
+    schema: str
+    table: str
+    columns: list[ColumnInfo]
+
+    def get_columns_by_class(self, class_name: str):
+        return [ClassifiedColumn(col.name, class_name) for col in self.columns if class_name in col.classes]
+
+
+@dataclass
+class ClassifiedColumn:
+    name: str
+    class_name: str
+
+
+@dataclass
+class ScanContent:
+    table_list: List[TableInfo]
+    catalogs: Set[str]
+    schemas: Set[str]
+
+    @property
+    def n_catalogs(self) -> int:
+        return len(self.catalogs)
+
+    @property
+    def n_schemas(self) -> int:
+        return len(self.schemas)
+
+    @property
+    def n_tables(self) -> int:
+        return len(self.table_list)
+
+
+@dataclass
+class ScanResult:
+    df: pd.DataFrame
+
+    @property
+    def is_empty(self) -> bool:
+        return self.df.empty
+
+    @property
+    def n_scanned_columns(self) -> int:
+        return len(self.df[["table_catalog", "table_schema", "table_name", "column_name"]].drop_duplicates())
+
+
+class Scanner:
+
+    COLUMNS_TABLE_NAME = "system.information_schema.columns"
+    MAX_WORKERS = 10
+
+    def __init__(
+        self,
+        spark: SparkSession,
+        rules: Rules,
+        catalogs: str = "*",
+        schemas: str = "*",
+        tables: str = "*",
+        rule_filter: str = "*",
+        sample_size: int = 1000,
+        what_if: bool = False,
+    ):
+        self.spark = spark
+        self.rules = rules
+        self.catalogs = catalogs
+        self.schemas = schemas
+        self.tables = tables
+        self.rules_filter = rule_filter
+        self.sample_size = sample_size
+        self.what_if = what_if
+
+        self.content: ScanContent = self._resolve_scan_content()
+        self.rule_list = self.rules.get_rules(rule_filter=self.rules_filter)
+        self.scan_result: Optional[ScanResult] = None
+
+    def _get_list_of_tables(self) -> List[TableInfo]:
+        table_list_sql = self._get_table_list_sql()
+
+        rows = self.spark.sql(table_list_sql).collect()
+        filtered_tables = [
+            TableInfo(
+                row["table_catalog"],
+                row["table_schema"],
+                row["table_name"],
+                [
+                    ColumnInfo(col["column_name"], col["data_type"], col["partition_index"], [])
+                    for col in row["table_columns"]
+                ],
+            )
+            for row in rows
+        ]
+        return filtered_tables
+
+    def _get_table_list_sql(self):
+        """
+        Returns a SQL expression which returns a list of columns matching
+        the specified filters
+
+        Returns:
+            string: The SQL expression
+        """
+
+        catalog_sql = f"""AND regexp_like(table_catalog, "^{self.catalogs.replace("*", ".*")}$")"""
+        schema_sql = f"""AND regexp_like(table_schema, "^{self.schemas.replace("*", ".*")}$")"""
+        table_sql = f"""AND regexp_like(table_name, "^{self.tables.replace("*", ".*")}$")"""
+
+        sql = f"""
+        SELECT 
+            table_catalog, 
+            table_schema, 
+            table_name, 
+            collect_list(struct(column_name, data_type, partition_index)) as table_columns
+        FROM {self.COLUMNS_TABLE_NAME}
+        WHERE 
+            table_schema != "information_schema" 
+            {catalog_sql if self.catalogs != "*" else ""}
+            {schema_sql if self.schemas != "*" else ""}
+            {table_sql if self.tables != "*" else ""}
+        GROUP BY table_catalog, table_schema, table_name
+        """
+
+        return strip_margin(sql)
+
+    def _resolve_scan_content(self) -> ScanContent:
+        table_list = self._get_list_of_tables()
+        catalogs = set(map(lambda x: x.catalog, table_list))
+        schemas = set(map(lambda x: f"{x.catalog}.{x.schema}", table_list))
+
+        return ScanContent(table_list, catalogs, schemas)
+
+    def scan_table(self, table):
+        try:
+            if self.what_if:
+                logger.friendly(f"SQL that would be executed for '{table.catalog}.{table.schema}.{table.table}'")
+            else:
+                logger.friendly(f"Scanning table '{table.catalog}.{table.schema}.{table.table}'")
+
+            # Build rule matching SQL
+            sql = self._rule_matching_sql(table)
+
+            if self.what_if:
+                logger.friendly(sql)
+            else:
+                # Execute SQL and return the result
+                return self.spark.sql(sql).toPandas()
+        except Exception as e:
+            logger.error(f"Error while scanning table '{table.catalog}.{table.schema}.{table.table}': {e}")
+            return None
+
+    def scan(self):
+
+        logger.friendly("""Ok, I'm going to scan your lakehouse for data that matches your rules.""")
+        text = f"""
+                This is what you asked for:
+
+                    catalogs ({self.content.n_catalogs}) = {self.catalogs}
+                    schemas ({self.content.n_schemas}) = {self.schemas}
+                    tables ({self.content.n_tables}) = {self.tables}
+                    rules ({len(self.rule_list)}) = {self.rules_filter}
+                    sample_size = {self.sample_size}
+
+                This may take a while, so please be patient. I'll let you know when I'm done.
+                ...
+                """
+
+        logger.friendly(strip_margin(text))
+
+        logger.debug("Launching lakehouse scanning task\n")
+
+        if len(self.content.table_list) == 0:
+            raise Exception("No tables found matching your filters")
+
+        dfs = []
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.MAX_WORKERS) as executor:
+            # Submit tasks to the thread pool
+            futures = [executor.submit(self.scan_table, table) for table in self.content.table_list]
+
+            # Process completed tasks
+            for future in concurrent.futures.as_completed(futures):
+                result = future.result()
+                if result is not None:
+                    dfs.append(result)
+
+        logger.debug("Finished lakehouse scanning task")
+
+        if dfs:
+            self.scan_result = ScanResult(df=pd.concat(dfs))
+        else:
+            raise Exception("No tables were scanned successfully.")
+
+    def _rule_matching_sql(self, table_info: TableInfo):
+        """
+        Given a table and a set of rules this method will return a
+        SQL expression which matches the table's columns against rules.
+        If executed on the table using SQL the output will contain a
+        matching frequency (probability) for each column and rule.
+        Args:
+            table_info (TableInfo): Specifies the table to be scanned
+
+        Returns:
+            string: The SQL expression
+
+        """
+
+        expressions = [r for r in self.rule_list if r.type == RuleTypes.REGEX]
+        cols = [c for c in table_info.columns if c.data_type.lower() == "string"]
+
+        if not cols:
+            raise Exception(f"There are no columns of type string to be scanned in {table_info.table}")
+
+        if not expressions:
+            raise Exception(f"There are no rules to scan for.")
+
+        catalog_str = f"{table_info.catalog}." if table_info.catalog else ""
+        matching_columns = [
+            f"INT(regexp_like(value, '{format_regex(r.definition)}')) AS `{r.name}`" for r in expressions
+        ]
+        matching_string = ",\n                    ".join(matching_columns)
+
+        unpivot_expressions = ", ".join([f"'{r.name}', `{r.name}`" for r in expressions])
+        unpivot_columns = ", ".join([f"'{c.name}', `{c.name}`" for c in cols])
+
+        sql = f"""
+            SELECT 
+                '{table_info.catalog}' as table_catalog,
+                '{table_info.schema}' as table_schema,
+                '{table_info.table}' as table_name, 
+                column_name,
+                class_name,
+                (sum(value) / count(value)) as frequency
+            FROM
+            (
+                SELECT column_name, stack({len(expressions)}, {unpivot_expressions}) as (class_name, value)
+                FROM 
+                (
+                    SELECT
+                    column_name,
+                    {matching_string}
+                    FROM (
+                        SELECT
+                            stack({len(cols)}, {unpivot_columns}) AS (column_name, value)
+                        FROM {catalog_str}{table_info.schema}.{table_info.table}
+                        TABLESAMPLE ({self.sample_size} ROWS)
+                    )
+                )
+            )
+            GROUP BY table_catalog, table_schema, table_name, column_name, class_name
+        """
+
+        return strip_margin(sql)
