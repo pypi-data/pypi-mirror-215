e7989697bedbb6370ab13a8a4ffc269a10c7d569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:07 +0000

    Add package nuclia/1.0.3/nuclia-1.0.3.tar.gz

---
+++ packages/nuclia/nuclia-1.0.3.tar.gz/nuclia-1.0.3/nuclia/tests/test_auth.py
@@ -0,0 +1,18 @@
+from nuclia.sdk.auth import NucliaAuth
+from nuclia.tests.fixtures import TESTING_KB
+
+
+def test_auth_user(testing_user: str):
+    na = NucliaAuth()
+    assert na._validate_user_token(testing_user)
+
+
+def test_auth_kb(testing_kb: str):
+    na = NucliaAuth()
+    assert na.validate_kb(TESTING_KB, testing_kb)
+
+
+def test_auth_nua(testing_nua: str):
+    pass
+    # na = NucliaAuth()
+    # assert na.validate_nua("europe-1", testing_nua)
