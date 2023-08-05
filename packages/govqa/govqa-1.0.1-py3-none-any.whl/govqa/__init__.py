6a8dfbfcbb879af688eb26dd6360f3cf3972a139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:35 +0000

    Add package govqa/1.0.1/govqa-1.0.1-py3-none-any.whl

---
+++ packages/govqa/govqa-1.0.1-py3-none-any.whl/govqa/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "1.0.1"
+
+from .base import (
+    EmailAlreadyExists,
+    FormValidationError,
+    GovQA,
+    IncorrectCaptcha,
+    UnauthenticatedError,
+    UnsupportedSite,
+)
