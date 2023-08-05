6d74d96b7875e2a44ddd7eefebf2f94819507169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:43 +0000

    Add package chatexampy/0.1/chatexampy-0.1.tar.gz

---
+++ packages/chatexampy/chatexampy-0.1.tar.gz/chatexampy-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='chatexampy', #name задает полное имя пакета.
+      version = "0.1",
+      description = "example chat",
+      author = "Liuba Kundas",
+      author_email = "kundasl1@gmail.com",
+      #url = "http://www.blog.pythonlibrary.org",
+      packages=["src"]
+      )
