a0ce6602f20f999081cb65f2dc35d1bdf7bb38e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:21 +0000

    Add package nlp-models/2.3.2/nlp-models-2.3.2.tar.gz

---
+++ packages/nlp-models/nlp-models-2.3.2.tar.gz/nlp-models-2.3.2/src/bert_classifier/data.py
@@ -0,0 +1,63 @@
+'''
+data modules
+'''
+import torch
+from torch.utils.data import Dataset
+
+
+class CustomDataset(Dataset):
+    '''
+    Class to construct torch Dataset from dataframe
+    '''
+    def __init__(self,
+                 dataframe,
+                 data_field,
+                 label_field,
+                 tokenizer,
+                 max_len,
+                 multi_label=False
+                 ):
+        self.max_len = max_len
+        self.data = dataframe
+        self.tokenizer = tokenizer
+        self.content = self.data[data_field]
+        self.label = self.data[label_field]
+        self.multi_label = multi_label
+
+    def __len__(self):
+        return len(self.content)
+
+    def __getitem__(self, index):
+        content = str(self.content[index])
+        content = " ".join(content.split())
+        encoded_content = self.tokenizer.encode_plus(
+            content,
+            add_special_tokens=True,
+            truncation=True,
+            padding='max_length',
+            max_length=self.max_len,
+            return_attention_mask=True,
+            return_token_type_ids=True,
+            return_tensors='pt'
+        )
+        features = encoded_content
+        if self.multi_label:
+            labels = torch.tensor(self.label[index], dtype=torch.float)
+        else:
+            labels = torch.tensor(self.label[index], dtype=torch.long)
+
+        return features, labels
+
+
+def create_label_dict(dataframe, label_col):
+    labels = dataframe.groupby(label_col).size().sort_values(ascending=False).index.tolist()
+    label_dict = dict([(d, i) for i, d in enumerate(labels)])
+    return label_dict
+
+
+def label2id(dataframe, label_col, label_dict, multi_label=False):
+    if multi_label:
+        dataframe[label_col] = dataframe[label_col].apply(lambda c: [int(k in c) for k in label_dict.keys()])
+    else:
+        dataframe[label_col] = dataframe[label_col].apply(lambda c: label_dict[c])
+    return dataframe
---
+++ packages/nlp-models/nlp-models-2.3.2.tar.gz/nlp-models-2.3.2/src/multi_task_model/metrics.py
@@ -0,0 +1,44 @@
+from typing import Optional, Literal
+import torch
+from torchmetrics.classification import (
+    MultilabelHammingDistance,
+    MulticlassHammingDistance,
+    MultilabelAccuracy,
+    MulticlassAccuracy
+)
+
+
+def hamming_distance(
+    outputs,
+    targets,
+    num_labels: int,
+    multi_label: bool = False,
+    device: torch.device = torch.device('cpu'),
+    average: Literal['micro', 'macro', 'weighted', 'none'] = 'macro'
+):
+    if not num_labels:
+        raise ValueError('num_labels is required')
+    if multi_label:
+        hamming = MultilabelHammingDistance(num_labels, average=average)
+    else:
+        hamming = MulticlassHammingDistance(num_labels, average=average)
+    hamming.to(device)
+    return hamming(outputs, targets)
+
+
+def accuracy(
+    outputs,
+    targets,
+    num_labels: int,
+    multi_label: bool = False,
+    device: torch.device = torch.device('cpu'),
+    average: Literal['micro', 'macro', 'weighted', 'none'] = 'macro'
+):
+    if not num_labels:
+        raise ValueError('num_labels is required')
+    if multi_label:
+        acc = MultilabelAccuracy(num_labels, average=average)
+    else:
+        acc = MulticlassAccuracy(num_labels, average=average)
+    acc.to(device)
+    return acc(outputs, targets)
