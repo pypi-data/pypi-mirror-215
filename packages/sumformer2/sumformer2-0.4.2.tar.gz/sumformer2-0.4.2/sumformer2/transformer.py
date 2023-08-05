0f07faaf5ce31f395f7d0e305a11a27bf8eb2aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:38 +0000

    Add package sumformer2/0.4.2/sumformer2-0.4.2.tar.gz

---
+++ packages/sumformer2/sumformer2-0.4.2.tar.gz/sumformer2-0.4.2/sumformer2/transformer.py
@@ -0,0 +1,134 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+from torch.utils.checkpoint import checkpoint
+
+from .modules import *
+
+class Sumformer(nn.Module):
+    """Text summarization transformer."""
+    def __init__(self, device, emb_dim, vocab_size, max_len, pos_encoding, GLU, norm, checkpoint, enc_heads, enc_hidden, enc_depth, enc_dropout, dec_heads, dec_hidden, dec_depth, dec_dropout):
+        super().__init__()
+
+        self.device = device
+        self.emb_dim = emb_dim
+        self.vocab_size = vocab_size
+        self.max_len = max_len
+        self.norm = norm
+        self.checkpoint = checkpoint
+
+        self.token_embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=emb_dim).to(device)
+        if pos_encoding:
+            self.pos_embedding = self.compute_encodings(max_len, emb_dim).to(device)
+        else:
+            self.pos_embedding = nn.Embedding(num_embeddings=max_len, embedding_dim=emb_dim).to(device)
+
+        self.encoder = nn.ModuleList([EncoderBlock(emb_dim, enc_heads, enc_hidden, enc_dropout, GLU, norm) for _ in range(enc_depth)])
+        for module in self.encoder: module.to(device)
+        self.decoder = nn.ModuleList([DecoderBlock(emb_dim, dec_heads, dec_hidden, dec_dropout, GLU, norm) for _ in range(dec_depth)])
+        for module in self.decoder: module.to(device)
+
+        self.toProbs = nn.Linear(emb_dim, vocab_size).to(device)  # convert to probabilities over vocab
+
+    def compute_encodings(self, max_len, emb_dim):
+        """Initialise positional encodings."""
+        pos_encodings = torch.zeros(max_len, emb_dim).float()
+        position = torch.arange(0, max_len).float().unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, emb_dim, 2).float() * -(np.log(10000.0) / emb_dim))
+
+        pos_encodings[:, 0::2] = torch.sin(position * div_term)
+        pos_encodings[:, 1::2] = torch.cos(position * div_term)
+
+        return nn.Embedding.from_pretrained(pos_encodings, freeze=True)
+
+    def encode(self, source, source_mask=None):
+        tokens_source = self.token_embedding(source.to(self.device))
+        b, t_s, k = tokens_source.size()
+        positions_source = self.pos_embedding(torch.arange(t_s, device=self.device))[None, :, :].expand(b, t_s, k)
+        x = tokens_source + positions_source
+        for enc_layer in self.encoder:
+            if self.checkpoint:
+                x = checkpoint(enc_layer, x, source_mask)
+            else:
+                x = enc_layer(x, source_mask)
+        return x
+    
+    def decode(self, target, context, target_mask=None):
+        tokens_target = self.token_embedding(target.to(self.device))
+        b, t_t, k = tokens_target.size()
+        positions_target = self.pos_embedding(torch.arange(t_t, device=self.device))[None, :, :].expand(b, t_t, k)
+        y = tokens_target + positions_target
+        for dec_layer in self.decoder:
+            if self.checkpoint:
+                y = checkpoint(dec_layer, y, context, target_mask)
+            else:
+                y = dec_layer(y, context, target_mask)
+        return self.toProbs(y)
+
+    def forward(self, source, target, source_mask=None, target_mask=None):
+        context = self.encode(source, source_mask)
+        return self.decode(target, context, target_mask)
+    
+    def greedy(self, source, start_token=0, end_token=1, max_len=256, source_mask=None, logits=False):
+        self.eval()
+        logit_list = []
+
+        context = self.encode(source, source_mask)
+
+        # Initialize the generated sequence with the start token
+        generated = torch.full((source.size(0), 1), start_token, dtype=torch.long, device=self.device)
+        
+        for _ in range(max_len):
+            output = self.decode(generated, context)
+
+            next_token_logits = output[:, -1, :]  # (b, t, vocab_size) -> (b, 1, vocab_size) (take the last token of the sequence)
+
+            # Select the token with the highest probability
+            next_token = torch.argmax(next_token_logits, dim=-1).unsqueeze(-1)
+
+            # Concatenate the token to the sequence
+            generated = torch.cat((generated, next_token), dim=-1)
+
+            logit_list.append(next_token_logits)
+
+            # Stop if the end token is generated
+            if torch.eq(next_token, end_token).all():
+                break
+        
+        if logits:
+            return generated, torch.stack(logit_list, dim=1)
+        else:
+            return generated
+
+
+    def beam(self, source, start_token=0, end_token=1, max_len=256, source_mask=None, num_beams=3, length_penalty=0.6):
+        self.eval()
+        with torch.no_grad():
+            generated = torch.full((num_beams, 1), start_token, dtype=torch.long, device=self.device)
+            scores = torch.zeros((num_beams,), dtype=torch.float, device=self.device)
+
+            for _ in range(max_len):
+                output = self.forward(source.repeat(num_beams, 1), generated, source_mask=source_mask.repeat(num_beams, 1, 1))
+                next_token_logits = output[:, -1, :]  # (b, t, vocab_size) -> (b, vocab_size)
+
+                # Apply a softmax to convert the logits into probabilities
+                probs = F.softmax(next_token_logits, dim=-1)  # (b, vocab_size)
+                
+                # Multiply the probabilities by the scores and find the top num_beams sequences
+                # Apply length penalty: the longer the sentence, the smaller the score.
+                scores = scores.view(-1, 1) * probs / (generated.size(-1)**length_penalty)  # (b, vocab_size)
+                scores, indices = scores.view(-1).topk(num_beams)
+
+                # Convert flat indices to actual token indices
+                next_tokens = indices % probs.size(-1)  # tokens
+                beam_indices = indices // probs.size(-1)  # beam indices
+                
+                # Add the most probable tokens to the sequence
+                generated = torch.cat([generated[beam_indices], next_tokens.unsqueeze(-1)], dim=-1)
+
+                # Stop when end_token is generated in all num_beams sequences
+                if all(next_token == end_token for next_token in next_tokens):
+                    break
+
+        return generated[0]  # return the first beam
---
+++ packages/sumformer2/sumformer2-0.4.2.tar.gz/sumformer2-0.4.2/sumformer2/utils.py
@@ -0,0 +1,126 @@
+import math
+import os
+import torch
+import wandb
+
+from datasets import load_dataset, concatenate_datasets
+from torch.optim import lr_scheduler
+from torch.utils.data import DataLoader, SequentialSampler, BatchSampler
+
+
+def create_data_loader(dataset, batch_size, collate_fn):
+    sampler = SequentialSampler(dataset)
+    batch_sampler = BatchSampler(sampler, batch_size, drop_last=False)
+    return DataLoader(dataset, batch_sampler=batch_sampler, collate_fn=collate_fn)
+
+
+def load_xsum():
+    """Load the XSum dataset and split into train, validation, and test sets. Keep only the docs and their summary."""
+    train_dataset = load_dataset("xsum", split="train")
+    val_dataset = load_dataset("xsum", split="validation")
+    test_dataset = load_dataset("xsum", split="test")
+
+    # Filter out columns with empty documents or summaries
+    train_dataset = train_dataset.filter(lambda x: len(x['summary']) > 0 and len(x['document']) > 1.5*len(x['summary']))
+    val_dataset = val_dataset.filter(lambda x: len(x['summary']) > 0 and len(x['document']) > 1.5*len(x['summary']))
+    test_dataset = test_dataset.filter(lambda x: len(x['summary']) > 0 and len(x['document']) > 1.5*len(x['summary']))
+
+    # Adding a the doc length of each instance
+    train_dataset = train_dataset.map(lambda x: {'doc_len': len(x['document'])})
+    val_dataset = val_dataset.map(lambda x: {'doc_len': len(x['document'])})
+    test_dataset = test_dataset.map(lambda x: {'doc_len': len(x['document'])})
+
+    # Sort the datasets by document length
+    train_dataset = train_dataset.sort('doc_len')
+    val_dataset = val_dataset.sort('doc_len')
+    test_dataset = test_dataset.sort('doc_len')
+
+    return train_dataset, val_dataset, test_dataset
+
+
+def load_reddit(train_split, val_split, min_len=50):
+    """Concatenate the short and long reddit TIFU datasets and split into train, validation, and test sets. Keep only the docs and their summary."""
+    dataset_short = load_dataset("reddit_tifu", "short")
+    dataset_long = load_dataset("reddit_tifu", "long")
+    dataset = concatenate_datasets([dataset_short["train"], dataset_long["train"]])
+    dataset = dataset.rename_columns({'documents': 'document', 'tldr': 'summary'})
+    dataset = dataset.map(lambda x: {'document': x['title'] + " " + x['document'], 'summary': x['summary']})  # Concatenate the contents of the title column to the front of the documents column for every row
+    dataset = dataset.remove_columns(['ups', 'num_comments', 'upvote_ratio', 'score', 'title'])
+
+    # Filtering out too short documents and summaries
+    dataset = dataset.filter(lambda x:
+                             len(x["document"]) > min_len
+                             and len(x["summary"]) > min_len
+                             and len(x["document"]) > 1.5 * len(x["summary"]))
+
+    # Adding a the doc length of each instance
+    dataset = dataset.map(lambda x: {'doc_len': len(x['document'])})
+
+    # Split the dataset into train, validation, and test sets after shuffling
+    train_dataset, val_dataset, test_dataset = split_data(dataset, train_split, val_split)
+
+    # Sort the datasets by document length
+    train_dataset = train_dataset.sort('doc_len')
+    val_dataset = val_dataset.sort('doc_len')
+    test_dataset = test_dataset.sort('doc_len')
+
+    return train_dataset, val_dataset, test_dataset
+
+
+def split_data(dataset, train_split, val_split):
+    # Shuffle the dataset
+    dataset = dataset.shuffle()
+    dataset = dataset.flatten_indices()  # rewrite the shuffled dataset on disk again as contiguous chunks for speed
+    
+    dataset = dataset.train_test_split(test_size=1-train_split)
+    train_dataset = dataset['train']
+    test_dataset = dataset['test'].train_test_split(test_size=1-(val_split/(1-train_split)))
+    val_dataset = test_dataset['train']
+    test_dataset = test_dataset['test']
+
+    return train_dataset, val_dataset, test_dataset
+
+
+def init_schedule(optimizer, sched, train_loader, lr, epochs, emb_dim):
+    warmup_steps = 0.05 * len(train_loader) * epochs
+    if sched == "constant" or sched == "none":
+        scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda _: 1)
+    elif sched == "cosineannealing":
+        scheduler = lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=1, eta_min=0.0, last_epoch=-1)
+    elif sched == "invsqrt":
+        scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda epoch: 1/math.sqrt(epoch) if epoch > 0 else 1)
+    elif sched == "linear":
+        scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda current_step: current_step/warmup_steps if current_step < warmup_steps else 1.0)
+    elif sched == "onecycle":
+        scheduler = lr_scheduler.OneCycleLR(optimizer, max_lr=lr, total_steps=len(train_loader)*epochs, pct_start=0.05, anneal_strategy="linear")
+    elif sched == "noam":  # TODO: fix this!!
+        scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda current_step: (emb_dim ** -0.5) * min((current_step+1) ** -0.5, (current_step+1) * (warmup_steps ** -1.5)))
+    elif sched == "plateau":
+        scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, threshold=0.0001, threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-08, verbose=True)
+    elif sched == "warmup":
+        warmup_steps = 1000
+        def lr_lambda(current_step):
+            if current_step < warmup_steps:
+                return (lr - lr/10) / warmup_steps * current_step + lr/10
+            else:
+                return ((1/5 * lr) - lr) / (len(train_loader)*epochs - warmup_steps) * (current_step - warmup_steps) + lr
+        scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
+    else:
+        raise ValueError("Invalid scheduler option provided.")
+    return scheduler
+
+
+def save_best_model(model, epoch, model_params):  # Local
+    models_dir = os.path.join(os.path.dirname(__file__), "models", wandb.run.name)
+    
+    if not os.path.exists(models_dir):
+        os.makedirs(models_dir, exist_ok=True)
+    
+    model_path = os.path.join(models_dir, f"model_{wandb.run.name}_e{epoch}.pt")
+    model_info = {
+        'params': model_params,
+        'state_dict': model.state_dict()
+    }
+    
+    torch.save(model_info, model_path)
+    wandb.save(model_path)
