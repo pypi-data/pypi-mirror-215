cad997a76f7e2daabd72192ef09739c1262194af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:53 +0000

    Add package civics-cdf-validator/1.34/civics_cdf_validator-1.34.tar.gz

---
+++ packages/civics-cdf-validator/civics_cdf_validator-1.34.tar.gz/civics_cdf_validator-1.34/version.py
@@ -0,0 +1,11 @@
+"""Single source package version.
+
+No dependencies should be added to this module.
+
+See https://packaging.python.org/guides/single-sourcing-package-version/
+"""
+
+__version__ = '1.34'
+
+
+
