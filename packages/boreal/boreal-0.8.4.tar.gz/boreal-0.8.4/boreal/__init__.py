c2e64ad7220dcb032b93abaad2de5262ad7b3066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:46 +0000

    Add package boreal/0.8.4/boreal-0.8.4.tar.gz

---
+++ packages/boreal/boreal-0.8.4.tar.gz/boreal-0.8.4/boreal/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for Boreal."""
+
+__author__ = """George Tzanetakis"""
+__email__ = 'gtzan@cs.uvic.ca'
+__version__ = '0.8.4'
+
+
+
+from .boreal import * 
+from .audio_widgets import * 
