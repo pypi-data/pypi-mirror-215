8f80669207d0fa0f0fbfd6dd91f38b9f3f1d7a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:44 +0000

    Add package genetic-algo/0.0.3/genetic-algo-0.0.3.tar.gz

---
+++ packages/genetic-algo/genetic-algo-0.0.3.tar.gz/genetic-algo-0.0.3/genetic_algo/document.py
@@ -0,0 +1,18 @@
+# document.py
+
+# noinspection PyUnresolvedReferences
+from genetic_algo.solution import *
+# noinspection PyUnresolvedReferences
+from genetic_algo.environment import *
+# noinspection PyUnresolvedReferences
+from genetic_algo.attributes import *
+# noinspection PyUnresolvedReferences
+from genetic_algo.driver import *
+
+from genetic_algo.base import document
+
+class Documentation:
+    """"""
+# end Documentation
+
+document(Documentation)
