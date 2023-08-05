f008a4afdbb00c7976f404dbeaea397f07c6a8c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:03 +0000

    Add package kingscriptstest/1.0.8/kingscriptstest-1.0.8.tar.gz

---
+++ packages/kingscriptstest/kingscriptstest-1.0.8.tar.gz/kingscriptstest-1.0.8/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+# reading long description from file
+with open('app/Readme.md', 'r') as file:
+    long_description = file.read()
+
+
+# specify requirements of your package here
+REQUIREMENTS = ['requests', 'pandas']
+
+# some more details
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Internet',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+]
+
+# calling the setup function
+setup(name='kingscriptstest',
+      version='1.0.8',
+      description='Test push of the King Scripts',
+      long_description=long_description,
+      url='https://github.com/kingoperating/v2',
+      author='Michael Tanner',
+      author_email='mtanner@kingoperating.com',
+      license='MIT',
+      packages_dir={'': 'kingscriptstest'},
+      packages=find_packages(where='kingscriptstest'),
+      classifiers=CLASSIFIERS,
+      install_requires=REQUIREMENTS,
+      extras_require={"dev": ["twine>=3.2.0"]},
+      keywords='maps location address'
+      )
