5e2c0794db3499d7ab9df23e463b64d9315978f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:19 +0000

    Add package cgcomponents/2.0.3/cgcomponents-2.0.3.tar.gz

---
+++ packages/cgcomponents/cgcomponents-2.0.3.tar.gz/cgcomponents-2.0.3/cgcomponents/exceptions.py
@@ -0,0 +1,80 @@
+
+
+class CGExceptionsErros(Exception):
+    """Base class for CG exceptions"""
+    pass
+
+
+class CGExceptionsRDS(IOError):
+    """Base class for RDS exceptions"""
+    pass
+
+
+class CGExceptionsRequestsErros(IOError):
+    """Base class for requestsCG exceptions"""
+    pass
+
+
+""" Generic Exceptions """
+
+
+class ErrorJson(CGExceptionsRequestsErros):
+
+    def __init__(self, component, message="JSON Tratase de um DataSet, por gentileza utilize json.dumps(json)"):
+        self.component = component
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.component} -> {self.message}'
+
+
+class ErrorCredentials(CGExceptionsErros):
+
+    def __init__(self, component, message="Credenciais de Login incorretas. Por gentileza verifique-as"):
+        self.component = component
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.component} -> {self.message}'
+
+
+""" BackEnd Exceptions """
+
+
+class BackEndConnectionError(CGExceptionsRequestsErros):
+    def __init__(self, suggestion="Inicie o Serviço do BackEnd", message="Falha ao conectar-se ao servidor Django"):
+        self.message = message
+        self.suggestion = suggestion
+        super().__init__(self.message, suggestion)
+
+    def __str__(self):
+        return f'{self.suggestion} -> {self.message}'
+
+
+""" INTRANET EXCEPTIONS """
+
+
+class IntranetConnectionError(CGExceptionsRequestsErros):
+    def __init__(self, suggestion="Conect-se ao FortClient", message="Falha ao conectar-se com o Intranet"):
+        self.message = message
+        self.suggestion = suggestion
+        super().__init__(self.message, suggestion)
+
+    def __str__(self):
+        return f'{self.suggestion} -> {self.message}'
+
+
+""" RDS EXCEPTIONS """
+
+
+class ErrorConnectionRDS(CGExceptionsRDS):
+
+    def __init__(self, component, message="Erro ao tentar conectar-se ao banco RDS"):
+        self.component = component
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'{self.component} -> {self.message}'
