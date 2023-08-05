171cc3aa2bd814580da98021d6d216a9b5938cbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:57 +0000

    Add package weni-rp-apps/2.5.0a5/weni_rp_apps-2.5.0a5.tar.gz

---
+++ packages/weni-rp-apps/weni_rp_apps-2.5.0a5.tar.gz/weni_rp_apps-2.5.0a5/weni/internal/flows/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+from weni.grpc.core import serializers as weni_serializers
+from temba.flows.models import Flow
+
+
+User = get_user_model()
+
+
+class FlowSerializer(serializers.ModelSerializer):
+    org = weni_serializers.OrgUUIDRelatedField()
+    sample_flow = serializers.JSONField(write_only=True)
+
+    class Meta:
+        model = Flow
+        fields = ("org", "uuid", "sample_flow")
+
+    def create(self, validated_data):
+        org = validated_data.get("org")
+        sample_flows = validated_data.get("sample_flow")
+        org.import_app(sample_flows, org.created_by)
+        self.disable_flows_has_issues(org, sample_flows)
+        return org.flows.order_by("created_on").last()
+
+    def disable_flows_has_issues(self, org, sample_flows):
+        flows_name = list(map(lambda flow: flow.get("name"), sample_flows.get("flows")))
+        org.flows.filter(name__in=flows_name).update(has_issues=False)
+
+
+class FlowListSerializer(serializers.Serializer):
+    flow_name = serializers.CharField(required=True, write_only=True)
+    org_uuid = weni_serializers.OrgUUIDRelatedField(required=True, write_only=True)
+    uuid = serializers.CharField(read_only=True)
+    name = serializers.CharField(read_only=True)
