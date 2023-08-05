93c795bbd50507dbd781fa6ebe92fe0bdf71c54e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:32 +0000

    Add package user-management-api/0.3/user-management-api-0.3.tar.gz

---
+++ packages/user-management-api/user-management-api-0.3.tar.gz/user-management-api-0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='user-management-api',
+    version='0.3',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
