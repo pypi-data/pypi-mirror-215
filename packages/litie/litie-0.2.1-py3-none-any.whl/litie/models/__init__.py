4c732333d0f7343351fbba7d40d1d85b5141a277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:01 +0000

    Add package litie/0.2.1/litie-0.2.1-py3-none-any.whl

---
+++ packages/litie/litie-0.2.1-py3-none-any.whl/litie/models/__init__.py
@@ -0,0 +1,4 @@
+from .ee import AutoEventExtractionModel
+from .ner import AutoNerModel
+from .re import AutoRelationExtractionModel
+from .uie import PaddleUIEModel
---
+++ packages/litie/litie-0.2.1-py3-none-any.whl/litie/pipelines/__init__.py
@@ -0,0 +1,4 @@
+from .ee import EventExtractionPipeline
+from .ner import NerPipeline
+from .re import RelationExtractionPipeline
+from .uie import UIEPipeline
