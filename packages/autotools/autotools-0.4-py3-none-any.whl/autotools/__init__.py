1b89d42e1c2ecb9097730578dc4477bd6d6b28ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:35 +0000

    Add package autotools/0.4/autotools-0.4-py3-none-any.whl

---
+++ packages/autotools/autotools-0.4-py3-none-any.whl/autotools/__init__.py
@@ -0,0 +1,19 @@
+from .fast_gpt.chatgpt import chat
+from .fast_gpt.chatgpt import set_api
+from .fast_gpt.chatgpt import ask_gpt
+from .fast_gpt.chatgpt import set_pov
+from .fast_gpt.chatgpt import voice_chat
+from .voice.voice_get_play import speak
+from .voice.voice_get_play import speech
+from .fast_gpt.chatgpt import set_tem
+from .fast_gpt.chatgpt import set_token
+from .fast_gpt.chatgpt import send
+from .fast_gpt.chatgpt import math
+from .fast_gpt.chatgpt import sendi
+from .fast_gpt.chatgpt import link
+from .fast_gpt.chatgpt import synonimize
+from .fast_gpt.chatgpt import code
+from .fast_gpt.chatgpt import scen
+from .fast_gpt.chatgpt import img
+
+
