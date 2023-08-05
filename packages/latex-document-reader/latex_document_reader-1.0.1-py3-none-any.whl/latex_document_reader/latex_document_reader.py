8d8a01303dea04068d851f0bfb9d34fc04ad554a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:45 +0000

    Add package latex-document-reader/1.0.1/latex_document_reader-1.0.1-py3-none-any.whl

---
+++ packages/latex-document-reader/latex_document_reader-1.0.1-py3-none-any.whl/latex_document_reader/latex_document_reader.py
@@ -0,0 +1,48 @@
+import re
+from pylatex.base_classes import Environment, CommandBase
+from pylatex import Document, Command
+class LaTeXDocumentReader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def read_document(self):
+        with open(self.file_path, 'r') as file:
+            content = file.read()
+        return content
+
+    def read_bibtex(self, bibtex_file_path):
+        with open(bibtex_file_path, 'r') as bibtex_file:
+            bibtex_content = bibtex_file.read()
+        return bibtex_content
+
+    def make_readable(self, content):
+        # Perform necessary processing to make the content human-readable
+        # For example, you can remove LaTeX commands or format the text
+
+        # Replace LaTeX commands with empty strings
+        content = re.sub(r'\\[a-zA-Z]+\{[^\}]*\}', '', content)
+
+        # Replace multiple whitespaces with a single whitespace
+        content = re.sub(r'\s+', ' ', content)
+
+        # Return the processed content
+        return content
+
+    def convert_tex_to_pdf(self, tex_file_path, output_file):
+        # Convert the tex file to pdf
+        try:
+            output_file = output_file if output_file.endswith('.pdf') else output_file + '.pdf'
+            doc = Document()
+            doc.append(Command('input', tex_file_path))
+            doc.generate_pdf(output_file, clean_tex=False)
+            return output_file
+        except Exception as e:
+            print(e)
+            return None
+
+
+
+
+
+
+
