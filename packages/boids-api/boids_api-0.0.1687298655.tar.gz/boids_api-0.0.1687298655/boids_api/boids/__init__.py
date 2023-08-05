47c6406a9ccd83a40a7380e927de1c70a103b54d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:44 +0000

    Add package boids-api/0.0.1687298655/boids_api-0.0.1687298655.tar.gz

---
+++ packages/boids-api/boids_api-0.0.1687298655.tar.gz/boids_api-0.0.1687298655/boids_api/boids/__init__.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from boids_api.boids.boid import Boid
+from boids_api.boids.boid_list import BoidList
+from boids_api.boids.boids_config import BoidsConfig
+from boids_api.boids.boids_config_status import BoidsConfigStatus
+from boids_api.boids.boids_config_status_limits import BoidsConfigStatusLimits
+from boids_api.boids.cartesian_triple import CartesianTriple
+from boids_api.boids.error_model import ErrorModel
+from boids_api.boids.integer_range import IntegerRange
+from boids_api.boids.pagination import Pagination
+from boids_api.boids.simulation_config_status import SimulationConfigStatus
+from boids_api.boids.simulation_config_update_request import SimulationConfigUpdateRequest
+from boids_api.boids.simulation_control_status import SimulationControlStatus
+from boids_api.boids.simulation_control_update_request import SimulationControlUpdateRequest
+from boids_api.boids.simulation_state import SimulationState
+from boids_api.boids.simulation_timestamp import SimulationTimestamp
+from boids_api.boids.system_event import SystemEvent
+from boids_api.boids.system_event_level import SystemEventLevel
+from boids_api.boids.system_event_list import SystemEventList
+from boids_api.boids.world_config import WorldConfig
+from boids_api.boids.world_config_status import WorldConfigStatus
+from boids_api.boids.world_config_status_limits import WorldConfigStatusLimits
