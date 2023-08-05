2959fef103b00453862882b158c0f4e725957575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:49 +0000

    Add package invest-tools/0.2.5/invest_tools-0.2.5-py3-none-any.whl

---
+++ packages/invest-tools/invest_tools-0.2.5-py3-none-any.whl/invest_tools/plot.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+plt.style.use("fivethirtyeight")
+
+
+def plot_histogram(
+    returns: pd.Series,
+    percent_returns: pd.Series,
+    plot_title: str,
+    save: bool = False,
+) -> None:
+    axs = plt.figure(constrained_layout=True).subplots(1, 2)
+    plots = [
+        (axs[0], "Returns", returns, False),
+        (axs[1], "Percent Returns", percent_returns, True),
+    ]
+    for ax, title, data, density in plots:
+        ax.set(title=title)
+        ax.hist(data, bins=75, density=density)
+    if save:
+        plt.savefig(plot_title)
+    plt.show()
+
+
+def plot_heatmap(matrix: pd.DataFrame, plot_title: str, save: bool = False) -> None:
+    sns.heatmap(
+        matrix,
+        annot=True,
+        cmap="YlGnBu",
+        linewidth=0.3,
+        annot_kws={"size": 9},
+    )
+    plt.xticks(rotation=90)
+    plt.yticks(rotation=0)
+    if save:
+        plt.savefig(plot_title)
+    plt.show()
+
+
+def plot_excess_returns(
+    cumulative_returns: pd.DataFrame, plot_title: str, save: bool = False
+) -> None:
+    cumulative_returns.plot()
+    plt.show()
