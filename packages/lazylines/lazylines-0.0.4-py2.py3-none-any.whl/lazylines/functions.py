4c1febac704fd63c89c0ccb4af0a7b343b15865a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:47 +0000

    Add package lazylines/0.0.4/lazylines-0.0.4-py2.py3-none-any.whl

---
+++ packages/lazylines/lazylines-0.0.4-py2.py3-none-any.whl/lazylines/functions.py
@@ -0,0 +1,41 @@
+from lazylines import LazyLines 
+import datetime as dt 
+import itertools as it 
+
+
+def round_timestamp(ts:int, to="day"):
+    mapping = {
+        "day": "%Y-%m-%d",
+        "week": "%Y-%W",
+        "month": "%Y-%m"
+    }
+    if to not in mapping:
+        raise ValueError(f"`to` must be in {mapping.keys}. got {to}.")
+    return str(dt.datetime.fromtimestamp(ts).strftime(mapping[to]))
+
+
+def to_nested_pairs(lines: LazyLines , subset_key: str="subset"):
+    for ex in lines:
+        for c1, c2 in it.combinations(ex[subset_key], r=2):
+            yield {**ex, "subset": [c1, c2]}
+
+
+def pluck_from_subset(key:str, subset_key:str="subset"):
+    def func(item):
+        arr = item[subset_key]
+        return [ex[key] for ex in arr if key in ex]
+    return func
+
+
+def calc_agreement(lines: LazyLines, label: str):
+    return (lines
+         .nest_by("text")
+         .mutate(annot=pluck_from_subset("love"))
+         .drop("subset")
+         .keep(lambda d: len(d['annot']) >= 3)
+         .mutate(agreement = lambda d: len(set(d['annot'])) == 1)
+         .nest_by("agreement")
+         .mutate(n=lambda d: len(d['subset']))
+         .drop("subset")
+         .show(2)
+    )
