d1c47a97809c5228ec24155a4db979e966fbe31f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:55 +0000

    Add package netbox-config-backup/1.4.7/netbox_config_backup-1.4.7.tar.gz

---
+++ packages/netbox-config-backup/netbox_config_backup-1.4.7.tar.gz/netbox_config_backup-1.4.7/netbox_config_backup/navigation.py
@@ -0,0 +1,23 @@
+from extras.plugins import PluginMenuItem, PluginMenuButton, PluginMenu
+from utilities.choices import ButtonColorChoices
+
+item = PluginMenuItem(
+    link='plugins:netbox_config_backup:backup_list',
+    link_text='Devices',
+    permissions=['netbox_config_backup.view_backups'],
+    buttons=[
+        PluginMenuButton(
+            link="plugins:netbox_config_backup:backup_add",
+            title="Add",
+            icon_class="mdi mdi-plus",
+            color=ButtonColorChoices.GREEN,
+        ),
+    ]
+)
+
+menu = PluginMenu(
+    label="Configuration Backup",
+    groups=(
+        ('Backup Jobs', (item,)),
+    )
+)
