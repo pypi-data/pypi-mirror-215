d38725671bcd22e89663db27e53694ad3e79a07d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:25 +0000

    Add package img2table/1.0.5/img2table-1.0.5.tar.gz

---
+++ packages/img2table/img2table-1.0.5.tar.gz/img2table-1.0.5/src/img2table/tables/processing/bordered_tables/tables/implicit_rows.py
@@ -0,0 +1,58 @@
+# coding:utf-8
+import copy
+from typing import List
+
+import numpy as np
+
+from img2table.tables.objects.table import Table
+from img2table.tables.processing.common import get_contours_cell
+
+
+def handle_implicit_rows_table(img: np.ndarray, table: Table) -> Table:
+    """
+    Find implicit rows and update tables based on those
+    :param img: image array
+    :param table: Table object
+    :return: reprocessed table with implicit rows
+    """
+    # If table is a single cell, do not search for implicit rows
+    if table.nb_columns * table.nb_rows <= 1:
+        return table
+
+    list_splitted_rows = list()
+    # Check if each row can be splitted
+    for row in table.items:
+        # If row is not vertically consistent, it is not relevant to split it
+        if not row.v_consistent:
+            list_splitted_rows.append(row)
+            continue
+
+        # Compute contours
+        contours = get_contours_cell(img=copy.deepcopy(img),
+                                     cell=row,
+                                     margin=-5,
+                                     blur_size=5,
+                                     kernel_size=5,
+                                     merge_vertically=True)
+
+        # Compute vertical delimiters
+        vertical_delimiters = sorted([int(round((cnt_1.y2 + cnt_2.y1) / 2)) for cnt_1, cnt_2 in zip(contours, contours[1:])])
+
+        # Split row into multiple rows from vertical delimiters
+        list_splitted_rows += row.split_in_rows(vertical_delimiters=vertical_delimiters)
+
+    return Table(rows=list_splitted_rows)
+
+
+def handle_implicit_rows(img: np.ndarray, tables: List[Table]) -> List[Table]:
+    """
+    Detect and handle implicit rows in image tables
+    :param img: image array
+    :param tables: list of Table objects
+    :return: list of Table objects updated taking into account implicit rows
+    """
+    # Detect implicit rows
+    tables_implicit_rows = [handle_implicit_rows_table(img=img, table=table) for table in tables]
+
+    return tables_implicit_rows
+
---
+++ packages/img2table/img2table-1.0.5.tar.gz/img2table-1.0.5/tests/tables/processing/bordered_tables/tables/test_implicit_rows.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+import json
+
+import cv2
+
+from img2table.tables.objects.cell import Cell
+from img2table.tables.objects.row import Row
+from img2table.tables.objects.table import Table
+from img2table.tables.processing.bordered_tables.tables.implicit_rows import handle_implicit_rows_table, \
+    handle_implicit_rows
+
+
+def test_handle_implicit_rows_table():
+    img = cv2.imread("test_data/implicit.png", cv2.IMREAD_GRAYSCALE)
+
+    with open("test_data/implicit_table.json", 'r') as f:
+        table = Table(rows=[Row(cells=[Cell(**el) for el in row]) for row in json.load(f)])
+
+    result = handle_implicit_rows_table(img=img, table=table)
+
+    # Check that 2 more rows have been created
+    assert result.nb_rows == table.nb_rows + 2
+
+
+def test_handle_implicit_rows():
+    img = cv2.imread("test_data/implicit.png", cv2.IMREAD_GRAYSCALE)
+
+    with open("test_data/implicit_table.json", 'r') as f:
+        table = Table(rows=[Row(cells=[Cell(**el) for el in row]) for row in json.load(f)])
+
+    result = handle_implicit_rows(img=img, tables=[table])
+
+    # Check that 2 more rows have been created
+    assert result[0].nb_rows == table.nb_rows + 2
