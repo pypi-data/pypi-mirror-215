685f71b591581601a1eb3acf3d73da81a49fc408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:48 +0000

    Add package m-abac-test/1.0.18/m-abac-test-1.0.18.tar.gz

---
+++ packages/m-abac-test/m-abac-test-1.0.18.tar.gz/m-abac-test-1.0.18/mobio/libs/abac/policy/conditions/day/day_gt.py
@@ -0,0 +1,34 @@
+
+from marshmallow import post_load
+from .base import DayCondition, DayConditionSchema
+
+
+class DayGt(DayCondition):
+
+    def _is_satisfied(self) -> bool:
+        if not self.check_format_input_day(self.what):
+            return False
+        if self.qualifier == self.Qualifier.ForAnyValue:
+            for i in self.values:
+                if self.check_format_input_day(i):
+                    if self.what > i:
+                        return True
+            return False
+        else:
+            for i in self.values:
+                if not self.check_format_input_day(i):
+                    return False
+                if self.what <= i:
+                    return False
+            return True
+
+
+class DayGtSchema(DayConditionSchema):
+    """
+        JSON schema for greater than datetime condition
+    """
+
+    @post_load
+    def post_load(self, data, **_):  # pylint: disable=missing-docstring,no-self-use
+        # self.validate(data)
+        return DayGt(**data)
