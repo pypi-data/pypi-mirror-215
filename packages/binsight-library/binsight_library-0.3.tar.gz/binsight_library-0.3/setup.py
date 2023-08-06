47728b4b58c08361ae7f6ec49e9cc0ef04c02545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:08 +0000

    Add package binsight-library/0.3/binsight_library-0.3.tar.gz

---
+++ packages/binsight-library/binsight_library-0.3.tar.gz/binsight_library-0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='binsight_library',
+    version='0.3',
+    packages=find_packages(),
+    description='Binsight library for data processing',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Daniel Blanc',
+    author_email='dblanc@binsight.app',
+    url='https://github.com/username/my-package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+       'pickle',
+       'boto3',
+       'pandas',
+       'io',
+       'os',
+    ],
+)
