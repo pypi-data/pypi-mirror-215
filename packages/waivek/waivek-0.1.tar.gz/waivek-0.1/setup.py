8ea45252cae8acb760ecec0cbbdaa088d079f020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:03 +0000

    Add package waivek/0.1/waivek-0.1.tar.gz

---
+++ packages/waivek/waivek-0.1.tar.gz/waivek-0.1/setup.py
@@ -0,0 +1,16 @@
+# https://www.python.org/dev/peps/pep-0508/#environment-markers
+from setuptools import setup
+setup(
+        name='waivek',
+        version='0.1',
+        packages=['waivek'],
+        py_modules = ['color', 'common', 'db', 'error', 'frame', 'get', 'ic', 'print_utils', 'reltools', 'timer', 'trace'],
+        install_requires = [
+            'aiohttp==3.8.1',
+            'executing==0.8.2',
+            'python_dateutil==2.8.2',
+            'timeago==1.0.14',
+            'pysqlite3-binary; platform_system=="Linux"'
+        ]
+)
+
