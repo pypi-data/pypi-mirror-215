2e28d7d9e5dd42d55380d915ac0af2fa475a6e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:30 +0000

    Add package ercotutils/1.1.0/ercotutils-1.1.0.tar.gz

---
+++ packages/ercotutils/ercotutils-1.1.0.tar.gz/ercotutils-1.1.0/README.md
@@ -0,0 +1,22 @@
+# ERCOT Python Utility
+This library aims to ease the use of the ERCOT MIS system.
+
+### How to install
+    pip install ercotutils
+
+### How to use
+```python
+from ercotutils import misutil
+
+# Get a list of all the ICE documents for a given report type
+ice_doc_list = misutil.get_ice_doc_list('13499')
+
+....
+
+
+# Get the file contents of a zipped csv document
+byte_content = misutil.get_zipped_file_contents('4433444')
+```
+
+### License
+MIT License
---
+++ packages/ercotutils/ercotutils-1.1.0.tar.gz/ercotutils-1.1.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ercotutils',
+    version='1.1.0',
+    author='Matthew McCollom',
+    author_email='matthewmccollom1989@gmail.com',
+    description="ERCOT utils for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mmccollom/ercot_mis_pylib',
+    project_urls={
+        "Bug Tracker": "https://github.com/mmccollom/ercot_mis_pylib/issues"
+    },
+    install_requires=['requests>=2.5.0'],
+)
