016e80217473fdc6770f7177eb219220c5b7c257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:32 +0000

    Add package cmh-lims-py/1.0.16/cmh_lims_py-1.0.16.tar.gz

---
+++ packages/cmh-lims-py/cmh_lims_py-1.0.16.tar.gz/cmh_lims_py-1.0.16/README.md
@@ -0,0 +1,38 @@
+# cmhlims_py
+
+## install the dependencies
+pip install -r requirements.txt
+
+## building a package
+pip install wheel
+python setup.py sdist bdist_wheel
+
+## installing python package
+pip install dist/cmh_lims_py-1.0.2-py3-none-any.whl
+
+
+Usage:
+
+1. import cmhlims package
+   pip install cmhlims
+2. Set Config file
+   from cmhlims.setConfig import update_config
+   update_config('cmhlims/config.ini', "production")
+3. Connect to Lims
+   from cmhlims.connetToLims import connect_to_lims
+   connect_to_lims()
+4. Get Analysis
+   from cmhlims.getAnalysis import get_analyses
+   get_analyses(["cmh000514"], ["GRCh38"])
+5. Get Analysis Files
+   from cmhlims.getAnalysisFiles import getAnalysisFies
+   get_analysis_files(["2287"])
+6. Get Sample Names
+   from cmhlims.getSampleNames import get_sample_names
+   get_sample_names()
+   
+    
+    
+
+
+
---
+++ packages/cmh-lims-py/cmh_lims_py-1.0.16.tar.gz/cmh_lims_py-1.0.16/cmhlims/getAnalysis.py
@@ -0,0 +1,52 @@
+import pymysql
+import pandas as pd
+from cmhlims.connectToLIMS import connect_to_lims
+
+def get_analyses(sample_names, reference_genome):
+    if len(reference_genome) != 1:
+        raise ValueError("getAnalyses() supports only a single reference genome")
+
+    if len(sample_names) == 0:
+        raise ValueError("getAnalyses() requires at least one sample name")
+
+    analysis_query_template = """select s.label as sample_name,
+        a.id as analysis_id,
+        a.label as analysis_name,
+        a.base_dir as analysis_dir,
+        a.analysis_date as analysis_date,
+        q.label as sequence_type,
+        t.label as analysis_type,
+        r.label as reference_genome
+        from samples s,
+            downstream_analyses a,
+            downstream_analysis_types t,
+            sequence_types q,
+            reference_genomes r
+        where s.id = a.sample_id
+            and a.sequence_type_id = q.id
+            and a.reference_genome_id = r.id
+            and a.downstream_analysis_type_id = t.id
+            and r.label = '{ref_genome}'
+            and s.label IN ({sample_list});"""
+
+    sample_list = "'" + "','".join(sample_names) + "'"
+
+    analysis_query = analysis_query_template.format(ref_genome=reference_genome[0], sample_list=sample_list)
+    lims_db = connect_to_lims()
+
+    try:
+        with lims_db.cursor() as cursor:
+            print("Before executing query")
+            cursor.execute(analysis_query)
+            print("After executing query")
+            print(cursor.description)
+            print("::::::")
+            columns = [column[0] for column in cursor.description]
+            print(columns)
+            analyses_data = cursor.fetchall()
+            print("Query result:", analyses_data)
+            analyses_df = pd.DataFrame(analyses_data, columns=columns)
+    finally:
+        lims_db.close()
+
+    return analyses_df
