be07033eca7f58101fc508d53c375bcdffd49851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:33 +0000

    Add package Marl-Factory-Grid/0.0.9/Marl-Factory-Grid-0.0.9.tar.gz

---
+++ packages/Marl-Factory-Grid/Marl-Factory-Grid-0.0.9.tar.gz/Marl-Factory-Grid-0.0.9/marl_factory_grid/__init__.py
@@ -0,0 +1,6 @@
+from environment.factory import BaseFactory
+from environment.factory import OBSBuilder
+
+from utils.tools import ConfigExplainer
+
+from quickstart import init
---
+++ packages/Marl-Factory-Grid/Marl-Factory-Grid-0.0.9.tar.gz/Marl-Factory-Grid-0.0.9/marl_factory_grid/modules/machines/constants.py
@@ -0,0 +1,11 @@
+
+MACHINES = 'Machines'
+MACHINE  = 'Machine'
+
+STATE_WORK = 'working'
+STATE_IDLE = 'idling'
+STATE_MAINTAIN = 'maintenance'
+
+SYMBOL_WORK = 1
+SYMBOL_IDLE = 0.6
+SYMBOL_MAINTAIN = 0.3
