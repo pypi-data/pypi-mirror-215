cbc851922d78ab3acc393cd6a2c21185d3761037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:38 +0000

    Add package nlpcube/0.3.1.2/nlpcube-0.3.1.2.tar.gz

---
+++ packages/nlpcube/nlpcube-0.3.1.2.tar.gz/nlpcube-0.3.1.2/cube/api.py
@@ -0,0 +1,211 @@
+#
+# Copyright (c) 2018 Adobe Systems Incorporated. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import sys
+import os
+import yaml
+import string
+import requests
+import tarfile
+from tqdm import tqdm
+
+sys.path.append('')
+from typing import Optional, Union
+from cube.io_utils.objects import Document, Word, Token, Sentence
+from cube.io_utils.encodings import Encodings
+from cube.io_utils.config import CompoundConfig, TokenizerConfig, ParserConfig, LemmatizerConfig
+from cube.networks.compound import Compound
+from cube.networks.parser import Parser
+from cube.networks.tokenizer import Tokenizer
+from cube.networks.lemmatizer import Lemmatizer
+from pathlib import Path
+from cube.networks.lm import LMHelperHF, LMHelperFT
+from cube.networks.utils_tokenizer import TokenCollateHF, TokenCollateFTLanguasito
+from cube.networks.utils import MorphoCollate, Word2TargetCollate
+
+
+class CubeObj:
+    def __init__(self, model_base: str, device: str = 'cpu', lang: str = None):
+        self._cwe = None
+        # word expander
+        path = '{0}-trf-cwe'.format(model_base)
+        if os.path.exists('{0}.best'.format(path)):
+            config = CompoundConfig(filename='{0}.config'.format(path))
+            encodings = Encodings()
+            encodings.load('{0}.encodings'.format(path))
+            self._cwe = Compound(config, encodings)
+            self._cwe.load('{0}.best'.format(path))
+            self._cwe.to(device)
+
+        # tokenizer
+        path = '{0}-trf-tokenizer'.format(model_base)
+        g_conf = yaml.safe_load(open('{0}.yaml'.format(path)))
+        self._lang2id = {}
+        for lng in g_conf['language_codes']:
+            self._lang2id[lng] = len(self._lang2id)
+        self._default_lang_id = self._lang2id[g_conf['language_map'][lang]]
+        self._default_lang = lang
+        config = TokenizerConfig(filename='{0}.config'.format(path))
+        lm_model = config.lm_model
+        encodings = Encodings()
+        encodings.load('{0}.encodings'.format(path))
+        if lm_model.startswith('transformer'):
+            self._tokenizer_collate = TokenCollateHF(encodings,
+                                                     lm_device=device,
+                                                     lm_model=lm_model.split(':')[-1],
+                                                     no_space_lang=config.no_space_lang,
+                                                     lang_id=self._default_lang_id)
+        else:
+            self._tokenizer_collate = TokenCollateFTLanguasito(encodings,
+                                                               lm_device=device,
+                                                               lm_model=lm_model,
+                                                               no_space_lang=config.no_space_lang,
+                                                               lang_id=self._default_lang_id)
+
+        self._tokenizer = Tokenizer(config, encodings, language_codes=g_conf['language_codes'],
+                                    ext_word_emb=self._tokenizer_collate.get_embeddings_size())
+        self._tokenizer.load('{0}.best'.format(path))
+        self._tokenizer.to(device)
+
+        # lemmatizer
+        path = '{0}-trf-lemmatizer'.format(model_base)
+        config = LemmatizerConfig(filename='{0}.config'.format(path))
+        encodings = Encodings()
+        encodings.load('{0}.encodings'.format(path))
+        self._lemmatizer = Lemmatizer(config, encodings)
+        self._lemmatizer.load('{0}.best'.format(path))
+        self._lemmatizer.to(device)
+        self._lemmatizer_collate = Word2TargetCollate(encodings)
+        # parser-tagger
+        path = '{0}-trf-parser'.format(model_base)
+        config = ParserConfig(filename='{0}.config'.format(path))
+        lm_model = config.lm_model
+        if lm_model.startswith('transformer'):
+            self._lm_helper = LMHelperHF(model=lm_model.split(':')[-1])
+        else:
+            self._lm_helper = LMHelperFT(model=lm_model.split(':')[-1])
+
+        encodings = Encodings()
+        encodings.load('{0}.encodings'.format(path))
+        self._parser = Parser(config, encodings, language_codes=g_conf['language_codes'],
+                              ext_word_emb=self._lm_helper.get_embedding_size())
+        self._parser.load('{0}.best'.format(path))
+        self._parser.to(device)
+        self._parser_collate = MorphoCollate(encodings)
+
+    def __call__(self, text: Union[str, Document], flavour: Optional[str] = None):
+        lang_id = self._default_lang_id
+        if flavour is not None:
+            if flavour not in self._lang2id:
+                print("Unsupported language flavour")
+                print("Please choose from: {0}".format(' '.join([k for k in self._lang2id])))
+                raise Exception("Unsupported language flavour\nPlease choose from: {0}".
+                                format(' '.join([k for k in self._lang2id])))
+            lang_id = self._lang2id[flavour]
+        if isinstance(text, str):
+            doc = self._tokenizer.process(text, self._tokenizer_collate, lang_id=lang_id, num_workers=0)
+            if self._cwe is not None:
+                doc = self._cwe.process(doc, self._lemmatizer_collate, num_workers=0)
+        else:
+            doc = text
+
+        self._lm_helper.apply(doc)
+        self._parser.process(doc, self._parser_collate, num_workers=0)
+        self._lemmatizer.process(doc, self._lemmatizer_collate, num_workers=0)
+
+        return doc
+
+
+def _download_file(url: str, filename: str, description=None):
+    r = requests.get(url, stream=True)
+    if r.status_code != 200:
+        raise Exception(f"Error getting {url}, received status_code {r.status_code}")
+    file_size = int(r.headers['Content-Length'])
+    chunk_size = 1024
+
+    with open(filename, 'wb') as fp:
+        with tqdm(total=file_size, unit='B', unit_scale=True, desc=description, unit_divisor=1024,
+                  disable=True if description is None else False, leave=False) as progressbar:
+            for chunk in r.iter_content(chunk_size=chunk_size):
+                if chunk is not None:
+                    fp.write(chunk)
+                    fp.flush()
+                    progressbar.update(len(chunk))
+
+    return r.status_code
+
+
+def _download_model(local_path, lang):
+    download_base = "https://github.com/adobe/NLP-Cube-Models/raw/3.0/models/{0}.tar.gz-a".format(lang)
+    file_base = "{0}.tar.gz-a".format(lang)
+    terminations = string.ascii_lowercase[:20]
+    file_list = []
+    for t in terminations:
+        download_url = '{0}{1}'.format(download_base, t)
+        target_file = str(os.path.join(local_path, file_base))
+        target_file = '{0}{1}'.format(target_file, t)
+        try:
+            if _download_file(download_url, target_file, description='Part {0}'.format(t)) != 200:
+                break
+        except:
+            break
+        file_list.append(target_file)
+
+    target_file = os.path.join(local_path, file_base[:-2])
+
+    f_out = open(target_file, 'wb')
+    for file in file_list:
+        f_in = open(file, 'rb')
+        while True:
+            buffer = f_in.read(1024 * 1024)
+            if not buffer:
+                break
+            f_out.write(buffer)
+    f_out.close()
+
+    tar = tarfile.open(target_file, 'r:gz')
+    tar.extractall(local_path)
+    tar.close()
+
+
+def _load(lang: str, device: Optional[str] = 'cpu') -> CubeObj:
+    try:
+        local_user_home = str(Path.home())
+        local_user_storage = os.path.join(local_user_home, '.nlpcube', '3.0')
+        os.makedirs(local_user_storage, exist_ok=True)
+        lang_path = os.path.join(local_user_storage, lang)
+        if not os.path.exists(lang_path):
+            _download_model(local_user_storage, lang)
+
+        return CubeObj('{0}/{1}'.format(lang_path, lang), device=device, lang=lang)
+    except:
+        raise Exception("There was a problem retrieving this language. Either it is unsupported or your Internet "
+                        "connection is down.\n\nTo check for supported languages, visit "
+                        "https://github.com/adobe/NLP-Cube/\n\nIt is hard to maintain models for all UD Treebanks. "
+                        "This is way we are only including a handful of"
+                        "languages with the official distribution. "
+                        "However, we can include additional languages upon request"
+                        "\n\nTo make a request for supporting a new language please create an issue on GitHub")
+
+
+class Cube:
+    def __init__(self, verbose=False):
+        self._instance = None
+
+    def load(self, lang: str, device: Optional[str] = 'cpu'):
+        self._instance = _load(lang, device)
+
+    def __call__(self, text: Union[str, Document], flavour: Optional[str] = None):
+        return self._instance(text, flavour=flavour)
---
+++ packages/nlpcube/nlpcube-0.3.1.2.tar.gz/nlpcube-0.3.1.2/cube/io_utils/config.py
@@ -0,0 +1,318 @@
+#
+# Authors: Tiberiu Boros, Stefan Daniel Dumitrescu
+#
+# Copyright (c) 2018 Adobe Systems Incorporated. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import sys
+import ast
+from builtins import object, super
+from cube.io_utils.misc import fopen
+import collections
+
+import configparser
+
+
+class Config(object):
+    """Generic base class that implements load/save utilities."""
+
+    def __init__(self):
+        """Call to set config object name."""
+        self.__config__ = self.__class__.__name__
+
+    def _auto_cast(self, s):
+        """Autocasts string s to its original type."""
+        try:
+            return ast.literal_eval(s)
+        except:
+            return s
+
+    def save(self, filename):
+        """Save configuration to file."""
+        sorted_dict = collections.OrderedDict(sorted(self.__dict__.items()))  # sort dictionary
+        if sys.version_info[0] == 2:
+            config = ConfigParser.ConfigParser()
+        else:
+            config = configparser.ConfigParser()
+        config.add_section(self.__config__)  # write header
+        if sys.version_info[0] == 2:
+            items = sorted_dict.iteritems()
+        else:
+            items = sorted_dict.items()
+        for k, v in items:  # for python3 use .items()
+            if not k.startswith("_"):  # write only non-private properties
+                if isinstance(v, float):  # if we are dealing with a float
+                    str_v = str(v)
+                    if "e" not in str_v and "." not in str_v:  # stop possible confusion with an int by appending a ".0"
+                        v = str_v + ".0"
+                v = str(v)
+                config.set(self.__config__, k, v)
+        with fopen(filename, 'w') as cfgfile:
+            config.write(cfgfile)
+
+    def load(self, filename):
+        """Load configuration from file."""
+        if sys.version_info[0] == 2:
+            config = ConfigParser.ConfigParser()
+        else:
+            config = configparser.ConfigParser()
+        config.read(filename)
+        # check to see if the config file has the appropriate section
+        if not config.has_section(self.__config__):
+            sys.stderr.write(
+                "ERROR: File \"" + filename + "\" is not a valid configuration file for the selected task: Missing section [" + self.__config__ + "]!\n")
+            sys.exit(1)
+        for k, v in config.items(self.__config__):
+            self.__dict__[k] = self._auto_cast(v)
+
+
+class TokenizerConfig(Config):
+    def __init__(self, filename=None, verbose=False):
+        super().__init__()
+        self.cnn_filter = 512
+        self.lang_emb_size = 100
+        self.cnn_layers = 5
+        self.rnn_size = 50
+        self.rnn_layers = 2
+        self.external_proj_size = 2
+
+        self.no_space_lang = False
+
+        if filename is None:
+            if verbose:
+                sys.stdout.write("No configuration file supplied. Using default values.\n")
+        else:
+            if verbose:
+                sys.stdout.write("Reading configuration file " + filename + " \n")
+            self.load(filename)
+
+        self._valid = True
+
+
+class TaggerConfig(Config):
+    def __init__(self, filename=None, verbose=False):
+        super().__init__()
+        self.char_emb_size = 256
+        self.char_filter_size = 512
+        self.char_layers = 3
+        self.word_emb_size = 256
+        self.lang_emb_size = 64
+        self.cnn_filter = 512
+        self.cnn_layers = 5
+        self.external_proj_size = 300
+        self.lm_model = 'xlm-roberta-base'
+        self._valid = True
+
+        if filename is None:
+            if verbose:
+                sys.stdout.write("No configuration file supplied. Using default values.\n")
+        else:
+            if verbose:
+                sys.stdout.write("Reading configuration file " + filename + " \n")
+            self.load(filename)
+
+
+class ParserConfig(Config):
+    def __init__(self, filename=None, verbose=False):
+        super().__init__()
+        self.char_emb_size = 256
+        self.char_filter_size = 512
+        self.char_layers = 5
+        self.word_emb_size = 256
+        self.lang_emb_size = 64
+        self.cnn_filter = 512
+        self.cnn_layers = 5
+        self.aux_softmax_location = 5
+        self.pre_parser_size = 500
+        self.head_size = 100
+        self.label_size = 200
+        self.lm_model = 'xlm-roberta-base'
+        self.external_proj_size = 2
+        self.rhl_win_size = 2
+        self.rnn_size = 200
+
+        self.rnn_layers = 3
+
+        self._valid = True
+
+        if filename is None:
+            if verbose:
+                sys.stdout.write("No configuration file supplied. Using default values.\n")
+        else:
+            if verbose:
+                sys.stdout.write("Reading configuration file " + filename + " \n")
+            self.load(filename)
+
+
+class LemmatizerConfig(Config):
+    def __init__(self, filename=None, verbose=False):
+        super().__init__()
+        self.encoder_layers = 2
+        self.encoder_size = 200
+        self.decoder_layers = 2
+        self.decoder_size = 400
+        self.att_proj_size = 100
+        self.upos_emb_size = 100
+        self.lang_emb_size = 100
+        self.char_emb_size = 100
+        self._valid = True
+
+        if filename is None:
+            if verbose:
+                sys.stdout.write("No configuration file supplied. Using default values.\n")
+        else:
+            if verbose:
+                sys.stdout.write("Reading configuration file " + filename + " \n")
+            self.load(filename)
+
+
+class CompoundConfig(Config):
+    def __init__(self, filename=None, verbose=False):
+        super().__init__()
+        self.encoder_layers = 2
+        self.encoder_size = 200
+        self.decoder_layers = 2
+        self.decoder_size = 400
+        self.att_proj_size = 100
+        self.lang_emb_size = 100
+        self.char_emb_size = 100
+        self._valid = True
+
+        if filename is None:
+            if verbose:
+                sys.stdout.write("No configuration file supplied. Using default values.\n")
+        else:
+            if verbose:
+                sys.stdout.write("Reading configuration file " + filename + " \n")
+            self.load(filename)
+
+
+class NMTConfig(Config):
+    def __init__(self, filename=None):
+        super().__init__()
+        self.encoder_layers = [300, 300]
+        self.encoder_layer_dropouts = [0.33, 0.33]
+        self.decoder_layers = 2
+        self.decoder_size = 300
+        self.decoder_dropout = 0.33
+        self.input_size = 100
+        self.aux_we_layer_size = 100
+        self.input_dropout_prob = 0.33
+
+        if filename is None:
+            sys.stdout.write("No configuration file supplied. Using default values.\n")
+        else:
+            sys.stdout.write("Reading configuration file " + filename + " \n")
+            self.load(filename)
+
+
+class TieredTokenizerConfig(Config):
+    def __init__(self, filename=None, verbose=False):
+        super().__init__()
+        # sentece splitting
+        self.ss_char_embeddings_size = 100
+        self.ss_char_peek_count = 5
+        self.ss_mlp_layers = [100]
+        self.ss_mlp_dropouts = [0.33]
+        self.ss_lstm_size = 64
+        self.ss_lstm_layers = 1
+        self.ss_lstm_dropout = 0.33
+        self.ss_peek_lstm_size = 64
+        self.ss_peek_lstm_layers = 1
+        self.ss_peek_lstm_dropout = 0.33
+        # tokenization
+        self.tok_char_embeddings_size = 100
+        self.tok_word_embeddings_size = 100
+        self.tok_mlp_layers = [100]
+        self.tok_mlp_dropouts = [0.33]
+        self.tok_char_lstm_layers = 2
+        self.tok_char_lstm_size = 200
+        self.tok_char_lstm_dropout = 0.33
+        self.tok_word_lstm_layers = 2
+        self.tok_word_lstm_size = 200
+        self.tok_word_lstm_dropout = 0.33
+        self.tok_char_peek_lstm_layers = 2
+        self.tok_char_peek_lstm_size = 200
+        self.tok_char_peek_lstm_dropout = 0.33
+
+        if filename is None:
+            if verbose:
+                sys.stdout.write("No configuration file supplied. Using default values.\n")
+        else:
+            if verbose:
+                sys.stdout.write("Reading configuration file " + filename + " \n")
+            self.load(filename)
+
+        self._valid = True
+
+
+class CompoundWordConfig(Config):
+    def __init__(self, filename=None, verbose=False):
+        super().__init__()
+        self.character_embeddings_size = 100
+        self.encoder_size = 200
+        self.encoder_layers = 2
+        self.decoder_size = 200
+        self.decoder_layers = 2
+
+        if filename is None:
+            if verbose:
+                sys.stdout.write("No configuration file supplied. Using default values.\n")
+        else:
+            if verbose:
+                sys.stdout.write("Reading configuration file " + filename + " \n")
+            self.load(filename)
+
+
+class DCWEConfig(Config):
+    def __init__(self, filename=None, verbose=False):
+        super().__init__()
+        self.char_emb_size = 256
+        self.case_emb_size = 32
+        self.num_filters = 512
+        self.kernel_size = 5
+        self.lang_emb_size = 32
+        self.num_layers = 8
+        self.output_size = 300 # this will be automatically updated at training time, so do not change
+
+        if filename is None:
+            if verbose:
+                sys.stdout.write("No configuration file supplied. Using default values.\n")
+        else:
+            if verbose:
+                sys.stdout.write("Reading configuration file " + filename + " \n")
+            self.load(filename)
+
+
+class GDBConfig(Config):
+    def __init__(self, filename=None, verbose=False):
+        super().__init__()
+        self.use_char_embeddings = True
+        self.char_rnn_layers = 2
+        self.char_rnn_size = 100
+        self.embeddings_size = 100
+        self.arc_rnn_layers = [200, 200]
+        self.label_rnn_size = 100
+        self.proj_size = 100
+
+        if filename is None:
+            if verbose:
+                sys.stdout.write("No configuration file supplied. Using default values.\n")
+        else:
+            if verbose:
+                sys.stdout.write("Reading configuration file " + filename + " \n")
+            self.load(filename)
+
+        self._valid = True
