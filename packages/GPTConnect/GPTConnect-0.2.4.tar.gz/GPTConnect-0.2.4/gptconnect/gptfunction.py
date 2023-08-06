cc28d48fe3c4c98faaf4aeb9f33e712e2fb9f546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:44 +0000

    Add package GPTConnect/0.2.4/GPTConnect-0.2.4.tar.gz

---
+++ packages/GPTConnect/GPTConnect-0.2.4.tar.gz/GPTConnect-0.2.4/gptconnect/gptfunction.py
@@ -0,0 +1,38 @@
+from gptconnect import functions
+from .dataclasses import Params
+
+
+def GPTFunction(
+    group: str = "default", description: str = "", params: Params = None
+) -> callable:
+    """
+    Decorator for creating a GPT function
+
+    :param group: The group of the function
+    :param description: The description of the function
+    :param params: The parameters of the function, as a Params object
+    """
+
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            return function(*args, **kwargs)
+
+        params_ = {
+            "type": "object",
+            "properties": params.properties_dict(),
+            "required": params.required,
+        }
+
+        details = {
+            "function": function,
+            "group": group,
+            "name": function.__name__,
+            "description": description,
+            "parameters": params_,
+        }
+
+        functions.append(details)
+
+        return wrapper
+
+    return decorator
