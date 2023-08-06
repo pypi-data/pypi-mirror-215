96f739c286c407cca87ba5ab5c0481e9b4bbf557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:08 +0000

    Add package testless-autocomplete/0.0.106/testless_autocomplete-0.0.106.tar.gz

---
+++ packages/testless-autocomplete/testless_autocomplete-0.0.106.tar.gz/testless_autocomplete-0.0.106/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="testless_autocomplete",
+    version="0.0.106",
+    description="Auto Complete Package",
+    package_data={
+        '../Auto_Complete/testless_autocomplete': ['src/*.csv'],
+    },
+    package_dir={"": "../Auto_Complete"},
+    packages=find_packages(where="../Auto_Complete"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="TestLess",
+    zip_safe=False,
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["bson >= 0.5.10"],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    
+)
