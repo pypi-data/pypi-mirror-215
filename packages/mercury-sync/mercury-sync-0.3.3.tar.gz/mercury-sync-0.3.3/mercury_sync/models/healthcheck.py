944f9ed3b3e03be4b701bf0258368f511d680f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:08 +0000

    Add package mercury-sync/0.3.3/mercury-sync-0.3.3.tar.gz

---
+++ packages/mercury-sync/mercury-sync-0.3.3.tar.gz/mercury-sync-0.3.3/mercury_sync/models/healthcheck.py
@@ -0,0 +1,23 @@
+from pydantic import (
+    StrictStr,
+    StrictInt
+)
+from typing import Literal, Optional, List
+from .message import Message
+
+
+HealthStatus = Literal[
+    "initializing",
+    "healthy", 
+    "suspect", 
+    "degraded", 
+    "failed"
+]
+
+class HealthCheck(Message):
+    target_host: Optional[StrictStr]
+    target_port: Optional[StrictInt]
+    target_status: Optional[StrictStr]
+    source_host: StrictStr
+    source_port: StrictInt
+    status: HealthStatus
