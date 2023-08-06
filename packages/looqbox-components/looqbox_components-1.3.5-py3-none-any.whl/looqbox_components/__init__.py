4031ae9e7d5edc000934965b020a32f0375255ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:09 +0000

    Add package looqbox-components/1.3.5/looqbox_components-1.3.5-py3-none-any.whl

---
+++ packages/looqbox-components/looqbox_components-1.3.5-py3-none-any.whl/looqbox_components/__init__.py
@@ -0,0 +1,4 @@
+from looqbox_components.templates.container import Container
+from looqbox_components.templates.tag import Tag
+from looqbox_components.templates.toplist import TopList
+from looqbox_components.templates.simple_card import SimpleCard
