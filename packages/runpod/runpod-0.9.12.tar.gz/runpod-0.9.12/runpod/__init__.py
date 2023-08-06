37773bbc47739361f15f4b67b129ad91040802df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:56 +0000

    Add package runpod/0.9.12/runpod-0.9.12.tar.gz

---
+++ packages/runpod/runpod-0.9.12.tar.gz/runpod-0.9.12/runpod/__init__.py
@@ -0,0 +1,15 @@
+""" Allows runpod to be imported as a module. """
+
+from . import serverless
+from .endpoint import Endpoint
+from .endpoint import AsyncioEndpoint, AsyncioJob
+from .api_wrapper.ctl_commands import(
+    get_gpus, get_gpu,
+    create_pod, stop_pod, resume_pod, terminate_pod
+)
+
+api_key = None  # pylint: disable=invalid-name
+
+api_url_base = "https://api.runpod.io"  # pylint: disable=invalid-name
+
+endpoint_url_base = "https://api.runpod.ai/v2"  # pylint: disable=invalid-name
