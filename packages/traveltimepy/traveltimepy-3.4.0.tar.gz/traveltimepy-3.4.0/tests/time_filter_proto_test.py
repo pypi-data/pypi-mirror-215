4fb78062a631b845b54a17bd56b72e6d29f89909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:46 +0000

    Add package traveltimepy/3.4.0/traveltimepy-3.4.0.tar.gz

---
+++ packages/traveltimepy/traveltimepy-3.4.0.tar.gz/traveltimepy-3.4.0/tests/time_filter_proto_test.py
@@ -0,0 +1,19 @@
+from traveltimepy import Coordinates
+from traveltimepy.dto.requests.time_filter_proto import (
+    ProtoTransportation,
+    ProtoCountry,
+)
+
+
+def test_proto(proto_sdk):
+    travel_times = proto_sdk.time_filter_proto(
+        origin=Coordinates(lat=51.425709, lng=-0.122061),
+        destinations=[
+            Coordinates(lat=51.348605, lng=-0.314783),
+            Coordinates(lat=51.337205, lng=-0.315793),
+        ],
+        transportation=ProtoTransportation.DRIVING_FERRY,
+        travel_time=7200,
+        country=ProtoCountry.UNITED_KINGDOM,
+    )
+    assert len(travel_times) == 2
