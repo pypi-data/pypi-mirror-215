035244a5318f65375ae3f581ca3c0b2a4f4d3837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:17 +0000

    Add package dowell-permutation/0.1.0/dowell-permutation-0.1.0.tar.gz

---
+++ packages/dowell-permutation/dowell-permutation-0.1.0.tar.gz/dowell-permutation-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name="dowell-permutation",
+    version="0.1.0",
+    description="Dowell Permutation API Python Libray",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Dennis RK.',
+    license="MIT",
+    packages=["permutation"],
+    include_package_data=True,
+    install_requires=["requests"]
+)
