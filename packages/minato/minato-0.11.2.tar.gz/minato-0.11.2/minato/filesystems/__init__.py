20f0e0373729c788e92b322301c216802108f9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:01 +0000

    Add package minato/0.11.2/minato-0.11.2.tar.gz

---
+++ packages/minato/minato-0.11.2.tar.gz/minato-0.11.2/minato/filesystems/__init__.py
@@ -0,0 +1,13 @@
+from minato.filesystems.filesystem import (  # noqa: F401
+    FileSystem,
+    delete,
+    download,
+    exists,
+    get_version,
+    open_file,
+    upload,
+)
+from minato.filesystems.gcs import GCSFileSystem  # noqa: F401
+from minato.filesystems.http import HttpFileSystem  # noqa: F401
+from minato.filesystems.osfs import OSFileSystem  # noqa: F401
+from minato.filesystems.s3 import S3FileSystem  # noqa: F401
