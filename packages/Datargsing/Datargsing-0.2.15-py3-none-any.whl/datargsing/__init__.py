160190700e90d2683efb417a9925be95f0672e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:56 +0000

    Add package Datargsing/0.2.15/Datargsing-0.2.15-py3-none-any.whl

---
+++ packages/Datargsing/Datargsing-0.2.15-py3-none-any.whl/datargsing/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""
+Datargsing Module
+"""
+
+from datargsing.datargsing import CSV_JSON_Manager as dGDM, Tools as dTools, datargsing_Failure as dFailure, datargsing_Error as dError, datargsing_Complete as dComplete, __version__ as dVersion
