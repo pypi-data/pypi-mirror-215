4c234c53fc63e9b22f751530de61e9828575f4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:02 +0000

    Add package FluidCubeGame/0.1.2/FluidCubeGame-0.1.2.tar.gz

---
+++ packages/FluidCubeGame/FluidCubeGame-0.1.2.tar.gz/FluidCubeGame-0.1.2/setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup
+
+version = '0.1.2'
+
+long_description = 'FluidCubeGame'
+
+setup(
+    name = 'FluidCubeGame',
+    version = version,
+
+    author = 'Leshin Vladimir, Spektoruk Ilya, Krukovskiy Vasiliy',
+    author_email = 'i.spektoruk@g.nsu.ru',
+
+    description = 'FluidCubeGame',
+    long_description = long_description,
+
+    url = 'https://github.com/Spektoruk3/PAK',
+
+    packages = ['src'],
+    install_requires = ['numpy', 'pygame'],
+    )
+
+
+    
