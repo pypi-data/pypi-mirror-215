cd266c21f2b3ac28e26a962d1d8f5e50500ae924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:47 +0000

    Add package confidence/0.15/confidence-0.15-py3-none-any.whl

---
+++ packages/confidence/confidence-0.15-py3-none-any.whl/confidence/__init__.py
@@ -0,0 +1,16 @@
+import logging
+
+from confidence.exceptions import ConfigurationError, ConfiguredReferenceError, MergeConflictError, NotConfiguredError
+from confidence.io import DEFAULT_LOAD_ORDER, dump, dumpf, dumps, load, load_name, loaders, loadf, loads, Locality
+from confidence.models import Configuration, Missing, NotConfigured, unwrap
+
+
+__all__ = (
+    'ConfigurationError', 'ConfiguredReferenceError', 'MergeConflictError', 'NotConfiguredError',
+    'DEFAULT_LOAD_ORDER', 'dump', 'dumpf', 'dumps', 'load', 'load_name', 'loaders', 'loadf', 'loads', 'Locality',
+    'Configuration', 'Missing', 'NotConfigured', 'unwrap',
+)
+
+
+# default confidence' loggers to silence, can be overridden from logging later if needed
+logging.getLogger(__name__).addHandler(logging.NullHandler())
