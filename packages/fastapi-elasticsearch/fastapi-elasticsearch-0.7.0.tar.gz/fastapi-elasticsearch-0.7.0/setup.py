9f31e97cc035952cd6b9747e9e8639db2f30b6fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:00 +0000

    Add package fastapi-elasticsearch/0.7.0/fastapi-elasticsearch-0.7.0.tar.gz

---
+++ packages/fastapi-elasticsearch/fastapi-elasticsearch-0.7.0.tar.gz/fastapi-elasticsearch-0.7.0/setup.py
@@ -0,0 +1,20 @@
+import pathlib
+
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "../README.md").read_text()
+
+required = (HERE / "requirements.txt").read_text().splitlines()
+
+setup(
+    name="fastapi-elasticsearch",
+    version="0.7.0",
+    description="Query Utility for Elasticsearch",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/euler-io/fastapi-elasticsearch",
+    license="LGPL-2.1 License",
+    packages=find_packages(exclude=("development",)),
+    install_requires=required,
+)
