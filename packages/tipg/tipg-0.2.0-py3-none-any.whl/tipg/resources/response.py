3511c876da76e40685cac4cd17947a05eadd1e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:21 +0000

    Add package tipg/0.2.0/tipg-0.2.0-py3-none-any.whl

---
+++ packages/tipg/tipg-0.2.0-py3-none-any.whl/tipg/resources/response.py
@@ -0,0 +1,15 @@
+"""tipg custom responses."""
+
+from fastapi.responses import ORJSONResponse
+
+
+class GeoJSONResponse(ORJSONResponse):
+    """GeoJSON Response"""
+
+    media_type = "application/geo+json"
+
+
+class SchemaJSONResponse(ORJSONResponse):
+    """Schema Response"""
+
+    media_type = "application/schema+json"
