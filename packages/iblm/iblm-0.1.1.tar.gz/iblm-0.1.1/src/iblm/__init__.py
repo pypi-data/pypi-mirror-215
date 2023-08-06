f32a95d2ec308d80ff9d84e1b668e771eb08134d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:46 +0000

    Add package iblm/0.1.1/iblm-0.1.1.tar.gz

---
+++ packages/iblm/iblm-0.1.1.tar.gz/iblm-0.1.1/src/iblm/__init__.py
@@ -0,0 +1,6 @@
+#from ._version import version
+
+from .iblmodel.iblm_classifier import IBLMClassifier
+from .iblmodel.iblm_regressor import IBLMRegressor
+
+from .ibboosting.ibboosting_classifier import IBBoostingClassifier
