0c7896805aebe3b02728a337afe302750e6bc3fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:21 +0000

    Add package async-notify/1.0.1/async_notify-1.0.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/async-notify/async_notify-1.0.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/notify/version.py
@@ -0,0 +1,12 @@
+"""Notify Meta information."""
+
+__title__ = "async-notify"
+__description__ = (
+    "Library for send notifications. "
+    "simple and powerful asyncio-based library for sending notifications."
+)
+__version__ = "1.0.1"
+__copyright__ = "Copyright (c) 2020-2023 Jesus Lara"
+__author__ = "Jesus Lara"
+__author_email__ = "jesuslara@phenobarbital.info"
+__license__ = "BSD"
