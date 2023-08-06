2d502d38ffed5f85f699fdeb51fb319a400f566f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:48 +0000

    Add package ReprTraceback/1.0.4/ReprTraceback-1.0.4.tar.gz

---
+++ packages/ReprTraceback/ReprTraceback-1.0.4.tar.gz/ReprTraceback-1.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ReprTraceback',
+    version='1.0.4',
+    description='Improve stack traces with the values associated with each argument in each frame.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='NateRiz',
+    author_email='natezriz@gmail.com',
+    url='https://github.com/NateRiz/ReprTraceback',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
