d38feda56bbad8543b226331246b716887bd2053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:28 +0000

    Add package aws-sam-translator/1.70.0/aws_sam_translator-1.70.0-py3-none-any.whl

---
+++ packages/aws-sam-translator/aws_sam_translator-1.70.0-py3-none-any.whl/samtranslator/model/iot.py
@@ -0,0 +1,11 @@
+from samtranslator.model import GeneratedProperty, Resource
+from samtranslator.model.intrinsics import fnGetAtt, ref
+
+
+class IotTopicRule(Resource):
+    resource_type = "AWS::IoT::TopicRule"
+    property_types = {
+        "TopicRulePayload": GeneratedProperty(),
+    }
+
+    runtime_attrs = {"name": lambda self: ref(self.logical_id), "arn": lambda self: fnGetAtt(self.logical_id, "Arn")}
