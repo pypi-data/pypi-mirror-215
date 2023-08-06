fd09e3627f770d9a2978aa21cc8b317ce01af0fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:49 +0000

    Add package crypto-info/0.0.5/crypto-info-0.0.5.tar.gz

---
+++ packages/crypto-info/crypto-info-0.0.5.tar.gz/crypto-info-0.0.5/crypto_info/document.py
@@ -0,0 +1,16 @@
+# document.py
+
+# noinspection PyUnresolvedReferences
+from crypto_info.table import *
+# noinspection PyUnresolvedReferences
+from crypto_info.coins import *
+# noinspection PyUnresolvedReferences
+from crypto_info.exchanges import *
+
+from crypto_info.base import document
+
+class Documentation:
+    """"""
+# end Documentation
+
+document(Documentation)
