ea873b51705212d9697e1e1911bffd3421545b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:19 +0000

    Add package sktime/0.19.2/sktime-0.19.2-py3-none-any.whl

---
+++ packages/sktime/sktime-0.19.2-py3-none-any.whl/sktime/regression/tests/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+"""Tests for all time series regressors."""
---
+++ packages/sktime/sktime-0.19.2-py3-none-any.whl/sktime/series_as_features/base/estimators/interval_based/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Interval based series-as-features estimators."""
+
+__all__ = ["BaseTimeSeriesForest"]
+
+from sktime.series_as_features.base.estimators.interval_based._tsf import (
+    BaseTimeSeriesForest,
+)
