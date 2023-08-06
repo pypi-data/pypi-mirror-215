ab41961f2537b1e6b4b0f1391d3f432840ef8837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:16 +0000

    Add package jj-wingman/0.3.3/jj_wingman-0.3.3.tar.gz

---
+++ packages/jj-wingman/jj_wingman-0.3.3.tar.gz/jj_wingman-0.3.3/wingman/utils.py
@@ -0,0 +1,47 @@
+"""Some simple Wingman functionality."""
+from __future__ import annotations
+
+import numpy as np
+
+try:
+    import torch
+except ImportError as e:
+    raise ImportError(
+        "Could not import torch, this is not bundled as part of Wingman and has to be installed manually."
+    ) from e
+
+
+def gpuize(input, device) -> torch.Tensor:
+    """gpuize.
+
+    Args:
+        input: the array that we want to gpuize
+        device: a string of the device we want to move the thing to
+    """
+    if torch.is_tensor(input):
+        if input.device == device:
+            return input.float()
+        return input.to(device).float()
+    return torch.tensor(input).float().to(device)
+
+
+def cpuize(input) -> np.ndarray:
+    """cpuize.
+
+    Args:
+        input: the array of the thing we want to put on the cpu
+    """
+    if torch.is_tensor(input):
+        return input.detach().cpu().numpy()
+    else:
+        return input
+
+
+def shutdown_handler(*_):
+    """shutdown_handler.
+
+    Args:
+        _:
+    """
+    print("ctrl-c invoked")
+    exit(0)
