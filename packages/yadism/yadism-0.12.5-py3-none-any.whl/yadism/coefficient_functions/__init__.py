555ac38a67d6cfce4423d2c12c0893164650eced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:01 +0000

    Add package yadism/0.12.5/yadism-0.12.5-py3-none-any.whl

---
+++ packages/yadism/yadism-0.12.5-py3-none-any.whl/yadism/coefficient_functions/__init__.py
@@ -0,0 +1,266 @@
+import numpy as np
+from eko.matchings import nf_default
+
+from . import fonll, heavy, intrinsic, kernels, light
+from .partonic_channel import EmptyPartonicChannel
+
+
+class Component(list):
+    """
+    Used for organize elements and debugging purpose.
+    """
+
+    heavyness = {0: "light", 4: "charm", 5: "bottom", 6: "top"}
+
+    def __init__(self, heavy, kernels=None):
+        self.heavy = self.heavyness[heavy]
+
+        if kernels is None:
+            kernels = []
+        super().__init__(kernels)
+
+    def __repr__(self):
+        return self.heavy + f"({len(self)} kernels)"
+
+
+# TODO add more doc strings
+
+
+class Combiner:
+    """
+    Does the matching between coefficient functions and partons with their approptiate coupling
+    strength.
+
+    Parameters
+    ----------
+        esf : EvaluateStructureFunction
+            current ESF
+    """
+
+    def __init__(self, esf):
+        self.esf = esf
+        self.masses = {4 + i: not mass for i, mass in enumerate(esf.info.ZMq)}
+        self.intrinsic = esf.info.intrinsic_range
+        self.obs_name = esf.info.obs_name
+        self.nf = nf_default(esf.Q2, esf.info.threshold)
+        self.target = esf.info.target
+
+    def collect(self):
+        comps = []
+
+        family = self.obs_name.flavor_family
+
+        # Adding light component
+        if family in ["light", "total"]:
+            comps.append(self.light_component())
+        if family == "heavy":
+            #  the only case in which an heavy contribution is not present in those
+            #  accounted for in total, it's whene heavy already became heavylight
+            comps.extend(self.heavylight_components())
+        if family in ["heavy", "total"]:
+            comps.extend(self.heavy_components())
+
+        return comps
+
+    def light_component(self):
+        nf = self.nf
+        masses = self.masses
+
+        comp = Component(0)
+
+        # the first condition essentially checks nf != 3
+        if nf in masses and masses[nf]:
+            nl = nf - 1
+            comp.extend(
+                fonll.kernels.generate_light(
+                    self.esf,
+                    nl,
+                    self.esf.info.theory["pto_evol"],
+                    self.esf.info.theory["pto"],
+                )
+            )
+            comp.extend(heavy.kernels.generate_missing(self.esf, nl, nl + 1))
+            comp.extend(
+                self.damp_elems(
+                    nl,
+                    fonll.kernels.generate_light_diff(
+                        self.esf,
+                        nl,
+                        self.esf.info.theory["pto_evol"],
+                    ),
+                )
+            )
+        else:
+            comp.extend(light.kernels.generate(self.esf, nf))
+
+        for ihq in range(nf + 1, 7):
+            if masses[ihq]:
+                comp.extend(heavy.kernels.generate_missing(self.esf, nf, ihq))
+        return comp
+
+    def heavylight_components(self):
+        nf = self.nf
+        hq = self.obs_name.hqnumber
+        masses = self.masses
+
+        comps = []
+
+        if hq < nf or (hq == nf and not masses[hq]):
+            heavylight = Component(hq)
+            heavylight.extend(kernels.generate_single_flavor_light(self.esf, nf, hq))
+            comps.append(heavylight)
+
+        return comps
+
+    def heavy_components(self):
+        nf = self.nf
+        hq = self.obs_name.hqnumber
+        masses = self.masses
+
+        comps = []
+
+        heavy_comps = {}
+        for sfh in range(nf, 7):
+            # exclude sfh=3, since heavy contributions are there for [4,5,6]
+            # if it's ZM you don't even have the component
+            if sfh not in masses or not masses[sfh]:
+                continue
+
+            heavy_comps[sfh] = Component(sfh)
+            if hq not in (0, sfh):
+                continue
+
+            if sfh == nf:
+                # then it is FONLL
+                nl = nf - 1
+                heavy_comps[sfh].extend(heavy.kernels.generate(self.esf, nl, ihq=sfh))
+                if sfh not in self.intrinsic:
+                    heavy_comps[sfh].extend(
+                        self.damp_elems(
+                            nl,
+                            fonll.kernels.generate_heavy_diff(
+                                self.esf, nl, self.esf.info.theory["pto_evol"]
+                            ),
+                        )
+                    )
+                else:
+                    heavy_comps[sfh].extend(
+                        intrinsic.kernels.generate(self.esf, ihq=sfh)
+                    )
+                    heavy_comps[sfh].extend(
+                        self.damp_elems(
+                            nl,
+                            fonll.kernels.generate_heavy_intrinsic_diff(
+                                self.esf, nl, self.esf.info.theory["pto_evol"]
+                            ),
+                        )
+                    )
+            else:
+                # then it is *not* FONLL
+                heavy_comps[sfh].extend(heavy.kernels.generate(self.esf, nf, ihq=sfh))
+                if sfh in self.intrinsic:
+                    heavy_comps[sfh].extend(
+                        intrinsic.kernels.generate(self.esf, ihq=sfh)
+                    )
+
+            for ihq in range(sfh + 1, 7):
+                if masses[ihq]:
+                    heavy_comps[sfh].extend(
+                        heavy.kernels.generate_missing(self.esf, nf, ihq, icoupl=sfh)
+                    )
+            comps.append(heavy_comps[sfh])
+
+        return comps
+
+    def damp_elems(self, nl, elems):
+        """
+        Damp FONLL difference contributions if necessary.
+
+        Parameters
+        ----------
+            nl : int
+                number of *light* flavors
+            elems : list(Kernel)
+                kernels to be modified
+
+        Returns
+        -------
+            elems : list(Kernel)
+                modified kernels
+        """
+        if not self.esf.info.FONLL_damping:
+            return elems
+        nhq = nl + 1
+        # TODO: replace mass with threshold?
+        m2hq = self.esf.info.m2hq[nhq - 4]
+        power = self.esf.info.damping_powers[nhq - 4]
+        if self.esf.Q2 > m2hq:
+            damp = np.power(1.0 - m2hq / self.esf.Q2, power)
+        else:
+            damp = 0.0
+        return (damp * e for e in elems)
+
+    @staticmethod
+    def apply_isospin(full, z, a):
+        """
+        Apply isospin symmetry to u and d distributions.
+
+        Parameters
+        ----------
+            full : list(yadism.kernels.Kernel)
+                all participants
+            z : float
+                number of protons
+            a : float
+                atomic mass number
+        """
+        nucl_factors = np.array([[z, a - z], [a - z, z]]) / a
+        for ker in full:
+            for sign in [-1, 1]:
+                ps = np.array(
+                    [ker.partons.get(sign * 1, 0), ker.partons.get(sign * 2, 0)]
+                )
+                ker.partons[sign * 1], ker.partons[sign * 2] = nucl_factors @ ps
+
+    @staticmethod
+    def drop_empty(full):
+        """
+        Drop kernels with :class:`EmptyPartonicChannel` or its partons with empty weight.
+
+        Parameters
+        ----------
+            elems : list(yadism.kernels.Kernel)
+                all participants
+
+        Returns
+        -------
+            filtered_kernels : list(yadism.kernels.Kernel)
+                improved participants
+        """
+        filtered_kernels = []
+        for ker in full:
+            ker.partons = {p: w for p, w in ker.partons.items() if w != 0}
+            if len(ker.partons) > 0 and not isinstance(ker.coeff, EmptyPartonicChannel):
+                filtered_kernels.append(ker)
+        return filtered_kernels
+
+    def collect_elems(self):
+        """
+        Collects all kernels according to the |FNS|.
+
+        Returns
+        -------
+            elems : list(yadism.kernels.Kernel)
+                all participants
+        """
+        components = self.collect()
+
+        full = []
+        for comp in components:
+            full.extend(comp)
+
+        # add level-0 nuclear correction: apply isospin symmetry
+        self.apply_isospin(full, self.target["Z"], self.target["A"])
+
+        # drop all kernels with 0 weight, or empty coeffs
+        return self.drop_empty(full)
---
+++ packages/yadism/yadism-0.12.5-py3-none-any.whl/yadism/input/compatibility.py
@@ -0,0 +1,139 @@
+import numpy as np
+
+hqfl = "cbt"
+
+
+def update(theory, observables):
+    """
+    Upgrade the legacy theory and observable runcards with the new settings.
+
+    Parameters
+    ----------
+        theory : dict
+            theory runcard
+        observables : dict
+            observable runcard
+
+    Returns
+    -------
+        new_theory : dict
+            upgraded theory runcard
+        new_obs : dict
+            upgraded observable runcard
+    """
+    new_theory = theory.copy()
+    new_obs = observables.copy()
+    update_fns(new_theory)
+    update_scale_variations(new_theory)
+    update_target(new_obs)
+    if "alphaqed" in new_theory:
+        new_theory["alphaem"] = new_theory.pop("alphaqed")
+    # TODO: update Yadism syntax and remove PTO everywhere?
+    if "QED" in new_theory:
+        new_theory["order"] = (new_theory["PTO"] + 1, new_theory.pop("QED"))
+    return new_theory, new_obs
+
+
+def update_scale_variations(theory):
+    """
+    Extract necessity to compute scale variations.
+
+    Parameters
+    ----------
+        theory : dict
+            theory runcard
+    """
+    if "RenScaleVar" not in theory:
+        theory["RenScaleVar"] = not np.isclose(theory["XIR"], 1.0)
+    if "FactScaleVar" not in theory:
+        theory["FactScaleVar"] = not np.isclose(theory["XIF"], 1.0)
+
+
+def update_target(obs):
+    """
+    Map TargetDIS string to a (Z,A) dict.
+
+    Parameters
+    ----------
+        obs : dict
+            observable runcard
+    """
+    target = obs["TargetDIS"]
+    if not isinstance(target, str):
+        return
+    if target == "proton":
+        obs["TargetDIS"] = {"Z": 1.0, "A": 1.0}
+        # proton = 2212
+        obs["TargetDISid"] = "2212"
+    elif target == "neutron":
+        obs["TargetDIS"] = {"Z": 0.0, "A": 1.0}
+        # neutron = 2112
+        obs["TargetDISid"] = "2112"
+    elif target == "isoscalar":
+        obs["TargetDIS"] = {"Z": 1.0, "A": 2.0}
+        # deuteron = 10 0 001 002 0
+        obs["TargetDISid"] = "1000010020"
+    elif target == "iron":
+        obs["TargetDIS"] = {
+            "Z": 23.403,
+            "A": 49.618,
+        }  # Fe=26 and we don't know how these factors got inside APFEL
+        # iron = 10 0 026 056 0
+        obs["TargetDISid"] = "1000260560"
+    elif target == "lead":
+        obs["TargetDIS"] = {"Z": 82.0, "A": 208.0}  # from hep-ex/0102049
+        # lead = 10 0 082 208 0
+        obs["TargetDISid"] = "1000822080"
+    elif target == "neon":
+        obs["TargetDIS"] = {"Z": 10.0, "A": 20.0}
+        obs["TargetDISid"] = "1000100200"
+    elif target == "marble":
+        A = (40 + 3 * 16 + 12) / 5  # A(CaCO3): average atomic mass
+        Z = (20 + 3 * 8 + 6) / 5  # Z(CaCO3): avaerage atomic number
+        obs["TargetDIS"] = {"Z": Z, "A": A}
+        obs["TargetDISid"] = f"1000{str(Z)}{str(A)}0"
+    else:
+        raise ValueError(f"Unknown TargetDIS '{target}'")
+
+
+def update_fns(theory):
+    """
+    Shifts kcThr around and add the kDIScThr parameter (likewise for all heavy quarks).
+
+    Parameters
+    ----------
+        theory : dict
+            theory runcard
+    """
+    fns = theory["FNS"]
+    nf = theory["NfFF"]
+
+    if "FONLL" in fns:
+        theory["ZMc"] = False
+        theory["ZMb"] = False
+        theory["ZMt"] = True
+    elif fns == "ZM-VFNS":
+        for fl in hqfl:
+            theory[f"ZM{fl}"] = True
+    elif "FFNS" in fns:
+        # enforce correct settings moving all thresholds to 0 or oo
+        for k, fl in enumerate(hqfl):
+            if k + 4 <= nf:
+                theory[f"k{fl}Thr"] = 0.0
+                theory[f"ZM{fl}"] = True
+            else:
+                theory[f"k{fl}Thr"] = np.inf
+                theory[f"ZM{fl}"] = False
+    else:
+        raise ValueError(f"Scheme '{fns}' not recognized.")
+
+    # here there is no difference between DGLAP and DIS
+    for fl in hqfl:
+        theory[f"kDIS{fl}Thr"] = theory[f"k{fl}Thr"]
+
+    # fix FONLL-B and introduce PTODIS
+    if fns == "FONLL-B":
+        theory["PTODIS"] = 2
+        theory["PTO"] = 1
+    else:
+        theory["PTODIS"] = theory["PTO"]
