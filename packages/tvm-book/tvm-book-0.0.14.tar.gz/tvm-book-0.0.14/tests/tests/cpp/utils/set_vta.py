ba898ecf30a2c5457f764fb1adc062c73994a8de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:39 +0000

    Add package tvm-book/0.0.14/tvm-book-0.0.14.tar.gz

---
+++ packages/tvm-book/tvm-book-0.0.14.tar.gz/tvm-book-0.0.14/tests/tests/cpp/utils/set_vta.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+from tvm_book.tvm.env import set_tvm
+# TVM_ROOT = Path(__file__).absolute().parents[5]
+TVM_ROOT = "/media/pc/data/4tb/lxw/workspace/tvm-dev"
+# print(TVM_ROOT)
+set_tvm(TVM_ROOT)
