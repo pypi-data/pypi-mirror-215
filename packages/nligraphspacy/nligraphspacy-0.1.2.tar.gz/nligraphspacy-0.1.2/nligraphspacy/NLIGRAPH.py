f5629d47d71e9bc7a38a1001a01b7572fdaabf02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:34 +0000

    Add package nligraphspacy/0.1.2/nligraphspacy-0.1.2.tar.gz

---
+++ packages/nligraphspacy/nligraphspacy-0.1.2.tar.gz/nligraphspacy-0.1.2/nligraphspacy/NLIGRAPH.py
@@ -0,0 +1,19 @@
+import spacy
+
+class RelationEntityExtract:
+  def __init__(self, text):
+    
+    self.text = text
+    
+  def load_model(self):
+    
+    nlp = spacy.load("./kgmodel/")
+    return nlp
+  
+  def process_text(self):
+    
+    model = self.load_model()
+    doc = model(self.text)
+    return doc.ents
+  
+  
