4c2e37982ca0477fdc903a227506049fb00e5972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:40 +0000

    Add package pypi-jump-to/0.1.0/pypi_jump_to-0.1.0-py3-none-any.whl

---
+++ packages/pypi-jump-to/pypi_jump_to-0.1.0-py3-none-any.whl/pjt/app.py
@@ -0,0 +1,22 @@
+"""pjt (pip-jump-to) - a quick navigation tool for the PyPI packages."""
+
+from pjt import __title__
+from pjt import __version__
+from pjt import cli
+
+
+application = cli.application.SingleCommandApplication(
+    name=__title__,
+    version=__version__,
+)
+application.add(command=cli.commands.DefaultCommand(), default=True)
+
+
+def run() -> int:  # pragma: no cover
+    """Run the application."""
+
+    return application.run()
+
+
+if __name__ == "__main__":
+    run()
