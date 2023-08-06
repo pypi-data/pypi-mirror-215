a0049d27044fad1177be908e31464903b45524fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:35 +0000

    Add package morel/1.6.4/morel-1.6.4.tar.gz

---
+++ packages/morel/morel-1.6.4.tar.gz/morel-1.6.4/src/morel/target_template.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def main():
+    """
+    Define this function to get your results in the form of a dictionary and return it.
+    """
+
+    targets = {}
+
+    """
+    Your awesome logic goes here
+
+    example:
+    targets = requests.get(f"http://10.10.0.1:8081/flagIds").json()
+    """
+
+    return targets
