f5806b0f720976a64068f37c5ad5d92a8988da6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:42 +0000

    Add package Resource-Locator-Program/0.0.3/Resource%20Locator%20Program-0.0.3.tar.gz

---
+++ packages/Resource-Locator-Program/Resource%20Locator%20Program-0.0.3.tar.gz/Resource Locator Program-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+  long_description = f.read()
+
+
+setup(name="Resource Locator Program",
+  version="0.0.3",
+  description="Resource Locator Program",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author="Rick Wierenga",
+  author_email="rick_wierenga@icloud.com",
+  url="https://www.github.com/pylabrobot/resource-locator-program/",
+  install_requires=["PyQt6", "pylabrobot"],
+  packages=["resource_locator_program"],
+  entry_points={
+    "console_scripts": [
+        "resource-locator-program = resource_locator_program.app:main"
+    ]
+  }
+)
