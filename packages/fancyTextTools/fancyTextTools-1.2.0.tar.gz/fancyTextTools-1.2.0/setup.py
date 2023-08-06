aadc886b05c1965a09576856f1c01988255954a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:22 +0000

    Add package fancyTextTools/1.2.0/fancyTextTools-1.2.0.tar.gz

---
+++ packages/fancyTextTools/fancyTextTools-1.2.0.tar.gz/fancyTextTools-1.2.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='fancyTextTools',
+    version="1.2.0",    
+    description='A fancyTextTools Python package',
+    url='https://github.com/becker63/textTools',
+    author='Taylor Johnson',
+    author_email='johnsontaylor6320@gmail.com',
+    license='BSD 2-clause',
+    packages=['fancyTextTools'],
+
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
