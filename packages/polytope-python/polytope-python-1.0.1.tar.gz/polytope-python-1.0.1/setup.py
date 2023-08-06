f3ffd16bbf3e06e67d30073e303dea8ba2e9d633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:22 +0000

    Add package polytope-python/1.0.1/polytope-python-1.0.1.tar.gz

---
+++ packages/polytope-python/polytope-python-1.0.1.tar.gz/polytope-python-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import io
+import re
+
+from setuptools import find_packages, setup
+
+__version__ = re.search(
+    r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+    io.open("polytope/version.py", encoding="utf_8_sig").read(),
+).group(1)
+
+
+setup(
+    name="polytope-python",
+    version=__version__,
+    description="Polytope datacube feature extraction library",
+    url="https://github.com/ecmwf/polytope",
+    author="ECMWF",
+    author_email="James.Hawkes@ecmwf.int, Mathilde.Leuridan@ecmwf.int",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+)
