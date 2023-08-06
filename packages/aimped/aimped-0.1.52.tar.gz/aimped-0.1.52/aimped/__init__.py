0f389e242f381436b2bd33ea2e95b5d99746289a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:07 +0000

    Add package aimped/0.1.52/aimped-0.1.52.tar.gz

---
+++ packages/aimped/aimped-0.1.52.tar.gz/aimped-0.1.52/aimped/__init__.py
@@ -0,0 +1,7 @@
+from aimped import nlp
+from aimped import utils
+from aimped import nitro
+from aimped import models
+from aimped import version
+from aimped import io_tasks
+from aimped.version import __version__
