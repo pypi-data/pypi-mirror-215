43a266c8e90dae0467b0d2af1227eeffeedc19b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:47 +0000

    Add package petisco/2.0.0rc1/petisco-2.0.0rc1.tar.gz

---
+++ packages/petisco/petisco-2.0.0rc1.tar.gz/petisco-2.0.0rc1/petisco/extra/sqlalchemy/__init__.py
@@ -0,0 +1,26 @@
+from petisco.extra.sqlalchemy.is_sqlalchemy_available import is_sqlalchemy_available
+
+if is_sqlalchemy_available():
+    from petisco.extra.sqlalchemy.sql.async_sql_database import AsyncSqlDatabase
+    from petisco.extra.sqlalchemy.sql.mysql.legacy_mysql_database import (
+        LegacyMySqlDatabase,
+    )
+    from petisco.extra.sqlalchemy.sql.mysql.mysql_connection import MySqlConnection
+    from petisco.extra.sqlalchemy.sql.sql_database import SqlDatabase
+    from petisco.extra.sqlalchemy.sql.sql_executor import SqlExecutor
+    from petisco.extra.sqlalchemy.sql.sqlite.legacy_sqlite_database import (
+        LegacySqliteDatabase,
+    )
+    from petisco.extra.sqlalchemy.sql.sqlite.sqlite_connection import SqliteConnection
+
+    __all__ = [
+        "MySqlConnection",
+        "LegacyMySqlDatabase",
+        "SqliteConnection",
+        "LegacySqliteDatabase",
+        "SqlExecutor",
+        "SqlDatabase",
+        "AsyncSqlDatabase",
+    ]
+else:
+    __all__ = []
