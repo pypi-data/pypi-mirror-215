87f2ae146dfd8b0af579775df6fa7fc6455ca0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:44 +0000

    Add package libprick/1.1.1/libprick-1.1.1.tar.gz

---
+++ packages/libprick/libprick-1.1.1.tar.gz/libprick-1.1.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+    name = 'libprick',
+    packages = ['libprick'],
+    version = '1.1.1',
+    description = 'Python interface to the Last.FM API/website with caching support',
+    url = 'https://github.com/spiritualized/libprick',
+    download_url = 'https://github.com/libprick/lastfmcache/archive/v1.1.1.tar.gz',
+    keywords = ['pricker', 'python', 'hash', 'sha256'],
+    install_requires = [
+                    'av==10.0.0'
+                ],
+
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
