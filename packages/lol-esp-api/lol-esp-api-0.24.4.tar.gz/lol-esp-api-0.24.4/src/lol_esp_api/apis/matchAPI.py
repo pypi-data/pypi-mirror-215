2d016583ea61e8c87af072bd11e454c15d556c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:01 +0000

    Add package lol-esp-api/0.24.4/lol-esp-api-0.24.4.tar.gz

---
+++ packages/lol-esp-api/lol-esp-api-0.24.4.tar.gz/lol-esp-api-0.24.4/src/lol_esp_api/apis/matchAPI.py
@@ -0,0 +1,16 @@
+from supfunc import request
+
+def getWindow(gameID="", startingTime="", participantIds=""):
+    url = "https://feed.lolesports.com/livestats/v1/window/{0}?startingTime={1}&participentId={2}".format(
+        gameID, startingTime, participantIds
+    )
+
+    return request(url)
+
+
+def getDetails(gameID, startingTime="", participantIds=""):
+    url = "https://feed.lolesports.com/livestats/v1/details/{0}?startingTime={1}&participentId={2}".format(
+        gameID, startingTime, participantIds
+    )
+
+    return request(url)
