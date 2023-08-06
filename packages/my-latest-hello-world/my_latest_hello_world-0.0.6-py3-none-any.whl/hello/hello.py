84e095b2a21ca99a8c881f22dc6faa870a07692a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:06 +0000

    Add package my-latest-hello-world/0.0.6/my_latest_hello_world-0.0.6-py3-none-any.whl

---
+++ packages/my-latest-hello-world/my_latest_hello_world-0.0.6-py3-none-any.whl/hello/hello.py
@@ -0,0 +1,15 @@
+"""
+A function to print Hello World
+"""
+
+def hello_world():
+    """
+    My first hello world function
+    """
+    print("Hello World, Welcome to this tutorial")
+
+def say_hello_to_me(name):
+    """
+    Say hello to me
+    """
+    print(f"Hello {name}, Welcome to this tutorial")
