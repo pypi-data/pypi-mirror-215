f5e0acd76e7f746f217ecc974e5e3a0eb7a88d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:55 +0000

    Add package nitric/0.13.0.dev2/nitric-0.13.0.dev2.tar.gz

---
+++ packages/nitric/nitric-0.13.0.dev2.tar.gz/nitric-0.13.0.dev2/nitric/resources/buckets.py
@@ -0,0 +1,115 @@
+#
+# Copyright (c) 2021 Nitric Technologies Pty Ltd.
+#
+# This file is part of Nitric Python 3 SDK.
+# See https://github.com/nitrictech/python-sdk for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from __future__ import annotations
+
+from nitric.exception import exception_from_grpc_error
+from nitric.api.storage import BucketRef, Storage
+from typing import List, Union
+from enum import Enum
+from grpclib import GRPCError
+
+from nitric.application import Nitric
+from nitric.faas import FunctionServer, BucketNotificationWorkerOptions, Middleware, BucketNotificationContext
+from nitric.proto.nitric.resource.v1 import (
+    Resource,
+    ResourceType,
+    Action,
+    ResourceDeclareRequest,
+)
+
+from nitric.resources.resource import SecureResource
+
+
+class BucketPermission(Enum):
+    """Valid query expression operators."""
+
+    reading = "reading"
+    writing = "writing"
+    deleting = "deleting"
+
+
+class Bucket(SecureResource):
+    """A bucket resource, used for storage and retrieval of blob/binary data."""
+
+    name: str
+    actions: List[Action]
+    _server: FunctionServer
+
+    def __init__(self, name: str):
+        """Create a bucket with the name provided or references it if it already exists."""
+        super().__init__()
+        self.name = name
+
+    async def _register(self):
+        try:
+            await self._resources_stub.declare(
+                resource_declare_request=ResourceDeclareRequest(resource=self._to_resource())
+            )
+        except GRPCError as grpc_err:
+            raise exception_from_grpc_error(grpc_err)
+
+    def _perms_to_actions(self, *args: Union[BucketPermission, str]) -> List[Action]:
+        permission_actions_map = {
+            BucketPermission.reading: [Action.BucketFileGet, Action.BucketFileList],
+            BucketPermission.writing: [Action.BucketFilePut],
+            BucketPermission.deleting: [Action.BucketFileDelete],
+        }
+        # convert strings to the enum value where needed
+        perms = [
+            permission if isinstance(permission, BucketPermission) else BucketPermission[permission.lower()]
+            for permission in args
+        ]
+        return [action for perm in perms for action in permission_actions_map[perm]]
+
+    def _to_resource(self) -> Resource:
+        return Resource(name=self.name, type=ResourceType.Bucket)
+
+    def allow(self, *args: Union[BucketPermission, str]) -> BucketRef:
+        """Request the required permissions for this resource."""
+        str_args = [str(permission) for permission in args]
+        self._register_policy(*str_args)
+
+        return Storage().bucket(self.name)
+
+    def on(self, notification_type: str, notification_prefix_filter: str):
+        """Create and return a bucket notification decorator for this bucket."""
+        print("this has been called")
+
+        def decorator(func: Middleware[BucketNotificationContext]):
+            print("this has been called")
+            self._server = FunctionServer(
+                BucketNotificationWorkerOptions(
+                    bucket_name=self.name,
+                    notification_type=notification_type,
+                    notification_prefix_filter=notification_prefix_filter,
+                )
+            )
+            self._server.bucket_notification(func)
+            return Nitric._register_worker(self._server)  # type: ignore
+
+        return decorator
+
+
+def bucket(name: str) -> Bucket:
+    """
+    Create and register a bucket.
+
+    If a bucket has already been registered with the same name, the original reference will be reused.
+    """
+    return Nitric._create_resource(Bucket, name)  # type: ignore
---
+++ packages/nitric/nitric-0.13.0.dev2.tar.gz/nitric-0.13.0.dev2/nitric/resources/collections.py
@@ -0,0 +1,99 @@
+#
+# Copyright (c) 2021 Nitric Technologies Pty Ltd.
+#
+# This file is part of Nitric Python 3 SDK.
+# See https://github.com/nitrictech/python-sdk for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from __future__ import annotations
+
+from nitric.api.documents import CollectionRef, Documents
+from nitric.exception import exception_from_grpc_error
+from typing import List, Union
+from enum import Enum
+from grpclib import GRPCError
+
+from nitric.application import Nitric
+from nitric.proto.nitric.resource.v1 import (
+    Resource,
+    ResourceType,
+    Action,
+    ResourceDeclareRequest,
+)
+
+from nitric.resources.resource import SecureResource
+
+
+class CollectionPermission(Enum):
+    """Valid query expression operators."""
+
+    reading = "reading"
+    writing = "writing"
+    deleting = "deleting"
+
+
+class Collection(SecureResource):
+    """A document collection resource."""
+
+    def __init__(self, name: str):
+        """Construct a new document collection."""
+        super().__init__()
+        self.name = name
+
+    async def _register(self):
+        try:
+            await self._resources_stub.declare(
+                resource_declare_request=ResourceDeclareRequest(resource=self._to_resource())
+            )
+        except GRPCError as grpc_err:
+            raise exception_from_grpc_error(grpc_err)
+
+    def _to_resource(self) -> Resource:
+        return Resource(name=self.name, type=ResourceType.Collection)
+
+    def _perms_to_actions(self, *args: Union[CollectionPermission, str]) -> List[Action]:
+        permission_actions_map = {
+            CollectionPermission.reading: [
+                Action.CollectionDocumentRead,
+                Action.CollectionQuery,
+                Action.CollectionList,
+            ],
+            CollectionPermission.writing: [Action.CollectionDocumentWrite, Action.CollectionList],
+            CollectionPermission.deleting: [Action.CollectionDocumentDelete, Action.CollectionList],
+        }
+        # convert strings to the enum value where needed
+        perms = [
+            permission if isinstance(permission, CollectionPermission) else CollectionPermission[permission.lower()]
+            for permission in args
+        ]
+
+        return [action for perm in perms for action in permission_actions_map[perm]]
+
+    def allow(self, *args: Union[CollectionPermission, str]) -> CollectionRef:
+        """Request the required permissions for this collection."""
+        # Ensure registration of the resource is complete before requesting permissions.
+        str_args = [str(permission) for permission in args]
+        self._register_policy(*str_args)
+
+        return Documents().collection(self.name)
+
+
+def collection(name: str) -> Collection:
+    """
+    Create and register a collection.
+
+    If a collection has already been registered with the same name, the original reference will be reused.
+    """
+    # type ignored because the register call is treated as protected.
+    return Nitric._create_resource(Collection, name)  # type: ignore
