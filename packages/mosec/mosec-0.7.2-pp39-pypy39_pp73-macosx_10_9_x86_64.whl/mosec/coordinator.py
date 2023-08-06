d7c6dcda5c520b1f634abe7dde22ae6414b4cbe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:01 +0000

    Add package mosec/0.7.2/mosec-0.7.2-pp39-pypy39_pp73-macosx_10_9_x86_64.whl

---
+++ packages/mosec/mosec-0.7.2-pp39-pypy39_pp73-macosx_10_9_x86_64.whl/mosec/coordinator.py
@@ -0,0 +1,359 @@
+# Copyright 2022 MOSEC Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""The Coordinator is used to control the data flow between `Worker` and `Server`."""
+
+import os
+import queue
+import signal
+import socket
+import struct
+import threading
+import time
+import traceback
+from contextlib import contextmanager
+from multiprocessing.synchronize import Event
+from typing import Any, Callable, Optional, Sequence, Tuple, Type
+
+from mosec.errors import MosecError, MosecTimeoutError
+from mosec.ipc import IPCWrapper
+from mosec.log import get_internal_logger
+from mosec.protocol import HTTPStautsCode, Protocol
+from mosec.worker import SSEWorker, Worker
+
+logger = get_internal_logger()
+
+
+CONN_MAX_RETRY = 10
+CONN_CHECK_INTERVAL = 1
+
+STAGE_INGRESS = "ingress"
+STAGE_EGRESS = "egress"
+
+PROTOCOL_TIMEOUT = 2.0
+
+
+@contextmanager
+def set_mosec_timeout(duration: int):
+    """Context manager to set a timeout for a code block.
+
+    Args:
+        duration (float): the duration in seconds before timing out
+    """
+
+    def handler(signum, frame):
+        raise MosecTimeoutError(
+            f"[{signum}]`forward` timeout after {duration}s: {frame}"
+        )
+
+    signal.signal(signal.SIGALRM, handler)
+    signal.alarm(duration)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+
+
+class FakeSemaphore:
+    """Fake semaphore interface for non-SSE workers."""
+
+    def __init__(self, value: int = 1):
+        """Not intend to be used by users."""
+        self.value = value
+
+    def acquire(self, timeout: Optional[float] = None):
+        """Acquire the semaphore."""
+
+    def release(self):
+        """Release the semaphore."""
+
+    __enter__ = acquire
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Exit the context."""
+        self.release()
+
+
+class Coordinator:
+    """Coordinator controls the data flow.
+
+    This private class defines a set of coordination behaviors
+    to receive tasks via `Protocol` and process them via `Worker`.
+    """
+
+    # pylint: disable=too-many-arguments
+    def __init__(
+        self,
+        worker: Type[Worker],
+        max_batch_size: int,
+        stage: str,
+        shutdown: Event,
+        shutdown_notify: Event,
+        socket_prefix: str,
+        stage_id: int,
+        worker_id: int,
+        ipc_wrapper: Optional[Callable[..., IPCWrapper]],
+        timeout: int,
+    ):
+        """Initialize the mosec coordinator.
+
+        Args:
+            worker (Worker): subclass of `mosec.Worker` implemented by users.
+            max_batch_size (int): maximum batch size for this worker.
+            stage (str): identifier to distinguish the first and last stages.
+            shutdown (Event): `multiprocessing.synchronize.Event` object for shutdown
+                IPC.
+            socket_prefix (str): prefix for the socket addresses.
+            stage_id (int): identification number for worker stages.
+            worker_id (int): identification number for worker processes at the same
+                stage.
+            ipc_wrapper (IPCWrapper): IPC wrapper class to be initialized.
+            timeout (int): timeout for the `forward` function.
+
+        Raises:
+            TypeError: ipc_wrapper should inherit from `IPCWrapper`
+        """
+        self.worker = worker()
+        self.worker.worker_id = worker_id
+        self.worker.max_batch_size = max_batch_size
+        self.worker.stage = stage
+        self.timeout = timeout
+        self.name = f"<{stage_id}|{worker.__name__}|{worker_id}>"
+        self.current_ids: Sequence[bytes] = []
+        self.semaphore = FakeSemaphore()  # type: ignore
+
+        self.protocol = Protocol(
+            name=self.name,
+            addr=os.path.join(socket_prefix, f"ipc_{stage_id}.socket"),
+            timeout=PROTOCOL_TIMEOUT,
+        )
+
+        # optional plugin features - ipc wrapper
+        self.ipc_wrapper: Optional[IPCWrapper] = None
+        if ipc_wrapper is not None:
+            self.ipc_wrapper = ipc_wrapper()
+            if not issubclass(type(self.ipc_wrapper), IPCWrapper):
+                raise TypeError(
+                    "ipc_wrapper must be the subclass of mosec.plugins.IPCWrapper"
+                )
+
+        # ignore termination & interruption signal
+        signal.signal(signal.SIGTERM, signal.SIG_IGN)
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
+        self.shutdown = shutdown
+        self.shutdown_notify = shutdown_notify
+
+        # SSE support
+        if issubclass(worker, SSEWorker):
+            self.stream: queue.SimpleQueue = queue.SimpleQueue()
+            self.semaphore: threading.Semaphore = threading.Semaphore()  # type: ignore
+            self.worker._stream_queue = self.stream  # type: ignore
+            self.worker._stream_semaphore = self.semaphore  # type: ignore
+            threading.Thread(target=self.streaming, daemon=True).start()
+
+        self.warmup()
+        self.init_protocol()
+        self.run()
+
+    def init_protocol(self):
+        """Check socket readiness."""
+        retry_count = 0
+        while retry_count < CONN_MAX_RETRY and not self.shutdown.is_set():
+            if os.path.exists(self.protocol.addr):
+                return
+            retry_count += 1
+            logger.debug(
+                "%s is trying to find the socket file: %s (%d/%d)",
+                self.name,
+                self.protocol.addr,
+                retry_count,
+                CONN_MAX_RETRY,
+            )
+            time.sleep(CONN_CHECK_INTERVAL)
+            continue
+
+        logger.error(
+            "%s cannot find the socket file: %s, will shutdown",
+            self.name,
+            self.protocol.addr,
+        )
+
+    def streaming(self):
+        """Send stream data from the worker to the server through the socket."""
+        while not self.shutdown.is_set():
+            try:
+                text, index = self.stream.get(timeout=self.timeout)
+                # encode the text with UTF-8
+                payloads = (text.encode(),)
+                ids = (self.current_ids[index],)
+                self.protocol.send(HTTPStautsCode.STREAM_EVENT, ids, payloads)
+                self.semaphore.release()
+            except queue.Empty:
+                continue
+
+    def warmup(self):
+        """Warmup to allocate resources (useful for GPU workload)[Optional]."""
+        need_warmup = self.worker.example or self.worker.multi_examples
+        if self.shutdown.is_set() or not need_warmup:
+            return
+        try:
+            # Prioritize multi_examples if both appear.
+            if self.worker.multi_examples:
+                num_eg = len(self.worker.multi_examples)
+                for i, example in enumerate(self.worker.multi_examples):
+                    self.worker.forward(example)
+                    logger.debug(
+                        "warming up... (%d/%d)",
+                        i + 1,
+                        num_eg,
+                    )
+            else:
+                self.worker.forward(self.worker.example)
+            logger.info("%s warmup successfully", self.name)
+        # pylint: disable=broad-except
+        except Exception:
+            logger.error(
+                "%s warmup failed: %s, please ensure"
+                " worker's example meets its forward input format",
+                self.name,
+                traceback.format_exc().replace("\n", " "),
+            )
+
+    def run(self):
+        """Maintain the protocol connection and run the coordination."""
+        while not self.shutdown.is_set():
+            # reconnect if needed
+            try:
+                self.protocol.open()
+            except OSError as err:
+                if not self.shutdown_notify.is_set():
+                    logger.error("%s socket connection error: %s", self.name, err)
+                break
+
+            self.coordinate()
+
+    def get_decoder(self) -> Callable[[bytes], Any]:
+        """Get the decoder function for this stage.
+
+        The first stage will use the worker's deserialize function.
+        """
+        if STAGE_INGRESS in self.worker.stage:
+            return self.worker.deserialize
+        return self.worker.deserialize_ipc
+
+    def get_encoder(self) -> Callable[[Any], bytes]:
+        """Get the encoder function for this stage.
+
+        The last stage will use the worker's serialize function.
+        """
+        if STAGE_EGRESS in self.worker.stage:
+            return self.worker.serialize
+        return self.worker.serialize_ipc
+
+    def get_protocol_recv(
+        self,
+    ) -> Callable[[], Tuple[bytes, Sequence[bytes], Sequence[bytes]]]:
+        """Get the protocol receive function for this stage.
+
+        IPC wrapper will be used if it's provided and the stage is not the first one.
+        """
+        if STAGE_INGRESS in self.worker.stage or self.ipc_wrapper is None:
+            return self.protocol.receive
+
+        # TODO(kemingy) find a better way
+        def wrapped_recv() -> Tuple[bytes, Sequence[bytes], Sequence[bytes]]:
+            flag, ids, payloads = self.protocol.receive()
+            payloads = self.ipc_wrapper.get(  # type: ignore
+                [bytes(x) for x in payloads]
+            )
+            return flag, ids, payloads
+
+        return wrapped_recv
+
+    def get_protocol_send(
+        self,
+    ) -> Callable[[int, Sequence[bytes], Sequence[bytes]], None]:
+        """Get the protocol send function for this stage.
+
+        IPC wrapper will be used if it's provided and the stage is not the last one.
+        """
+        if STAGE_EGRESS in self.worker.stage or self.ipc_wrapper is None:
+            return self.protocol.send
+
+        # TODO(kemingy) find a better way
+        def wrapped_send(flag: int, ids: Sequence[bytes], payloads: Sequence[bytes]):
+            if flag == HTTPStautsCode.OK:
+                payloads = self.ipc_wrapper.put(payloads)  # type: ignore
+            return self.protocol.send(flag, ids, payloads)
+
+        return wrapped_send
+
+    def coordinate(self):
+        """Start coordinating the protocol's communication and worker's forward pass."""
+        decoder = self.get_decoder()
+        encoder = self.get_encoder()
+        protocol_recv = self.get_protocol_recv()
+        protocol_send = self.get_protocol_send()
+        while not self.shutdown.is_set():
+            try:
+                _, ids, payloads = protocol_recv()
+                # expose here to be used by stream event
+                self.current_ids = ids
+            except socket.timeout:
+                continue
+            except (struct.error, OSError) as err:
+                if not self.shutdown_notify.is_set():
+                    logger.error("%s failed to read from socket: %s", self.name, err)
+                break
+
+            # pylint: disable=broad-except
+            length = len(payloads)
+            try:
+                data = [decoder(item) for item in payloads]
+                with set_mosec_timeout(self.timeout):
+                    data = (
+                        self.worker.forward(data)
+                        if self.worker.max_batch_size > 1
+                        else (self.worker.forward(data[0]),)
+                    )
+                if len(data) != length:
+                    raise ValueError(
+                        "returned data size doesn't match the input data size:"
+                        f"input({length})!=output({len(data)})"
+                    )
+                status = HTTPStautsCode.OK
+                payloads = [encoder(item) for item in data]
+            except (MosecError, MosecTimeoutError) as err:
+                err_msg = str(err).replace("\n", " - ")
+                err_msg = err_msg if err_msg else err.msg
+                logger.info("%s %s: %s", self.name, err.msg, err_msg)
+                status = err.code
+                payloads = [f"{err.msg}: {err_msg}".encode()] * length
+            except Exception:
+                logger.warning(traceback.format_exc().replace("\n", " "))
+                status = HTTPStautsCode.INTERNAL_ERROR
+                payloads = ["inference internal error".encode()] * length
+
+            try:
+                # pylint: disable=consider-using-with
+                self.semaphore.acquire(timeout=self.timeout)
+                protocol_send(status, ids, payloads)
+            except OSError as err:
+                logger.error("%s failed to send to socket: %s", self.name, err)
+                break
+            finally:
+                self.semaphore.release()
+
+        self.protocol.close()
+        time.sleep(CONN_CHECK_INTERVAL)
---
+++ packages/mosec/mosec-0.7.2-pp39-pypy39_pp73-macosx_10_9_x86_64.whl/mosec/server.py
@@ -0,0 +1,273 @@
+# Copyright 2022 MOSEC Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""MOSEC server interface.
+
+This module provides a way to define the service components for machine learning
+model serving.
+
+Dynamic Batching
+----------------
+
+    The user may enable the dynamic batching feature for any stage when the
+    corresponding worker is appended, by setting the
+    :py:meth:`append_worker(max_batch_size) <Server.append_worker>`.
+
+Multiprocess
+------------
+
+    The user may spawn multiple processes for any stage when the
+    corresponding worker is appended, by setting the
+    :py:meth:`append_worker(num) <Server.append_worker>`.
+"""
+
+import json
+import multiprocessing as mp
+import os
+import pathlib
+import shutil
+import signal
+import subprocess
+import traceback
+from functools import partial
+from multiprocessing.synchronize import Event
+from time import sleep
+from typing import Dict, List, Optional, Type, Union
+
+from mosec.args import parse_arguments
+from mosec.dry_run import DryRunner
+from mosec.ipc import IPCWrapper
+from mosec.log import get_internal_logger
+from mosec.runtime import PyRuntimeManager, RsRuntimeManager, Runtime
+from mosec.utils import ParseTarget
+from mosec.worker import MOSEC_REF_TEMPLATE, Worker
+
+logger = get_internal_logger()
+
+
+GUARD_CHECK_INTERVAL = 1
+MOSEC_OPENAPI_PATH = "mosec_openapi.json"
+
+
+class Server:
+    """MOSEC server interface.
+
+    It allows users to sequentially append workers they implemented, builds
+    the workflow pipeline automatically and starts up the server.
+    """
+
+    # pylint: disable=too-many-instance-attributes
+    def __init__(
+        self,
+        ipc_wrapper: Optional[Union[IPCWrapper, partial]] = None,
+        endpoint: str = "/inference",
+    ):
+        """Initialize a MOSEC Server.
+
+        Args:
+            ipc_wrapper: (deprecated) wrapper function (before and after) IPC
+            endpoint: path to route inference
+        """
+        self.ipc_wrapper = ipc_wrapper
+        self.endpoint = endpoint
+
+        self._shutdown: Event = mp.get_context("spawn").Event()
+        self._shutdown_notify: Event = mp.get_context("spawn").Event()
+        self._configs: dict = vars(parse_arguments())
+
+        self.coordinator_manager: PyRuntimeManager = PyRuntimeManager(
+            self._configs["path"], self._shutdown, self._shutdown_notify
+        )
+        self.controller = RsRuntimeManager(
+            self.coordinator_manager, endpoint, self._configs
+        )
+
+        self._daemon: Dict[str, Union[subprocess.Popen, mp.Process]] = {}
+
+        self._server_shutdown: bool = False
+
+    def _handle_signal(self):
+        signal.signal(signal.SIGTERM, self._terminate)
+        signal.signal(signal.SIGINT, self._terminate)
+
+    def _validate_server(self):
+        assert self.coordinator_manager.worker_count > 0, (
+            "no worker registered\n"
+            "help: use `.append_worker(...)` to register at least one worker"
+        )
+
+    def _check_daemon(self):
+        for name, proc in self._daemon.items():
+            if proc is None:
+                continue
+            code = None
+            if isinstance(proc, mp.Process):
+                code = proc.exitcode
+            elif isinstance(proc, subprocess.Popen):
+                code = proc.poll()
+
+            if code is not None:
+                self._terminate(
+                    code,
+                    f"mosec daemon [{name}] exited on error code: {code}",
+                )
+
+    def _start_controller(self):
+        """Subprocess to start controller program."""
+        if self._server_shutdown:
+            return
+        process = self.controller.start()
+        self.register_daemon("controller", process)
+
+    def _terminate(self, signum, framestack):
+        logger.info("received signum[%s], terminating server [%s]", signum, framestack)
+        self._server_shutdown = True
+
+    def _manage_coordinators(self):
+        first = True
+        while not self._server_shutdown:
+            failed_worker = self.coordinator_manager.check_and_start(first)
+            if failed_worker is not None:
+                self._terminate(
+                    1,
+                    f"all the {failed_worker.worker.__name__} workers"
+                    f" at stage {failed_worker.stage_id} exited;"
+                    " please check for bugs or socket connection issues",
+                )
+            first = False
+            self._check_daemon()
+            sleep(GUARD_CHECK_INTERVAL)
+
+    def _halt(self):
+        """Graceful shutdown."""
+        # notify coordinators for the shutdown
+        self._shutdown_notify.set()
+        self.controller.halt()
+        # shutdown coordinators
+        self._shutdown.set()
+        shutil.rmtree(self._configs["path"], ignore_errors=True)
+        logger.info("mosec exited normally")
+
+    def register_daemon(self, name: str, proc: subprocess.Popen):
+        """Register a daemon to be monitored.
+
+        Args:
+            name: the name of this daemon
+            proc: the process handle of the daemon
+        """
+        assert isinstance(name, str), "daemon name should be a string"
+        assert isinstance(
+            proc, (mp.Process, subprocess.Popen)
+        ), f"{type(proc)} is not a process or subprocess"
+        self._daemon[name] = proc
+
+    # pylint: disable=too-many-arguments
+    def append_worker(
+        self,
+        worker: Type[Worker],
+        num: int = 1,
+        max_batch_size: int = 1,
+        max_wait_time: int = 0,
+        start_method: str = "spawn",
+        env: Union[None, List[Dict[str, str]]] = None,
+        timeout: int = 0,
+    ):
+        """Sequentially appends workers to the workflow pipeline.
+
+        Args:
+            worker: the class you inherit from :class:`Worker<mosec.worker.Worker>`
+                which implements the :py:meth:`forward<mosec.worker.Worker.forward>`
+            num: the number of processes for parallel computing (>=1)
+            max_batch_size: the maximum batch size allowed (>=1), will enable the
+                dynamic batching if it > 1
+            max_wait_time: the maximum wait time (millisecond) for dynamic batching,
+                needs to be used with `max_batch_size` to enable the feature. If not
+                configure, will use the CLI argument `--wait` (default=10ms)
+            start_method: the process starting method ("spawn" or "fork")
+            env: the environment variables to set before starting the process
+            timeout: the timeout (second) for each worker forward processing (>=1)
+        """
+        timeout = timeout if timeout >= 1 else self._configs["timeout"] // 1000
+        max_wait_time = max_wait_time if max_wait_time >= 1 else self._configs["wait"]
+        stage_id = self.coordinator_manager.worker_count
+        runtime = Runtime(
+            worker,
+            num,
+            max_batch_size,
+            max_wait_time,
+            stage_id + 1,
+            timeout,
+            start_method,
+            env,
+            None,
+        )
+        runtime.validate()
+        self.coordinator_manager.append(runtime)
+
+    def _generate_openapi(self):
+        """Generate the OpenAPI specification."""
+        if self.coordinator_manager.worker_count <= 0:
+            return
+        workers = self.coordinator_manager.workers
+        request_worker_cls, response_worker_cls = workers[0], workers[-1]
+        input_schema, input_components = request_worker_cls.get_forward_json_schema(
+            ParseTarget.INPUT, MOSEC_REF_TEMPLATE
+        )
+        return_schema, return_components = response_worker_cls.get_forward_json_schema(
+            ParseTarget.RETURN, MOSEC_REF_TEMPLATE
+        )
+
+        def make_body(description, mime, schema):
+            if not schema:
+                return None
+            return {"description": description, "content": {mime: {"schema": schema}}}
+
+        schema = {
+            "request_body": make_body(
+                "Mosec Inference Request Body",
+                request_worker_cls.resp_mime_type,
+                input_schema,
+            ),
+            "responses": None
+            if not return_schema
+            else {
+                200: make_body(
+                    "Mosec Inference Result",
+                    response_worker_cls.resp_mime_type,
+                    return_schema,
+                )
+            },
+            "schemas": {**input_components, **return_components},
+        }
+        tmp_path = pathlib.Path(os.path.join(self._configs["path"], MOSEC_OPENAPI_PATH))
+        tmp_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(tmp_path, "w", encoding="utf-8") as file:
+            json.dump(schema, file)
+
+    def run(self):
+        """Start the mosec model server."""
+        self._validate_server()
+        if self._configs["dry_run"]:
+            DryRunner(self.coordinator_manager).run()
+            return
+
+        self._handle_signal()
+        self._generate_openapi()
+        self._start_controller()
+        try:
+            self._manage_coordinators()
+        # pylint: disable=broad-except
+        except Exception:
+            logger.error(traceback.format_exc().replace("\n", " "))
+        self._halt()
