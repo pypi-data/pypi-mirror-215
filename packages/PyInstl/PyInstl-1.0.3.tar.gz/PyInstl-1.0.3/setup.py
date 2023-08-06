4f5addc49cf21585b2742fded62964af452050e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:43 +0000

    Add package PyInstl/1.0.3/PyInstl-1.0.3.tar.gz

---
+++ packages/PyInstl/PyInstl-1.0.3.tar.gz/PyInstl-1.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, find_namespace_packages
+from setuptools import setup
+
+setup(
+    name="PyInstl",
+    version="1.0.3",
+    description="A package installer cli for python",
+    author="Muhammed Mehmood",
+    author_email="ifreezeu2@gmail.com",
+    url="https://github.com/Mo-Xcoder/PyInstl",
+    install_requires=["argparse", "pyfiglet", "clint"],
+    packages=find_namespace_packages(include=["PyInstl.*"]),
+    entry_points={
+        "console_scripts": [
+            "PyInstl = PyInstl.main:main",
+        ],
+    },
+)
