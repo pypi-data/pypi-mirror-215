6c18c64097d7972617650659448b8a3cd0b843a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:25 +0000

    Add package automation-utilities/1.1.1/automation-utilities-1.1.1.tar.gz

---
+++ packages/automation-utilities/automation-utilities-1.1.1.tar.gz/automation-utilities-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='automation-utilities',
+    version='1.1.1',
+    author='Hazem Oukal',
+    description='Added Files module that have the add method',
+    packages=['automation_utilities'],
+    install_requires=[],
+)
