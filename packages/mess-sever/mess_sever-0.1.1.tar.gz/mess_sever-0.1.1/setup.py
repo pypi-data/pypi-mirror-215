d03ec97998639fa6323619a0f9076f2bae0286df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:50 +0000

    Add package mess-sever/0.1.1/mess_sever-0.1.1.tar.gz

---
+++ packages/mess-sever/mess_sever-0.1.1.tar.gz/mess_sever-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="mess_sever",
+      version="0.1.1",
+      description="mess_client",
+      author="Artem Gladkov",
+      author_email="2020artem@mail.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
