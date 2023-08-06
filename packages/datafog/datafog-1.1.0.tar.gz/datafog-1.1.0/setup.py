37ea13299c704ca65663ecb9f254de7e5718e21e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:37 +0000

    Add package datafog/1.1.0/datafog-1.1.0.tar.gz

---
+++ packages/datafog/datafog-1.1.0.tar.gz/datafog-1.1.0/setup.py
@@ -0,0 +1,21 @@
+#  package using pip, navigate to the directory that contains the setup.py file and type pip install .
+from setuptools import setup, find_packages
+
+
+
+
+setup(
+    name='datafog',
+    version='1.1.0',  # versioning of your package
+    packages=find_packages(),  # automatically find all packages
+    author='Sid Mohan',
+    author_email='sid@datafog.dev',
+    description='A Python package that provides several methods for data handling',  # a brief description of your package
+    long_description=open('README.md').read(),  # a long description read from the README.md file
+    install_requires=['faker', 'pandas','sqlalchemy','sqlalchemy.orm','werkzeug','typing','hashlib'],  # a list of other Python packages required by this package
+    classifiers=[
+        'License :: OSI Approved :: BSD License',  # Choose a license
+        'Programming Language :: Python :: 3.10',  # Python version
+        # etc.
+    ],
+)
