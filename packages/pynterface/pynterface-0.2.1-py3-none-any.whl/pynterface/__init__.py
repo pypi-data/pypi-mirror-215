334fb74da9e2555010f04161f13dbf110215e04e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:10 +0000

    Add package pynterface/0.2.1/pynterface-0.2.1-py3-none-any.whl

---
+++ packages/pynterface/pynterface-0.2.1-py3-none-any.whl/pynterface/__init__.py
@@ -0,0 +1,12 @@
+"""
+A python module for dealing with terminal-based inputs and outputs.
+"""
+
+__version__ = "0.2.1"
+__author__ = "Vivaan Singhvi"
+
+from .menu import *
+from .input import *
+from .style import *
+from .loading import *
+from .printing import *
