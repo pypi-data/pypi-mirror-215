c996c36cf952a569da6d4468f2ae11945109658c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:11 +0000

    Add package mypiprandomdata/0.8.0/mypiprandomdata-0.8.0-py3-none-any.whl

---
+++ packages/mypiprandomdata/mypiprandomdata-0.8.0-py3-none-any.whl/mypiprandomdata/cli.py
@@ -0,0 +1,77 @@
+import random
+import string
+import json
+import pkg_resources
+
+class RandomData:
+    def __init__(self, first_name, last_name, address, city, state, zip_code, phone_number, user_agent, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.phone_number = phone_number
+        self.user_agent = user_agent
+        self.email = email
+        self.password = password
+
+def get_random_data():
+    data_dir = pkg_resources.resource_filename('mypiprandomdata', 'data')
+
+    with open(data_dir + '/data.json', 'r') as file:
+        data = json.load(file)
+
+    random_generator = random.randint(0, len(data) - 1)
+    addresses = data[random_generator]['addresses']
+
+    random_generator2 = random.randint(0, len(addresses) - 1)
+
+    random_data = {
+        "address": addresses[random_generator2]['street'],
+        "city": addresses[random_generator2]['city'],
+        "state": addresses[random_generator2]['state'],
+        "zip_code": addresses[random_generator2]['zipcode'],
+        "phone_number": addresses[random_generator2]['phone_number']
+    }
+
+    first_name = get_random_name_from_file(data_dir, 'FirstName.txt', split_by=',')
+    last_name = get_random_name_from_file(data_dir, 'LastName.txt', split_by=',')
+    user_agent = get_random_name_from_file(data_dir, 'UserAgent.txt', split_by='\n')
+    email = f"{first_name.lower()}{last_name.lower()}{random.randint(999, 9999)}{random.choice(['@gmail.com', '@yahoo.com'])}"
+    password = generate_random_password()
+
+    random_data = RandomData(first_name, last_name, random_data['address'], random_data['city'],
+                             random_data['state'], random_data['zip_code'], random_data['phone_number'],
+                             user_agent, email, password)
+
+    return random_data
+
+def get_random_name_from_file(directory, filename, split_by=None):
+    file_path = directory + '/' + filename
+    with open(file_path, 'r') as file:
+        names = file.read().strip().split(split_by) if split_by else file.readlines()
+    return random.choice(names).strip().strip('",')
+
+def generate_random_password():
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password_length = random.randint(8, 12)
+    password = ''.join(random.choice(characters) for _ in range(password_length))
+    return password
+
+def main():
+    random_data = get_random_data()
+    print("Randomly Generated Data:")
+    print("First Name:", random_data.first_name)
+    print("Last Name:", random_data.last_name)
+    print("Email:", random_data.email)
+    print("Password:", random_data.password)
+    print("Address:", random_data.address)
+    print("City:", random_data.city)
+    print("State:", random_data.state)
+    print("Zip Code:", random_data.zip_code)
+    print("Phone Number:", random_data.phone_number)
+    print("User Agent:", random_data.user_agent)
+
+if __name__ == '__main__':
+    main()
