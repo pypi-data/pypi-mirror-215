f7d37e6bc5ddbaff21646d70b2fa0e4e42716486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:18 +0000

    Add package verysimplemodule-lais/0.0.1.2/verysimplemodule_lais-0.0.1.2.tar.gz

---
+++ packages/verysimplemodule-lais/verysimplemodule_lais-0.0.1.2.tar.gz/verysimplemodule_lais-0.0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1.2' 
+DESCRIPTION = 'Meu primeiro pacote em Python'
+LONG_DESCRIPTION = 'Meu primeiro pacote em Python para baixar um PDF'
+
+# Setting up
+setup(
+       # 'name' deve corresponder ao nome da pasta 'verysimplemodule'
+        name="verysimplemodule_lais", 
+        version=VERSION,
+        author="Lais Sousa",
+        author_email="<laissouroch@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['requests'], # adicione outros pacotes que 
+        # precisem ser instalados com o seu pacote. Ex: 'caer'
+        py_modules=['baixar_arquivo'],
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
