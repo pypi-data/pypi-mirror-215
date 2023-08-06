ea9d486ae0037e0e75cdcfb33e264dc69808c61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:42 +0000

    Add package psynet/10.0.0/psynet-10.0.0.tar.gz

---
+++ packages/psynet/psynet-10.0.0.tar.gz/psynet-10.0.0/psynet/trial/video.py
@@ -0,0 +1,68 @@
+# pylint: disable=unused-argument,abstract-method
+
+from ..utils import get_logger
+from .record import (
+    MediaImitationChainNetwork,
+    MediaImitationChainNode,
+    MediaImitationChainTrial,
+    MediaImitationChainTrialMaker,
+    RecordTrial,
+)
+
+logger = get_logger()
+
+
+class CameraRecordTrial(RecordTrial):
+    __extra_vars__ = {}
+
+    recording_url_name = "camera_url"
+    recording_id_name = "camera_id"
+
+
+class ScreenRecordTrial(RecordTrial):
+    __extra_vars__ = {}
+
+    recording_url_name = "screen_url"
+    recording_id_name = "screen_id"
+
+
+class CameraImitationChainNetwork(MediaImitationChainNetwork):
+    """
+    A Network class for camera imitation chains.
+    """
+
+    media_extension = ".webm"
+
+
+class CameraImitationChainTrial(CameraRecordTrial, MediaImitationChainTrial):
+    """
+    A Trial class for camera imitation chains.
+    The user must override
+    :meth:`~psynet.trial.video_imitation_chain.analyze_recording` and
+    :meth:`~psynet.trial.video_imitation_chain.show_trial`.
+    """
+
+    pass
+
+
+class CameraImitationChainNode(MediaImitationChainNode):
+    """
+    A Node class for camera imitation chains.
+    Users must override the
+    :meth:`~psynet.trial.audio.VideoImitationChainNode.synthesize_target` method.
+    """
+
+    media_extension = ".webm"
+
+
+class CameraImitationChainTrialMaker(MediaImitationChainTrialMaker):
+    """
+    A TrialMaker class for camera imitation chains;
+    see the documentation for
+    :class:`~psynet.trial.chain.ChainTrialMaker`
+    for usage instructions.
+    """
+
+    @property
+    def default_network_class(self):
+        return CameraImitationChainNetwork
