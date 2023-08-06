beb2deee4a556fc7ad93b69300dfc59fa7306528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:40 +0000

    Add package citation-docket/0.1.4/citation_docket-0.1.4-py3-none-any.whl

---
+++ packages/citation-docket/citation_docket-0.1.4-py3-none-any.whl/citation_docket/utils/__init__.py
@@ -0,0 +1,3 @@
+from .sc_website_2023 import DOCKET_PATTERN, extract_docket_meta
+from .simple_matcher import get_cat_idx_from_docket, get_cat_idx_from_orig
+from .specials import OLD_SPANISH, remove_prefix
