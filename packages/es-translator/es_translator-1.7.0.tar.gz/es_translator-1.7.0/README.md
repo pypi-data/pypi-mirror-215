c08358f70aa30599a38de342751254c7673ba32a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:32 +0000

    Add package es-translator/1.7.0/es_translator-1.7.0.tar.gz

---
+++ packages/es-translator/es_translator-1.7.0.tar.gz/es_translator-1.7.0/README.md
@@ -0,0 +1,103 @@
+# ES Translator [![](https://img.shields.io/github/actions/workflow/status/icij/es-translator/main.yml)](https://github.com/ICIJ/es-translator/actions)
+
+
+A lazy yet bulletproof machine translation tool for Elastichsearch.
+
+```
+Usage: es-translator [OPTIONS]
+
+Options:
+  -u, --url TEXT                  Elastichsearch URL
+  -i, --index TEXT                Elastichsearch Index  [required]
+  -r, --interpreter TEXT          Interpreter to use to perform the
+                                  translation
+  -s, --source-language TEXT      Source language to translate from
+                                  [required]
+  -t, --target-language TEXT      Target language to translate to  [required]
+  --intermediary-language TEXT    An intermediary language to use when no
+                                  translation is available between the source
+                                  and the target. If none is provided this
+                                  will be calculated automatically.
+  --source-field TEXT             Document field to translate
+  --target-field TEXT             Document field where the translations are
+                                  stored
+  -q, --query-string TEXT         Search query string to filter result
+  -d, --data-dir PATH             Path to the directory where to language
+                                  model will be downloaded
+  --scan-scroll TEXT              Scroll duration (set to higher value if
+                                  you're processing a lot of documents)
+  --dry-run                       Don't save anything in Elasticsearch
+  --pool-size INTEGER             Number of parallel processes to start
+  --pool-timeout INTEGER          Timeout to add a translation
+  --throttle INTEGER              Throttle between each translation (in ms)
+  --syslog-address TEXT           Syslog address
+  --syslog-port INTEGER           Syslog port
+  --syslog-facility TEXT          Syslog facility
+  --stdout-loglevel TEXT          Change the default log level for stdout
+                                  error handler
+  --progressbar / --no-progressbar
+                                  Display a progressbar
+  --help                          Show this message and exit.
+```
+
+## Installation (Ubuntu)
+
+Install Apertium:
+
+```
+wget https://apertium.projectjj.com/apt/install-nightly.sh -O - | sudo bash
+sudo apt install apertium-all-dev
+```
+
+Create a Virtualenv and install Pip packages with Poetry:
+
+```
+make install
+```
+
+On Ubuntu 22.04 some additional packages might be needed if you use the version from Ubuntu's repository:
+
+```
+sudo apt install cg3 apertium-get apertium-lex-tools
+```
+
+
+## Installation (Docker)
+
+Nothing to do as long as you have Docker on your system:
+
+```
+docker run -it icij/es-translator poetry run es-translator --help
+```
+
+## Examples
+
+Translates documents from French to Spanish on a local Elasticsearch. The translated field is `content` (the default).
+
+```bash
+poetry run es-translator --url "http://localhost:9200" --index my-index --source-language fr --target-language es
+```
+
+Translates documents from French to English on a local Elasticsearch using Apertium:
+
+```bash
+poetry run es-translator --url "http://localhost:9200" --index my-index --source-language fr --target-language en --interpreter apertium
+```
+
+To translate the `title` field we could do:
+
+```bash
+poetry run es-translator --url "http://localhost:9200" --index my-index --source-language fr --target-language es --source-field title
+```
+
+Translates documents from English to Spanish on a local Elasticsearch using 4 threads:
+
+```bash
+poetry run es-translator --url "http://localhost:9200" --index my-index --source-language en --target-language es --pool-size 4
+```
+
+Translates documents from Portuguese to English, using an intermediary language (Apertium doesn't offer this translation pair):
+
+```bash
+poetry run es-translator --url "http://localhost:9200" --index my-index --source-language pt --intermediary-language es --target-language en
+```
---
+++ packages/es-translator/es_translator-1.7.0.tar.gz/es_translator-1.7.0/setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['es_translator',
+ 'es_translator.interpreters',
+ 'es_translator.interpreters.apertium',
+ 'es_translator.interpreters.argos']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['argostranslate>=1.7,<2.0',
+ 'celery[redis]>=5.3.1,<6.0.0',
+ 'click>=8,<9',
+ 'coloredlogs',
+ 'deb-pkg-tools>=8.4,<9.0',
+ 'elasticsearch-dsl>=7.4.0,<8.0.0',
+ 'elasticsearch>=7.10,<7.11',
+ 'pycountry>=22.3,<23.0',
+ 'rich>=12,<13',
+ 'sh>=1,<2',
+ 'torch>=1.13,<2.0',
+ 'urllib3>=1.26,<2.0']
+
+entry_points = \
+{'console_scripts': ['es-translator = es_translator.cli:translate',
+                     'es-translator-pairs = es_translator.cli:pairs',
+                     'es-translator-tasks = es_translator.cli:tasks']}
+
+setup_kwargs = {
+    'name': 'es-translator',
+    'version': '1.7.0',
+    'description': 'A lazy yet bulletproof machine translation tool for Elastichsearch.',
+    'long_description': '# ES Translator [![](https://img.shields.io/github/actions/workflow/status/icij/es-translator/main.yml)](https://github.com/ICIJ/es-translator/actions)\n\n\nA lazy yet bulletproof machine translation tool for Elastichsearch.\n\n```\nUsage: es-translator [OPTIONS]\n\nOptions:\n  -u, --url TEXT                  Elastichsearch URL\n  -i, --index TEXT                Elastichsearch Index  [required]\n  -r, --interpreter TEXT          Interpreter to use to perform the\n                                  translation\n  -s, --source-language TEXT      Source language to translate from\n                                  [required]\n  -t, --target-language TEXT      Target language to translate to  [required]\n  --intermediary-language TEXT    An intermediary language to use when no\n                                  translation is available between the source\n                                  and the target. If none is provided this\n                                  will be calculated automatically.\n  --source-field TEXT             Document field to translate\n  --target-field TEXT             Document field where the translations are\n                                  stored\n  -q, --query-string TEXT         Search query string to filter result\n  -d, --data-dir PATH             Path to the directory where to language\n                                  model will be downloaded\n  --scan-scroll TEXT              Scroll duration (set to higher value if\n                                  you\'re processing a lot of documents)\n  --dry-run                       Don\'t save anything in Elasticsearch\n  --pool-size INTEGER             Number of parallel processes to start\n  --pool-timeout INTEGER          Timeout to add a translation\n  --throttle INTEGER              Throttle between each translation (in ms)\n  --syslog-address TEXT           Syslog address\n  --syslog-port INTEGER           Syslog port\n  --syslog-facility TEXT          Syslog facility\n  --stdout-loglevel TEXT          Change the default log level for stdout\n                                  error handler\n  --progressbar / --no-progressbar\n                                  Display a progressbar\n  --help                          Show this message and exit.\n```\n\n## Installation (Ubuntu)\n\nInstall Apertium:\n\n```\nwget https://apertium.projectjj.com/apt/install-nightly.sh -O - | sudo bash\nsudo apt install apertium-all-dev\n```\n\nCreate a Virtualenv and install Pip packages with Poetry:\n\n```\nmake install\n```\n\nOn Ubuntu 22.04 some additional packages might be needed if you use the version from Ubuntu\'s repository:\n\n```\nsudo apt install cg3 apertium-get apertium-lex-tools\n```\n\n\n## Installation (Docker)\n\nNothing to do as long as you have Docker on your system:\n\n```\ndocker run -it icij/es-translator poetry run es-translator --help\n```\n\n## Examples\n\nTranslates documents from French to Spanish on a local Elasticsearch. The translated field is `content` (the default).\n\n```bash\npoetry run es-translator --url "http://localhost:9200" --index my-index --source-language fr --target-language es\n```\n\nTranslates documents from French to English on a local Elasticsearch using Apertium:\n\n```bash\npoetry run es-translator --url "http://localhost:9200" --index my-index --source-language fr --target-language en --interpreter apertium\n```\n\nTo translate the `title` field we could do:\n\n```bash\npoetry run es-translator --url "http://localhost:9200" --index my-index --source-language fr --target-language es --source-field title\n```\n\nTranslates documents from English to Spanish on a local Elasticsearch using 4 threads:\n\n```bash\npoetry run es-translator --url "http://localhost:9200" --index my-index --source-language en --target-language es --pool-size 4\n```\n\nTranslates documents from Portuguese to English, using an intermediary language (Apertium doesn\'t offer this translation pair):\n\n```bash\npoetry run es-translator --url "http://localhost:9200" --index my-index --source-language pt --intermediary-language es --target-language en\n```\n',
+    'author': 'ICIJ',
+    'author_email': 'engineering@icij.org',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8.2,<3.11',
+}
+
+
+setup(**setup_kwargs)
