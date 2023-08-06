233ebc44297de2eee4e3d01e1d663c2ad3f5e236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:41 +0000

    Add package fictional-names/0.0.2/fictional_names-0.0.2-py3-none-any.whl

---
+++ packages/fictional-names/fictional_names-0.0.2-py3-none-any.whl/fictional_names/human_rowling.py
@@ -0,0 +1,48 @@
+from supportive_functions import generate_female_name, generate_male_name, generate_surname_less
+
+
+female_prefix = [
+    "ada", "al", "am", "an", "bel", "beth", "ca", "ce", "cha", "da", "de", "dra", "ela", "el", "ema",
+    "en", "fa", "fay", "ga", "ge", "gi", "ha", "he", "hi", "ila", "il", "ima", "in", "ja", "je", "ka",
+    "ke", "ki", "la", "le", "li", "ma", "me", "mi", "na", "ne", "ni", "ola", "ol", "oma", "pa", "pe",
+    "pi", "qua", "ra", "re", "ri", "sa", "se", "sha", "ta", "te", "tha", "u", "va", "ve", "vi", "wa",
+    "we", "wi", "ya", "ye", "yi", "za", "ze", "zi"
+]
+
+female_suffix = [
+    "a", "ana", "ara", "beth", "ca", "cel", "cia", "da", "dora", "e", "ella", "fa", "fay", "ga", "gine",
+    "ha", "hilda", "ia", "ina", "ira", "la", "lina", "lissa", "ma", "mia", "na", "ne", "nia", "nissa",
+    "ola", "ona", "ora", "pa", "pia", "ra", "ria", "rina", "sa", "sina", "sta", "ta", "tia", "trina",
+    "va", "via", "vina", "wa", "wena", "wilda", "ya", "yna", "yra", "za", "zia", "zina"
+]
+
+male_prefix = [
+    "al", "ar", "ben", "cor", "dan", "el", "fin", "gar", "hir", "ian", "jam", "kil", "lin",
+    "mor", "nix", "or", "per", "quin", "ron", "sam", "ter", "ul", "ver", "wes", "xan", "yor",
+    "zach"
+]
+
+male_suffix = [
+    "ald", "bert", "cor", "dard", "el", "finn", "gald", "hart", "ian", "jack", "kyle", "land",
+    "mack", "nald", "orin", "paul", "quin", "rick", "sam", "tin", "ulph", "vald", "wick", "xander",
+    "yor", "zeph"
+]
+
+surname_prefix = [
+    'amber', 'ash', 'beech', 'birch', 'black', 'bramble', 'bright', 'cedar', 'cypress','dark', 'ebony',
+    'ember', 'fern', 'fire', 'glen', 'gold', 'hallow', 'hazel', 'holly', 'ivy', 'juniper', 'larch',
+    'maple', 'moon', 'noble', 'oak', 'pine', 'proud', 'raven', 'rowan', 'sequoia', 'shadow', 'silver',
+    'snow', 'spruce', 'star', 'stone', 'storm', 'swift', 'thistle', 'thorn', 'violet', 'willow', 'yew'
+]
+
+# Harry Potter-based surname suffixes
+surname_suffix = [
+    'ash', 'blaze', 'blossom', 'branch', 'breeze', 'brook', 'dawn', 'dusk', 'fall', 'feather', 'flame',
+    'forest', 'glade', 'glimmer', 'glimpse', 'gust', 'heart', 'leaf', 'light', 'mist', 'moon', 'petal',
+    'quill', 'ray', 'rose', 'shade', 'shimmer', 'song', 'spell', 'star', 'sun', 'thorn', 'twilight',
+    'vale', 'whisper', 'wind', 'wisp', 'wood', 'wraith', 'wren'
+]
+
+female = generate_female_name(female_prefix, female_suffix, min=0, max=1)
+male = generate_male_name(male_prefix, male_suffix, min=0, max=1)
+surname = generate_surname_less(surname_prefix, surname_suffix)
---
+++ packages/fictional-names/fictional_names-0.0.2-py3-none-any.whl/fictional_names/name_generator.py
@@ -0,0 +1,121 @@
+import dwarven, elven, giant, halfling, orc, human_steampunk, human_modern, human_medieval, human_greek, human_norsemen, human_arab, human_oriental, human_tolkien, human_jordan, human_erikson, human_rowling, human_sapkowski
+
+
+def generate_name(gender='male', style='modern'):
+    if gender.lower() == 'male':
+        match style:
+            case 'elven':
+                name = elven.male
+            case 'dwarven':
+                name = dwarven.male
+            case 'giant':
+                name = giant.male
+            case 'halfling':
+                name = halfling.male
+            case 'orc':
+                name = orc.male
+            case 'modern':
+                name = human_modern.male
+            case 'greek':
+                name = human_greek.male
+            case 'norsemen':
+                name = human_norsemen.male
+            case 'arab':
+                name = human_arab.male
+            case 'oriental':
+                name = human_oriental.male
+            case 'steampunk':
+                name = human_steampunk.male
+            case 'medieval':
+                name = human_medieval.male
+            case 'tolkien':
+                name = human_tolkien.male
+            case 'jordan':
+                name = human_jordan.male
+            case 'erikson':
+                name = human_erikson.male
+            case 'rowling':
+                name = human_rowling.male
+            case 'sapkowski':
+                name = human_sapkowski.male
+    elif gender.lower() == 'female':
+        match style:
+            case 'elven':
+                name = elven.female
+            case 'dwarven':
+                name = dwarven.female
+            case 'giant':
+                name = giant.female
+            case 'halfling':
+                name = halfling.female
+            case 'orc':
+                name = orc.female
+            case 'modern':
+                name = human_modern.female
+            case 'greek':
+                name = human_greek.female
+            case 'norsemen':
+                name = human_norsemen.female
+            case 'arab':
+                name = human_arab.female
+            case 'oriental':
+                name = human_oriental.female
+            case 'steampunk':
+                name = human_steampunk.female
+            case 'medieval':
+                name = human_medieval.female
+            case 'tolkien':
+                name = human_tolkien.female
+            case 'jordan':
+                name = human_jordan.female
+            case 'erikson':
+                name = human_erikson.female
+            case 'rowling':
+                name = human_rowling.female
+            case 'sapkowski':
+                name = human_sapkowski.female
+            
+    else:
+        raise ValueError("Invalid gender specified. Please choose 'male' or 'female'.")
+    
+    if style.lower() == 'elven':
+        surname = elven.surname
+    elif style.lower() == 'dwarven':
+        surname = dwarven.surname
+    elif style.lower() == 'giant':
+        surname = giant.surname
+    elif style.lower() == 'halfling':
+        surname = halfling.surname
+    elif style.lower() == 'orc':
+        surname = orc.surname
+    elif style.lower() == 'modern':
+        surname = human_modern.surname
+    elif style.lower() == 'greek':
+        surname = human_greek.surname
+    elif style.lower() == 'norsemen':
+        surname = human_norsemen.surname
+    elif style.lower() == 'arab':
+        surname = human_arab.surname
+    elif style.lower() == 'oriental':
+        surname = human_oriental.surname
+    elif style.lower() == 'steampunk':
+        surname = human_steampunk.surname
+    elif style.lower() == 'medieval':
+        surname = human_medieval.surname
+    elif style.lower() == 'tolkien':
+        surname = human_tolkien.surname
+    elif style.lower() == 'jordan':
+        surname = human_jordan.surname
+    elif style.lower() == 'erikson':
+        surname = human_erikson.surname
+    elif style.lower() == 'rowling':
+        surname = human_rowling.surname
+    elif style.lower() == 'sapkowski':
+        surname = human_sapkowski.surname
+    
+    return f"{name} {surname}"
+
+
+if __name__ == '__main__':
+    name = generate_name()
+    print(name)
