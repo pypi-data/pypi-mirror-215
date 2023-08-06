960a5f6efd19da2e707c76f9cd98a30f13334e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:50 +0000

    Add package spyx/0.0.1/spyx-0.0.1.tar.gz

---
+++ packages/spyx/spyx-0.0.1.tar.gz/spyx-0.0.1/tests/test_networks.py
@@ -0,0 +1,29 @@
+import unittest
+
+#from spyx import
+
+
+
+class neuron_model_tests(unittest.TestCase):
+    """
+    Needs to be built.
+    
+    Train a number of simple/shallow networks on the rate-MNIST
+    to verify the networks actually build.
+    """
+
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
