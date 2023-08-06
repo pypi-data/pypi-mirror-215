9129db04174713127c9821a5a19ba3c29d7ce97e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:38 +0000

    Add package statute-utils/0.3.1/statute_utils-0.3.1.tar.gz

---
+++ packages/statute-utils/statute_utils-0.3.1.tar.gz/statute_utils-0.3.1/statute_utils/components/__init__.py
@@ -0,0 +1,13 @@
+from .category import StatuteSerialCategory, StatuteTitle, StatuteTitleCategory
+from .details import StatuteDetails
+from .rule import BaseCollection, BasePattern, Rule
+from .utils import (
+    NON_ACT_INDICATORS,
+    add_blg,
+    add_num,
+    get_regexes,
+    limited_acts,
+    ltr,
+    not_prefixed_by_any,
+    stx,
+)
