2ef0c9ed03b03cae8ed21f677880e0b48de42d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:09 +0000

    Add package SimpleVectors/0.0.3/SimpleVectors-0.0.3.tar.gz

---
+++ packages/SimpleVectors/SimpleVectors-0.0.3.tar.gz/SimpleVectors-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3' 
+DESCRIPTION = 'Python Package for playing with Simple Vectors passing through a point'
+LONG_DESCRIPTION = 'Very easy to use module which provides to play with vectors using their magnitude and direction (angle) w.r.t x-axis, also all the vectors which will be added are assumed to be passing through the same line'
+
+# Setting up
+setup(
+        name="SimpleVectors", 
+        version=VERSION,
+        author="Jonathan Ghodke",
+        author_email="Jonathanghodke@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['numpy', 'matplotlib'],
+        keywords=['python', 'Vectors', 'vectors', 'simple vectors'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
