ddf9049658d243de8975a450f04256168f49ab03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:46 +0000

    Add package python-dataschema/0.1.5/python_dataschema-0.1.5-py3-none-any.whl

---
+++ packages/python-dataschema/python_dataschema-0.1.5-py3-none-any.whl/dataschema/__init__.py
@@ -0,0 +1,33 @@
+from .base import Spec
+from .specs import (
+    Type,
+    CType,
+    EnumSpec,
+    TypeSpec,
+    IterSpec,
+    SeqSpec,
+    DictSpec,
+    ConditionalDictSpec,
+    Post,
+    Update,
+    Test,
+    InvalidValueError,
+    BadSchemaError,
+)
+
+__all__ = (
+    'Spec',
+    'Type',
+    'CType',
+    'EnumSpec',
+    'TypeSpec',
+    'IterSpec',
+    'SeqSpec',
+    'DictSpec',
+    'ConditionalDictSpec',
+    'Post',
+    'Update',
+    'Test',
+    'InvalidValueError',
+    'BadSchemaError',
+)
