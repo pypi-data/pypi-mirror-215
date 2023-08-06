10acd9e2724ba9efd11b16ffa7b9bc701c2b9097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:14 +0000

    Add package coola/0.0.12/coola-0.0.12.tar.gz

---
+++ packages/coola/coola-0.0.12.tar.gz/coola-0.0.12/src/coola/__init__.py
@@ -0,0 +1,54 @@
+__all__ = [
+    "AllCloseTester",
+    "BaseAllCloseOperator",
+    "BaseAllCloseTester",
+    "BaseEqualityOperator",
+    "BaseEqualityTester",
+    "EqualityTester",
+    "LocalAllCloseTester",
+    "LocalEqualityTester",
+    "objects_are_allclose",
+    "objects_are_equal",
+]
+
+from coola.allclose import (
+    AllCloseTester,
+    BaseAllCloseOperator,
+    BaseAllCloseTester,
+    LocalAllCloseTester,
+    objects_are_allclose,
+)
+from coola.equality import (
+    BaseEqualityOperator,
+    BaseEqualityTester,
+    EqualityTester,
+    LocalEqualityTester,
+    objects_are_equal,
+)
+from coola.utils.imports import (
+    is_numpy_available,
+    is_pandas_available,
+    is_polars_available,
+    is_torch_available,
+    is_xarray_available,
+)
+
+# Register NumPy comparators
+if is_numpy_available():  # pragma: no cover
+    from coola import _numpy  # noqa: F401
+
+# Register pandas comparators
+if is_pandas_available():  # pragma: no cover
+    from coola import _pandas  # noqa: F401
+
+# Register polars comparators
+if is_polars_available():  # pragma: no cover
+    from coola import _polars  # noqa: F401
+
+# Register PyTorch comparators
+if is_torch_available():  # pragma: no cover
+    from coola import _torch  # noqa: F401
+
+# Register xarray comparators
+if is_xarray_available():  # pragma: no cover
+    from coola import _xarray  # noqa: F401
