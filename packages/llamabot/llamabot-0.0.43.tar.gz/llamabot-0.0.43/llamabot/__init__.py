359b79599e60c04b39dc385ebdfab15bd2b40a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:51 +0000

    Add package llamabot/0.0.43/llamabot-0.0.43.tar.gz

---
+++ packages/llamabot/llamabot-0.0.43.tar.gz/llamabot-0.0.43/llamabot/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level API for llamabot.
+
+This is the file from which you can do:
+
+    from llamabot import some_function
+
+Use it to control the top-level API of your Python data science project.
+"""
+from .bot import ChatBot, QueryBot, SimpleBot
+from .recorder import PromptRecorder
+
+__all__ = ["ChatBot", "SimpleBot", "QueryBot", "PromptRecorder"]
