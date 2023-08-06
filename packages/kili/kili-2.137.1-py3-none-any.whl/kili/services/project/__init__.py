c4cd29a45d1202af6ef328e86fd0fa8d6627acd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:16 +0000

    Add package kili/2.137.1/kili-2.137.1-py3-none-any.whl

---
+++ packages/kili/kili-2.137.1-py3-none-any.whl/kili/services/project/__init__.py
@@ -0,0 +1,31 @@
+"""Service module for projects."""
+
+from typing import List
+
+from kili.core.authentication import KiliAuth
+from kili.core.graphql import QueryOptions
+from kili.core.graphql.operations.project.queries import ProjectQuery, ProjectWhere
+from kili.exceptions import NotFound
+
+
+def get_project(auth: KiliAuth, project_id: str, fields: List[str]):
+    """Get a project from its id or raise a NotFound Error if not found."""
+    projects = list(
+        ProjectQuery(auth.client)(
+            ProjectWhere(project_id=project_id), fields, QueryOptions(disable_tqdm=True, first=1)
+        )
+    )
+    if len(projects) == 0:
+        raise NotFound(
+            f"project ID: {project_id}. Maybe your KILI_API_KEY does not belong to a member of the"
+            " project."
+        )
+    return projects[0]
+
+
+def get_project_field(auth: KiliAuth, project_id: str, field: str):
+    """Get one project field from a the project id.
+
+    Raise a NotFound Error if the project is not found.
+    """
+    return get_project(auth, project_id, [field])[field]
