33c905cf05a0a936f876d49bdcbcbce783808382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:15 +0000

    Add package ppioner/0.0.2/ppioner-0.0.2-py3-none-any.whl

---
+++ packages/ppioner/ppioner-0.0.2-py3-none-any.whl/pioner/back/ai_device.py
@@ -0,0 +1,78 @@
+import logging
+from ctypes import Array
+from typing import Tuple
+import uldaq as ul
+
+
+class AiParams:
+    def __init__(self):
+        self.sample_rate = -1  # Hz
+        self.range_id = -1
+        self.low_channel = -1
+        self.high_channel = -1
+        self.input_mode = ul.AiInputMode.SINGLE_ENDED  # 2
+        self.scan_flags = ul.AInScanFlag.DEFAULT  # 0
+        self.options = ul.ScanOption.CONTINUOUS  # 8
+
+    def __str__(self):
+        return str(vars(self))
+
+
+class AiDeviceHandler:
+    """Wraps the analog-input uldaq.AiDevice."""
+
+    def __init__(self, ai_device_from_daq: ul.AiDevice,
+                 params: AiParams):
+        """Initializes AI device and all parameters.
+
+        Args:
+            ai_device_from_daq: uldaq.AiDevice obtained from uldaq.DaqDevice.
+            params: An AiParams instance, containing all needed analog-input parameters parsed from JSON.
+        Raises:
+            RuntimeError if the DAQ device doesn't support analog input or hardware paced analog input.
+        """
+        self._ai_device = ai_device_from_daq
+        self._params = params
+        self._check_device()
+        self._init_buffer()
+
+    def _check_device(self):
+        if self._ai_device is None:
+            error_str = "Error. DAQ device doesn't support analog input."
+            logging.error(error_str)
+            raise RuntimeError(error_str)
+
+        info = self._ai_device.get_info()
+        if not info.has_pacer():
+            error_str = "Error. DAQ device doesn't support hardware paced analog input."
+            logging.error(error_str)
+            raise RuntimeError(error_str)
+
+        if info.get_num_chans_by_mode(ul.AiInputMode.SINGLE_ENDED) <= 0:
+            self._params.input_mode = ul.AiInputMode.DIFFERENTIAL
+
+    def _init_buffer(self):
+        channel_count = self._params.high_channel - self._params.low_channel + 1
+        self._buffer = ul.create_float_buffer(channel_count, self._params.sample_rate)
+
+    def get(self) -> ul.AiDevice:
+        """Provides explicit access to the uldaq.AiDevice."""
+        return self._ai_device                                   
+
+    def get_buffer(self) -> Array[float]:
+        """Returns an array of double precision floating point sample values."""
+        return self._buffer
+
+    def stop(self):
+        self._ai_device.scan_stop()
+  
+    def status(self) -> Tuple[ul.ScanStatus, ul.TransferStatus]:
+        return self._ai_device.get_scan_status()
+
+    # returns actual input scan rate
+    def scan(self) -> float:
+        analog_range = ul.Range(self._params.range_id)
+        return self._ai_device.a_in_scan(self._params.low_channel, self._params.high_channel, 
+                                         self._params.input_mode, analog_range, self._params.sample_rate,
+                                         self._params.sample_rate, self._params.options, 
+                                         self._params.scan_flags, self._buffer)
---
+++ packages/ppioner/ppioner-0.0.2-py3-none-any.whl/pioner/back/iso_mode.py
@@ -0,0 +1,69 @@
+import logging
+
+from shared.constants import *
+from shared.settings import BackSettings
+from shared.calibration import Calibration
+from shared.utils import temperature_to_voltage
+from back.daq_device import DaqDeviceHandler
+from back.experiment_manager import ExperimentManager
+
+
+
+class IsoMode:
+    # receives chan_temp_volt as dict :
+    # {"ch0": {"temp":float} }
+    # voltage can be used instead of temperature ("volt" instead of "temp")
+    # if "volt" - no calibration applied
+    # if "temp" - calibration applied
+
+    def __init__(self, daq_device_handler: DaqDeviceHandler,
+                 settings: BackSettings,
+                 chan_temp_volt: dict,
+                 calibration: Calibration):
+        self._daq_device_handler = daq_device_handler
+        self._settings = settings
+        self._chan_temp_volt = chan_temp_volt
+        self._calibration = calibration
+
+        self._channel = None
+        self._voltage = None
+
+    def arm(self) -> [int, float]:
+        self._channel = int(list(self._chan_temp_volt.keys())[0].replace('ch', ''))
+        key = list(list(self._chan_temp_volt.values())[0].keys())[0]
+        self._voltage = float(list(list(self._chan_temp_volt.values())[0].values())[0])
+        if key=='temp':
+            self._voltage = float(temperature_to_voltage([self._voltage], self._calibration))
+        return self._channel, self._voltage
+
+    def is_armed(self) -> bool:
+        return bool(self._channel is not None and self._voltage is not None)
+
+    def run(self, do_ai:bool):
+        with ExperimentManager(self._daq_device_handler,
+                               self._settings) as self.em:
+            self.em.ao_set(self._channel, self._voltage)
+            if do_ai:
+                self.em.ai_continuous([0,1,2,3,4,5], do_save_data=False)
+
+    def ai_stop(self):
+        self.em.ai_continuous_stop()
+
+
+if __name__ == '__main__':
+
+    settings = BackSettings(SETTINGS_FILE_REL_PATH)
+    chan_temp_volt = {'ch2':{'volt':0},
+                        }
+    calibration = Calibration()
+
+    daq_params = settings.daq_params
+    daq_device_handler = DaqDeviceHandler(daq_params)
+    daq_device_handler.try_connect()
+
+    sm = IsoMode(daq_device_handler, settings, chan_temp_volt, calibration)
+    sm.is_armed()
+    sm.arm()
+    sm.run(do_ai=True)
+
+    daq_device_handler.disconnect()
