fe856327f5e0ff0e83b4281f5ad9b49cb752dbfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:19 +0000

    Add package proteinshake/0.3.12/proteinshake-0.3.12-py3-none-any.whl

---
+++ packages/proteinshake/proteinshake-0.3.12-py3-none-any.whl/proteinshake/utils/__init__.py
@@ -0,0 +1,20 @@
+from .embeddings import *
+from .io import *
+from .similarity import *
+from .uniprot import *
+
+__all__ = ['onehot',
+           'tokenize',
+           'positional_encoding',
+           'compose_embeddings',
+           'save',
+           'load',
+           'download_url',
+           'extract_tar',
+           'zip_file',
+           'unzip_file',
+           'write_avro',
+           'uniprot_query',
+           ]
+
+classes = __all__
