76ee8974734ab8a910c6a5024f6d84295b30492f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:56 +0000

    Add package qiskit-trebugger/1.1.0/qiskit_trebugger-1.1.0.tar.gz

---
+++ packages/qiskit-trebugger/qiskit_trebugger-1.1.0.tar.gz/qiskit_trebugger-1.1.0/src/qiskit_trebugger/views/cli/config.py
@@ -0,0 +1,10 @@
+import curses
+
+
+# To Do :
+class COLORS:
+    TITLE = {"back": curses.COLOR_WHITE, "front": curses.COLOR_MAGENTA}
+
+    STATUS = {"back": curses.COLOR_CYAN, "front": curses.COLOR_BLACK}
+
+    BASE_PASSES_TITLE = {"back": curses.COLOR_BLACK, "front": curses.COLOR_CYAN}
