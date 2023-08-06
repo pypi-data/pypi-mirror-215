186f22b7c87cb72ff4a7bcce0922e9ce099f6847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:08 +0000

    Add package graphreduce/0.2/graphreduce-0.2.tar.gz

---
+++ packages/graphreduce/graphreduce-0.2.tar.gz/graphreduce-0.2/setup.py
@@ -0,0 +1,59 @@
+import importlib.util
+import pathlib
+import setuptools
+import typing
+
+
+KEYWORDS = [
+    "feature engineering",
+    "mlops",
+    "entity linking",
+    "graph algorithms",
+    ]
+
+
+
+if __name__ == "__main__":
+
+    setuptools.setup(
+        name="graphreduce",
+        version = 0.2,
+        url="https://github.com/wesmadrigal/graphreduce",
+        packages = setuptools.find_packages(exclude=[ "docs", "examples" ]),
+        install_requires = [
+            "structlog >= 22.3.0",
+            "dask >= 2023.1.1",
+            "networkx >= 2.8.2",
+            "pyvis >= 0.2.1",
+            "pandas >= 1.3.2",
+            "pyarrow >= 7.0.0",
+            "pyspark >= 3.2.0"
+            ],
+        author="Wes Madrigal",
+        author_email="wes@madconsulting.ai",
+        license="MIT",
+
+        description="Leveraging graph data structures for complex feature engineering pipelines.",
+        long_description = pathlib.Path("README.md").read_text(),
+        long_description_content_type = "text/markdown",
+
+        keywords = ", ".join(KEYWORDS),
+        classifiers = [
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Education",
+            "Intended Audience :: Information Technology",
+            "Intended Audience :: Science/Research",
+            "Topic :: Scientific/Engineering :: Information Analysis",
+            ],
+
+        project_urls = {
+            "Source" : "http://github.com/wesmadrigal/graphreduce",
+            "Issue Tracker" : "https://github.com/wesmadrigal/graphreduce/issues"
+            },
+
+        zip_safe=False,
+        )
