b7b631f856e26b9b280b444843a2df94be7b7078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:52 +0000

    Add package prefect/2.10.17/prefect-2.10.17.tar.gz

---
+++ packages/prefect/prefect-2.10.17.tar.gz/prefect-2.10.17/src/prefect/server/ui/assets/Dashboard-8f65232a.js
@@ -0,0 +1,1 @@
+import{g as c,h as r,w as n,o as _,p as d}from"./index-9c193a50.js";const l=(t,e)=>{const o=t.__vccOpts||t;for(const[a,s]of e)o[a]=s;return o},p={};function f(t,e){const o=c("p-layout-default");return _(),r(o,{class:"workspace-dashboard"},{default:n(()=>[d(" Dashboard ")]),_:1})}const h=l(p,[["render",f]]);export{h as default};
