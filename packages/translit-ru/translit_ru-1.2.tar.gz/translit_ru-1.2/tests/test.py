4fac8b8637dd9b47957836fdda101240ce2ce6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:51 +0000

    Add package translit-ru/1.2/translit_ru-1.2.tar.gz

---
+++ packages/translit-ru/translit_ru-1.2.tar.gz/translit_ru-1.2/tests/test.py
@@ -0,0 +1,57 @@
+import unittest
+from ayum import translit
+
+cases_cyr_lat = {
+    'авдотья': 'avdothya',
+    'отъявленный': 'otyavlennqiy',
+    'бактерицидный': 'baktericidnqiy',
+    'вьюга': 'vhyuga',
+    'юг': 'yug',
+    'эра': 'era',
+    'цирк': 'cirk',
+    'жюри': 'juri',
+    'щебетать': 'whebetath',
+    'парашют': 'parawut',
+    'аллофон': 'allofon',
+    'съезд': 'syezd',
+    'метр': 'metr',
+    'мэтр': 'mqetr',
+    'лук': 'luk',
+    'люк': 'lhuk',
+    'клык': 'klqik',
+    'клик': 'klik',
+    'клён': 'klhon',
+    'клон': 'klon',
+    'раса': 'rasa',
+    'ряса': 'rhasa',
+    'пот': 'pot',
+    'под': 'pod',
+    'топ': 'top',
+    'топь': 'toph',
+    'йод': 'yod',
+    'яд': 'yad',
+    'подыграть': 'podqigrath',
+    'вожжи': 'vojji',
+    'жужжать': 'jujjath',
+    'объём': 'obyom',
+    'выемка': 'vqiemka',
+    'каньон': 'kanyon',
+    'гальюн': 'galyun',
+    'веет': 'veet',
+    'веешь': 'veew',
+    'епархия': 'yeparxiya',
+    'ералаш': 'yeralaw',
+    'красивые': 'krasivqie',
+    'коммерсантъ': 'kommersantq',
+}
+
+
+class TestToLatin(unittest.TestCase):
+
+    def test_all(self):
+        for cyr, lat in cases_cyr_lat.items():
+            self.assertEqual(translit.to_latin(cyr), lat)
+
+
+if __name__ == '__main__':
+    unittest.main()
