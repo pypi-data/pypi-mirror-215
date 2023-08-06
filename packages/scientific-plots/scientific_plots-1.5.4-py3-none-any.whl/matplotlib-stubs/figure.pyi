b3feb28ffc10ecf80382f67d8e29edcc5e157e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:57 +0000

    Add package scientific-plots/1.5.4/scientific_plots-1.5.4-py3-none-any.whl

---
+++ packages/scientific-plots/scientific_plots-1.5.4-py3-none-any.whl/matplotlib-stubs/figure.pyi
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""This is the stub for the figure-type in matplot-lib."""
+from typing import Optional, Tuple, Iterable
+
+from .pyplot import Axes
+
+
+class Canvas:
+    """Canvas, which contains the figure."""
+
+    def draw(self) -> None: ...
+
+    def fush_events(self) -> None: ...
+
+
+class Figure:
+
+    axes: Iterable[Axes]
+
+    canvas: Canvas
+
+    def __init__(
+        self, figsize: Optional[Tuple[float, float]] = None) -> None: ...
+
+    def gca(self) -> Axes: ...
+
+    def set_size_inches(self, x: float, y: float) -> None: ...
+
+    def subplots_adjust(self, bottom: Optional[float] = None,
+                        right: Optional[float] = None,
+                        left: Optional[float] = None,
+                        top: Optional[float] = None,
+                        ) -> None: ...
+
+    def add_subplot(self, position: int = 111,
+                    projection: str = "2d") -> Axes: ...
