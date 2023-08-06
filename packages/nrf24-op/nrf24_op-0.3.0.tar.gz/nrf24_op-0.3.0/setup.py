d84a3f6afa20a1fabc491c5225037ea08c6b8cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:39 +0000

    Add package nrf24-op/0.3.0/nrf24_op-0.3.0.tar.gz

---
+++ packages/nrf24-op/nrf24_op-0.3.0.tar.gz/nrf24_op-0.3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nrf24_op',
+    version='0.3.0',
+    author='Nofal Elahi',
+    license='MIT',
+    description='Python package for interacting with NRF24 wireless module',
+    packages=find_packages(include=['nrf24_op']),
+    install_requires=['spidev', 'OPi.GPIO']
+)
