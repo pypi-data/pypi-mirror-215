057ee30e5e990f84e599b299abc802f9ed48cf1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:25 +0000

    Add package persist-ext/1.1.0/persist_ext-1.1.0.tar.gz

---
+++ packages/persist-ext/persist_ext-1.1.0.tar.gz/persist_ext-1.1.0/CHANGELOG.md
@@ -0,0 +1,36 @@
+# Changelog
+
+<!-- <START NEW CHANGELOG ENTRY> -->
+
+## 1.1.0
+
+No merged PRs
+
+<!-- <END NEW CHANGELOG ENTRY> -->
+
+## 1.0.1
+
+No merged PRs
+
+## 1.0.0
+
+([Full Changelog](https://github.com/visdesignlab/persist/compare/5ed90bee518fff9daf81df3fb927c143e0807e46...264ad73011e88db77c4b44a09fb1bf0de7363326))
+
+### Enhancements made
+
+- Rename extension to `persist_ext` [#58](https://github.com/visdesignlab/persist/pull/58) ([@kirangadhave](https://github.com/kirangadhave))
+- Major features [#44](https://github.com/visdesignlab/persist/pull/44) ([@kirangadhave](https://github.com/kirangadhave))
+
+### Other merged PRs
+
+- feat(log-console): added log console ext + integrated with global logger [#20](https://github.com/visdesignlab/persist/pull/20) ([@kirangadhave](https://github.com/kirangadhave))
+- CI test probe [#19](https://github.com/visdesignlab/persist/pull/19) ([@kirangadhave](https://github.com/kirangadhave))
+- Testing github actions [#18](https://github.com/visdesignlab/persist/pull/18) ([@kirangadhave](https://github.com/kirangadhave))
+- testing actions [#15](https://github.com/visdesignlab/persist/pull/15) ([@kirangadhave](https://github.com/kirangadhave))
+- Testing kernel comms [#1](https://github.com/visdesignlab/persist/pull/1) ([@kirangadhave](https://github.com/kirangadhave))
+
+### Contributors to this release
+
+([GitHub contributors page for this release](https://github.com/visdesignlab/persist/graphs/contributors?from=2023-01-16&to=2023-06-25&type=c))
+
+[@github-actions](https://github.com/search?q=repo%3Avisdesignlab%2Fpersist+involves%3Agithub-actions+updated%3A2023-01-16..2023-06-25&type=Issues) | [@kirangadhave](https://github.com/search?q=repo%3Avisdesignlab%2Fpersist+involves%3Akirangadhave+updated%3A2023-01-16..2023-06-25&type=Issues)
---
+++ packages/persist-ext/persist_ext-1.1.0.tar.gz/persist_ext-1.1.0/src/cells/output/commands.ts
@@ -0,0 +1,89 @@
+import { CommandRegistry } from '@lumino/commands';
+import { UUID } from '@lumino/coreutils';
+import { TrrackableCell } from '../trrackableCell';
+import { extractDfAndCopyName } from './extract_helpers';
+
+export namespace OutputCommandIds {
+  export const reset = 'output:reset';
+  export const filter = 'output:filter';
+  export const aggregate = 'output:aggregate';
+  export const copyDynamic = 'output:copy-dynamic';
+}
+
+export class OutputCommandRegistry {
+  private _commands: CommandRegistry;
+
+  constructor(private _cell: TrrackableCell) {
+    this._commands = new CommandRegistry();
+
+    if (!this._cell) {
+      return;
+    }
+    this._setup();
+  }
+
+  get commands(): CommandRegistry {
+    return this._commands;
+  }
+
+  private _setup() {
+    this._commands.addCommand(OutputCommandIds.reset, {
+      execute: () => {
+        this._cell.trrackManager.reset();
+      },
+      isEnabled: () => !this._cell.trrackManager.hasOnlyRoot,
+      label: 'Reset'
+    });
+
+    this._commands.addCommand(OutputCommandIds.filter, {
+      execute: () => {
+        filter(this._cell);
+      },
+      isEnabled: () => {
+        return this._cell.trrackManager.hasSelections;
+      },
+      label: 'Filter'
+    });
+
+    this._commands.addCommand(OutputCommandIds.aggregate, {
+      execute: () => {
+        aggregate(this._cell);
+      },
+      isEnabled: () => {
+        return this._cell.trrackManager.hasSelections;
+      },
+      label: 'Aggregate'
+    });
+
+    this._commands.addCommand(OutputCommandIds.copyDynamic, {
+      execute: () => {
+        extractDfAndCopyName(this._cell);
+      },
+      label: 'Create Dynamic Dataframe',
+      caption:
+        'Generate variable which has the dataframe for current provenance node'
+    });
+
+    this._cell.trrackManager.currentChange.connect((_, __) => {
+      this._commands.notifyCommandChanged();
+    });
+  }
+}
+
+async function aggregate(cell: TrrackableCell) {
+  const id = UUID.uuid4();
+
+  await cell.trrackManager.actions.addAggregate({
+    id,
+    agg_name: `Agg_${id.split('-')[0]}`,
+    type: 'aggregate'
+  });
+}
+
+async function filter(cell: TrrackableCell, direction: 'in' | 'out' = 'out') {
+  await cell.trrackManager.actions.addFilter({
+    id: UUID.uuid4(),
+    type: 'filter',
+    direction
+  });
+}
