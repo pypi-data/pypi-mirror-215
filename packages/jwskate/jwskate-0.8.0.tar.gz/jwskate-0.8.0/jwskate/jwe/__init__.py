980f93bdf651e487f6a012aa329f8458d09dbe8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:51 +0000

    Add package jwskate/0.8.0/jwskate-0.8.0.tar.gz

---
+++ packages/jwskate/jwskate-0.8.0.tar.gz/jwskate-0.8.0/jwskate/jwe/__init__.py
@@ -0,0 +1,10 @@
+"""This module implements Json Web Encryption as described in [RFC7516].
+
+[RFC7516]
+: https: //www.rfc-editor.org/rfc/rfc7516
+
+"""
+
+from .compact import InvalidJwe, JweCompact
+
+__all__ = ["InvalidJwe", "JweCompact"]
---
+++ packages/jwskate/jwskate-0.8.0.tar.gz/jwskate-0.8.0/tests/test_jwa/test_signature.py
@@ -0,0 +1,11 @@
+"""Tests for jwskate.jwa.signature submodule."""
+
+import pytest
+
+from jwskate import ES256
+
+
+def test_ec_signature_exceptions() -> None:
+    es256 = ES256.with_random_key()
+    with pytest.raises(ValueError):
+        es256.public_alg().verify(b"foo", b"bar")
