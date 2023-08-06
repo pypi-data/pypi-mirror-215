d321d13e5f30ebd441151086e545904f8fe04a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:41 +0000

    Add package dev-assistant-client/0.1.0/dev-assistant-client-0.1.0.tar.gz

---
+++ packages/dev-assistant-client/dev-assistant-client-0.1.0.tar.gz/dev-assistant-client-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dev-assistant-client',
+    version='0.1.0',
+    url='https://github.com/lucianotonet/dev-assistant',
+    author='Luciano Tonet',
+    author_email='tonet.dev@gmail.com',
+    description='A local extension for ChatGPT plugin DevAssistant, which helps you with your development tasks straight in your machine.',
+    packages=find_packages(),
+    install_requires=[],
+)
