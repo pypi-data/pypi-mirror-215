73887f80b654c6e4536eb98d22c0c53ecb46d8e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:08 +0000

    Add package testless-autocomplete/0.0.106/testless_autocomplete-0.0.106-py3-none-any.whl

---
+++ packages/testless-autocomplete/testless_autocomplete-0.0.106-py3-none-any.whl/testless_autocomplete/src/AutoCompleteAPI.py
@@ -0,0 +1,18 @@
+from .preprocessing import preprocess_text
+import pandas as pd
+
+
+def autocomplete(text):
+    # if text is empty or contains only spaces, return an empty list
+    try:
+
+        if text == '':
+            return {'words': []}
+        words = preprocess_text(text)
+        # with autocomplete.app_context():
+        data = pd.read_csv('Auto_Complete/testless_autocomplete/src/ngram.csv')
+
+        data.set_index('ngram', inplace=True)        
+        return {'words': data.loc[words[-1]]['next_word']} 
+    except:
+        return {'words': []} 
