689838cbcb99a04ad743fd8c4198718e70e7320d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:00 +0000

    Add package qinglan-bot/0.1.3/qinglan-bot-0.1.3.tar.gz

---
+++ packages/qinglan-bot/qinglan-bot-0.1.3.tar.gz/qinglan-bot-0.1.3/qinglan_bot/__init__.py
@@ -0,0 +1,39 @@
+from nonebot import get_driver, require
+from nonebot.plugin import PluginMetadata
+from nonebot.plugin.manager import PluginLoader
+
+if isinstance(globals()["__loader__"], PluginLoader):
+    require("nonebot_plugin_guild_patch")
+    from .config import Config
+
+    __plugin_meta__ = PluginMetadata(
+        name="青岚Bot",
+        description="基于NoneBot的与Minecraft Server互通消息的机器人",
+        homepage="https://github.com/17TheWord/qinglan_bot",
+        usage="配置完成后在群聊发送消息即可同步至 Minecraft 服务器",
+        config=Config,
+        type="application",
+        supported_adapters={
+            "nonebot.adapters.onebot.v11"
+        }
+    )
+    driver = get_driver()
+
+    plugin_config: Config = Config.parse_obj(get_driver().config)
+
+    from .ws_server import start_ws_server, stop_ws_server
+    from . import plugins
+
+
+    # Bot 连接时
+    @driver.on_bot_connect
+    async def on_start():
+        # 启动 WebSocket 服务器
+        await start_ws_server()
+
+
+    # Bot 断开时
+    @driver.on_bot_disconnect
+    async def on_close():
+        # 关闭 WebSocket 服务器
+        await stop_ws_server()
---
+++ packages/qinglan-bot/qinglan-bot-0.1.3.tar.gz/qinglan-bot-0.1.3/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="qinglan-bot",  # 项目名称，保证它的唯一性，不要跟已存在的包名冲突即可
+    version="0.1.3",  # 程序版本
+    author="17TheWord",  # 项目作者
+    author_email="17theword@gmail.com",  # 作者邮件
+    description="基于NoneBot的QQ群聊与Minecraft Server消息互通机器人",  # 项目的一句话描述
+    long_description=long_description,  # 加长版描述？
+    long_description_content_type="text/markdown",  # 描述使用Markdown
+    url="https://github.com/17TheWord/qinglan_bot",  # 项目地址
+    packages=["qinglan_bot"],
+    classifiers=[
+        "Programming Language :: Python :: 3.9",  # 使用Python3.9
+        "License :: OSI Approved :: GNU Affero General Public License v3",  # 开源协议
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'mcqq-tool==0.0.4',
+        'nonebot2>=2.0.0',
+        'nonebot-adapter-onebot>=2.1.5',
+        'nonebot-plugin-guild-patch>=0.2.0',
+        'websockets>=10.3',
+        'tortoise-orm>=0.19.3',
+        'aio-mc-rcon>=3.2.0',
+        'click>=8.0.4',
+    ],
+    entry_points={'console_scripts': [
+        'ql=qinglan_bot.cli_start:main',
+    ]},
+)
