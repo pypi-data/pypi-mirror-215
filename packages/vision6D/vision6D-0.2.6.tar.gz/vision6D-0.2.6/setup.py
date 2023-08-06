a1a139bdc3c92db937b36a8c125ef8ef6c2c6981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:27 +0000

    Add package vision6D/0.2.6/vision6D-0.2.6.tar.gz

---
+++ packages/vision6D/vision6D-0.2.6.tar.gz/vision6D-0.2.6/setup.py
@@ -0,0 +1,13 @@
+# For development mode
+import setuptools
+
+setuptools.setup(
+    name = "vision6D",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    entry_points = {
+        'console_scripts':[
+            'vision6D = entry.main:main',
+        ]
+    }
+)
---
+++ packages/vision6D/vision6D-0.2.6.tar.gz/vision6D-0.2.6/vision6D/__init__.py
@@ -0,0 +1,35 @@
+import logging.config
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "formatter": "standard",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",  # Default is stderr
+        }
+    },
+    # "loggers": {
+    #     "": {  # root logger
+    #         "handlers": ["console"],
+    #         "level": "DEBUG",
+    #         "propagate": True,
+    #     }
+    # },
+}
+
+# Setup the logging configuration
+logging.config.dictConfig(LOGGING_CONFIG)
+
+from .mainwindow import MyMainWindow
+from .GUI import Interface
+from . import utils
+from . import widgets_gui
