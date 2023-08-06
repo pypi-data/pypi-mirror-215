8565f335c9a949ce59e256dcce8ae469cd97eaa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:29 +0000

    Add package agixt/1.2.18/agixt-1.2.18-py3-none-any.whl

---
+++ packages/agixt/agixt-1.2.18-py3-none-any.whl/agixt/provider/openai.py
@@ -0,0 +1,47 @@
+import openai
+
+
+class OpenaiProvider:
+    def __init__(
+        self,
+        OPENAI_API_KEY: str = "",
+        AI_MODEL: str = "gpt-3.5-turbo",
+        AI_TEMPERATURE: float = 0.7,
+        AI_TOP_P: float = 0.7,
+        MAX_TOKENS: int = 4096,
+        **kwargs
+    ):
+        self.requirements = ["openai"]
+        self.AI_MODEL = AI_MODEL
+        self.AI_TEMPERATURE = AI_TEMPERATURE
+        self.AI_TOP_P = AI_TOP_P
+        self.MAX_TOKENS = MAX_TOKENS
+        openai.api_key = OPENAI_API_KEY
+
+    async def instruct(self, prompt, tokens: int = 0):
+        max_new_tokens = int(self.MAX_TOKENS) - tokens
+        if not self.AI_MODEL.startswith("gpt-"):
+            # Use completion API
+            response = openai.Completion.create(
+                engine=self.AI_MODEL,
+                prompt=prompt,
+                temperature=float(self.AI_TEMPERATURE),
+                max_tokens=max_new_tokens,
+                top_p=float(self.AI_TOP_P),
+                frequency_penalty=0,
+                presence_penalty=0,
+            )
+            return response.choices[0].text.strip()
+        else:
+            # Use chat completion API
+            messages = [{"role": "system", "content": prompt}]
+            response = openai.ChatCompletion.create(
+                model=self.AI_MODEL,
+                messages=messages,
+                temperature=float(self.AI_TEMPERATURE),
+                max_tokens=max_new_tokens,
+                top_p=float(self.AI_TOP_P),
+                n=1,
+                stop=None,
+            )
+            return response.choices[0].message.content.strip()
