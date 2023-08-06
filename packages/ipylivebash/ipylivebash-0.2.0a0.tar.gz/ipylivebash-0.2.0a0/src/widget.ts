d6b1757161fa56090aa38ecff7b4caf74dd88b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:55 +0000

    Add package ipylivebash/0.2.0a0/ipylivebash-0.2.0a0.tar.gz

---
+++ packages/ipylivebash/ipylivebash-0.2.0a0.tar.gz/ipylivebash-0.2.0a0/src/widget.ts
@@ -0,0 +1,176 @@
+import {
+  DOMWidgetModel,
+  DOMWidgetView,
+  ISerializers,
+} from '@jupyter-widgets/base';
+
+import { MODULE_NAME, MODULE_VERSION } from './version';
+import '../css/widget.css';
+import './webcomp';
+
+export class LogViewModel extends DOMWidgetModel {
+  defaults() {
+    return {
+      ...super.defaults(),
+      _model_name: LogViewModel.model_name,
+      _model_module: LogViewModel.model_module,
+      _model_module_version: LogViewModel.model_module_version,
+      _view_name: LogViewModel.view_name,
+      _view_module: LogViewModel.view_module,
+      _view_module_version: LogViewModel.view_module_version,
+      messages: [],
+      divider_text: '',
+      height: 0,
+      status: [],
+      running: false,
+      notification_permission_request: false,
+      notification_permission: '',
+      notification_message: '',
+      response: '',
+      action: '',
+      confirmation_required: false,
+    };
+  }
+
+  static serializers: ISerializers = {
+    ...DOMWidgetModel.serializers,
+  };
+
+  static model_name = 'LogViewModel';
+  static model_module = MODULE_NAME;
+  static model_module_version = MODULE_VERSION;
+  static view_name = 'LogView';
+  static view_module = MODULE_NAME;
+  static view_module_version = MODULE_VERSION;
+}
+
+let responseId = 0;
+
+export class LogView extends DOMWidgetView {
+  views: any = {};
+  status = '';
+  panel: HTMLElement;
+
+  render() {
+    const panel = document.createElement('live-bash-panel');
+    this.el.appendChild(panel);
+    this.panel = panel;
+    this.panel.setAttribute('tabIndex', '1');
+    this.panel.addEventListener('response', (e: any) => {
+      this.onPanelResponse(JSON.stringify(e.detail));
+    });
+
+    this.heightChanged();
+    this.runningChanged();
+    this.onConfirmationRequiredChanged();
+
+    this.model.on('change:messages', this.messagesChanged, this);
+    this.model.on('change:status_header', this.statusHeaderChanged, this);
+    this.model.on('change:status', this.statusChanged, this);
+    this.model.on('change:height', this.heightChanged, this);
+    this.model.on('change:running', this.runningChanged, this);
+    this.model.on(
+      'change:notification_permission_request',
+      this.onNotificationPermissionRequestChanged,
+      this
+    );
+    this.model.on(
+      'change:notification_message',
+      this.onNotificationMessageChanged,
+      this
+    );
+    this.model.on('change:action', this.onActionChanged, this);
+    this.model.on(
+      'change:confirmation_required',
+      this.onConfirmationRequiredChanged,
+      this
+    );
+  }
+
+  messagesChanged() {
+    const value = this.model.get('messages');
+    this.panel.setAttribute(
+      'messages',
+      value
+        .map((v: string) => {
+          const str = Number.isInteger(v) ? v.toString() : v;
+          return str.replace(/^\s+|\s+$/gm, '');
+        })
+        .join('\n')
+    );
+  }
+
+  statusHeaderChanged() {
+    const value = this.model.get('status_header');
+    this.panel.setAttribute('status-header', value);
+  }
+
+  statusChanged() {
+    const value = this.model.get('status');
+    this.panel.setAttribute('status', value.join('\n'));
+  }
+
+  heightChanged() {
+    const value = this.model.get('height');
+    this.panel.setAttribute('height-in-lines', value);
+  }
+
+  onNotificationPermissionRequestChanged() {
+    const value = this.model.get('notification_permission_request');
+    if (!value) {
+      return;
+    }
+    if (!('Notification' in window)) {
+      alert('This browser does not support desktop notification');
+      return;
+    }
+
+    if (Notification.permission === 'denied') {
+      this.model.set('notification_permission', Notification.permission);
+      this.model.save_changes();
+      return;
+    }
+
+    Notification.requestPermission().then((permission) => {
+      this.model.set('notification_permission', Notification.permission);
+      this.model.save_changes();
+    });
+  }
+
+  onNotificationMessageChanged() {
+    const value = this.model.get('notification_message');
+    const notification = new Notification('livebash', {
+      body: value,
+      requireInteraction: true
+    });
+    notification.onclick = (e) => {
+      notification.close();
+    };
+  }
+
+  runningChanged() {
+    const value = this.model.get('running');
+    this.panel.setAttribute('is-running', value);
+  }
+
+  onPanelResponse(content: string) {
+    const value = {
+      id: responseId++,
+      content,
+    };
+
+    this.model.set('response', JSON.stringify(value));
+    this.model.save_changes();
+    this.touch();
+  }
+
+  onConfirmationRequiredChanged() {
+    const value = this.model.get('confirmation_required');
+    this.panel.setAttribute('confirmation-required', value);
+  }
+
+  onActionChanged() {
+    const value = this.model.get('action');
+    this.panel.setAttribute('action', value);
+  }
+}
