fd8f46568f3c870d2a327dea3eb4763128c3aad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:45 +0000

    Add package doxapy/0.9.2/doxapy-0.9.2.tar.gz

---
+++ packages/doxapy/doxapy-0.9.2.tar.gz/doxapy-0.9.2/setup.py
@@ -0,0 +1,35 @@
+import sys
+from setuptools import setup
+from pathlib import Path
+from pybind11.setup_helpers import Pybind11Extension
+
+cflags = []
+if sys.platform == "win32":
+    cflags.append("/permissive-")
+    
+ext_modules = [
+    Pybind11Extension("doxapy",
+        ["DoxaPy.cpp"],
+        include_dirs=["Doxa/"],
+        cxx_std=17,
+        extra_compile_args=cflags
+    ),
+]
+
+# Read contents from the README
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+if __name__ == "__main__":
+    setup(
+        author="Brandon M. Petty",
+        author_email="brandonpetty1981@gmail.com",
+        name="doxapy",
+        description="An image binarization library focussing on local adaptive thresholding",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        version="0.9.2",
+        url="https://github.com/brandonmpetty/doxa",
+        license="CC0-1.0",
+        ext_modules=ext_modules
+    )
