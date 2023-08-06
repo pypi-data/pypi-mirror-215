6f286ab27aa5b4651a19a9e612607e20593d9add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:06 +0000

    Add package chilitools/0.2.0/chilitools-0.2.0-py3-none-any.whl

---
+++ packages/chilitools/chilitools-0.2.0-py3-none-any.whl/chilitools/grafx/document.py
@@ -0,0 +1,31 @@
+import json
+
+class GraFxDocument:
+    def __init__(self, doc: str, id: str = None, name: str = None):
+        try:
+            # We were passed a parsed JSON document
+            if isinstance(doc, dict):
+                self.doc = doc
+            # We were passed the raw JSON string of the document
+            if isinstance(doc, str):
+                self.doc = json.loads(doc)
+        except Exception as e:
+            self.doc = {}
+            print(f"FAILURE PARSING GRAFX DOCUMENT: {e}")
+
+        if id: self.id = id
+        if name: self.name = name
+
+    @property
+    def json(self): return str(self)
+
+    @property
+    def images(self):
+      image_frames = []
+      for page in self.doc["pages"]:
+          for frame in page["frames"]:
+              if frame["frameType"] == "image": image_frames.append(frame)
+      return image_frames
+
+    def __str__(self): return json.dumps(self.doc)
+    def __repr__(self): return self.doc
