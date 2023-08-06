0b9a492f65b55950a119832315c9dfec48fd0adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:55 +0000

    Add package streamlit-chat-plus/0.0.0.2/streamlit_chat_plus-0.0.0.2-py3-none-any.whl

---
+++ packages/streamlit-chat-plus/streamlit_chat_plus-0.0.0.2-py3-none-any.whl/streamlit_chat_plus/frontend/build/precache-manifest.5a8309b3244e24c3a7b6826a7d10eb60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251b6dbfbfcaba297e4bc078797acd3a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "651ffc673ec5976a9c88",
+    "url": "./static/css/2.fbbf07ed.chunk.css"
+  },
+  {
+    "revision": "3ee3b46809bcc25e824a",
+    "url": "./static/css/main.58e2c10d.chunk.css"
+  },
+  {
+    "revision": "651ffc673ec5976a9c88",
+    "url": "./static/js/2.5a1388d3.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.5a1388d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee3b46809bcc25e824a",
+    "url": "./static/js/main.0d7b3492.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
