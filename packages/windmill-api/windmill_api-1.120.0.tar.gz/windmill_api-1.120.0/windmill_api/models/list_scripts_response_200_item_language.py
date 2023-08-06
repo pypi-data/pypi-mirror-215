e985174e5128dcc7ca5ee64d229ecca898c7f9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:36 +0000

    Add package windmill-api/1.120.0/windmill_api-1.120.0.tar.gz

---
+++ packages/windmill-api/windmill_api-1.120.0.tar.gz/windmill_api-1.120.0/windmill_api/models/list_scripts_response_200_item_language.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class ListScriptsResponse200ItemLanguage(str, Enum):
+    PYTHON3 = "python3"
+    DENO = "deno"
+    GO = "go"
+    BASH = "bash"
+
+    def __str__(self) -> str:
+        return str(self.value)
