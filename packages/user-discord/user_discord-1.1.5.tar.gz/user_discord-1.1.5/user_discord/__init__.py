2dabf54d3a8c220b44463853b2c810a09fd05032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:07 +0000

    Add package user-discord/1.1.5/user_discord-1.1.5.tar.gz

---
+++ packages/user-discord/user_discord-1.1.5.tar.gz/user_discord-1.1.5/user_discord/__init__.py
@@ -0,0 +1,18 @@
+#Using template from https://github.com/Minori101/Amino.fix/blob/main/aminofix/__init__.py :D THANKS
+
+__title__ = 'user_discord'
+__author__ = 'nxSlayer'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2023 nxSlayer'
+
+from .user-discord import SocketDiscord
+from .user_discord import ClientDiscord
+from .utils import objects, payloads
+
+from requests import get
+from json import loads
+
+__newest__ = loads(get("https://pypi.org/pypi/user-discord/json").text)["info"]["version"]
+
+if '1.1.0' != __newest__:
+    print(f"(user-discord) There is a new version, please update for better results")
