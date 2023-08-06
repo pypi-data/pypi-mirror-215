711f8119b5ec36c7bf16f5f9db960600bc2837be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:26 +0000

    Add package assistorg-api/0.0.3/assistorg-api-0.0.3.tar.gz

---
+++ packages/assistorg-api/assistorg-api-0.0.3.tar.gz/assistorg-api-0.0.3/tests/test_assist_api_wrapper.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `assist_api_wrapper` package."""
+
+
+import unittest
+
+from assist_api_wrapper import assist_api_wrapper
+
+
+class TestAssist_api_wrapper(unittest.TestCase):
+    """Tests for `assist_api_wrapper` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
