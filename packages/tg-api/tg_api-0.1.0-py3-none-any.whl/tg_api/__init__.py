95a15b6ebe33a142378c0df570bf1b9f3922db77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:07 +0000

    Add package tg-api/0.1.0/tg_api-0.1.0-py3-none-any.whl

---
+++ packages/tg-api/tg_api-0.1.0-py3-none-any.whl/tg_api/__init__.py
@@ -0,0 +1,31 @@
+from .client import AsyncTgClient, SyncTgClient, raise_for_tg_response_status  # noqa F401
+from .exceptions import TgHttpStatusError, TgRuntimeError  # noqa F401
+from .tg_methods import (  # noqa F401
+    SendMessageResponse,
+    SendMessageRequest,
+    SendPhotoResponse,
+    SendUrlPhotoRequest,
+    SendBytesPhotoRequest,
+    SendDocumentResponse,
+    SendUrlDocumentRequest,
+    SendBytesDocumentRequest,
+)
+from .tg_types import (  # noqa F401
+    ParseMode,
+    User,
+    Chat,
+    KeyboardButton,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    ForceReply,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Invoice,
+    SuccessfulPayment,
+    OrderInfo,
+    ShippingAddress,
+    Message,
+    MessageEntity,
+    MessageReplyMarkup,
+    Update,
+)
