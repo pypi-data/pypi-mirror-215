4ad2c943ec42f49bbd7d3785b2a4021ccec9707d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:29 +0000

    Add package BlitzChain/0.0.0/BlitzChain-0.0.0.tar.gz

---
+++ packages/BlitzChain/BlitzChain-0.0.0.tar.gz/BlitzChain-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BlitzChain',
+    version='0.0.0',
+    url='https://github.com/mr-gpt/blitzchain',
+    author='Twilix',
+    author_email='jacky@twilix.io',
+    description='BlitzChain',
+    packages=find_packages(),    
+    install_requires=['requests'],
+)
