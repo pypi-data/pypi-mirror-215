0822ce626d6d18121d14beff1af3ab6fb30b548e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:50 +0000

    Add package captif-cpx-db/0.13/captif_cpx_db-0.13.tar.gz

---
+++ packages/captif-cpx-db/captif_cpx_db-0.13.tar.gz/captif_cpx_db-0.13/captif_cpx_db/__init__.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from sqlmodel import SQLModel, create_engine
+
+from .cpx import models  # noqa
+from .version import __version__  # noqa
+
+
+def create_sqlite_engine(path, echo: bool = False):
+    engine = create_engine(f"sqlite:///{Path(path).absolute()}", echo=echo)
+
+    # Create tables:
+    SQLModel.metadata.create_all(engine)
+
+    return engine
