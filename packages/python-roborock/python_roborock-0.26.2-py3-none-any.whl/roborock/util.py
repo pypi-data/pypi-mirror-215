c2fb5d4e70d6d9223f39875ed5bf4075fe11e3ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:17 +0000

    Add package python-roborock/0.26.2/python_roborock-0.26.2-py3-none-any.whl

---
+++ packages/python-roborock/python_roborock-0.26.2-py3-none-any.whl/roborock/util.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+import asyncio
+import datetime
+import functools
+from asyncio import AbstractEventLoop
+from typing import Optional, Tuple, TypeVar
+
+T = TypeVar("T")
+DEFAULT_TIME_ZONE: Optional[datetime.tzinfo] = datetime.datetime.now().astimezone().tzinfo
+
+
+def unpack_list(value: list[T], size: int) -> list[Optional[T]]:
+    return (value + [None] * size)[:size]  # type: ignore
+
+
+def get_running_loop_or_create_one() -> AbstractEventLoop:
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+    return loop
+
+
+def parse_datetime_to_roborock_datetime(
+    start_datetime: datetime.datetime, end_datetime: datetime.datetime
+) -> Tuple[datetime.datetime, datetime.datetime]:
+    now = datetime.datetime.now(DEFAULT_TIME_ZONE)
+    start_datetime = start_datetime.replace(
+        year=now.year, month=now.month, day=now.day, second=0, microsecond=0, tzinfo=DEFAULT_TIME_ZONE
+    )
+    end_datetime = end_datetime.replace(
+        year=now.year, month=now.month, day=now.day, second=0, microsecond=0, tzinfo=DEFAULT_TIME_ZONE
+    )
+    if start_datetime > end_datetime:
+        end_datetime += datetime.timedelta(days=1)
+    elif end_datetime < now:
+        start_datetime += datetime.timedelta(days=1)
+        end_datetime += datetime.timedelta(days=1)
+
+    return start_datetime, end_datetime
+
+
+def parse_time_to_datetime(
+    start_time: datetime.time, end_time: datetime.time
+) -> Tuple[datetime.datetime, datetime.datetime]:
+    """Help to handle time data."""
+    start_datetime = datetime.datetime.now(DEFAULT_TIME_ZONE).replace(
+        hour=start_time.hour, minute=start_time.minute, second=0, microsecond=0
+    )
+    end_datetime = datetime.datetime.now(DEFAULT_TIME_ZONE).replace(
+        hour=end_time.hour, minute=end_time.minute, second=0, microsecond=0
+    )
+
+    return parse_datetime_to_roborock_datetime(start_datetime, end_datetime)
+
+
+def run_sync():
+    loop = get_running_loop_or_create_one()
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            return loop.run_until_complete(func(*args, **kwargs))
+
+        return wrapped
+
+    return decorator
