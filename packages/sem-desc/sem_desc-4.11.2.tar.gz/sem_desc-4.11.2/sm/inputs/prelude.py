7d32c15fa6433c1ca30467ff0182df3c8bdc1ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:46 +0000

    Add package sem-desc/4.11.2/sem_desc-4.11.2.tar.gz

---
+++ packages/sem-desc/sem_desc-4.11.2.tar.gz/sem_desc-4.11.2/sm/inputs/prelude.py
@@ -0,0 +1,14 @@
+from .column import Column
+from .table import ColumnBasedTable
+from .context import Context
+from .link import EntityId, Link, WIKIDATA, WIKIDATA_NIL_ENTITY
+
+__all__ = [
+    "Column",
+    "ColumnBasedTable",
+    "Context",
+    "EntityId",
+    "Link",
+    "WIKIDATA",
+    "WIKIDATA_NIL_ENTITY",
+]
