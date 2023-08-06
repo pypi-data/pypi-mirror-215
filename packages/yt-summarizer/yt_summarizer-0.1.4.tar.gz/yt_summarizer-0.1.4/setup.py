779d3b21a7c6751b4a60b6c8032d35643a06fb59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:37 +0000

    Add package yt-summarizer/0.1.4/yt_summarizer-0.1.4.tar.gz

---
+++ packages/yt-summarizer/yt_summarizer-0.1.4.tar.gz/yt_summarizer-0.1.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='yt_summarizer',
+    version='0.1.4',
+    author='Isaac Duong',
+    author_email='isaaacduong@gmail.com',
+    license='MIT',    
+    url='https://github.com/isaacduong/youtube_summarizer',
+    description=("Python script to extract video transcripts " 
+                 "based on the video id, and summarize the " 
+                 "text content using the GPT-3.5 Turbo model."),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['yt_summarizer'],
+    install_requires=[            # I get to this in a second
+          'google-api-python-client',
+          'youtube_transcript_api',
+          'openai',
+          'tqdm',
+          'nltk',
+          'gtts',
+          'langdetect',
+      ],
+   
+)
