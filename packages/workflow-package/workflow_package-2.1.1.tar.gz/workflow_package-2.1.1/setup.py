1f9078746e9ed2a3d1862de3bc6ec7e8ed34141f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:42 +0000

    Add package workflow-package/2.1.1/workflow_package-2.1.1.tar.gz

---
+++ packages/workflow-package/workflow_package-2.1.1.tar.gz/workflow_package-2.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='workflow_package',
+    version='2.1.1',
+    author='Ninja Coder',
+    author_email='ninjacoder@gmail.com',
+    description='A package for workflow details',
+    packages=find_packages(),
+)
