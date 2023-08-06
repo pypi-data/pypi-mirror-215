a360e1d8ab818d735fe8c660a8c8d5e710422667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:02 +0000

    Add package whatsapp-chatbot-python/0.4.0/whatsapp_chatbot_python-0.4.0-py3-none-any.whl

---
+++ packages/whatsapp-chatbot-python/whatsapp_chatbot_python-0.4.0-py3-none-any.whl/whatsapp_chatbot_python/__init__.py
@@ -0,0 +1,4 @@
+from .bot import Bot, GreenAPIBot, GreenAPI, GreenAPIError
+from .manager.handler import Notification
+
+__all__ = ["Bot", "GreenAPIBot", "GreenAPI", "GreenAPIError", "Notification"]
---
+++ packages/whatsapp-chatbot-python/whatsapp_chatbot_python-0.4.0-py3-none-any.whl/whatsapp_chatbot_python/filters.py
@@ -0,0 +1,131 @@
+from abc import ABC, abstractmethod
+from re import fullmatch
+from typing import Dict, List, Optional, Type, Union
+
+
+class AbstractFilter(ABC):
+    @abstractmethod
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def check_event(self, event: dict) -> bool:
+        pass
+
+
+class ChatIDFilter(AbstractFilter):
+    def __init__(self, chat: Union[str, List[str]]):
+        self.chat = chat
+        if isinstance(chat, str):
+            self.chat = [chat]
+
+    def check_event(self, event: dict) -> bool:
+        chat = event["senderData"]["chatId"]
+
+        if chat in self.chat:
+            return True
+        return False
+
+
+class SenderFilter(AbstractFilter):
+    def __init__(self, sender: Union[str, List[str]]):
+        self.sender = sender
+        if isinstance(sender, str):
+            self.sender = [sender]
+
+    def check_event(self, event: dict) -> bool:
+        sender = event["senderData"]["sender"]
+
+        if sender in self.sender:
+            return True
+        return False
+
+
+class TypeMessageFilter(AbstractFilter):
+    def __init__(self, type_message: Union[str, List[str]]):
+        self.type_message = type_message
+        if isinstance(type_message, str):
+            self.type_message = [type_message]
+
+    def check_event(self, event: dict) -> bool:
+        type_message = event["messageData"]["typeMessage"]
+
+        if type_message in self.type_message:
+            return True
+        return False
+
+
+class TextMessageFilter(AbstractFilter):
+    def __init__(self, text_message: Union[str, List[str]]):
+        self.text_message = text_message
+        if isinstance(text_message, str):
+            self.text_message = [text_message]
+
+    def check_event(self, event: dict) -> bool:
+        text_message = self.get_text_message(event)
+
+        if text_message in self.text_message:
+            return True
+        return False
+
+    @staticmethod
+    def get_text_message(event: dict) -> Optional[str]:
+        message_data = event["messageData"]
+
+        type_message = message_data["typeMessage"]
+        if type_message == "textMessage":
+            return message_data["textMessageData"]["textMessage"]
+        elif type_message == "extendedTextMessage":
+            return message_data["extendedTextMessageData"]["text"]
+
+
+class RegExpFilter(AbstractFilter):
+    def __init__(self, pattern: str):
+        self.pattern = pattern
+
+    def check_event(self, event: dict) -> bool:
+        text_message = TextMessageFilter.get_text_message(event)
+
+        if fullmatch(self.pattern, text_message):
+            return True
+        return False
+
+
+class CommandFilter(AbstractFilter):
+    def __init__(self, command: str, prefixes: str = "/"):
+        if isinstance(command, str):
+            self.command = command
+            self.prefixes = prefixes
+        elif isinstance(command, tuple):
+            if len(command) == 2:
+                self.command, self.prefixes = command
+
+    def check_event(self, event: dict) -> bool:
+        text_message = TextMessageFilter.get_text_message(event)
+
+        for prefix in self.prefixes:
+            if text_message.split()[0] != f"{prefix}{self.command}":
+                continue
+            return True
+        return False
+
+
+filters: Dict[str, Type[AbstractFilter]] = {
+    "from_chat": ChatIDFilter,
+    "from_sender": SenderFilter,
+    "type_message": TypeMessageFilter,
+    "text_message": TextMessageFilter,
+    "regexp": RegExpFilter,
+    "command": CommandFilter
+}
+
+__all__ = [
+    "AbstractFilter",
+    "ChatIDFilter",
+    "SenderFilter",
+    "TypeMessageFilter",
+    "TextMessageFilter",
+    "RegExpFilter",
+    "CommandFilter",
+    "filters"
+]
