e5197dad2038772c367f571d612eb55f236b86a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:53 +0000

    Add package enfobench/0.1.0/enfobench-0.1.0.tar.gz

---
+++ packages/enfobench/enfobench-0.1.0.tar.gz/enfobench-0.1.0/src/enfobench/evaluation/__init__.py
@@ -0,0 +1,8 @@
+from ._cross_validate import cross_validate
+from ._evaluate import (
+    evaluate_metric_on_forecast,
+    evaluate_metric_on_forecasts,
+    evaluate_metrics_on_forecast,
+    evaluate_metrics_on_forecasts,
+)
+from .client import ForecastClient
