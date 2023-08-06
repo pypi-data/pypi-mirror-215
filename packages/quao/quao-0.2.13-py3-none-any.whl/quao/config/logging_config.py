79c08e3b78fc67d1cb5ec8242cf9d84871c1c028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:54 +0000

    Add package quao/0.2.13/quao-0.2.13-py3-none-any.whl

---
+++ packages/quao/quao-0.2.13-py3-none-any.whl/quao/config/logging_config.py
@@ -0,0 +1,6 @@
+"""
+    QuaO Project logging_config.py Copyright © CITYNOW Co. Ltd. All rights reserved.
+"""
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
