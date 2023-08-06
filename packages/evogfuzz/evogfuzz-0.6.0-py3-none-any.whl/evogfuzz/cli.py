5aa3050c166b576aaea31f538af0f03b0f7478b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:44 +0000

    Add package evogfuzz/0.6.0/evogfuzz-0.6.0-py3-none-any.whl

---
+++ packages/evogfuzz/evogfuzz-0.6.0-py3-none-any.whl/evogfuzz/cli.py
@@ -0,0 +1,6 @@
+def main():
+    print("CLI not supported yet! Coming soon!")
+
+
+if __name__ == "__main__":
+    exit(main())
---
+++ packages/evogfuzz/evogfuzz-0.6.0-py3-none-any.whl/evogfuzz/tournament_selection.py
@@ -0,0 +1,38 @@
+import logging
+from typing import Set, List
+
+from evogfuzz.input import Input
+
+
+class Tournament:
+    def __init__(
+        self,
+        test_inputs: Set[Input],
+        tournament_rounds: int = 10,
+        tournament_size: int = 10,
+    ):
+        self.test_inputs: Set[Input] = test_inputs
+        self.tournament_rounds: int = tournament_rounds
+        self.tournament_size: int = tournament_size
+
+    def select_fittest_individuals(self):
+        # print(len(self.test_inputs), self.tournament_rounds)
+        # assert self.tournament_rounds < len(self.test_inputs)
+
+        fittest: Set[Input] = set()
+
+        try:
+            for _ in range(self.tournament_rounds):
+                current_round: List[Input] = list(self.test_inputs)[
+                    : self.tournament_size
+                ]
+                for inp in current_round:
+                    self.test_inputs.remove(inp)
+                fi = sorted(
+                    current_round, key=lambda inp: inp.fitness, reverse=False
+                ).pop()
+                fittest.add(fi)
+        except IndexError:
+            logging.debug("Tournament Size too big! No more Inputs left to select!")
+
+        return fittest
