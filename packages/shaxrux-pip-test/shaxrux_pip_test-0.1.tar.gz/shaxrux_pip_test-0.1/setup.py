260765a740a3ceb25db74c93562e0e034b4a0054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:27 +0000

    Add package shaxrux-pip-test/0.1/shaxrux_pip_test-0.1.tar.gz

---
+++ packages/shaxrux-pip-test/shaxrux_pip_test-0.1.tar.gz/shaxrux_pip_test-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='shaxrux_pip_test',
+      version='0.1',
+      description='Shaxrux`s testing pip project',
+      packages=['shaxrux_pip_test'],
+      author_email='tleubaevshaxrux@outlook.com',
+      zip_safe=False)
---
+++ packages/shaxrux-pip-test/shaxrux_pip_test-0.1.tar.gz/shaxrux_pip_test-0.1/shaxrux_pip_test/_init_.py
@@ -0,0 +1,1 @@
+from .programm import name
