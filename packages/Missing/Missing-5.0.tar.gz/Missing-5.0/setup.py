7efd8ef5790803879ed64d8c78d89ffc383822d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:50 +0000

    Add package Missing/5.0/Missing-5.0.tar.gz

---
+++ packages/Missing/Missing-5.0.tar.gz/Missing-5.0/setup.py
@@ -0,0 +1,63 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+README = open('README.rst').read()
+CHANGES = open('CHANGES.rst').read()
+
+version = '5.0'
+
+setup(
+    name='Missing',
+    version=version,
+    url='https://github.com/zopefoundation/Missing',
+    project_urls={
+        'Issue Tracker': 'https://github.com/zopefoundation/Missing/issues',
+        'Sources': 'https://github.com/zopefoundation/Missing',
+    },
+    license='ZPL 2.1',
+    description="Special Missing objects used in Zope.",
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.dev',
+    long_description='\n\n'.join([README, CHANGES]),
+    keywords="zope missing object",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'ExtensionClass >= 4.1a1',
+        'zope.deferredimport',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
