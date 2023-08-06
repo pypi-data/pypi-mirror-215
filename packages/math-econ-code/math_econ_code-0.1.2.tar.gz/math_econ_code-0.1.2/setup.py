d621d94cc7c779fa1e54e23709ffbc06c9ff9e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:27 +0000

    Add package math-econ-code/0.1.2/math_econ_code-0.1.2.tar.gz

---
+++ packages/math-econ-code/math_econ_code-0.1.2.tar.gz/math_econ_code-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name="math_econ_code",
+	version="0.1.2",
+	url="",
+	authors=["Alfred Galichon", "Antoine Jacquet"],
+	author_email="ag133@nyu.edu",
+	licence="GNU",
+	python_requires=">= 3",
+	py_modules=['meclp'],				# ['pymec_lp', 'pymec_ot', ...]
+	description="description of the package"	# can link markdown file here
+)
