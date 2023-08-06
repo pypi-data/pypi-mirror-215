90ebd5802755873e03f3017f4c07d9c0b72d1c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:39 +0000

    Add package openweathersdk/0.1.0/openweathersdk-0.1.0.tar.gz

---
+++ packages/openweathersdk/openweathersdk-0.1.0.tar.gz/openweathersdk-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='openweathersdk',
+    version='0.1.0',
+    description='An SDK for getting weather and forecast information, using OpenWeatherMap api',
+    author='Marcus Vinicius da Silva',
+    author_email='msilva@ice.ufjf.br',
+    url='https://github.com/dasilvactu/openweatherSDK',
+    packages=['openweathersdk'],
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+    ],
+)
