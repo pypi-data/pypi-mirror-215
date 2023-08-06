6ff99f9a0907db74de9185c891e840d5819c87eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:52 +0000

    Add package codeforlife-portal/6.32.0/codeforlife-portal-6.32.0.tar.gz

---
+++ packages/codeforlife-portal/codeforlife-portal-6.32.0.tar.gz/codeforlife-portal-6.32.0/portal/helpers/password.py
@@ -0,0 +1,108 @@
+import re
+from enum import Enum, auto
+
+from django import forms
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.hashers import PBKDF2PasswordHasher as ph
+from django.core.exceptions import ValidationError
+
+
+class PasswordStrength(Enum):
+    STUDENT = auto()
+    INDEPENDENT = auto()
+    TEACHER = auto()
+
+    def password_test(self, password):
+        if self is PasswordStrength.STUDENT:
+            minimum_password_length = 6
+            # Make student password case insensitive
+            password = password.lower()
+            if password and not password_strength_test(
+                password=password,
+                minimum_password_length=minimum_password_length,
+                upper=False,
+                lower=False,
+                numbers=False,
+                special_char=False,
+            ):
+                raise forms.ValidationError(
+                    f"Password not strong enough, consider using at least {minimum_password_length} characters and making it hard to guess."
+                )
+        elif self is PasswordStrength.INDEPENDENT:
+            minimum_password_length = 8
+            if password and not password_strength_test(
+                password=password,
+                minimum_password_length=minimum_password_length,
+                upper=True,
+                lower=True,
+                numbers=True,
+                special_char=False,
+            ):
+                raise forms.ValidationError(
+                    f"Password not strong enough, consider using at least {minimum_password_length} characters, "
+                    "upper and lower case letters, and numbers and making it hard to guess."
+                )
+        else:
+            minimum_password_length = 10
+            if password and not password_strength_test(
+                password=password,
+                minimum_password_length=minimum_password_length,
+                upper=True,
+                lower=True,
+                numbers=True,
+                special_char=True,
+            ):
+                raise forms.ValidationError(
+                    f"Password not strong enough, consider using at least {minimum_password_length} characters, "
+                    "upper and lower case letters, numbers, special characters and making it hard to guess."
+                )
+        return password
+
+
+def password_strength_test(
+    password,
+    minimum_password_length,
+    upper=True,
+    lower=True,
+    numbers=True,
+    special_char=True,
+):
+    most_used_passwords = [
+        "Abcd1234",
+        "Password1",
+        "Qwerty123",
+        "password",
+        "qwerty",
+        "abcdef",
+    ]
+    return (
+        len(password) >= minimum_password_length
+        and (not upper or re.search(r"[A-Z]", password))
+        and (not lower or re.search(r"[a-z]", password))
+        and (not numbers or re.search(r"[0-9]", password))
+        and (not special_char or re.search(r"[!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?]", password))
+        and (password not in most_used_passwords)
+    )
+
+
+def form_clean_password(self, password_field_name, strength: PasswordStrength):
+    password = self.cleaned_data.get(password_field_name, None)
+    password = strength.password_test(password)
+    if hasattr(self, "user"):
+        current_user_password = self.user.password
+        algorithm, iterations, salt, hash = current_user_password.split("$")
+        new_hashed_password = ph().encode(password, salt)
+        if new_hashed_password == current_user_password:
+            raise ValidationError(f"Please choose a password that you haven't used before")
+    return password
+
+
+def check_update_password(form, user, request, data):
+    changing_password = False
+    if data["password"] != "":
+        changing_password = True
+        user.set_password(data["password"])
+        user.save()
+        update_session_auth_hash(request, form.user)
+
+    return changing_password
---
+++ packages/codeforlife-portal/codeforlife-portal-6.32.0.tar.gz/codeforlife-portal-6.32.0/portal/static/portal/js/passwordStrength.js
@@ -0,0 +1,82 @@
+var TEACHER_PASSWORD_FIELD_ID = '';
+var INDEP_STUDENT_PASSWORD_FIELD_ID = '';
+let teacher_password_field = '';
+let indep_student_password_field = '';
+let most_used_passwords = ['Abcd1234', 'Password1', 'Qwerty123'];
+
+let password_strengths = [
+    { name: 'No password!', colour: '#FF0000' },
+    { name: 'Password too weak!', colour: '#DBA901' },
+    { name: 'Strong password!', colour: '#088A08' },
+    { name: 'Password too common!', colour: '#DBA901' }
+];
+
+$(function() {
+
+    teacher_password_field = $('#' + TEACHER_PASSWORD_FIELD_ID);
+    indep_student_password_field = $('#' + INDEP_STUDENT_PASSWORD_FIELD_ID);
+
+    setUpDynamicUpdates(teacher_password_field, true);
+    setUpDynamicUpdates(indep_student_password_field, false);
+
+    updatePasswordStrength(true);
+    updatePasswordStrength(false);
+});
+
+function setUpDynamicUpdates(password_field, isTeacher) {
+    password_field.on('keyup', function(){
+        updatePasswordStrength(isTeacher)
+    });
+    password_field.on('paste', function(){
+        updatePasswordStrength(isTeacher)
+    });
+    password_field.on('cut', function(){
+        updatePasswordStrength(isTeacher)
+    });
+}
+
+function updatePasswordStrength(isTeacher) {
+    // The reason for the timeout is that if we just got $('#...').val() we'd get the
+    // old value before the keypress / change. Apparently even jQuery itself implements
+    // things this way, so maybe there is no better workaround.
+
+    setTimeout(function() {
+        let password;
+
+        if (isTeacher) {
+            password = $('#' + TEACHER_PASSWORD_FIELD_ID).val();
+        }
+        else {
+            password = $('#' + INDEP_STUDENT_PASSWORD_FIELD_ID).val();
+        }
+
+        let strength = 0;
+        if (password.length > 0) { strength++; }
+        if (isPasswordStrong(password, isTeacher)) { strength++; }
+
+        if ($.inArray(password, most_used_passwords) >= 0 && strength == 2) { strength = 3; }
+
+        if (isTeacher) {
+            updatePasswordCSS('#teacher-password-sign', '#teacher-password-text', strength);
+        }
+        else {
+            updatePasswordCSS('#student-password-sign', '#student-password-text', strength);
+        }
+
+    });
+}
+
+function isPasswordStrong(password, isTeacher) {
+    if (isTeacher) {
+        return password.length >= 10 && !(password.search(/[A-Z]/) === -1 || password.search(/[a-z]/) === -1 || password.search(/[0-9]/) === -1 || password.search(/[!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?]/) === -1)
+    }
+    else {
+        return password.length >= 8 && !(password.search(/[A-Z]/) === -1 || password.search(/[a-z]/) === -1 || password.search(/[0-9]/) === -1)
+    }
+}
+
+function updatePasswordCSS(passwordStrengthSign, passwordStrengthText, strength) {
+    $(passwordStrengthSign).css('background-color', password_strengths[strength].colour);
+    $(passwordStrengthText).html(password_strengths[strength].name);
+}
+
