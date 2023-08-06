9069666e53505823286bbc8dc73aa4c31f0e99f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:34 +0000

    Add package RLS-Task-GUI/0.12.dev0/RLS%20Task%20GUI-0.12dev.tar.gz

---
+++ packages/RLS-Task-GUI/RLS%20Task%20GUI-0.12dev.tar.gz/RLS Task GUI-0.12dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='RLS Task GUI',
+    version='0.12dev',
+    packages=['RLS'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    # long_description=open('README.txt').read(),
+)
