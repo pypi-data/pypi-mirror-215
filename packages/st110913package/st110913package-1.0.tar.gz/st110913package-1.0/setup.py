daebb60a033c711e8a550758fc574af2083b1f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:18 +0000

    Add package st110913package/1.0/st110913package-1.0.tar.gz

---
+++ packages/st110913package/st110913package-1.0.tar.gz/st110913package-1.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+  name = 'st110913package', # 包的名字        
+  packages = ['st110913package'], # 包含的文件夹  
+  version = '1.0', # 版本号     
+  license='MIT',        
+  description = 'None',  
+  author = 'st110913', # 你的pypi用户名                  
+  author_email = 'st20110913@outlook.com', # 你的电子邮箱     
+  
+  keywords = [],   
+  long_description = """# 示例
+st110913的示例""", 
+  long_description_content_type="text/markdown",
+
+  classifiers=[ 
+    'Programming Language :: Python :: 3.6',
+
+  ],
+)
