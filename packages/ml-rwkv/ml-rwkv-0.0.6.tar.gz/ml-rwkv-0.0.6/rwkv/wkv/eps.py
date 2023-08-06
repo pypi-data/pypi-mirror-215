c8f1604d3cfb8cac977ec3f53b1789797138b4e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:48 +0000

    Add package ml-rwkv/0.0.6/ml-rwkv-0.0.6.tar.gz

---
+++ packages/ml-rwkv/ml-rwkv-0.0.6.tar.gz/ml-rwkv-0.0.6/rwkv/wkv/eps.py
@@ -0,0 +1,186 @@
+# mypy: disable-error-code="override"
+"""Provides a numerically-stable implementation of the WKV computation.
+
+This implementation follows the official implementation.
+"""
+
+from typing import cast
+
+import torch
+from torch import Tensor
+from torch.autograd.function import Function, FunctionCtx, once_differentiable
+
+
+@torch.jit.script
+def wkv_with_eps_forward(w: Tensor, u: Tensor, k: Tensor, v: Tensor, state: Tensor) -> tuple[Tensor, Tensor]:
+    bsz, tsz, chans = k.shape
+
+    assert w.shape == u.shape == (chans,)
+    assert v.shape == (bsz, tsz, chans)
+    assert state.shape == (bsz, 3, 1, chans)
+
+    alpha, beta, eps = state[:, :, -1].chunk(3, dim=1)  # (B, 1, D), (B, 1, D), (B, 1, D)
+
+    _, tsz, _ = k.shape
+
+    wkvs = []
+    alphas = [alpha]
+    betas = [beta]
+    epss = [eps]
+
+    for t in range(tsz):
+        kt, vt = k[:, t : t + 1], v[:, t : t + 1]
+        ukt = u + kt
+        tau = torch.maximum(ukt, eps)
+        e1 = torch.exp(eps - tau)
+        e2 = torch.exp(ukt - tau)
+        wkv = (e1 * alpha + e2 * vt) / (e1 * beta + e2)
+        wkvs.append(wkv)
+
+        w_eps = w + eps
+        eps = torch.maximum(w_eps, kt)
+        e1 = torch.exp(w_eps - eps)
+        e2 = torch.exp(kt - eps)
+        alpha = e1 * alpha + e2 * vt
+        beta = e1 * beta + e2
+
+        alphas.append(alpha)
+        betas.append(beta)
+        epss.append(eps)
+
+    alpha = torch.stack(alphas, dim=2)
+    beta = torch.stack(betas, dim=2)
+    eps = torch.stack(epss, dim=2)
+
+    return torch.cat(wkvs, 1), torch.cat((alpha, beta, eps), dim=1)
+
+
+@torch.jit.script
+def wkv_with_eps_backward(
+    w: Tensor,
+    u: Tensor,
+    k: Tensor,
+    v: Tensor,
+    state: Tensor,
+    grad_wkv: Tensor,
+    grad_state: Tensor,
+) -> tuple[Tensor, Tensor, Tensor, Tensor, Tensor]:
+    bsz, tsz, chans = k.shape
+
+    assert w.shape == u.shape == (chans,)
+    assert v.shape == (bsz, tsz, chans)
+    assert state.shape == (bsz, 3, tsz + 1, chans)
+    assert grad_wkv.shape == (bsz, tsz, chans)
+    assert grad_state.shape == (bsz, 3, 1, chans)
+
+    alpha, beta, eps = state.chunk(3, dim=1)  # (B, 1, T + 1, D), (B, 1, T + 1, D), (B, 1, T + 1, D)
+    grad_alpha, grad_beta, grad_eps = grad_state[:, :, 0].chunk(3, dim=1)  # (B, 1, D), (B, 1, D), (B, 1, D)
+    grad_eps = grad_eps.clone()
+
+    grad_w = torch.zeros_like(w)
+    grad_u = torch.zeros_like(u)
+    grad_k = torch.zeros_like(k)
+    grad_v = torch.zeros_like(v)
+
+    for t in range(tsz - 1, -1, -1):
+        kt, vt = k[:, t : t + 1], v[:, t : t + 1]
+        alpha_prev, beta_prev, eps_prev = alpha[:, :, t], beta[:, :, t], eps[:, :, t]
+        alpha_curr, beta_curr, eps_curr = alpha[:, :, t + 1], beta[:, :, t + 1], eps[:, :, t + 1]
+        ukt = u + kt
+        tau = torch.maximum(ukt, eps_prev)
+        e1 = torch.exp(eps_prev - tau)
+        e2 = torch.exp(ukt - tau)
+
+        euke = torch.exp(ukt + eps_prev - 2 * tau)
+
+        denom = e1 * beta_prev + e2
+        denom_sq = denom * denom
+
+        grad_wkvt = grad_wkv[:, t : t + 1]
+
+        # Backpropagates wkv gradients.
+        grad_uk = grad_wkvt * e2 * (e1 * beta_prev * vt - e1 * alpha_prev) / denom_sq
+        grad_u += grad_uk.flatten(0, -2).sum(0)
+        grad_k[:, t : t + 1] += grad_uk
+        grad_v[:, t : t + 1] += grad_wkvt * e2 / denom
+
+        grad_alpha_wkv = grad_wkvt * e1 / denom
+        grad_beta_wkv = -grad_wkvt * e1 * (e2 * vt + e1 * alpha_prev) / denom_sq
+        grad_eps_wkv = grad_wkvt * euke * (alpha_prev - vt * beta_prev) / (e1 * beta_prev + e2) ** 2
+
+        e1 = torch.exp(w + eps_prev - eps_curr)
+        e2 = torch.exp(kt - eps_curr)
+
+        # Backpropagates alpha gradients.
+        grad_alpha_we = grad_alpha * e1 * alpha_prev
+        grad_w += grad_alpha_we.flatten(0, -2).sum(0)
+        grad_k[:, t : t + 1] += grad_alpha * e2 * vt
+        grad_v[:, t : t + 1] += grad_alpha * e2
+        grad_eps += grad_alpha * -alpha_curr
+
+        # Backpropagates beta gradients.
+        grad_beta_we = grad_beta * e1 * beta_prev
+        grad_w += grad_beta_we.flatten(0, -2).sum(0)
+        grad_k[:, t : t + 1] += grad_beta * e2
+        grad_eps += grad_beta * -beta_curr
+
+        # Backpropagates epsilon gradients.
+        eps_grad_mask = w + eps_prev > kt
+        grad_eps_we = torch.where(eps_grad_mask, grad_eps, torch.zeros_like(grad_eps))
+        grad_w += grad_eps_we.flatten(0, -2).sum(0)
+        grad_k[:, t : t + 1] += torch.where(eps_grad_mask, torch.zeros_like(grad_eps), grad_eps)
+
+        # Computes gradients for alpha, beta and epsilon.
+        grad_alpha = grad_alpha * e1 + grad_alpha_wkv
+        grad_beta = grad_beta * e1 + grad_beta_wkv
+        grad_eps = grad_alpha_we + grad_beta_we + grad_eps_we + grad_eps_wkv
+
+    return grad_w, grad_u, grad_k, grad_v, torch.stack((grad_alpha, grad_beta, grad_eps), dim=1)
+
+
+class WkvWithEps(Function):
+    @staticmethod
+    def forward(
+        ctx: FunctionCtx,
+        w: Tensor,
+        u: Tensor,
+        k: Tensor,
+        v: Tensor,
+        state: Tensor,
+    ) -> tuple[Tensor, Tensor]:
+        wkv, state_out = wkv_with_eps_forward(w, u, k, v, state)
+        ctx.save_for_backward(w, u, k, v, state_out)
+        return wkv, state_out[:, :, -1:]
+
+    @staticmethod
+    @once_differentiable
+    def backward(
+        ctx: FunctionCtx,
+        grad_wkv: Tensor,
+        grad_state: Tensor,
+    ) -> tuple[Tensor, Tensor, Tensor, Tensor, Tensor]:
+        w, u, k, v, state = cast(tuple[Tensor, ...], ctx.saved_tensors)
+        return wkv_with_eps_backward(w, u, k, v, state, grad_wkv, grad_state)
+
+
+def initial_state_with_eps(emb_dim: int) -> Tensor:
+    return torch.zeros(1, 3, 1, emb_dim)
+
+
+def wkv_with_eps(w: Tensor, u: Tensor, k: Tensor, v: Tensor, state: Tensor) -> tuple[Tensor, Tensor]:
+    """Runs the core WKV computation.
+
+    Args:
+        w: The decay tensor, with shape (D)
+        u: The output multiplier tensor, with shape (D)
+        k: The K tensor, with shape (B, T, D)
+        v: The V tensor, with shape (B, T, D)
+        state: The state tensor, with shape (B, 3, T, D), consisting of the
+            alpha, beta and eps tensors, each with shape (B, 1, T, D)
+
+    Returns:
+        The WKV tensor, with shape (B, T, D), and the next state, with shape
+        (B, 3, 1, D), consisting of the next alpha, beta and eps tensors, each
+        with shape (B, 1, 1, D)
+    """
+    return WkvWithEps.apply(w, u, k, v, state)
---
+++ packages/ml-rwkv/ml-rwkv-0.0.6.tar.gz/ml-rwkv-0.0.6/tests/test_eps_wkv.py
@@ -0,0 +1,73 @@
+"""Tests the WKV computation using epsilon offset for numerical stability."""
+
+from typing import cast
+
+import pytest
+import torch
+from torch import Tensor
+
+from rwkv.wkv.eps import initial_state_with_eps, wkv_with_eps, wkv_with_eps_forward
+
+
+def _get_dummy_tensors(bsz: int, tsz: int, chans: int, device: torch.device, dtype: torch.dtype) -> tuple[Tensor, ...]:
+    w = -torch.exp(torch.rand(chans, dtype=dtype, device=device))
+    u = torch.rand(chans, dtype=dtype, device=device)
+    k = torch.randn(bsz, tsz, chans, dtype=dtype, device=device)
+    v = torch.randn(bsz, tsz, chans, dtype=dtype, device=device)
+    return w, u, k, v
+
+
+def _copy_with_grad(*t: Tensor) -> tuple[Tensor, ...]:
+    return tuple(t_i.detach().clone().requires_grad_(True) for t_i in t)
+
+
+def _get_grads(*t: Tensor) -> tuple[Tensor | None, ...]:
+    return tuple(cast(Tensor, t_i.grad) for t_i in t)
+
+
+def test_eps_wkv() -> None:
+    bsz, tsz, chans = 2, 7, 16
+    device, dtype = torch.device("cpu"), torch.float32
+
+    w, u, k, v = _get_dummy_tensors(bsz, tsz, chans, device, dtype)
+    state = initial_state_with_eps(chans).repeat_interleave(bsz, dim=0).to(device, dtype)
+
+    # Runs in full mode.
+    out_full, _ = wkv_with_eps(w, u, k, v, state)
+
+    # Runs in iterative mode.
+    out_parts: list[Tensor] = []
+    for t in range(tsz):
+        out_part, state = wkv_with_eps(w, u, k[:, t : t + 1], v[:, t : t + 1], state)
+        out_parts.append(out_part)
+    out_partial = torch.cat(out_parts, dim=1)
+
+    assert torch.allclose(out_full, out_partial, atol=1e-5)
+
+
+@pytest.mark.parametrize("mode", ["state", "wkv", "both"])
+def test_gradients_eps_wkv(mode: str) -> None:
+    bsz, tsz, chans = 2, 7, 16
+    device, dtype = torch.device("cpu"), torch.float32
+
+    w, u, k, v = _get_dummy_tensors(bsz, tsz, chans, device, dtype)
+    state = initial_state_with_eps(chans).repeat_interleave(bsz, dim=0).to(device, dtype)
+
+    # Uses autograd to compute the gradients.
+    wt, ut, kt, vt, statet = _copy_with_grad(w, u, k, v, state)
+    wkv_ref, state_out_ref = wkv_with_eps_forward(wt, ut, kt, vt, statet)
+    state_out_ref = state_out_ref[:, :, -1:]
+    wkv_grad = torch.zeros_like(wkv_ref) if mode == "state" else torch.rand_like(wkv_ref)
+    state_out_grad = torch.zeros_like(state_out_ref) if mode == "wkv" else torch.rand_like(state_out_ref)
+    torch.autograd.backward((wkv_ref, state_out_ref), (wkv_grad, state_out_grad))
+    wgr, ugr, kgr, vgr, stategr = _get_grads(wt, ut, kt, vt, statet)
+
+    # Uses the manual gradient computation to compute the gradients.
+    wt, ut, kt, vt, statet = _copy_with_grad(w, u, k, v, state)
+    wkv_man, state_out_man = wkv_with_eps(wt, ut, kt, vt, statet)
+    torch.autograd.backward((wkv_man, state_out_man), (wkv_grad, state_out_grad))
+    wgm, ugm, kgm, vgm, stategm = _get_grads(wt, ut, kt, vt, statet)
+
+    for gr, gm in zip((wgr, ugr, kgr, vgr, stategr), (wgm, ugm, kgm, vgm, stategm)):
+        if gr is not None and gm is not None:
+            assert torch.allclose(gr, gm, atol=1e-5)
