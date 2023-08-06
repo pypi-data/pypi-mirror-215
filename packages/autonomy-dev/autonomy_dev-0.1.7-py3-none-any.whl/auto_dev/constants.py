4f7f1f14fdeab88868ff0cb26a968231791a5467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:51 +0000

    Add package autonomy-dev/0.1.7/autonomy_dev-0.1.7-py3-none-any.whl

---
+++ packages/autonomy-dev/autonomy_dev-0.1.7-py3-none-any.whl/auto_dev/constants.py
@@ -0,0 +1,13 @@
+"""
+Constants for the auto_dev package.
+"""
+
+import os
+from pathlib import Path
+
+DEFAULT_ENCODING = "utf-8"
+# package directory
+PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
+DEFAULT_PYLAMA_CONFIG = Path(PACKAGE_DIR) / "data" / "pylama.ini"
+AUTONOMY_PACKAGES_FILE = "packages/packages.json"
+PLUGIN_FOLDER = os.path.join(os.path.dirname(__file__), 'commands')
