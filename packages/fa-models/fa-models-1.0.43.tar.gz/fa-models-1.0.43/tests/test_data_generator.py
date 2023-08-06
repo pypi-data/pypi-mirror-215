35f3420526772e0d9319427bae7725e7abda05ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:09 +0000

    Add package fa-models/1.0.43/fa-models-1.0.43.tar.gz

---
+++ packages/fa-models/fa-models-1.0.43.tar.gz/fa-models-1.0.43/tests/test_data_generator.py
@@ -0,0 +1,9 @@
+import pytest
+from famodels.generators.data_generator import DataGenerator
+
+def test_generate_batch_of_signals():
+    gen = DataGenerator()
+    try:
+        gen.generate_batch_of_signals()
+    except Exception as e:
+        pytest.fail(f"Exception occured: {e}")
