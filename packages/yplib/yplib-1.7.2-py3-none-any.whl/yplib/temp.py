bfd6a3480fae9d83eb3101e6656aaf92f12763fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:45 +0000

    Add package yplib/1.7.2/yplib-1.7.2-py3-none-any.whl

---
+++ packages/yplib/yplib-1.7.2-py3-none-any.whl/yplib/temp.py
@@ -0,0 +1,14 @@
+from yplib.index import *
+
+
+# 有关文件的操作
+# 查询指定文件夹下面的所有的文件信息, 也可以是指定的文件
+# param list
+# return list
+def get_file1(file_path=None, prefix=None, contain=None, suffix=None):
+    print(file_path)
+
+# print(os.path.abspath('.'))
+
+
+# print('end')
