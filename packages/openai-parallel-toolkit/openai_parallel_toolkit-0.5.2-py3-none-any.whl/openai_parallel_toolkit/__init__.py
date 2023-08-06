0f73cd0432dbdf003a1b140ba352204e4bfbcd84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:23 +0000

    Add package openai-parallel-toolkit/0.5.2/openai_parallel_toolkit-0.5.2-py3-none-any.whl

---
+++ packages/openai-parallel-toolkit/openai_parallel_toolkit-0.5.2-py3-none-any.whl/openai_parallel_toolkit/__init__.py
@@ -0,0 +1,2 @@
+from .api import APIKeyManager, Gpt35Turbo, OpenAIModel, request_openai_api, Gpt35Turbo0613, Gpt4, Gpt35Turbo16K
+from .core import ParallelToolkit, multi_process_one, multi_thread_run
---
+++ packages/openai-parallel-toolkit/openai_parallel_toolkit-0.5.2-py3-none-any.whl/openai_parallel_toolkit/api/__init__.py
@@ -0,0 +1,3 @@
+from .api import OpenAIModel, Gpt35Turbo, Gpt35Turbo0613, Gpt4, Gpt35Turbo16K
+from .keys import APIKeyManager
+from .request import request_openai_api
