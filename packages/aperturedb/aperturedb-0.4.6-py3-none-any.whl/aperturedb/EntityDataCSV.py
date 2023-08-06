3fdb39cb9195edc725a1361e3cc001868c6f6676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:54 +0000

    Add package aperturedb/0.4.6/aperturedb-0.4.6-py3-none-any.whl

---
+++ packages/aperturedb/aperturedb-0.4.6-py3-none-any.whl/aperturedb/EntityDataCSV.py
@@ -0,0 +1,72 @@
+from aperturedb import CSVParser
+
+ENTITY_CLASS = "EntityClass"
+PROPERTIES   = "properties"
+CONSTRAINTS  = "constraints"
+
+
+class EntityDataCSV(CSVParser.CSVParser):
+    """**ApertureDB Entity Data.**
+
+    This class loads the Entity Data which is present in a csv file,
+    and converts it into a series of aperturedb queries.
+
+    .. note::
+        Expects a csv file with the following columns:
+
+            ``EntityClass``, ``PROP_NAME_1``, ... ``PROP_NAME_N``, ``constraint_PROP1``
+
+    Example csv file::
+
+        EntityClass,name,lastname,age,id,constraint_id
+        Person,John,Salchi,69,321423532,321423532
+        Person,Johna,Salchi,63,42342522,42342522
+        ...
+
+    Example usage:
+
+    .. code-block:: python
+
+        data = EntityDataCSV("/path/to/EntityData.csv")
+        loader = ParallelLoader(db)
+        loader.ingest(data)
+
+
+    .. important::
+        In the above example, the constraint_id ensures that a Entity with the specified
+        id would be only inserted if it does not already exist in the database.
+
+
+    """
+
+    def __init__(self, filename, df=None, use_dask=False):
+        super().__init__(filename, df=df, use_dask=use_dask)
+
+        self.props_keys = [x for x in self.header[1:]
+                           if not x.startswith(CSVParser.CONSTRAINTS_PREFIX)]
+        self.constraints_keys = [x for x in self.header[1:]
+                                 if x.startswith(CSVParser.CONSTRAINTS_PREFIX)]
+        self.command = "AddEntity"
+
+    def get_indices(self):
+        return {
+            "entity": {
+                cls: self.get_indexed_properties() for cls in self.df[ENTITY_CLASS].unique()
+            }
+        }
+
+    def getitem(self, idx):
+        idx = self.df.index.start + idx
+        eclass = self.df.loc[idx, ENTITY_CLASS]
+        q = []
+        ae = self._basic_command(idx,
+                                 custom_fields = {
+                                     "class": eclass
+                                 })
+
+        q.append(ae)
+        return q, []
+
+    def validate(self):
+        if self.header[0] != ENTITY_CLASS:
+            raise Exception("Error with CSV file field: " + ENTITY_CLASS)
---
+++ packages/aperturedb/aperturedb-0.4.6-py3-none-any.whl/test/conftest.py
@@ -0,0 +1,149 @@
+import pytest
+from aperturedb.Connector import Connector
+from aperturedb.ConnectorRest import ConnectorRest
+from aperturedb.ParallelLoader import ParallelLoader
+from aperturedb.BlobDataCSV import BlobDataCSV
+from aperturedb.EntityDataCSV import EntityDataCSV
+from aperturedb.ConnectionDataCSV import ConnectionDataCSV
+from aperturedb.DescriptorSetDataCSV import DescriptorSetDataCSV
+from aperturedb.DescriptorDataCSV import DescriptorDataCSV
+from aperturedb.ImageDataCSV import ImageDataCSV
+from aperturedb.BBoxDataCSV import BBoxDataCSV
+from aperturedb.Constraints import Constraints
+from aperturedb.Entities import Entities
+from aperturedb.Query import Query
+from aperturedb.Utils import Utils
+
+import dbinfo
+
+# These are test fixtures and can be used in any
+# pytest tests as function parmeters with same names.
+
+
+def pytest_generate_tests(metafunc):
+    if "db" in metafunc.fixturenames:
+        metafunc.parametrize("db", [
+            {"db": Connector(
+                host = dbinfo.DB_TCP_HOST,
+                port = dbinfo.DB_TCP_PORT,
+                user = dbinfo.DB_USER,
+                password = dbinfo.DB_PASSWORD,
+                use_ssl = True)},
+            {"db": ConnectorRest(
+                host = dbinfo.DB_REST_HOST,
+                port = dbinfo.DB_REST_PORT,
+                user = dbinfo.DB_USER,
+                password = dbinfo.DB_PASSWORD,
+                use_ssl = False
+            )}
+        ], indirect=True, ids=["TCP", "HTTP"])
+    if "insert_data_from_csv" in metafunc.fixturenames and metafunc.module.__name__ in \
+            ["test.test_Data"]:
+        metafunc.parametrize("insert_data_from_csv", [
+                             True, False], indirect=True, ids=["with_dask", "without_dask"])
+
+
+@pytest.fixture()
+def db(request):
+    db = request.param['db']
+    utils = Utils(db)
+    assert utils.remove_all_objects() == True
+    return db
+
+
+def check_response_regressions(queries, input_blobs, responses, output_blobs):
+    # Check that responses have no blobs
+    first_command = list(responses[0].keys())[0]
+    assert "blobs_start" not in responses[0][
+        first_command], f"responses[0]={responses[0]}"
+
+
+@pytest.fixture()
+def insert_data_from_csv(db, request):
+    """
+    A helper function that processes various .csv files supported
+    by aperturedb, and maps a corresponding DataCSV class that can be
+    used to parse semantics of the .csv file
+    """
+    def insert_data_from_csv(in_csv_file, rec_count=-1, expected_error_count=0, loader_result_lambda=None):
+        if rec_count > 0 and rec_count < 80:
+            request.param = False
+            print("Not enough records to test parallel loader. Using serial loader.")
+        file_data_pair = {
+            "./input/persons.adb.csv": EntityDataCSV,
+            "./input/images.adb.csv": ImageDataCSV,
+            "./input/connections-persons-images.adb.csv": ConnectionDataCSV,
+            "./input/bboxes.adb.csv": BBoxDataCSV,
+            "./input/blobs.adb.csv": BlobDataCSV,
+            "./input/descriptorset.adb.csv": DescriptorSetDataCSV,
+            "./input/setA.adb.csv": DescriptorDataCSV,
+            "./input/setB.adb.csv": DescriptorDataCSV,
+            "./input/s3_images.adb.csv": ImageDataCSV,
+            "./input/http_images.adb.csv": ImageDataCSV,
+            "./input/bboxes-constraints.adb.csv": BBoxDataCSV,
+            "./input/gs_images.adb.csv": ImageDataCSV,
+            "./input/persons-exist-base.adb.csv": EntityDataCSV,
+            "./input/persons-some-exist.adb.csv": EntityDataCSV
+        }
+        use_dask = False
+        if hasattr(request, "param"):
+            use_dask = request.param
+        data = file_data_pair[in_csv_file](in_csv_file, use_dask=use_dask)
+
+        setattr(data, "response_handler", check_response_regressions)
+        data.strict_response_validation = True
+
+        if rec_count != -1:
+            data = data[:rec_count]
+
+        loader = ParallelLoader(db)
+        loader.ingest(data, batchsize=99,
+                      numthreads=8,
+                      stats=True,
+                      )
+
+        # make sure all indices are present
+        if hasattr(data, "get_indices"):
+            expected_indices = data.get_indices()
+            observed_indices = loader.get_existing_indices()
+            for tp, classes in expected_indices.items():
+                for cls, props in classes.items():
+                    for prop in props:
+                        assert prop in observed_indices[tp][cls]
+
+        assert loader.error_counter == 0
+        assert len(data) - \
+            loader.get_suceeded_queries() == expected_error_count
+        if loader_result_lambda is not None:
+            loader_result_lambda(loader, data)
+
+        # Preserve loader so that dask manager is not auto deleted.
+        # ---------------
+        # Previously, dask's cluster and client were entirely managed in a context
+        # insede dask manager. A change upstream broke that, and now we keep the DM
+        # in scope just so that we can do computations after ingestion, as those
+        # things are lazily evaluated.
+        return data, loader
+
+    return insert_data_from_csv
+
+
+@pytest.fixture()
+def utils(db):
+    return Utils(db)
+
+
+@pytest.fixture()
+def images(insert_data_from_csv):
+    return insert_data_from_csv("./input/images.adb.csv")
+
+
+@pytest.fixture()
+def retired_persons(db, insert_data_from_csv, utils):
+    utils.remove_entities(class_name="Person")
+    loaded = insert_data_from_csv("./input/persons.adb.csv")
+    constraints = Constraints()
+    constraints.greaterequal("age", 60)
+    retired_persons = Entities.retrieve(db,
+                                        spec=Query.spec(with_class="Person", constraints=constraints))
+    return retired_persons
