cbc0b2ee3a9ec4bb3d6299a35c21a372a16c64e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:47 +0000

    Add package dsspkg/0.2.2/dsspkg-0.2.2.tar.gz

---
+++ packages/dsspkg/dsspkg-0.2.2.tar.gz/dsspkg-0.2.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="dsspkg",   # 包名字
+    version="0.2.2",
+    description="A data splitting and scaling package",
+    long_description=long_description,  # 将说明文件设置为README.md
+    long_description_content_type="text/markdown",
+    packages=find_packages(),   # 默认从当前目录下搜索包
+    author="DW",
+    author_email="prwe98@gmail.com",
+    python_requires='>=3.10',
+)
