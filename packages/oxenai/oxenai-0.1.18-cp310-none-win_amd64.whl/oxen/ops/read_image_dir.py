91e4749cda1b6d1991f020587336ac6ad343e865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:19 +0000

    Add package oxenai/0.1.18/oxenai-0.1.18-cp310-none-win_amd64.whl

---
+++ packages/oxenai/oxenai-0.1.18-cp310-none-win_amd64.whl/oxen/ops/read_image_dir.py
@@ -0,0 +1,26 @@
+import oxen
+from tqdm import tqdm
+import cv2
+
+
+class ReadImageDir(oxen.Op):
+    """
+    Reads in imagery as specified by a DataFrame column of paths. 
+
+    Args:
+        args[0] : str
+            Root imagery directory, is prefixed to DataFrame paths
+        args[1] : List[str]
+            Column of paths to imagery (relative to root directory specified in args[0])
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def call(self, args):
+        image_data = []
+        prefix = args[0]
+        for path in tqdm(args[1]):
+            img = cv2.imread(f"{prefix}/{path}", cv2.IMREAD_UNCHANGED)
+            image_data.append(img)
+        return image_data
