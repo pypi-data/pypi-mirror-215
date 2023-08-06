e6cc037ae8d9c5945a5c6b5a3b306c78e11f10fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:49 +0000

    Add package function-analyser/0.0.11/function-analyser-0.0.11.tar.gz

---
+++ packages/function-analyser/function-analyser-0.0.11.tar.gz/function-analyser-0.0.11/src/function_analyser/__main__.py
@@ -0,0 +1,7 @@
+"""Module allowing for ``python -m function_analyser ...``."""
+from __future__ import annotations
+
+from function_analyser.main import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
