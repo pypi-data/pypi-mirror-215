a142811399d0ebce328302c0c86eb2b684003ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:17 +0000

    Add package Digital-Farming-Solution/0.1.0/Digital_Farming_Solution-0.1.0.tar.gz

---
+++ packages/Digital-Farming-Solution/Digital_Farming_Solution-0.1.0.tar.gz/Digital_Farming_Solution-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="Digital_Farming_Solution",
+    version="0.1.0",
+    author="Bharatesha N S",
+    author_email="bharatesha.ns.ext@bayer.com",
+    description=" The packages are helps to the DFS Teams to perform some of the task easily by importing this packages.",
+    packages=["Digital_Farming_Solution"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
