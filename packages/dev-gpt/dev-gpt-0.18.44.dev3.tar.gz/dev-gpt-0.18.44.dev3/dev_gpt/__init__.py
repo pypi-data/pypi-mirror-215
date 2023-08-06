ec8d16cc50de07c72ee02de4a2c7d75e52d6d98a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:29 +0000

    Add package dev-gpt/0.18.44.dev3/dev-gpt-0.18.44.dev3.tar.gz

---
+++ packages/dev-gpt/dev-gpt-0.18.44.dev3.tar.gz/dev-gpt-0.18.44.dev3/dev_gpt/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.18.44.dev3'
+
+from dev_gpt.cli import main
---
+++ packages/dev-gpt/dev-gpt-0.18.44.dev3.tar.gz/dev-gpt-0.18.44.dev3/setup.py
@@ -0,0 +1,51 @@
+from os import path
+
+from setuptools import setup, find_packages
+
+def read_requirements():
+    with open('requirements.txt', 'r', encoding='utf-8') as f:
+        return [line.strip() for line in f.readlines() if not line.startswith('#')]
+
+
+try:
+    libinfo_py = path.join('dev_gpt', '__init__.py')
+    libinfo_content = open(libinfo_py, 'r', encoding='utf8').readlines()
+    version_line = [l.strip() for l in libinfo_content if l.startswith('__version__')][
+        0
+    ]
+    exec(version_line)  # gives __version__
+except FileNotFoundError:
+    __version__ = '0.0.0'
+
+
+setup(
+    name='dev-gpt',
+    version=__version__,
+    description='Use natural language interface to generate, deploy and update your microservice infrastructure.',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Jina AI',
+    author_email='hello@jina.ai',
+    license='Apache 2.0',
+    url='https://github.com/jina-ai/dev-gpt',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_requirements(),
+    scripts=['main.py'],
+    entry_points={
+        'console_scripts': [
+            'dev-gpt = dev_gpt:main',
+        ],
+    },
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
