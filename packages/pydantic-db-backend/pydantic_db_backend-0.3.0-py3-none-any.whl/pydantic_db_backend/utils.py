24e23ecbc75db13b2c96b881b1662f0b7b17ee78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:27 +0000

    Add package pydantic-db-backend/0.3.0/pydantic_db_backend-0.3.0-py3-none-any.whl

---
+++ packages/pydantic-db-backend/pydantic_db_backend-0.3.0-py3-none-any.whl/pydantic_db_backend/utils.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import datetime
+from uuid import uuid4
+
+from iso8601 import iso8601, ParseError
+
+
+def uid() -> str:
+    return str(uuid4()).replace("-", "")
+
+_uid = uid
+
+def utcnow() -> datetime.datetime:
+    return datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
+
+
+def str_to_datetime_if_parseable(value: str) -> datetime.datetime | str:
+    if len(value) < 8 or value.count('-') != 2:
+        return value
+    try:
+        ret = iso8601.parse_date(value)
+    except (ParseError, ValueError, TypeError):
+        ret = value
+    return ret
