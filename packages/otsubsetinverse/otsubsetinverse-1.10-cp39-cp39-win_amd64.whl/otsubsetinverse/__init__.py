96f09df6e1126eedb5ea60ce2259b7521746dba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:56 +0000

    Add package otsubsetinverse/1.10/otsubsetinverse-1.10-cp39-cp39-win_amd64.whl

---
+++ packages/otsubsetinverse/otsubsetinverse-1.10-cp39-cp39-win_amd64.whl/otsubsetinverse/__init__.py
@@ -0,0 +1,19 @@
+"""
+    otsubsetinverse --- An OpenTURNS module
+    ==================================
+
+    Contents
+    --------
+      'otsubsetinverse' is a module for OpenTURNS
+
+"""
+
+# flake8: noqa
+
+# ensures swig type tables order & dll load
+import openturns as _ot
+
+from .otsubsetinverse import *
+
+__version__ = '1.10'
+
