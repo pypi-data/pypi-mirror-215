5f1e8c8032c9e0ff0d7c56963c565fe1f0e1a847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:47 +0000

    Add package twitchify/1.1.1/twitchify-1.1.1.tar.gz

---
+++ packages/twitchify/twitchify-1.1.1.tar.gz/twitchify-1.1.1/twitch/__init__.py
@@ -0,0 +1,17 @@
+"""
+Twitchy
+
+A Python library for Twitch's WebSocket EventSub integration.
+
+:copyright: (c) 2023-present Snifo
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'Twitchify'
+__version__ = '1.1.1'
+__license__ = 'MIT License'
+__author__ = 'Snifo'
+__email__ = 'Snifo@mail.com'
+__github__ = 'https://github.com/mrsnifo/twitchify'
+
+from .client import Client
