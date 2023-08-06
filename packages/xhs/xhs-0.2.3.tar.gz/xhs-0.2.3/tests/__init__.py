af8060407bfa5399a190048b7db9a4380597ef68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:40 +0000

    Add package xhs/0.2.3/xhs-0.2.3.tar.gz

---
+++ packages/xhs/xhs-0.2.3.tar.gz/xhs-0.2.3/tests/__init__.py
@@ -0,0 +1,9 @@
+"""
+pytest commands:
+  pytest tests -- test all the tests directory test files
+  pytest -s tests -- print is work
+  pytest tests/test_help.py -- test specific file
+  pytest tests/test_help.py::test_sign_get -- test specific function
+"""
+
+test_cookie = "open-api_worker_plugin_uuid=7d8efd4116794035a86c8e0205f4d15f;a1=187d2defea8dz1fgwydnci40kw265ikh9fsxn66qs50000726043;webId=ba57f42593b9e55840a289fa0b755374;xhsTrackerId=793cad31-9269-477d-a07d-854bdb2f41ce;xhsTrackerId.sig=xJiFE97D1WnVhw2BTfgN04BZ_rJRSyYHbFXvM527-Ag;web_session=040069b253793fdd9ccd79950e364b000cde81;xhsTracker=url=user-profile&xhsshare=CopyLink;xhsTracker.sig=WS8d3HYlzoIfhHjyJtY_Y1QP5iYacJ96TpUFr1hgfm4;extra_exp_ids=yamcha_0327_clt,h5_1208_exp3,ques_clt2;extra_exp_ids.sig=-9P_FIY9nRpp4czlpi3JlPCL_zdr5ZMYd73Vy8sdzzY;gid=yYWfJfi820jSyYWfJfdidiKK0YfuyikEvfISMAM348TEJC28K23TxI888WJK84q8S4WfY2Sy;gid.sign=PSF1M3U6EBC/Jv6eGddPbmsWzLI=;sec_poison_id=9f2e6c0b-e61d-45fb-9ca9-7b44953f2fc7;websectiga=8886be45f388a1ee7bf611a69f3e174cae48f1ea02c0f8ec3256031b8be9c7ee;xsecappid=xhs-pc-web"
