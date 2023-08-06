77bd2883cd184036ebebbb4c20ed9fc34248fec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:41 +0000

    Add package blizzardwarcraftapi/0.2.0/blizzardwarcraftapi-0.2.0-py3-none-any.whl

---
+++ packages/blizzardwarcraftapi/blizzardwarcraftapi-0.2.0-py3-none-any.whl/BlizzardWarcraftAPI/modules/__init__.py
@@ -0,0 +1,13 @@
+from .gameData.AchievementAPI import AchievementAPI
+from .gameData.AuctionHouseAPI import AuctionHouseAPI
+from .gameData.ConnectedRealmAPI import ConnectedRealmAPI
+from .gameData.CreatureAPI import CreatureAPI
+
+from .profile.CharacterAchievementsAPI import CharacterAchievementsAPI
+from .profile.CharacterAppearanceAPI import CharacterAppearanceAPI
+from .profile.CharacterCollectionsAPI import CharacterCollectionsAPI
+from .profile.CharacterEncountersAPI import CharacterEncountersAPI
+from .profile.CharacterEquipmentAPI import CharacterEquipmentAPI
+from .profile.CharacterHunterPetsAPI import CharacterHunterPetsAPI
+
+from BlizzardWarcraftAPI.BlizzardAuthToken import BlizzardAuthToken
