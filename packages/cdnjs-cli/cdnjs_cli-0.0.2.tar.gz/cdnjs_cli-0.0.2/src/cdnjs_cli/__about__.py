107b9cf1fd20bb05d781598f40034ce979360663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:33 +0000

    Add package cdnjs-cli/0.0.2/cdnjs_cli-0.0.2.tar.gz

---
+++ packages/cdnjs-cli/cdnjs_cli-0.0.2.tar.gz/cdnjs_cli-0.0.2/src/cdnjs_cli/__about__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2023-present woidzero <woidzeroo@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+__version__ = "0.0.2"
+__prog_name__ = "cdnjs-cli"
---
+++ packages/cdnjs-cli/cdnjs_cli-0.0.2.tar.gz/cdnjs_cli-0.0.2/tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2023-present woidzero <71274141+woidzero@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
