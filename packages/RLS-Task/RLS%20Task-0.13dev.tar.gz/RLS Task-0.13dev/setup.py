ae5746db6217ef5631630e99e9e248b99ab504a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:46 +0000

    Add package RLS-Task/0.13.dev0/RLS%20Task-0.13dev.tar.gz

---
+++ packages/RLS-Task/RLS%20Task-0.13dev.tar.gz/RLS Task-0.13dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='RLS Task',
+    version='0.13dev',
+    packages=['RLS'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    # long_description=open('README.txt').read(),
+)
