d131f22d44813fcf1f956aff77056d474dbafa35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:56 +0000

    Add package styledlogger/0.1.0/styledlogger-0.1.0.tar.gz

---
+++ packages/styledlogger/styledlogger-0.1.0.tar.gz/styledlogger-0.1.0/styledlogger/__init__.py
@@ -0,0 +1,10 @@
+from .logger import (
+    Logger as StyledLogger
+)
+
+from .classes.styleconfig import (
+    StyleConfig
+)
+
+__AUTHOR__ = "ImAlek (https://github.com/barealek)"
+__LICENSE__ = "See https://github.com/barealek/StyledLogger for license"
