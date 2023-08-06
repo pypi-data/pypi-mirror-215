7ee3971d3048533c48464e04e4371c7fac8ac3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:40 +0000

    Add package image-processing-dp/0.0.2/image_processing_dp-0.0.2.tar.gz

---
+++ packages/image-processing-dp/image_processing_dp-0.0.2.tar.gz/image_processing_dp-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image_processing_dp",
+    version="0.0.2",
+    author="Danillo Souza Pereira",
+    description="Pacote para processar imagens usando Skimage",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DanilloSouza03/Pacote-de-processar-imagens",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.5',
+)
