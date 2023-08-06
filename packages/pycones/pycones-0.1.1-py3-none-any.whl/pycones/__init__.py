ed2258643446f485184d9c5f2a90a20bbbae23df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:57 +0000

    Add package pycones/0.1.1/pycones-0.1.1-py3-none-any.whl

---
+++ packages/pycones/pycones-0.1.1-py3-none-any.whl/pycones/__init__.py
@@ -0,0 +1,25 @@
+"""
+The pycones package provides useful functions
+"""
+
+__version__ = "0.1.1"
+
+def flatten(lst):
+    """
+    flatten a list
+    """
+    result = []
+    for item in lst:
+        if isinstance(item, list):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+    return result
+
+
+def list_join(*args, sep=""):
+    """
+    join lists into a string
+    """
+    res = sep.join(flatten(args))
+    return res
