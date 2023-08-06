9d3b43236d3151e031af91bd6415a5bcfcff9502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:31 +0000

    Add package django-khronos/0.1.1/django-khronos-0.1.1.tar.gz

---
+++ packages/django-khronos/django-khronos-0.1.1.tar.gz/django-khronos-0.1.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="django-khronos",
+    version="0.1.1",
+    description="Khronos is a Python library that benchmarks the duration of Django tests. It helps identify slow-running tests in your test suite, allowing you to optimize their performance.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Abhinav Prakash",
+    author_email="abhinavsp0730@gmail.com",
+    license="MIT License",
+    package_dir={"": "src"},
+    packages=setuptools.find_namespace_packages(where="src"),
+    zip_safe=False,
+    install_requires=["rich>=13.3.5", "gspread>=5.9.0"],
+    python_requires=">=3.9",
+    url="https://github.com/abhinavsp0730/khronos/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
