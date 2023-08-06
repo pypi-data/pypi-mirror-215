54449e71c7f907bbc88d2cd159a1312c5beb1dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:56 +0000

    Add package statute-utils/0.4.0/statute_utils-0.4.0-py3-none-any.whl

---
+++ packages/statute-utils/statute_utils-0.4.0-py3-none-any.whl/statute_utils/components/utils.py
@@ -0,0 +1,116 @@
+from collections.abc import Iterator
+
+import yaml
+
+
+class literal(str):
+    pass
+
+
+def literal_presenter(dumper, data):
+    return dumper.represent_scalar("tag:yaml.org,2002:str", data, style=">")
+
+
+yaml.add_representer(literal, literal_presenter)
+
+
+def represent_ordereddict(dumper, data):
+    value = []
+
+    for item_key, item_value in data.items():
+        node_key = dumper.represent_data(item_key)
+        node_value = dumper.represent_data(item_value)
+
+        value.append((node_key, node_value))
+
+    return yaml.nodes.MappingNode("tag:yaml.org,2002:map", value)
+
+
+yaml.add_representer(dict, represent_ordereddict)
+
+
+def walk(nodes: list[dict]):
+    """Converts raw nodes into a suitably formatted object for `yaml.dump()`.
+    Without this, there would be no formatting of content and the ordering
+    of the key-value pairs would not be in sync with the intended design.
+    """
+    if isinstance(nodes, list):
+        revised_nodes = []
+        for node in nodes:
+            data = []
+            if node.get("item"):
+                candidate = node["item"]
+                if candidate := str(node["item"]).strip():
+                    if candidate.isdigit():
+                        candidate = int(candidate)
+                data.append(("item", candidate))
+            if node.get("caption"):
+                data.append(("caption", node["caption"].strip()))
+            if node.get("content"):
+                formatted_content = literal(node["content"].strip())
+                data.append(("content", formatted_content))
+            if node.get("units", None):
+                walked_units = walk(node["units"])
+                data.append(("units", walked_units))
+            revised_nodes.append(dict(data))
+    return revised_nodes
+
+
+def stx(regex_text: str):
+    """Remove indention of raw regex strings. This makes regex more readable when using
+    rich.Syntax(<target_regex_string>, "python")"""
+    return rf"""
+{regex_text}
+"""
+
+
+def ltr(*args) -> str:
+    """
+    Most statutes are referred to in the following way:
+    RA 8424, P.D. 1606, EO. 1008, etc. with spatial errors like
+    B.  P.   22; some statutes are acronyms: "C.P.R."
+    (code of professional responsibility)
+    """
+    joined = r"\.?\s*".join(args)
+    return rf"(?:\b{joined}\.?)"
+
+
+def add_num(prefix: str) -> str:
+    num = r"(\s+No\.?s?\.?)?"
+    return rf"{prefix}{num}"
+
+
+def add_blg(prefix: str) -> str:
+    blg = r"(\s+Blg\.?)?"
+    return rf"{prefix}{blg}"
+
+
+def get_regexes(regexes: list[str], negate: bool = False) -> Iterator[str]:
+    for x in regexes:
+        if negate:
+            yield rf"""(?<!{x}\s)
+                """
+        else:
+            yield x
+
+
+def not_prefixed_by_any(regex: str, lookbehinds: list[str]) -> str:
+    """Add a list of "negative lookbehinds" (of fixed character lengths) to a
+    target `regex` string."""
+    return rf"""{''.join(get_regexes(lookbehinds, negate=True))}({regex})
+    """
+
+
+NON_ACT_INDICATORS = [
+    r"An",  # "An act to ..."
+    r"AN",  # "AN ACT ..."
+    r"Republic",  # "Republic Act"
+    r"Rep",
+    r"Rep\.",
+    r"REPUBLIC",
+    r"Commonwealth",
+    r"COMMONWEALTH",
+]
+"""If the word act is preceded by these phrases, do not consider the same to be a
+legacy act of congress."""
+limited_acts = not_prefixed_by_any(rf"{add_num(r'Acts?')}", NON_ACT_INDICATORS)
