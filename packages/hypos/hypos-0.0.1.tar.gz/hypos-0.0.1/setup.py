7698a71206eea32d4addaa25f9c8c743be8e90a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:07 +0000

    Add package hypos/0.0.1/hypos-0.0.1.tar.gz

---
+++ packages/hypos/hypos-0.0.1.tar.gz/hypos-0.0.1/setup.py
@@ -0,0 +1,29 @@
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="hypos",
+    version="0.0.1",
+    author="prabdalip",
+    author_email="anasterstudio@gmail.com",
+    description="PrABpY",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PrABpY/ML-Hypothesis.git",
+    license="MIT",
+    install_requires=[
+        'numpy',
+        'itertools'
+    ],
+    tests_require=[
+        'coverage', 'wheel', 'pytest', 'requests_mock'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha"
+    ]
+)
