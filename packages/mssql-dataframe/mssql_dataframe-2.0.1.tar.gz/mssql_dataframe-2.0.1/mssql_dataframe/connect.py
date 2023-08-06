0c58428d966387049a9132bc591a0d0222b90a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:48 +0000

    Add package mssql-dataframe/2.0.1/mssql_dataframe-2.0.1.tar.gz

---
+++ packages/mssql-dataframe/mssql_dataframe-2.0.1.tar.gz/mssql_dataframe-2.0.1/mssql_dataframe/connect.py
@@ -0,0 +1,107 @@
+"""Class for establishing connection to an SQL database."""
+
+import pyodbc
+
+from mssql_dataframe.core import custom_errors
+
+
+class connect:
+    r"""Connect to local, remote, or cloud SQL Server using ODBC connection.
+
+    Parameters
+    ----------
+    database (str, default='master') : name of database to connect to
+    server (str, default='localhost') : name of server to connect to
+    driver (str, default=None) : ODBC driver name to use, if not given is automatically determined
+    username (str, default=None) : if not given, use Windows account credentials to connect
+    password (str, default=None) : if not given, use Windows account credentials to connect
+
+    Properties
+    ----------
+    connection (pyodbc.Connection) : manage operations to database connection and database transactions
+
+    Examples
+    --------
+    Connect using Windows account credentials and inferring the ODBC driver.
+
+    >>> import env
+    >>> db = connect(server=env.server, database=env.database)
+
+    Connect to a remote server.
+
+    >>> db = connect(server='SomeServerName', database='tempdb') # doctest: +SKIP
+
+    Connect to Azure SQL Server instance using a username and password.
+
+    >>> db = connect(server='<server>.database.windows.net', username='<username>', password='<password>') # doctest: +SKIP
+
+    Connect to SQL Express Local DB
+    >>> db = connect(server=r"(localdb)\mssqllocaldb") # doctest: +SKIP
+
+    Connect using a specific driver.
+    >>> db = connect(driver_name='ODBC Driver 17 for SQL Server') # doctest: +SKIP
+    """
+
+    def __init__(
+        self,
+        database: str = "master",
+        server: str = "localhost",
+        driver: str = None,
+        username: str = None,
+        password: str = None,
+    ):
+        driver, drivers_installed = self._get_driver(driver)
+        self.connection_spec = {
+            "database": database,
+            "server": server,
+            "driver": driver,
+            "drivers_installed": drivers_installed,
+        }
+        if username is None:
+            self.connection_spec["trusted_connection"] = True
+        else:
+            self.connection_spec["trusted_connection"] = False
+
+        if self.connection_spec["trusted_connection"]:
+            self.connection = pyodbc.connect(
+                driver=self.connection_spec["driver"],
+                server=self.connection_spec["server"],
+                database=self.connection_spec["database"],
+                autocommit=False,
+                trusted_connection="yes",
+            )
+        else:
+            self.connection = pyodbc.connect(
+                driver=self.connection_spec["driver"],
+                server=self.connection_spec["server"],
+                database=self.connection_spec["database"],
+                autocommit=False,
+                UID=username,
+                PWD=password,
+            )
+
+    @staticmethod
+    def _get_driver(driver_search):
+        """Automatically determine ODBC driver if needed.
+
+        Parameters
+        ----------
+        driver_search (str) : name of ODBC driver, if None, automatically determine
+
+        Returns
+        -------
+        driver (str) : name of ODBC driver
+        drivers_installed (list) : drivers install for SQL Server
+        """
+        installed = [x for x in pyodbc.drivers() if x.endswith(" for SQL Server")]
+        if driver_search is None:
+            driver = [x for x in installed if x.endswith(" for SQL Server")]
+        else:
+            driver = [x for x in installed if x == driver_search]
+        if not driver:
+            raise custom_errors.EnvironmentODBCDriverNotFound(
+                "Unable to find ODBC driver."
+            )
+        driver = max(driver)
+
+        return driver, installed
---
+++ packages/mssql-dataframe/mssql_dataframe-2.0.1.tar.gz/mssql_dataframe-2.0.1/mssql_dataframe/package.py
@@ -0,0 +1,119 @@
+"""Methods for creating, modifying, reading, and writing between dataframes and SQL."""
+from importlib.metadata import version
+import sys
+import logging
+
+from mssql_dataframe.connect import connect
+from mssql_dataframe.core import (
+    custom_errors,
+    conversion,
+    create,
+    modify,
+    read,
+)
+from mssql_dataframe.core.write.write import write
+
+logger = logging.getLogger(__name__)
+
+
+class SQLServer(connect):
+    """Class containing methods for creating, modifying, reading, and writing between dataframes and SQL Server.
+
+    Parameters
+    ----------
+    database (str, default='master') : name of database to connect to
+    server (str, default='localhost') : name of server to connect to
+    driver (str, default=None) : ODBC driver name to use, if not given is automatically determined
+    username (str, default=None) : if not given, use Windows account credentials to connect
+    password (str, default=None) : if not given, use Windows account credentials to connect
+    include_metadata_timestamps (bool, default=False) : include metadata timestamps _time_insert & _time_update in server time for write operations
+
+    Properties
+    ----------
+    create : methods for creating SQL tables objects
+    modify : methods for modifying tables columns and primary keys
+    read : methods for reading from SQL tables
+    write : methods for inserting, updating, and merging records
+
+    Examples
+    --------
+    Connect to localhost server master database.
+
+    >>> import env
+    >>> sql = SQLServer(database=env.database, server=env.server)
+
+    Enable logging from mssql_dataframe.
+
+    >>> import logging
+    >>> logging.basicConfig(
+    ... filename='example.log', encoding='utf-8', level=logging.DEBUG,
+    ... format='%(asctime)s %(name)s %(filename)s %(levelname)s: %(message)s'
+    ... )
+    >>> logger = logging.getLogger('mssql_dataframe')
+    >>> sql = SQLServer(database=env.database, server=env.server)
+
+    See Also
+    --------
+    connect : Additional options for connecting to a server including remote, Azure, and username/password.
+    """
+
+    def __init__(
+        self,
+        database: str = "master",
+        server: str = "localhost",
+        driver: str = None,
+        username: str = None,
+        password: str = None,
+        include_metadata_timestamps: bool = False,
+    ):
+        connect.__init__(self, database, server, driver, username, password)
+
+        # log initialization details
+        self.log_init()
+
+        # initialize mssql_dataframe functionality with shared connection
+        self.exceptions = custom_errors
+        self.create = create.create(self.connection, include_metadata_timestamps)
+        self.modify = modify.modify(self.connection)
+        self.read = read.read(self.connection)
+        self.write = write(self.connection, include_metadata_timestamps)
+
+        # issue warnings for automated functionality
+        if include_metadata_timestamps:
+            msg = "SQL write operations will include metadata '_time_insert' & '_time_update' columns as 'include_metadata_timestamps=True'."
+            logger.warning(msg)
+
+    def log_init(self):
+        """Log connection info and versions for Python, SQL, and required packages."""
+        # determine versions for debugging
+        self.version_spec = {}
+        # Python
+        self.version_spec["python"] = sys.version_info
+        # SQL
+        cur = self.connection.cursor()
+        name = cur.execute("SELECT @@VERSION").fetchone()
+        self.version_spec["sql"] = name[0]
+        # packages
+        names = ["mssql-dataframe", "pyodbc", "pandas"]
+        for name in names:
+            self.version_spec[name] = version(name)
+
+        # output actual connection info (possibly derived within connection object)
+        logger.debug(f"Connection Info: {self.connection_spec}")
+        # output Python/SQL/package versions
+        logger.debug(f"Version Numbers: {self.version_spec}")
+
+    def get_schema(self, table_name: str):
+        """Get schema of an SQL table and the defined conversion rules between data types.
+
+        Parameters
+        ----------
+        table_name (str) : table name to read schema from
+
+        Returns
+        -------
+        schema (pandas.DataFrame) : table column specifications and conversion rules
+        """
+        schema, _ = conversion.get_schema(self.connection, table_name)
+
+        return schema
