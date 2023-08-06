84f40a546e1dac2d8b0c98fcd40787119b6d032a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:12 +0000

    Add package biobear/0.6.2/biobear-0.6.2-cp39-none-win32.whl

---
+++ packages/biobear/biobear-0.6.2-cp39-none-win32.whl/biobear/compression.py
@@ -0,0 +1,26 @@
+"""Compression configuration."""
+
+import os
+from enum import Enum
+from pathlib import Path
+
+
+class Compression(Enum):
+    """Compression types for files."""
+
+    INFERRED = "INFERRED"
+    NONE = "NONE"
+    GZIP = "GZIP"
+
+    @classmethod
+    def from_file(cls, path: os.PathLike) -> "Compression":
+        """Infer the compression type from the file extension."""
+        if Path(path).suffix == ".gz":
+            return Compression.GZIP
+        return Compression.NONE
+
+    def infer_or_use(self, path: os.PathLike) -> "Compression":
+        """Infer the compression type from the file extension if needed."""
+        if self == Compression.INFERRED:
+            return Compression.from_file(path)
+        return self
