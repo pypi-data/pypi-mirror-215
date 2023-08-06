aa8d08d3c64ab502dc1fbedecd87dcb3501bb801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:47 +0000

    Add package error-alerts/4.3/error-alerts-4.3.tar.gz

---
+++ packages/error-alerts/error-alerts-4.3.tar.gz/error-alerts-4.3/error_alerts/__init__.py
@@ -0,0 +1,58 @@
+import traceback
+from telegram import Bot
+
+class telegram:
+    def __init__(self, token=None, channel=None, logger=None, full_error=True, raise_error=False, resend_repeat_errors=True):
+        if token:
+            self.bot = Bot(token=token)
+        self.channel = channel
+        self.logger = logger
+        self.full_error = full_error
+        self.raise_error = raise_error
+        self.resend_repeat_errors = resend_repeat_errors
+        self.last_error = None
+    def send(self, title='Error', exception=None, channel=None):
+        if not channel:
+            channel = self.channel
+        if self.full_error:
+            error = traceback.format_exc()
+        else:
+            error = str(exception)
+        message = f'{title}: {error}'
+        if error == self.last_error and not self.resend_repeat_errors:
+            self.printer(message, level='error')
+        if error != self.last_error or self.resend_repeat_errors:
+            self.printer(message, level='error')
+            if channel:
+                try:
+                    self.bot.send_message(channel, message[:4096])
+                except Exception as telegram_error:
+                    self.printer('Error sending alert message to Telegram:', telegram_error, level='error')
+            self.last_error = error
+        if self.raise_error:
+            raise Exception('Raiser') from exception
+    def send_message(self, *messages, print_message=True, current_time=True, channel=None):
+        if not channel:
+            channel = self.channel
+        final_message = ''
+        for message in messages:
+            final_message += message
+            final_message += ' '
+        if print_message:
+            self.printer(final_message, current_time=current_time)
+        if channel:
+            try:
+                self.bot.send_message(channel, final_message[:4096])
+            except Exception as telegram_error:
+                self.printer('Error sending message to Telegram:', telegram_error, level='error')
+    
+    def printer(self, *items, level='info', current_time=True):
+        if self.logger:
+            if current_time:
+                self.logger.current_time(*items, level=level)
+            else:
+                self.logger.log(*items, level=level)
+            self.logger.log(level=level)
+        elif level != 'debug': # Don't print debug only messages
+            print(*items)
+            print()
