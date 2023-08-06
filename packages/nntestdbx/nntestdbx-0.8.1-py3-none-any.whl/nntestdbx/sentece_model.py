dd1f6e30fb9d0ded56f3b1e25c4f14cb024317f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:15 +0000

    Add package nntestdbx/0.8.1/nntestdbx-0.8.1-py3-none-any.whl

---
+++ packages/nntestdbx/nntestdbx-0.8.1-py3-none-any.whl/nntestdbx/sentece_model.py
@@ -0,0 +1,9 @@
+from modelutils.utils import TextUtils
+
+class SentenceModel(TextUtils):
+    def __init__(self) -> None:
+        pass
+
+    def sentence_to_words(self, s: str) -> list:
+        s_capit = self.capitalize_string(s)
+        return s_capit.split(" ")
