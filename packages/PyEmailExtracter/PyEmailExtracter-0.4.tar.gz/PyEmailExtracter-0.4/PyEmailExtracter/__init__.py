908af873602694be83d988ee1172e49537e025f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:30 +0000

    Add package PyEmailExtracter/0.4/PyEmailExtracter-0.4.tar.gz

---
+++ packages/PyEmailExtracter/PyEmailExtracter-0.4.tar.gz/PyEmailExtracter-0.4/PyEmailExtracter/__init__.py
@@ -0,0 +1,2 @@
+__all__ = ["extract_emails"]
+from .driver import extract_emails
---
+++ packages/PyEmailExtracter/PyEmailExtracter-0.4.tar.gz/PyEmailExtracter-0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="PyEmailExtracter",
+    version="0.4",
+    author="Bidut Karki",
+    author_email="bidutjava3@gmail.com",
+    description="Tool for extracting emails from pdf and docx files. (Designed especially for resumes)",
+    packages=["PyEmailExtracter"],
+    install_requires=[
+        "docx2txt==0.8",
+        "lxml==4.9.2",
+        "PyPDF2==3.0.1",
+        "python-docx==0.8.11",
+        "fitz",
+        "PyMuPDF==1.22.5",
+    ],
+)
