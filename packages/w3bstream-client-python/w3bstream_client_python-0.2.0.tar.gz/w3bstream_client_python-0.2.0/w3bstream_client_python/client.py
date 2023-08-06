3641b429f250f500eb0a1869fb1099ca8a3b309d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:45 +0000

    Add package w3bstream-client-python/0.2.0/w3bstream_client_python-0.2.0.tar.gz

---
+++ packages/w3bstream-client-python/w3bstream_client_python-0.2.0.tar.gz/w3bstream_client_python-0.2.0/w3bstream_client_python/client.py
@@ -0,0 +1,28 @@
+import json
+import requests
+import datetime
+
+
+class Client:
+    def __init__(self, endpoint, api_key):
+        self.endpoint = endpoint
+        self.api_key = api_key
+
+    # TODO: support push_data non-blocking, which can send data async in batch
+    # TODO: make push_data non-panic
+    def push_data(self, deviceID, data, event_type="DEFAULT"):
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + self.api_key,
+        }
+        body = {
+            'device_id': deviceID,
+            'event_type': event_type,
+            'payload': data,
+            'timestamp':  datetime.datetime.now(),
+        }
+        response = requests.post(
+            self.endpoint, data=json.dumps(body), headers=headers)
+        print(f"Hello, {self.endpoint}, {self.api_key}, {body}!")
+        print(response)
+        return response
