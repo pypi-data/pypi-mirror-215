e8d28fe984490577fc185562d71c15867a67e9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:48 +0000

    Add package grpcrest/0.11/grpcrest-0.11.tar.gz

---
+++ packages/grpcrest/grpcrest-0.11.tar.gz/grpcrest-0.11/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='grpcrest',
+      version='0.11',
+      description='REST Wrapper for GRPC',
+      url='http://github.com/storborg/notfound',
+      author='Dinesh RVL',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      include_package_data=True,
+      packages=['grpcrest'],
+      entry_points = {
+        'console_scripts': ['start-grpc-rest=grpcrest.grpc_rest:main'],
+        },
+      install_requires=[
+          'flask',
+      ],
+      zip_safe=False)
