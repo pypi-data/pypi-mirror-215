762109aef04b0d2bf4ffa19466b5d34786cc62c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:41 +0000

    Add package bludwig/0.0.6/bludwig-0.0.6-py3-none-any.whl

---
+++ packages/bludwig/bludwig-0.0.6-py3-none-any.whl/bludwig/__main__.py
@@ -0,0 +1,15 @@
+# __main__.py
+
+import bludwig
+from importlib.metadata import version
+
+def main():
+    print('Hello bludwig', version('bludwig') )
+
+    
+if __name__ == "__main__":
+    main()    
+    
+    
+    
+    
