0133d9c1b604ec7f7defaa0b2d57bf79bfa0a965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:33 +0000

    Add package dbt-snapshot-analysis/0.2.0/dbt_snapshot_analysis-0.2.0.tar.gz

---
+++ packages/dbt-snapshot-analysis/dbt_snapshot_analysis-0.2.0.tar.gz/dbt_snapshot_analysis-0.2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dbt_snapshot_analysis",
+    version="0.2.0",
+    packages=find_packages(),
+    py_modules=["dbt_snapshot_analysis"],
+    install_requires=["pandas", "matplotlib", "streamlit"],
+    entry_points={
+        "console_scripts": [
+            "dbt_snapshot_analysis=dbt_snapshot_analysis.streamlit_entry_point:main"
+        ]
+    },
+    author="Sammy Teillet",
+    author_email="sammy.teillet@gmail.com",
+    description="A package for analyzing snapshots",
+    url="https://github.com/data-drift/dbt-snapshot-analytics",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
