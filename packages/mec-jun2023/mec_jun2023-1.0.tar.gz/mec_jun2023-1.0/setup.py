b3b1fdc5a59495636b8746bc3a9cc75feca628c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:35 +0000

    Add package mec-jun2023/1.0/mec_jun2023-1.0.tar.gz

---
+++ packages/mec-jun2023/mec_jun2023-1.0.tar.gz/mec_jun2023-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name="mec_jun2023",
+	version="1.0",
+	url="",
+	author=["Alfred Galichon", "Antoine Jacquet"],
+	author_email="ag133@nyu.edu",
+	licence="GNU",
+	python_requires=">= 3",
+	py_modules=['pymec_lp'],
+	description="description of the package"	# can link markdown file here
+)
