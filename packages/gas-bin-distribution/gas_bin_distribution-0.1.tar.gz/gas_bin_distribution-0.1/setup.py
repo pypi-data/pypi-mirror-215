78008d01b99998e7955f66882d4fb6a5c3b9f88a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:37 +0000

    Add package gas-bin-distribution/0.1/gas_bin_distribution-0.1.tar.gz

---
+++ packages/gas-bin-distribution/gas_bin_distribution-0.1.tar.gz/gas_bin_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gas_bin_distribution',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gas_bin_distribution'],
+      author='Simon Peter Ofula Amoth',
+      author_email='simonofula@gmail.com',
+    zip_safe=False)
