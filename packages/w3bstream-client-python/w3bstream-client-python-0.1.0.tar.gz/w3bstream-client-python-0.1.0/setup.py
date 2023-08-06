f9791dd745e909a7c8d8deea25d492761f0cfffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:41 +0000

    Add package w3bstream-client-python/0.1.0/w3bstream-client-python-0.1.0.tar.gz

---
+++ packages/w3bstream-client-python/w3bstream-client-python-0.1.0.tar.gz/w3bstream-client-python-0.1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='w3bstream-client-python',
+    version='0.1.0',
+    description='A library to send data to w3bstream service',
+    packages=['w3bstream-client-python'],
+)
