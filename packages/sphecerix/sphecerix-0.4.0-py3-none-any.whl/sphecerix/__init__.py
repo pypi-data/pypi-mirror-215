c977c8c17185267ec48813f36a2a28baaabbf1bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:20 +0000

    Add package sphecerix/0.4.0/sphecerix-0.4.0-py3-none-any.whl

---
+++ packages/sphecerix/sphecerix-0.4.0-py3-none-any.whl/sphecerix/__init__.py
@@ -0,0 +1,6 @@
+from .wignerd import tesseral_wigner_D, wigner_D, tesseral_wigner_D_mirror,\
+                     tesseral_wigner_D_improper
+from .tesseral import tesseral_transformation, permutation_sh_car
+from .atomic_wave_functions import wfcart, wf, wffield, wffield_l
+
+from ._version import __version__
