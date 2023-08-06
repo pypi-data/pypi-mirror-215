a984866df2bd95349e34b1d41b611f57946442b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:39 +0000

    Add package lab-partner/0.7.57/lab-partner-0.7.57.tar.gz

---
+++ packages/lab-partner/lab-partner-0.7.57.tar.gz/lab-partner-0.7.57/install_from_source.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+. ./build.sh
+
+echo "Copying lab-partner wheel from docker image"
+container_id=$(docker create enclarify/lab-partner-packages:$(version))
+docker cp ${container_id}:/opt/lab/dist/init/lab_partner-$(version)-py3-none-any.whl - > /tmp/lab_partner-$(version)-py3-none-any.whl
+docker rm -f ${container_id}
+
+echo "Pushing Docker images to Docker Hub"
+docker push enclarify/lab-partner-cli:$(version)
+#docker push enclarify/lab-partner-jupyter:$(version)
+
+echo "Installing lab-partner on host"
+pip3 install --upgrade --force-reinstall /tmp/lab_partner-$(version)-py3-none-any.whl
+
+#pip3 install --upgrade --force-reinstall lab_partner
