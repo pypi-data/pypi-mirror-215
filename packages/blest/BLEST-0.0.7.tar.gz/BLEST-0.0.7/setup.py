d60d7b7daf6c1a85ec53987ba65d3c39ebb616dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:12 +0000

    Add package blest/0.0.7/BLEST-0.0.7.tar.gz

---
+++ packages/blest/BLEST-0.0.7.tar.gz/BLEST-0.0.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="BLEST",
+    version="0.0.7",
+    author="JHunt",
+    author_email="blest@jhunt.dev",
+    description="The Python reference implementation of BLEST (Batch-able, Lightweight, Encrypted State Transfer), an improved communication protocol for web APIs which leverages JSON, supports request batching and selective returns, and provides a modern alternative to REST.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jhuntdev/blest-py",
+    packages=find_packages(),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "asyncio",
+        "aiohttp",
+        "uuid",
+        "json",
+        "copy",
+        "os",
+        "re"
+    ],
+    python_requires=">=3.6",
+    platforms="any",
+)
