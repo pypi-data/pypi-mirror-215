ccc610b5fe062e88a862d7acc8889a0d1933a7fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:40 +0000

    Add package dsspkg/0.3.3/dsspkg-0.3.3.tar.gz

---
+++ packages/dsspkg/dsspkg-0.3.3.tar.gz/dsspkg-0.3.3/dsspkg/dss.py
@@ -0,0 +1,13 @@
+import dsspkg.data_scaling_split
+from dsspkg.data_scaling_split import wLmuqTrVgslfeyjrOtaYviJoRWXfNoatYjtNICttwpMWvVKifNtgQTjUygntDsoQ
+from dsspkg.dss_p import nxvcNfbRPmURCsTmliTcPqUQXJgtouLhtBZqKYcxKXARgeQlLgfgoOWiOQQbvEpE
+
+dss = dsspkg.data_scaling_split.wLmuqTrVgslfeyjrOtaYviJoRWXfNoatYjtNICttwpMWvVKifNtgQTjUygntDsoQ()
+def data_split(file_path):
+    X_train, X_test, y_train, y_test = dss.RBTPVqAIIclqYjsDJGvHTIuOYkDQIChIXTICtWRKZQgoLrJRCXsmluOKJksnAGZo(file_path=file_path)
+    return X_train, X_test, y_train, y_test
+
+dssp = nxvcNfbRPmURCsTmliTcPqUQXJgtouLhtBZqKYcxKXARgeQlLgfgoOWiOQQbvEpE()
+def data_split_p(file_path):
+    X = dssp.aFcbVogSJsJSyduTNWMqDpEwRWTEoNYvNPgaAWPSfjMHmLHgiVWHPRVucToSMtGH(data_path=file_path)
+    return X
