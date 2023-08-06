96496be2f486c8ad5d03848d4f9ae51bd9219bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:42 +0000

    Add package seypubpypi/1.0/seypubpypi-1.0.tar.gz

---
+++ packages/seypubpypi/seypubpypi-1.0.tar.gz/seypubpypi-1.0/setup.py
@@ -0,0 +1,6 @@
+import setuptools
+
+setuptools.setup(name="seypubpypi",
+                 version="1.0", 
+                 long_description="",
+                 packages=setuptools.find_packages(exclude=["data", "tests"]))
