48fe7cb4eb521eeaa9df19c386d96c1a167b719e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:32 +0000

    Add package mongeasy/0.2.0/mongeasy-0.2.0-py3-none-any.whl

---
+++ packages/mongeasy/mongeasy-0.2.0-py3-none-any.whl/mongeasy/__init__.py
@@ -0,0 +1,22 @@
+"""Top-level package for Mongeasy."""
+
+__author__ = """Joakim Wassberg"""
+__email__ = 'joakim.wassberg@arthead.se'
+__version__ = '0.2.0'
+
+from mongeasy.connection import MongeasyConnection
+from mongeasy.exceptions import MongeasyDBConnectionError
+from mongeasy.dynamics import create_document_class
+from mongeasy.core import Query
+from mongeasy.plugins.registry import PluginRegistry, Hook, plugin_dispatcher
+
+registry = PluginRegistry()
+connection = MongeasyConnection(registry=registry)
+
+
+def connect(connection_str: str=None, db_name: str=None):
+    connection.connect(connection_str, db_name)
+
+def disconnect():
+    connection.disconnect()
+#
