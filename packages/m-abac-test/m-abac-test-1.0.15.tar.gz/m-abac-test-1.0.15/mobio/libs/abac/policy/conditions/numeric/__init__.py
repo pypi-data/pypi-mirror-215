e4abb7b9f4e3c3a4385e43a2d1918b9e42a6da12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:26 +0000

    Add package m-abac-test/1.0.15/m-abac-test-1.0.15.tar.gz

---
+++ packages/m-abac-test/m-abac-test-1.0.15.tar.gz/m-abac-test-1.0.15/mobio/libs/abac/policy/conditions/numeric/__init__.py
@@ -0,0 +1,10 @@
+"""
+    Numeric conditions
+"""
+
+from .eq import EqSchema
+from .gt import GtSchema
+from .gte import GteSchema
+from .lt import LtSchema
+from .lte import LteSchema
+from .neq import NeqSchema
