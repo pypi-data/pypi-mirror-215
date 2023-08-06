43c04b144d2e1229a5f4b130a0d17b7fc7eeeebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:38 +0000

    Add package Digital-Forming-Solution/0.1.12/Digital_Forming_Solution-0.1.12.tar.gz

---
+++ packages/Digital-Forming-Solution/Digital_Forming_Solution-0.1.12.tar.gz/Digital_Forming_Solution-0.1.12/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="Digital_Forming_Solution",
+    version="0.1.12",
+    author="Bharatesha N S",
+    author_email="bharatesha.ns.ext@bayer.com",
+    description="DFS Projects Some of the Packages it help to perform task easily",
+    packages=["Digital_Forming_Solution"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
