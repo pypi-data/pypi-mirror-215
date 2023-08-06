5d8a6c1abc006b12f6c6cb65ff8e818fc43ed0bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:51 +0000

    Add package TestAPackage/0.2/TestAPackage-0.2.tar.gz

---
+++ packages/TestAPackage/TestAPackage-0.2.tar.gz/TestAPackage-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="TestAPackage",
+    version="0.2",
+    description="A publishing test",
+    author = "Test",
+    url="https://github.com/mohamed-fadl-hanalytics/package_test.git",
+    packages=["sum"]
+)
---
+++ packages/TestAPackage/TestAPackage-0.2.tar.gz/TestAPackage-0.2/sum/summing.py
@@ -0,0 +1,14 @@
+
+
+class summ :
+
+    def __init__ (self, a : float, b: float):
+        self.a = a
+        self.b = b
+    
+    def adding(self):
+        return self.a + self.b
+    
+    def printing(self):
+        print(self.a)
+        print(self.b)
