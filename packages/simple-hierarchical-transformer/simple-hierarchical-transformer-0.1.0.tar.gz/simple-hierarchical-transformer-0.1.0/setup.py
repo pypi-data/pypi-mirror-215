4a7a18b0ca2bedb48de83d47d6081f420e66ac04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:10 +0000

    Add package simple-hierarchical-transformer/0.1.0/simple-hierarchical-transformer-0.1.0.tar.gz

---
+++ packages/simple-hierarchical-transformer/simple-hierarchical-transformer-0.1.0.tar.gz/simple-hierarchical-transformer-0.1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'simple-hierarchical-transformer',
+  packages = find_packages(exclude=[]),
+  version = '0.1.0',
+  license='MIT',
+  description = 'Simple Hierarchical Transformer',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/simple-hierarchical-transformer',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformers',
+    'attention mechanism',
+    'hierarchical'
+  ],
+  install_requires=[
+    'accelerate',
+    'einops>=0.4',
+    'local-attention',
+    'torch>=1.6',
+    'vector-quantize-pytorch>=1.6.23'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
