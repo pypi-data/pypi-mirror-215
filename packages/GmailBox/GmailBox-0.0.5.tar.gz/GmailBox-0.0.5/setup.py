9e816de0a9b94d4a8753630a19cc0d0e5af3945e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:39 +0000

    Add package GmailBox/0.0.5/GmailBox-0.0.5.tar.gz

---
+++ packages/GmailBox/GmailBox-0.0.5.tar.gz/GmailBox-0.0.5/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
+
+setuptools.setup(
+	name= "GmailBox",
+	version= "0.0.5",
+	author= "Hamo",
+    keywords=["gmail","mail","GmailBox","inbox"],
+    install_requires=['requests','beautifulsoup4'],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+	description= "With this library, you can create random Gmail and receive messages",
+	packages=setuptools.find_packages(),
+    license="LGPLv3",
+	classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
