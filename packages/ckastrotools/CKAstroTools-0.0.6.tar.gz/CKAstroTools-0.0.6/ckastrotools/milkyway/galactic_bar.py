ccdde3dd662471aa5309285a83705193ee38ee4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:35 +0000

    Add package ckastrotools/0.0.6/CKAstroTools-0.0.6.tar.gz

---
+++ packages/ckastrotools/CKAstroTools-0.0.6.tar.gz/CKAstroTools-0.0.6/ckastrotools/milkyway/galactic_bar.py
@@ -0,0 +1,27 @@
+"""Module to get the position of the Galactic bar of the Milky Way."""
+import numpy as np
+from astropy.coordinates import Galactocentric
+from astropy import units as u
+
+def getGalacticBar(length=6, angle=-50):
+    """
+    Get the position of the Galactic bar of the Milky Way.
+
+    Parameters
+    ----------
+    length:float
+        Length of the bar in kpc.
+    angle:float
+        Angle of the bar in degrees.
+
+    Returns
+    -------
+    bar:astropy.coordinates.Galactocentric
+        The position of the bar.
+    """
+    x = np.linspace(-length/2., length/2., length*10) * np.cos(np.radians(angle)) * u.kpc
+    y = np.linspace(-length/2., length/2., length*10) * np.sin(np.radians(angle)) * u.kpc
+    return Galactocentric(x=x, y=y, z=np.zeros(len(x)) * u.kpc,
+                          galcen_distance=8.3*u.kpc, z_sun=27*u.pc)
+
+
