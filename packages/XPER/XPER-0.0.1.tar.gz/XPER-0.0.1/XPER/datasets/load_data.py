9aaf5f4e9cdce6fa34dd329a278eabd2ffa05574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:20 +0000

    Add package XPER/0.0.1/XPER-0.0.1.tar.gz

---
+++ packages/XPER/XPER-0.0.1.tar.gz/XPER-0.0.1/XPER/datasets/load_data.py
@@ -0,0 +1,25 @@
+import os
+from os.path import join
+import pandas as pd
+import numpy as np
+import sklearn.datasets
+
+def boston():
+    this_dir, this_filename = os.path.split(__file__)
+    DATA_PATH = os.path.join(this_dir,"boston", "BostonHousing.csv")
+    df = pd.read_csv(DATA_PATH)
+    return df
+
+def loan_status():
+    this_dir, this_filename = os.path.split(__file__)
+    DATA_PATH = os.path.join(this_dir,"loan", "Loan_Status.csv")
+    df = pd.read_csv(DATA_PATH)
+    return df
+
+def iris():
+    data = sklearn.datasets.load_iris()
+    return data
+
+def adult():
+    data =  pd.read_csv("adult.data", sep=",")
+    return data
