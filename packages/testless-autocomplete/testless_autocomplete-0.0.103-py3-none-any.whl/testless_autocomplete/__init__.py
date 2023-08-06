c35306a97f8da2fcba8bf95c792e31d0f2eca110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:21 +0000

    Add package testless-autocomplete/0.0.103/testless_autocomplete-0.0.103-py3-none-any.whl

---
+++ packages/testless-autocomplete/testless_autocomplete-0.0.103-py3-none-any.whl/testless_autocomplete/__init__.py
@@ -0,0 +1,13 @@
+from .src.AutoCompleteAPI import (
+   autocomplete
+)
+
+from .src.model import (
+   build_ngram_model,
+   suggest_words
+)
+
+from .src.preprocessing import (
+   preprocess_text,
+   preprocess_data
+)
