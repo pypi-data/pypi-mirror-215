749bac2713059d128e8f89e92cfe037c5e1a1a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:17 +0000

    Add package biblioteca2080/1.0.0/biblioteca2080-1.0.0.tar.gz

---
+++ packages/biblioteca2080/biblioteca2080-1.0.0.tar.gz/biblioteca2080-1.0.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="biblioteca2080",
+    version="1.0.0",
+    author="Saullo",
+    author_email="saullo.guilherme7@unifesspa.edu.br",
+    description="Biblioteca para entender sobre o funcionamento de bibliotecas",
+    packages=setuptools.find_packages(),
+    package_dir={"": "src"},
+    package_data={"biblioteca2080.data": ["*.csv", "*.zip"]}
+)
