698c516297c6c30829ede4f30e53d305f6f5fdb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:16 +0000

    Add package feincms3-downloads/0.5.3/feincms3_downloads-0.5.3-py3-none-any.whl

---
+++ packages/feincms3-downloads/feincms3_downloads-0.5.3-py3-none-any.whl/feincms3_downloads/previews.py
@@ -0,0 +1,36 @@
+import subprocess
+import tempfile
+
+
+def preview_as_jpeg(path):
+    with tempfile.TemporaryDirectory() as directory:
+        preview = "%s/preview.jpg" % directory
+        if path.lower().endswith(".pdf"):
+            cmd = [
+                "pdftocairo",
+                path,
+                "-jpeg",
+                "-singlefile",
+                "-scale-to-x",
+                "300",
+                "-scale-to-y",
+                "-1",
+                preview.replace(".jpg", ""),
+            ]
+        else:
+            cmd = [
+                "convert",
+                "-resize",
+                "300x300>",
+                "-quality",
+                "90",
+                "%s[0]" % path,
+                preview,
+            ]
+
+        # print(cmd)
+        ret = subprocess.call(cmd, env={"PATH": "/usr/local/bin:/usr/bin:/bin"})
+
+        if ret == 0:
+            with open(preview, "rb") as f:
+                return f.read()
