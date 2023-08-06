2c92fca0376b1c248ce6c6212e6f5b864f21f969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:11 +0000

    Add package streamlit-autorefresh/1.0.0/streamlit-autorefresh-1.0.0.tar.gz

---
+++ packages/streamlit-autorefresh/streamlit-autorefresh-1.0.0.tar.gz/streamlit-autorefresh-1.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit-autorefresh",
+    version="1.0.0",
+    author="Ken McGrady",
+    author_email="ken.mcgrady@gmail.com",
+    description="Simple way to autorefresh your Streamlit apps",
+    long_description="Simple way to autorefresh your Streamlit apps",
+    long_description_content_type="text/plain",
+    url="https://github.com/kmcgrady/streamlit-autorefresh",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.75",
+    ],
+)
