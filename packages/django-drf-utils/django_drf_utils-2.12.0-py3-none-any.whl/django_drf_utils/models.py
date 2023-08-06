f18c1ea929dbf9c16edcb166287190dab9b21e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:42 +0000

    Add package django-drf-utils/2.12.0/django_drf_utils-2.12.0-py3-none-any.whl

---
+++ packages/django-drf-utils/django_drf_utils-2.12.0-py3-none-any.whl/django_drf_utils/models.py
@@ -0,0 +1,48 @@
+from django.core import validators
+from django.db import models
+
+
+class CreatedMixin(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+def NameField(**kwargs):
+    unique = kwargs.pop("unique", True)
+    return models.CharField(
+        max_length=512,
+        unique=unique,
+        validators=(
+            validators.RegexValidator(
+                regex=r"^[a-zA-Z0-9\-_ ()ßàÀéÉèÈäÄëËïÏöÖüÜçÇâÂêÊîÎôÔûÛ']*$",
+                message="Allowed characters: "
+                """"a-z", "A-Z", "0-9", "-", "_", "(", ")", "'", """
+                "spaces and diacritics.",
+            ),
+        ),
+        **kwargs,
+    )
+
+
+def CodeField(lower: bool = True, **kwargs):
+    unique = kwargs.pop("unique", True)
+    char_set = "a-z" if lower else "A-Z"
+    return models.CharField(
+        max_length=32,
+        unique=unique,
+        validators=(
+            validators.RegexValidator(
+                regex=rf"^[{char_set}0-9_]*$",
+                message=f'Allowed characters: "{char_set}", "0-9", "_"',
+            ),
+        ),
+        **kwargs,
+    )
+
+
+def EnumField(choices, **kwargs):
+    return models.CharField(
+        max_length=max((len(key) for key, label_ in choices)), choices=choices, **kwargs
+    )
