b3f6404ae98803189bbd75297dcf74bad2c0d989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:52 +0000

    Add package django-bg-task/0.0.2/django_bg_task-0.0.2-py3-none-any.whl

---
+++ packages/django-bg-task/django_bg_task-0.0.2-py3-none-any.whl/django_task/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from django.apps import AppConfig
+
+
+class TaskManagerConfig(AppConfig):
+    name = "task_manager"
