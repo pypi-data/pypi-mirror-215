c54825ec161d6ac8a4e3664eaff6ba50e58e4274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:00 +0000

    Add package modular-diffusion/0.0.0/modular-diffusion-0.0.0.tar.gz

---
+++ packages/modular-diffusion/modular-diffusion-0.0.0.tar.gz/modular-diffusion-0.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='modular-diffusion',
+    version='0.0.0',
+    description='Placeholder package for Modular Diffusion',
+    long_description='This is a placeholder for the Modular Diffusion package. Stay tuned for the actual package.',
+    url='https://github.com/cabralpinto/modular-diffusion',
+    author='João Cabral Pinto',
+    author_email='jmcabralpinto@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    zip_safe=False
+)
