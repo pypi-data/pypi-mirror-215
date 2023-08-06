db35ef190f07829a8a65718cdaec2e4ce981d94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:38 +0000

    Add package pginter/0.2.12/pginter-0.2.12-py3-none-any.whl

---
+++ packages/pginter/pginter-0.2.12-py3-none-any.whl/pginter/widgets/__init__.py
@@ -0,0 +1,6 @@
+from ._supports_children import SupportsChildren
+from ._geo_manager import GeometryManager
+from ._entry import Entry
+from ._button import Button
+from ._frame import Frame
+from ._label import Label
