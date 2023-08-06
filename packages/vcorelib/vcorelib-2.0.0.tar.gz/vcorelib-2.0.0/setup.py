f99bf9b39e76a719f0b3a6f3f9a955c2f284366e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:11 +0000

    Add package vcorelib/2.0.0/vcorelib-2.0.0.tar.gz

---
+++ packages/vcorelib/vcorelib-2.0.0.tar.gz/vcorelib-2.0.0/setup.py
@@ -0,0 +1,41 @@
+# =====================================
+# generator=datazen
+# version=3.1.2
+# hash=2f5e369319178caaf8b067b6862054a7
+# =====================================
+
+"""
+vcorelib - Package definition for distribution.
+"""
+
+# third-party
+try:
+    from setuptools_wrapper.setup import setup
+except (ImportError, ModuleNotFoundError):
+    from vcorelib_bootstrap.setup import setup  # type: ignore
+
+# internal
+from vcorelib import DESCRIPTION, PKG_NAME, VERSION
+
+author_info = {
+    "name": "Vaughn Kottler",
+    "email": "vaughnkottler@gmail.com",
+    "username": "vkottler",
+}
+pkg_info = {
+    "name": PKG_NAME,
+    "slug": PKG_NAME.replace("-", "_"),
+    "version": VERSION,
+    "description": DESCRIPTION,
+    "versions": [
+        "3.7",
+        "3.8",
+        "3.9",
+        "3.10",
+        "3.11",
+    ],
+}
+setup(
+    pkg_info,
+    author_info,
+)
---
+++ packages/vcorelib/vcorelib-2.0.0.tar.gz/vcorelib-2.0.0/vcorelib/__init__.py
@@ -0,0 +1,17 @@
+# =====================================
+# generator=datazen
+# version=3.1.2
+# hash=4f5520f60ed64fc76ac05b90d573bee8
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A collection of core Python utilities."
+PKG_NAME = "vcorelib"
+VERSION = "2.0.0"
+
+# vcorelib-specific content.
+DEFAULT_INDENT = 2
+DEFAULT_ENCODING = "utf-8"
