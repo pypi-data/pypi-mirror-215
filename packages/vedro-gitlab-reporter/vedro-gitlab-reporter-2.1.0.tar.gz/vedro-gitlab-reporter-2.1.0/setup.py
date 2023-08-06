d650595b52f7326c979e04e84f3fb1b046082783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:24 +0000

    Add package vedro-gitlab-reporter/2.1.0/vedro-gitlab-reporter-2.1.0.tar.gz

---
+++ packages/vedro-gitlab-reporter/vedro-gitlab-reporter-2.1.0.tar.gz/vedro-gitlab-reporter-2.1.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+
+
+def find_required():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+def find_dev_required():
+    with open("requirements-dev.txt") as f:
+        return f.read().splitlines()
+
+
+setup(
+    name="vedro-gitlab-reporter",
+    version="2.1.0",
+    description="GitLab reporter with collapsable sections for Vedro framework",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Nikita Tsvetkov",
+    author_email="tsv1@fastmail.com",
+    python_requires=">=3.7",
+    url="https://github.com/vedro-universe/vedro-gitlab-reporter",
+    license="Apache-2.0",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={"vedro_gitlab_reporter": ["py.typed"]},
+    install_requires=find_required(),
+    tests_require=find_dev_required(),
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+    ],
+)
