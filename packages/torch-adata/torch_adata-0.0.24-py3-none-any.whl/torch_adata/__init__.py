5c6f40a13428b11e5b18fdec3bdd0eba72e90afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:04 +0000

    Add package torch-adata/0.0.24/torch_adata-0.0.24-py3-none-any.whl

---
+++ packages/torch-adata/torch_adata-0.0.24-py3-none-any.whl/torch_adata/__init__.py
@@ -0,0 +1,21 @@
+
+__module_name__ = "__init__.py"
+__doc__ = """Main __init__ module."""
+__author__ = ", ".join(["Michael E. Vinyard"])
+__email__ = ", ".join(["vinyard@g.harvard.edu"])
+
+
+# -- specify package version: --------------------------------------------------
+__version__ = "0.0.24"
+
+
+# -- import modules: -----------------------------------------------------------
+from ._core._AnnDataset import AnnDataset
+from ._core._lightning._lightning_anndata_module import LightningAnnDataModule
+from . import _tools as tl
+
+
+# -- import API-hidden core modules (for dev): ---------------------------------
+from ._core import _core_ancilliary as _core
+from . import _utils
+from . import _demo
