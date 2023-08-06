943b0b0b0247c1e0198f89d378c9e6c8f0163bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:02 +0000

    Add package pxutil/0.0.19/pxutil-0.0.19-cp37-cp37m-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pxutil/pxutil-0.0.19-cp37-cp37m-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pxutil/__init__.py
@@ -0,0 +1,12 @@
+"""
+pxutil
+~~~~~
+
+Peter Jiping Xie's personal utils.
+
+:copyright: © 2019 by Peter Jiping Xie.
+:license: MIT.
+"""
+# import func/classes from .pxutil here so users can them directly, i.e. pxutil.bash() instead of pxutl.pxutil.bash().
+from .pxutil import bash, bashx, trim_docstring, grep, purge, time2seconds, replace_in_file, normal_path, exit_on_exception
+# from .cython_pxutil import run_loop, fib
