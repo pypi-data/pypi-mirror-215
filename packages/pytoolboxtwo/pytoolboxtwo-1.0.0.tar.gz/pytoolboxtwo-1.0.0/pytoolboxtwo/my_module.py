2402f83b1ddc2c142d395605f3245bd8893ac324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:47 +0000

    Add package pytoolboxtwo/1.0.0/pytoolboxtwo-1.0.0.tar.gz

---
+++ packages/pytoolboxtwo/pytoolboxtwo-1.0.0.tar.gz/pytoolboxtwo-1.0.0/pytoolboxtwo/my_module.py
@@ -0,0 +1,5 @@
+def greet(name):
+    print(f"Hello, {name}!")
+
+def calculate_square(n):
+    return n * n
---
+++ packages/pytoolboxtwo/pytoolboxtwo-1.0.0.tar.gz/pytoolboxtwo-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='pytoolboxtwo',
+    version='1.0.0',
+    author='soujanya',
+    author_email='soujanyaacharya2001@example.com',
+    description='A description of pytoolboxtwo',
+    url='https://github.com/yourusername/pytoolboxtwo',
+    packages=['pytoolboxtwo'],
+    install_requires=[
+        'numpy',
+        'requests',
+    ],
+)
