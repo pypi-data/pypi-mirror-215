6ed72f261250f005234b72ed164e7a9adbb50de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:27 +0000

    Add package grpcrest/0.12/grpcrest-0.12.tar.gz

---
+++ packages/grpcrest/grpcrest-0.12.tar.gz/grpcrest-0.12/grpcrest/utils.py
@@ -0,0 +1,42 @@
+import platform, os, sys, subprocess
+def identify_platform():
+    path=os.path.realpath(__file__)
+    file_name=os.path.split("/")[-1]
+    print(file_name)
+    path=path.replace(file_name,"")
+    os_name=(platform.platform()).lower()
+    print("OS Details:"+os_name)
+    command_output=""
+    try:
+        if("windows" in os_name and "64" in os_name):
+            path+="grpcurl_1.8.7_windows_x86_64/"
+            print(path)
+            command=path+"grpcurl.exe -help"
+        elif("linux" in os_name and "arm" in os_name):
+            path+="grpcurl_1.8.7_linux_arm64/"
+            print(path)
+            command=path+"grpcurl -help"
+        elif("linux" in os_name and "64" in os_name):
+            path+="grpcurl_1.8.7_linux_x86_64/"
+            print(path)
+            command=path+"grpcurl -help"
+        elif("macos" in os_name):
+            path=''
+            os.system("brew install grpcurl")
+            command="grpcurl -help"
+        else:
+            print("unsupported OS")
+            sys.exit(1)
+        command_output=subprocess.getoutput(command)
+        print(command_output)
+        if("Usage:" in command_output):
+            print("grpcurl resolved successfully")
+        else:
+            path=''
+            print("grpcurl wasn't installed correctly, kindly consider installing grpcurl manually before using this module")
+    except Exception as e:
+        print(e)
+        path=''
+        print("grpcurl wasn't installed correctly, kindly consider installing grpcurl manually before using this module")
+    return path
+
