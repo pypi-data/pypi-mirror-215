3ec895de639cf9eaecc1019ba8dcdaa270b74d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:18 +0000

    Add package lol-esp-api/0.24.3/lol-esp-api-0.24.3.tar.gz

---
+++ packages/lol-esp-api/lol-esp-api-0.24.3.tar.gz/lol-esp-api-0.24.3/src/lol_esp_api/lol_esports_api.py
@@ -0,0 +1,10 @@
+import src.lol_esp_api.apis.eventsAPI as eventsAPI
+import src.lol_esp_api.apis.leaguesAPI as leaguesAPI
+import src.lol_esp_api.apis.matchAPI as matchAPI
+import src.lol_esp_api.apis.teamsAPI as teamsAPI
+
+
+def main():
+    print("Api working")
+
+main()
