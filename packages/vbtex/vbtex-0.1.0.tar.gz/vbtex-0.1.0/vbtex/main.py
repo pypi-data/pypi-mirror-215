643739642ad35ebe7de1d1bd3dd3d633795a2713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:26 +0000

    Add package vbtex/0.1.0/vbtex-0.1.0.tar.gz

---
+++ packages/vbtex/vbtex-0.1.0.tar.gz/vbtex-0.1.0/vbtex/main.py
@@ -0,0 +1,20 @@
+import click
+
+from .framed import framed
+
+
+
+CONTEXT_SETTINGS = dict(
+        help_option_names = [
+            '-h',
+            '--help'
+        ]
+)
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+def main():
+    pass
+
+
+main.add_command(framed)
+
