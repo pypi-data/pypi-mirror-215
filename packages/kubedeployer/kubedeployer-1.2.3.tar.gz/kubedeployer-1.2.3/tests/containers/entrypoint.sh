90be242540ec4f75ea5d951fb6c73c44ddfbc9f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:44 +0000

    Add package kubedeployer/1.2.3/kubedeployer-1.2.3.tar.gz

---
+++ packages/kubedeployer/kubedeployer-1.2.3.tar.gz/kubedeployer-1.2.3/tests/containers/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+prepare_kind() {
+  # Prepare kubernetes cluster for tests
+  chmod +x tests/containers/kind/prepare-kind.sh
+  ./tests/containers/kind/prepare-kind.sh
+}
+
+prepare_infrastructure() {
+  # Create infrastructure
+  kubectl apply -f tests/containers/manifests/rbac.yaml
+  secretname=`kubectl get serviceaccounts deployer -o=jsonpath='{.secrets[0].name}'`
+  secret=`kubectl get secret $secretname -o=jsonpath='{.data.token}' | base64 -d`
+  touch env_vars
+  echo KUBE_TOKEN=$secret >> env_vars
+  echo KUBE_URL="https://${REAL_IP}:6443" >> env_vars
+  cat env_vars
+}
+
+if [[ $VM_IP ]]; then
+  export REAL_IP=$VM_IP
+else
+  export REAL_IP=$(ip route|awk '/default/ { print $3 }')
+fi
+echo REAL_IP $REAL_IP
+
+prepare_kind
+prepare_infrastructure
