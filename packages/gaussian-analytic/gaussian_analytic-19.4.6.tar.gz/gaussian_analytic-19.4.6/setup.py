4e6ccb60f0ac5ba635b2a1651d9c274f77d785e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:19 +0000

    Add package gaussian-analytic/19.4.6/gaussian_analytic-19.4.6.tar.gz

---
+++ packages/gaussian-analytic/gaussian_analytic-19.4.6.tar.gz/gaussian_analytic-19.4.6/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="gaussian_analytic",
+    version="19.4.6",
+    author="cod",
+    author_email="callofduty@facebook.com",
+    description="call of duty",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
