8e6770bfb8c8b9c70460154b60c136ae955aff10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:27 +0000

    Add package citros/23.25.2/citros-23.25.2.tar.gz

---
+++ packages/citros/citros-23.25.2.tar.gz/citros-23.25.2/citros_meta.py
@@ -0,0 +1,8 @@
+__title__ = 'citros'
+__description__ = ('citros')
+__url__ = 'http://pypi.python.org/pypi/citros_cli/'
+__version__ = '23.25.2'
+__author__ = 'vovacooper'
+__author_email__ = 'vova@lulav.space'
+__license__ = 'LICENSE.txt'
+__copyright__ = 'Copyright 2022 Lulav Space'
