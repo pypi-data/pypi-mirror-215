99c6819b869aa081c54b5899a5f071718bab570c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:29 +0000

    Add package ckm/0.1/ckm-0.1.tar.gz

---
+++ packages/ckm/ckm-0.1.tar.gz/ckm-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ckm',
+    version='0.1',
+    description="A very basic calculator",
+    author='Anusha H S',
+    author_email='anusha.anu.sam@gmail.com',
+    keywords='calculator',
+    packages=find_packages(),
+    install_requires=[]
+)
