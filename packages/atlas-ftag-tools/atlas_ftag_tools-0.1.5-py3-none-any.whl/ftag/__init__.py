110491478811905f0179468b54ed2e6a367865ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:05 +0000

    Add package atlas-ftag-tools/0.1.5/atlas_ftag_tools-0.1.5-py3-none-any.whl

---
+++ packages/atlas-ftag-tools/atlas_ftag_tools-0.1.5-py3-none-any.whl/ftag/__init__.py
@@ -0,0 +1,25 @@
+"""atlas-ftag-tools - Common tools for ATLAS flavour tagging software."""
+
+
+__version__ = "v0.1.5"
+
+
+import ftag.hdf5 as hdf5
+from ftag.cuts import Cuts
+from ftag.flavour import Flavour, Flavours
+from ftag.mock import get_mock_file
+from ftag.sample import Sample
+from ftag.wps.discriminant import get_discriminant
+from ftag.wps.working_points import get_working_points
+
+__all__ = [
+    "Cuts",
+    "Flavour",
+    "Flavours",
+    "Sample",
+    "hdf5",
+    "get_mock_file",
+    "get_discriminant",
+    "get_working_points",
+    "__version__",
+]
