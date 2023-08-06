df2f1455adac2247668e896a2183863c810834bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:40 +0000

    Add package judini/0.0.5/judini-0.0.5-py3-none-any.whl

---
+++ packages/judini/judini-0.0.5-py3-none-any.whl/judini/agent.py
@@ -0,0 +1,44 @@
+# Agent
+import requests
+import json
+
+class Agent:
+    def __init__(self, api_key, agent_id):
+        self.api_key = api_key
+        self.agent_id = agent_id
+
+    def set_api_key(self, api_key):
+        self.api_key = api_key
+
+    def set_agent_id(self, agent_id):
+        self.agent_id = agent_id
+
+    def completion(self, prompt):
+        # Headers
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.api_key}"
+        }
+        # Endpoint
+        url = 'https://playground.judini.ai/api/v1/agent/'+ self.agent_id
+        data = {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        }
+        response = requests.post(url, json=data, headers=headers)
+        token = ''
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                raw_data = chunk.decode('utf-8').replace("data: ", '')
+                if raw_data != "[DONE]":
+                    try:
+                        json_object = json.loads(raw_data.strip())
+                        token += json_object['data']
+                    except json.JSONDecodeError as e:
+                        # TODO: fix this error
+                        error= e
+        return token
