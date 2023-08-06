b63209ac8e3620889d21823f763c162b749d89c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:26 +0000

    Add package automation-utilities/1.1.2/automation_utilities-1.1.2-py3-none-any.whl

---
+++ packages/automation-utilities/automation_utilities-1.1.2-py3-none-any.whl/automation_utilities/__init__.py
@@ -0,0 +1,33 @@
+import threading
+import json
+
+
+class Data:
+    """This class was made to deal with data used in automation projects"""
+
+    lock = threading.Lock()
+
+    def __init__(self, file_name: str):
+        self.file_name = file_name
+        self.data = json.load(open(file_name, 'r'))
+
+    def get(self, key):
+        if isinstance(self.data[key], int):
+            Data.lock.acquire()
+            self.data[key] += 1
+            json.dump(self.data, open(self.file_name, 'w'), indent=2)
+            Data.lock.release()
+            return self.data[key] - 1
+        else:
+            return self.data[key]
+
+
+class Files:
+
+    lock = threading.Lock()
+
+    @staticmethod
+    def add(text: str, to: str):
+        Files.lock.acquire()
+        open(to, 'a').write(text)
+        Files.lock.release()
