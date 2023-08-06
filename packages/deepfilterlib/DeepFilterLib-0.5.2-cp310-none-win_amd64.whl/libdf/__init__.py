aa0bc9cbeee7cb1d13ba17ccc3b2cd5beba13a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:23 +0000

    Add package deepfilterlib/0.5.2/DeepFilterLib-0.5.2-cp310-none-win_amd64.whl

---
+++ packages/deepfilterlib/DeepFilterLib-0.5.2-cp310-none-win_amd64.whl/libdf/__init__.py
@@ -0,0 +1,19 @@
+
+
+""""""# start delvewheel patch
+def _delvewheel_init_patch_1_3_8():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'DeepFilterLib.libs'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_init_patch_1_3_8()
+del _delvewheel_init_patch_1_3_8
+# end delvewheel patch
+
+from .libdf import *
+
+__doc__ = libdf.__doc__
+if hasattr(libdf, "__all__"):
+    __all__ = libdf.__all__
