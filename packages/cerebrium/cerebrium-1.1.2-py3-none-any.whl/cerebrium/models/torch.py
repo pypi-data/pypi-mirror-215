6edabb01597c6c7d94cc2843ffbc1e7d4847c844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:21 +0000

    Add package cerebrium/1.1.2/cerebrium-1.1.2-py3-none-any.whl

---
+++ packages/cerebrium/cerebrium-1.1.2-py3-none-any.whl/cerebrium/models/torch.py
@@ -0,0 +1,11 @@
+from torch import tensor, Tensor
+from numpy import ndarray
+from typing import Union, List
+from cerebrium.models.base import BaseModel
+
+
+class TorchModel(BaseModel):
+    def predict(self, input: Union[Tensor, ndarray, List]) -> list:
+        if not isinstance(input, Tensor):
+            input = tensor(input)
+        return self.model(input)
