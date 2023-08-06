3ea50522469d58971d50938b83f8ceff136bbb89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:19 +0000

    Add package blizzardwarcraftapi/0.1.5/blizzardwarcraftapi-0.1.5.tar.gz

---
+++ packages/blizzardwarcraftapi/blizzardwarcraftapi-0.1.5.tar.gz/blizzardwarcraftapi-0.1.5/BlizzardWarcraftAPI/BlizzardWarcraftAPI.py
@@ -0,0 +1,22 @@
+from .modules import *
+
+
+class BlizzardWarcraftAPI(AuctionHouseAPI, AchievementAPI,
+                          ConnectedRealmAPI, CharacterAchievementsAPI,
+                          CharacterAppearanceAPI, CharacterCollectionsAPI,
+                          CharacterEncountersAPI, CharacterEquipmentAPI,
+                          CharacterHunterPetsAPI):
+    """
+    Use BlizzardAuthToken class for generate access token.
+
+    :param str BlizzardAuthToken: Access token.
+    :param str region: The region of the data to retrieve.
+    :param locale: The locale to reflect in localized data.
+
+    :type BlizzardAuthToken: str
+    :type region: str
+    :type locale: str
+    """
+    def __init__(self, BlizzardAuthToken, region: str, locale: str):
+        super().__init__(BlizzardAuthToken, region, locale)
+
---
+++ packages/blizzardwarcraftapi/blizzardwarcraftapi-0.1.5.tar.gz/blizzardwarcraftapi-0.1.5/BlizzardWarcraftAPI/modules/__init__.py
@@ -0,0 +1,12 @@
+from .gameData.AchievementAPI import AchievementAPI
+from .gameData.AuctionHouseAPI import AuctionHouseAPI
+from .gameData.ConnectedRealmAPI import ConnectedRealmAPI
+
+from .profile.CharacterAchievementsAPI import CharacterAchievementsAPI
+from .profile.CharacterAppearanceAPI import CharacterAppearanceAPI
+from .profile.CharacterCollectionsAPI import CharacterCollectionsAPI
+from .profile.CharacterEncountersAPI import CharacterEncountersAPI
+from .profile.CharacterEquipmentAPI import CharacterEquipmentAPI
+from .profile.CharacterHunterPetsAPI import CharacterHunterPetsAPI
+
+from BlizzardWarcraftAPI.BlizzardAuthToken import BlizzardAuthToken
