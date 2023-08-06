bcd10bb14e06ef5ac62baaee9736535b6c8f1a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:33 +0000

    Add package easytree/0.2.0/easytree-0.2.0.tar.gz

---
+++ packages/easytree/easytree-0.2.0.tar.gz/easytree-0.2.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easytree",
+    version="0.2.0",
+    author="David Schenck",
+    author_email="david.schenck@outlook.com",
+    description="A lightweight library designed to easily read and write deeply-nested tree configurations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://easytree.readthedocs.io/en/latest",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+)
