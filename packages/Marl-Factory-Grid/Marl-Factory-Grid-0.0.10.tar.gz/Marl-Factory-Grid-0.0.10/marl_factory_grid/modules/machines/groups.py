1843385320194a0ef368bb0976a48053cee24196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:35 +0000

    Add package Marl-Factory-Grid/0.0.10/Marl-Factory-Grid-0.0.10.tar.gz

---
+++ packages/Marl-Factory-Grid/Marl-Factory-Grid-0.0.10.tar.gz/Marl-Factory-Grid-0.0.10/marl_factory_grid/modules/machines/groups.py
@@ -0,0 +1,13 @@
+from marl_factory_grid.environment.groups.env_objects import EnvObjects
+from marl_factory_grid.environment.groups.mixins import PositionMixin
+from marl_factory_grid.modules.machines.entitites import Machine
+
+
+class Machines(PositionMixin, EnvObjects):
+
+    _entity = Machine
+    is_blocking_light: bool = False
+    can_collide: bool = False
+
+    def __init__(self, *args, **kwargs):
+        super(Machines, self).__init__(*args, **kwargs)
