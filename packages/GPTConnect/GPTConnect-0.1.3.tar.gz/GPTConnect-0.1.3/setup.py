a1be98b80c256a5e5790be727d71489a18e7a168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:41 +0000

    Add package GPTConnect/0.1.3/GPTConnect-0.1.3.tar.gz

---
+++ packages/GPTConnect/GPTConnect-0.1.3.tar.gz/GPTConnect-0.1.3/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name="GPTConnect",
+    version="0.1.3",
+    author="SleepyStew",
+    description="A python package to make GPT functions easy",
+    packages=["gptconnect"],
+)
