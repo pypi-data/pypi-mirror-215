6e16f965e1a89f9459a448f7cd733d478cddce84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:08 +0000

    Add package pivotal-solver/0.0.2/pivotal-solver-0.0.2.tar.gz

---
+++ packages/pivotal-solver/pivotal-solver-0.0.2.tar.gz/pivotal-solver-0.0.2/pivotal/__init__.py
@@ -0,0 +1,6 @@
+from pivotal.api import Variable, minimize, maximize
+from pivotal.errors import Infeasible, Unbounded
+
+
+__version__ = "0.0.2"
+__all__ = ["Variable", "minimize", "maximize", "Infeasible", "Unbounded"]
