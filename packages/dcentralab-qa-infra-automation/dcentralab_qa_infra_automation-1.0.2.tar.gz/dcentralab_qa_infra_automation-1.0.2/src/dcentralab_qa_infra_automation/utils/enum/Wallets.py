65473b00ca8a4fcd1aa2077106df3dcaedf301ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:39 +0000

    Add package dcentralab-qa-infra-automation/1.0.2/dcentralab_qa_infra_automation-1.0.2.tar.gz

---
+++ packages/dcentralab-qa-infra-automation/dcentralab_qa_infra_automation-1.0.2.tar.gz/dcentralab_qa_infra_automation-1.0.2/src/dcentralab_qa_infra_automation/utils/enum/Wallets.py
@@ -0,0 +1,17 @@
+import enum
+
+"""
+wallets types enum 
+
+@Author: Efrat Cohen
+@Date: 02.2023
+"""
+
+
+class Wallet(enum.Enum):
+    """
+    the wallets names needs to be exactly the same as in the site.
+    Pay attention to upper and lower case letters
+    """
+    metamask = 'MetaMask'
+    coinbase = 'Coinbase'
