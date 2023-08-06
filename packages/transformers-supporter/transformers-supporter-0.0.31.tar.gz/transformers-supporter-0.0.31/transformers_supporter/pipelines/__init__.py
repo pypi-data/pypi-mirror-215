535f536b1de27411eae421a720d3abe82f238c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:01 +0000

    Add package transformers-supporter/0.0.31/transformers-supporter-0.0.31.tar.gz

---
+++ packages/transformers-supporter/transformers-supporter-0.0.31.tar.gz/transformers-supporter-0.0.31/transformers_supporter/pipelines/__init__.py
@@ -0,0 +1,9 @@
+from . import image_classification
+from . import tabular_classification
+from . import tabular_regression
+from . import fixed_length_translation
+
+from .image_classification import ImageClassificationPipeline
+from .tabular_classification import TabularClassificationPipeline
+from .tabular_regression import TabularRegressionPipeline
+from .fixed_length_translation import FixedLengthTranslationPipeline
