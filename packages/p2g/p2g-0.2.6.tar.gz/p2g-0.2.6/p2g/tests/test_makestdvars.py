5a94bf7feb2e59f2e6d27071fdbac453986a0054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:37 +0000

    Add package p2g/0.2.6/p2g-0.2.6.tar.gz

---
+++ packages/p2g/p2g-0.2.6.tar.gz/p2g-0.2.6/p2g/tests/test_makestdvars.py
@@ -0,0 +1,24 @@
+import sys
+
+import p2g
+
+
+sys.path.insert(0, "..")
+sys.path.insert(0, "../..")
+
+
+# just code coverage really.
+def test_native_tmpdir_makestdvars0(tmpdir):
+    srcfile = str(tmpdir) + "/def.py"
+    with open(srcfile, "w", encoding="utf-8") as of:
+        of.writelines([" # MACHINE GEN BELOW", " # MACHINE GEN ABOVE"])
+
+    p2g.main(
+        [
+            "stdvars",
+            "--py=" + srcfile,
+            "--dev=" + str(tmpdir / "ignore2"),
+            "--txt=" + str(tmpdir / "ignore3"),
+            "--org=" + str(tmpdir / "ignore4"),
+        ]
+    )
