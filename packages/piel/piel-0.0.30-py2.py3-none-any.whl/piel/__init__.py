4130dd0f63b28a0ef3eb119ea88f49aba652be2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:24 +0000

    Add package piel/0.0.30/piel-0.0.30-py2.py3-none-any.whl

---
+++ packages/piel/piel-0.0.30-py2.py3-none-any.whl/piel/__init__.py
@@ -0,0 +1,18 @@
+"""Top-level package for piel."""
+import os
+import pathlib
+
+from .cocotb import *
+from .defaults import *
+from .file_system import *
+from .integration import *
+from .openlane import *
+from .parametric import *
+
+os.environ["PIEL_PACKAGE_DIRECTORY"] = str(
+    pathlib.Path(__file__).parent.parent.resolve()
+)
+
+__author__ = """Dario Quintero"""
+__email__ = "darioaquintero@gmail.com"
+__version__ = "0.0.30"
