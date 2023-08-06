27bdbdcb294f64bfe658b55cb470683f9377bfd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:42 +0000

    Add package WebtoonScraper/0.1.2/WebtoonScraper-0.1.2-py3-none-any.whl

---
+++ packages/WebtoonScraper/WebtoonScraper-0.1.2-py3-none-any.whl/WebtoonScraper/__init__.py
@@ -0,0 +1,12 @@
+from WebtoonScraper.NaverWebtoonScraper import NaverWebtoonScraper
+from WebtoonScraper.foldermanagement import WebtoonFolderManagement
+from WebtoonScraper.WebtoonOriginalsScraper import WebtoonOriginalsScraper
+from WebtoonScraper.BestChallengeScraper import BestChallengeScraper
+from WebtoonScraper.WebtoonCanvasScraper import WebtoonCanvasScraper
+from WebtoonScraper import Webtoon
+from WebtoonScraper.TelescopeScraper import TelescopeScraper
+from WebtoonScraper.BufftoonScraper import BufftoonScraper
+from WebtoonScraper.NaverPostScraper import NaverPostScraper
+
+if __name__ == '__main__':
+    print('Testing codes.')
