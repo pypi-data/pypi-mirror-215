d91cb21fc6fc46b2df2413bbc73aca68fc265a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:40 +0000

    Add package cbpi4-iSpindle/0.0.13/cbpi4-iSpindle-0.0.13.tar.gz

---
+++ packages/cbpi4-iSpindle/cbpi4-iSpindle-0.0.13.tar.gz/cbpi4-iSpindle-0.0.13/README.md
@@ -0,0 +1,44 @@
+# CraftBeerPi4 iSpindle Sensor Plugin
+
+- Initial Version which has to be considered as beta
+    - Data Transfer is working from the SPindle itself and from the TCP server via forward-
+    - TCP Server does not require any changes. Only the port needs to be changed from 5000 to 8000
+    - The Spindel Connection needs to be changed from CraftbeerPi to HTTP as shown in the image below
+    - For Server, please enter the IP address of your CBPi4 Server.
+    - Enter 8000 for Port and '/api/hydrometer/v1/data' for path.
+
+![iSpindle Settings](https://github.com/avollkopf/cbpi4-iSpindle/blob/main/Spindle_Connection_Settings.png?raw=true)
+
+- Installation: 
+    - sudo pip3 install cbpi4-iSpindle (or from the GIT repo)
+
+- Usage:
+    - Add Hardware under Sensor and choose iSpindle as Type
+
+- Units are currently not displayed in CBPi4 for Sensors. You need to add that on you own in the Dashboard
+- If you want to retrieve multiple parameters from one Spindle, you need to add the Spindle several times as sensor and choose different Types
+    - The Sensor name should be different for each of your sensors
+- The settings are shown below.
+    - Name: This is the name of your sensor shown in CBPi4. e.g. iSpindleXXX - Angle
+    - iSpindle: Name of your Spindle that you entered in the Spindle setup page. This name must match to the Spindle. Otherwise no data will be collected.
+    - Type: Temperature, Gravity/Angle, Battery (Choose one type)
+        - If Gravity/Angle is choosen and the Polynomial field is left empty, the angle data is stored
+    - Polynomial: The polynomial to calculate gravity. '^' is not working. You need to use 'tilt\*tilt\*tilt' for '^3' and 'tilt*tilt' for '^2'
+    - Units: Choose SG, Brix or °P. This has impact on the calculated digits
+
+![CBPi4 Settings](https://github.com/avollkopf/cbpi4-iSpindle/blob/main/Settings.png?raw=true)
+
+### Changelog:
+
+- 25.06.23: (0.0.13) change logging from 'warning' to 'info'
+- 10.06.23: (0.0.12) bump to release
+- 06.03.23: (0.0.12.a2) Added DataType for datetime (TEST)
+- XX.XX.XX: (0.0.11) ???
+- 11.05.22: (0.0.10) Updated README (removed cbpi add)
+- 10.05.22: (0.0.9) Removed cbpi dependency
+- 16.01.22: (0.0.8) Adaption for cbpi 4.0.1.2
+- 13.01.22: (0.0.7) Reduced mqtt traffic -> update only for new value
+- 23.11.21: (0.0.6) Added RSSI to available parmeters
+- 10.06.21: (0.0.5) Fix to improve UI behavior with respect to frequency of value update
+- 17.04.21: Some small fixes
+- 15.03.21: Changes to suppoer cbpi >= 4.0.0.31
---
+++ packages/cbpi4-iSpindle/cbpi4-iSpindle-0.0.13.tar.gz/cbpi4-iSpindle-0.0.13/cbpi4-iSpindle/__init__.py
@@ -0,0 +1,169 @@
+
+# -*- coding: utf-8 -*-
+import os
+from aiohttp import web
+import logging
+from unittest.mock import MagicMock, patch
+import asyncio
+import random
+from cbpi.api import *
+from aiohttp import web
+from cbpi.api import *
+import re
+import time
+import json
+from cbpi.api.dataclasses import DataType
+
+logger = logging.getLogger(__name__)
+
+cache = {}
+
+async def calcGravity(polynom, tilt, unitsGravity):
+	if unitsGravity == "SG":
+		rounddec = 3
+	else:
+		rounddec = 2
+
+	# Calculate gravity from polynomial
+	tilt = float(tilt)
+	result = eval(polynom)
+	result = round(float(result),rounddec)
+	return result
+
+@parameters([Property.Text(label="iSpindle", configurable=True, description="Enter the name of your iSpindel"),
+             Property.Select("Type", options=["Temperature", "Gravity/Angle", "Battery", "RSSI", "DateTime"], description="Select which type of data to register for this sensor. For Angle, Polynomial has to be left empty"),
+             Property.Text(label="Polynomial", configurable=True, description="Enter your iSpindel polynomial. Use the variable tilt for the angle reading from iSpindel. Does not support ^ character."),
+             Property.Select("Units", options=["SG", "Brix", "°P"], description="Displays gravity reading with this unit if the Data Type is set to Gravity. Does not convert between units, to do that modify your polynomial."),
+             Property.Sensor("FermenterTemp",description="Select Fermenter Temp Sensor that you want to provide to TCP Server")])
+
+class iSpindle(CBPiSensor):
+    
+    def __init__(self, cbpi, id, props):
+        super(iSpindle, self).__init__(cbpi, id, props)
+        self.value = 0
+        self.key = self.props.get("iSpindle", None)
+        self.Polynomial = self.props.get("Polynomial", "tilt")
+        self.temp_sensor_id = self.props.get("FermenterTemp", None)
+        self.datatype = DataType.DATETIME if self.props.get("Type", None) == "DateTime" else DataType.VALUE
+        self.time_old = 0
+
+    def get_unit(self):
+        if self.props.get("Type") == "Temperature":
+            return "°C" if self.get_config_value("TEMP_UNIT", "C") == "C" else "°F"
+        elif self.props.get("Type") == "Gravity/Angle":
+            return self.props.Units
+        elif self.props.get("Type") == "Battery":
+            return "V"
+        elif self.props.get("Type") == "RSSI":
+            return "dB"
+        else:
+            return " "
+
+    async def run(self):
+        global cache
+        global fermenter_temp
+        Spindle_name = self.props.get("iSpindle") 
+        while self.running == True:
+            try:
+                if (float(cache[self.key]['Time']) > float(self.time_old)):
+                    self.time_old = float(cache[self.key]['Time'])
+                    if self.props.get("Type") == "Gravity/Angle":
+                        self.value = await calcGravity(self.Polynomial, cache[self.key]['Angle'], self.props.get("Units"))
+                    elif self.props.get("Type") == "DateTime":
+                        self.value=float(cache[self.key]['Time'])
+                    else:
+                        self.value = float(cache[self.key][self.props.Type])
+                    self.log_data(self.value)
+                    self.push_update(self.value)
+                self.push_update(self.value,False)
+                #self.cbpi.ws.send(dict(topic="sensorstate", id=self.id, value=self.value))
+                
+            except Exception as e:
+                pass
+            await asyncio.sleep(2)
+
+    def get_state(self):
+        return dict(value=self.value)
+
+class iSpindleEndpoint(CBPiExtension):
+    
+    def __init__(self, cbpi):
+        '''
+        Initializer
+        :param cbpi:
+        '''
+        self.pattern_check = re.compile("^[a-zA-Z0-9,.]{0,10}$")
+        self.cbpi = cbpi
+        self.sensor_controller : SensorController = cbpi.sensor
+        # register component for http, events
+        # In addtion the sub folder static is exposed to access static content via http
+        self.cbpi.register(self, "/api/hydrometer/v1/data")
+
+    async def run(self):
+        await self.get_spindle_sensor()
+
+    @request_mapping(path='', method="POST", auth_required=False)
+    async def http_new_value3(self, request):
+        import time
+        """
+        ---
+        description: Get iSpindle Value
+        tags:
+        - iSpindle 
+        parameters:
+        - name: "data"
+          in: "body"
+          description: "Data"
+          required: "name"
+          type: "object"
+          type: string
+        responses:
+            "204":
+                description: successful operation
+        """
+
+        global cache
+        try:
+            data = await request.json()
+        except Exception as e:
+            print(e)
+        logging.info(data)
+        time = time.time()
+        key = data['name']
+        temp = round(float(data['temperature']), 2)
+        angle = data['angle']
+        battery = data['battery']
+        try:
+            rssi = data['RSSI']
+        except:
+            rssi = 0
+        cache[key] = {'Time': time,'Temperature': temp, 'Angle': angle, 'Battery': battery, 'RSSI': rssi}
+
+
+    @request_mapping(path='/gettemp/{SpindleID}', method="POST", auth_required=False)
+    async def get_fermenter_temp(self, request):
+        SpindleID = request.match_info['SpindleID']
+        sensor_value = await self.get_spindle_sensor(SpindleID)
+        data = {'Temp': sensor_value}
+        return  web.json_response(data=data)
+
+    async def get_spindle_sensor(self, iSpindleID = None):
+        self.sensor = self.sensor_controller.get_state()
+        for id in self.sensor['data']:
+            if id['type'] == 'iSpindle':
+                name= id['props']['iSpindle']
+                if name == iSpindleID:
+                    try:
+                        sensor= id['props']['FermenterTemp']
+                    except:
+                        sensor = None
+                    if (sensor is not None) and sensor != "":
+                        sensor_value = self.cbpi.sensor.get_sensor_value(sensor).get('value')
+                    else:
+                        sensor_value = None
+                    return sensor_value
+
+def setup(cbpi):
+    cbpi.plugin.register("iSpindle", iSpindle)
+    cbpi.plugin.register("iSpindleEndpoint", iSpindleEndpoint)
+    pass
