5e584db2caadd3004ea4fbd99d9b94c20a82baa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:41 +0000

    Add package curcheck/1.1.3b0/curcheck-1.1.3b0.tar.gz

---
+++ packages/curcheck/curcheck-1.1.3b0.tar.gz/curcheck-1.1.3b0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('readme.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='curcheck',
+    version='1.1.3b',
+    description='Library for parsing SPA and MPA sites',
+    packages=['curcheck'],
+    author="BulatXam",
+    author_email='Khamdbulat@yandex.ru',
+    zip_safe=False,
+    install_requires=[
+        "pyppeteer",
+        "aiohttp",
+        "lxml",
+    ],
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
