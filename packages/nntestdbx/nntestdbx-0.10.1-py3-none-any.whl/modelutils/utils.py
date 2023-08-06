27d84d1aac657ebf70e933b7b40f824a3a2a36b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:08 +0000

    Add package nntestdbx/0.10.1/nntestdbx-0.10.1-py3-none-any.whl

---
+++ packages/nntestdbx/nntestdbx-0.10.1-py3-none-any.whl/modelutils/utils.py
@@ -0,0 +1,8 @@
+class TextUtils:
+    @staticmethod
+    def capitalize_string(s: str) -> str:
+        return s.upper()
+
+    @staticmethod
+    def lower_string(s: str) -> str:
+        return s.lower()
