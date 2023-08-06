43834a353b3acef1545368a6192c7bc08ab97597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:04 +0000

    Add package Gaon/0.1/Gaon-0.1.tar.gz

---
+++ packages/Gaon/Gaon-0.1.tar.gz/Gaon-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Gaon',
+    version='0.1',
+    packages=find_packages(),
+    license='MIT',
+    description='AI library for Sidedrawer',
+    long_description=open('README.md').read(),
+    install_requires=['pdf2image', 'opencv-python', 'pytesseract', 'numpy', 'pandas', 'scikit-learn'], # add other required packages
+    author='Gaon.ai',
+    author_email='support@gaon.ai'
+)
