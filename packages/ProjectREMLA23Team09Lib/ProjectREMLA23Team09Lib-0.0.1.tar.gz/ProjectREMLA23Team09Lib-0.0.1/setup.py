df3e2e9b449777278a422edab11fc36369138d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:07 +0000

    Add package ProjectREMLA23Team09Lib/0.0.1/ProjectREMLA23Team09Lib-0.0.1.tar.gz

---
+++ packages/ProjectREMLA23Team09Lib/ProjectREMLA23Team09Lib-0.0.1.tar.gz/ProjectREMLA23Team09Lib-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import setuptools
+from src.VersionUtil import VersionUtil
+
+
+setup(
+    name='ProjectREMLA23Team09Lib',
+    version=VersionUtil.get_version(),
+    description='A version-aware library',
+    author='Zhiyong Zhu',
+    author_email='z13913982280@gmail.com',
+    url='https://github.com/remla23-team09/lib',
+    license='MIT',
+    long_description="ProjectREMLA23Team09Lib",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+    ],
+)
