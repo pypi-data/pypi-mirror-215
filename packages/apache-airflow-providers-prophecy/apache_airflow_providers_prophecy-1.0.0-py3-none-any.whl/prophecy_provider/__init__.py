c88c4bcbc29fb9b028deae0f46cdcf50ab4cd66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:51 +0000

    Add package apache-airflow-providers-prophecy/1.0.0/apache_airflow_providers_prophecy-1.0.0-py3-none-any.whl

---
+++ packages/apache-airflow-providers-prophecy/apache_airflow_providers_prophecy-1.0.0-py3-none-any.whl/prophecy_provider/__init__.py
@@ -0,0 +1,7 @@
+def get_provider_info():
+    return {
+        "package-name": "apache-airflow-providers-prophecy",
+        "name": "Prophecy Airflow Provider",
+        "description": "Prophecy provider for Apache Airflow",
+        "versions": ["1.0.0"],
+    }
