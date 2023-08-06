70b3246e87e42309697f634fbd5c98cd59124eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:47 +0000

    Add package openai-functions/0.5.1/openai_functions-0.5.1.tar.gz

---
+++ packages/openai-functions/openai_functions-0.5.1.tar.gz/openai_functions-0.5.1/openai_functions/__init__.py
@@ -0,0 +1,23 @@
+"""ChatGPT function calling based on function docstrings."""
+from .conversation import Conversation
+from .functions import (
+    BasicFunctionSet,
+    FunctionNotFoundError,
+    FunctionSet,
+    FunctionWrapper,
+    WrapperConfig,
+)
+from .openai_types import Message
+from .parsers import ArgSchemaParser, defargparsers
+
+__all__ = [
+    "Conversation",
+    "BasicFunctionSet",
+    "FunctionNotFoundError",
+    "FunctionSet",
+    "defargparsers",
+    "ArgSchemaParser",
+    "FunctionWrapper",
+    "Message",
+    "WrapperConfig",
+]
