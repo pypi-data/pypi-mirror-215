37555b55fa189bac78f04d015149b9025facb9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:40 +0000

    Add package niapi/0.4.0/niapi-0.4.0-py3-none-any.whl

---
+++ packages/niapi/niapi-0.4.0-py3-none-any.whl/niapi/domain/urls.py
@@ -0,0 +1,13 @@
+"""NIAPI Domain URLs."""
+from __future__ import annotations
+
+from typing import Final
+
+INDEX: Final = "/"
+"""Index URL."""
+SITE_ROOT: Final = "/{path:str}"
+"""Site root URL."""
+OPENAPI_SCHEMA: Final = "/api"
+"""OpenAPI schema URL."""
+SYSTEM_HEALTH: Final = "/health"
+"""System health URL."""
