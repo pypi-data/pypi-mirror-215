fa3b54ecc69fead47a97aa5d5045b9f4acfc2978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:44 +0000

    Add package lollms/2.0.6/lollms-2.0.6-py3-none-any.whl

---
+++ packages/lollms/lollms-2.0.6-py3-none-any.whl/lollms/bindings_zoo/py_llama_cpp/__init__.py
@@ -0,0 +1,156 @@
+######
+# Project       : lollms
+# File          : binding.py
+# Author        : ParisNeo with the help of the community
+# Supported by Nomic-AI
+# license       : Apache 2.0
+# Description   : 
+# This is an interface class for lollms bindings.
+
+# This binding is a wrapper to abdeladim's binding
+# Follow him on his github project : https://github.com/abdeladim-s/pyllamacpp
+
+######
+from pathlib import Path
+from typing import Callable
+from lollms.config import BaseConfig, TypedConfig, ConfigTemplate, InstallOption
+from lollms.paths import LollmsPaths
+from lollms.binding import LLMBinding, LOLLMSConfig
+from lollms.helpers import ASCIIColors
+from lollms.types import MSG_TYPE
+import subprocess
+import yaml
+import re
+
+
+
+__author__ = "parisneo"
+__github__ = "https://github.com/ParisNeo/lollms_bindings_zoo"
+__copyright__ = "Copyright 2023, "
+__license__ = "Apache 2.0"
+
+binding_name = "PyLLAMACPP"
+binding_folder_name = "py_llama_cpp"
+
+class PyLLAMACPP(LLMBinding):
+    file_extension='*.bin'
+    def __init__(self, 
+                config: LOLLMSConfig, 
+                lollms_paths: LollmsPaths = LollmsPaths(), 
+                installation_option:InstallOption=InstallOption.INSTALL_IF_NECESSARY
+                ) -> None:
+        """
+        Initialize the Binding.
+
+        Args:
+            config (LOLLMSConfig): The configuration object for LOLLMS.
+            lollms_paths (LollmsPaths, optional): The paths object for LOLLMS. Defaults to LollmsPaths().
+            installation_option (InstallOption, optional): The installation option for LOLLMS. Defaults to InstallOption.INSTALL_IF_NECESSARY.
+        """
+        # Initialization code goes here
+
+        binding_config = TypedConfig(
+            ConfigTemplate([
+                {"name":"gpu_layers","type":"int","value":20, "min":0},
+                {"name":"use_avx2","type":"bool","value":True}
+            ]),
+            BaseConfig(config={
+                "use_avx2": True,     # use avx2
+                "gpu_layers": 20       #number of layers top offload to gpu                
+            })
+        )
+        super().__init__(
+                            Path(__file__).parent, 
+                            lollms_paths, 
+                            config, 
+                            binding_config, 
+                            installation_option
+                        )
+    def build_model(self):        
+        model_path = self.get_model_path()
+
+        from pyllamacpp.model import Model
+
+        self.model = Model(
+                model_path=str(model_path),
+                prompt_context="", prompt_prefix="", prompt_suffix="",
+                n_ctx=self.config['ctx_size'], 
+                seed=self.config['seed'],
+                )
+        return self.model
+
+    def install(self):
+        super().install()
+        requirements_file = self.binding_dir / "requirements.txt"
+        # install requirements
+        subprocess.run(["pip", "install", "--upgrade", "--no-cache-dir", "-r", str(requirements_file)])        
+        ASCIIColors.success("Installed successfully")
+
+    def tokenize(self, prompt:str):
+        """
+        Tokenizes the given prompt using the model's tokenizer.
+
+        Args:
+            prompt (str): The input prompt to be tokenized.
+
+        Returns:
+            list: A list of tokens representing the tokenized prompt.
+        """
+        return self.model.tokenize(prompt)
+
+    def detokenize(self, tokens_list:list):
+        """
+        Detokenizes the given list of tokens using the model's tokenizer.
+
+        Args:
+            tokens_list (list): A list of tokens to be detokenized.
+
+        Returns:
+            str: The detokenized text as a string.
+        """
+        return self.model.detokenize(tokens_list)
+    
+    def generate(self, 
+                 prompt:str,                  
+                 n_predict: int = 128,
+                 callback: Callable[[str], None] = bool,
+                 verbose: bool = False,
+                 **gpt_params ):
+        """Generates text out of a prompt
+
+        Args:
+            prompt (str): The prompt to use for generation
+            n_predict (int, optional): Number of tokens to prodict. Defaults to 128.
+            callback (Callable[[str], None], optional): A callback function that is called everytime a new text element is generated. Defaults to None.
+            verbose (bool, optional): If true, the code will spit many informations about the generation process. Defaults to False.
+        """
+        try:
+            self.model.reset()
+            output = ""
+            for tok in self.model.generate(prompt, 
+                                           n_predict=n_predict,                                           
+                                            temp=gpt_params['temperature'],
+                                            top_k=gpt_params['top_k'],
+                                            top_p=gpt_params['top_p'],
+                                            repeat_penalty=gpt_params['repeat_penalty'],
+                                            repeat_last_n = self.config['repeat_last_n'],
+                                            n_threads=self.config['n_threads'],
+                                           ):
+                output += tok
+                if callback is not None:
+                    if not callback(tok, MSG_TYPE.MSG_TYPE_CHUNK):
+                        return output
+        except Exception as ex:
+            print(ex)
+        return output
+            
+    @staticmethod
+    def get_available_models():
+        # Create the file path relative to the child class's directory
+        binding_path = Path(__file__).parent
+        file_path = binding_path/"models.yaml"
+
+        with open(file_path, 'r') as file:
+            yaml_data = yaml.safe_load(file)
+        
+        return yaml_data
