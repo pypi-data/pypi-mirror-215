6d6b8ff260617209640ab8bbf09d5adcfb1f8419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:33 +0000

    Add package BoxKit/2023.6/BoxKit-2023.6.tar.gz

---
+++ packages/BoxKit/BoxKit-2023.6.tar.gz/BoxKit-2023.6/boxkit/__meta__.py
@@ -0,0 +1,9 @@
+"""Metadata for BoxKit package"""
+
+__pkgname__ = "BoxKit"
+__version__ = "2023.06"
+__authors__ = "Akash Dhruv"
+__license__ = "Apache License"
+__copyright__ = "Copyright (c) Akash Dhruv 2023. All Rights Reserved."
+__description__ = "BoxKit utility"
+
---
+++ packages/BoxKit/BoxKit-2023.6.tar.gz/BoxKit-2023.6/boxkit/options.py
@@ -0,0 +1,7 @@
+"""Module with implementations for variables to set options"""
+CBOX = 1
+PYARROW = 0
+ZARR = 1
+DASK = 1
+SERVER = 1
+TESTING = 1
