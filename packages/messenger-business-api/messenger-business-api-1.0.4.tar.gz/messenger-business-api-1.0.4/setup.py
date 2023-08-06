c2ecaf05a157f254dca7659d2c2ed2c3634b2ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:18 +0000

    Add package messenger-business-api/1.0.4/messenger-business-api-1.0.4.tar.gz

---
+++ packages/messenger-business-api/messenger-business-api-1.0.4.tar.gz/messenger-business-api-1.0.4/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import find_packages, setup
+
+with open('README.rst') as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name="messenger-business-api",
+    version="1.0.4",
+    author="Yuri Ramos Lima",
+    author_email="yurilima93@hotmail.com",
+    description="This repository is a wrapper for Meta messenger api for Messenger and Instagram",
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=README,
+    install_requires=[ "pydantic", "requests"],
+    license="MIT License",
+    keywords=["python","Meta", "Facebook", "Instagram", "message", "chat-bot"],
+   
+)
