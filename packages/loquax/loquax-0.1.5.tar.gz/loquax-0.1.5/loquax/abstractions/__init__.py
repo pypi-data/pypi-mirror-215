daff957aa447c1ce45b585db50c476e72e61b7ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:43 +0000

    Add package loquax/0.1.5/loquax-0.1.5.tar.gz

---
+++ packages/loquax/loquax-0.1.5.tar.gz/loquax-0.1.5/loquax/abstractions/__init__.py
@@ -0,0 +1,12 @@
+from .linguistic_entities import (
+    Language,
+    Phoneme,
+    Syllable,
+    Tokenizer,
+    PhonemeSyllabificationRuleStore,
+    MorphismStore,
+    Morphism,
+    RuleSequence,
+    Rule,
+    Constants,
+)
