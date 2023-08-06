f31c65ea4875b9372806530cf7073db7b93de241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:49 +0000

    Add package ledfx/2.0.69/ledfx-2.0.69-py3-none-any.whl

---
+++ packages/ledfx/ledfx-2.0.69-py3-none-any.whl/ledfx/effects/blocks.py
@@ -0,0 +1,40 @@
+import numpy as np
+import voluptuous as vol
+
+from ledfx.effects.audio import AudioReactiveEffect
+from ledfx.effects.gradient import GradientEffect
+
+
+class BlocksAudioEffect(AudioReactiveEffect, GradientEffect):
+    NAME = "Blocks"
+    CATEGORY = "2D"
+
+    CONFIG_SCHEMA = vol.Schema(
+        {
+            vol.Optional(
+                "block_count",
+                description="Number of color blocks",
+                default=4,
+            ): vol.All(vol.Coerce(int), vol.Range(min=1, max=10))
+        }
+    )
+
+    def on_activate(self, pixel_count):
+        self.r = np.zeros(pixel_count)
+
+    def audio_data_updated(self, data):
+        # Grab the filtered melbank
+        self.r = self.melbank(filtered=True, size=self.pixel_count)
+
+    def render(self):
+        out = np.tile(self.r, (3, 1))
+        out_split = np.array_split(out, self._config["block_count"], axis=1)
+        for i in range(self._config["block_count"]):
+            color = self.get_gradient_color(i / self._config["block_count"])[
+                :, np.newaxis
+            ]
+            out_split[i] = np.multiply(
+                out_split[i], (out_split[i].max() * color)
+            )
+
+        self.pixels = np.hstack(out_split).T
---
+++ packages/ledfx/ledfx-2.0.69-py3-none-any.whl/ledfx_frontend/messages.md
@@ -0,0 +1,8 @@
++ Release 2.0.69
++ Add OpenRGB Devices and Launchpad to Assistant
++ Workflow file
++ Add workflow file for open-source repository
++ Adjust some styles and settings
++ Add some features to Melbank DevGraph
++ visibility logic for startup assistant
++ Show Intro only when no devices in config
