2138772f370a30dc1a980c0d4f56cdfc4aa62236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:18 +0000

    Add package pin-pink/1.0.0/pin_pink-1.0.0-py3-none-any.whl

---
+++ packages/pin-pink/pin_pink-1.0.0-py3-none-any.whl/pink/tasks/__init__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2022 Stéphane Caron
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Kinematic tasks."""
+
+from .exceptions import TargetNotSet, TaskJacobianNotSet
+from .frame_task import FrameTask
+from .joint_coupling_task import JointCouplingTask
+from .linear_holonomic_task import LinearHolonomicTask
+from .posture_task import PostureTask
+from .task import Task
+
+__all__ = [
+    "FrameTask",
+    "TargetNotSet",
+    "TaskJacobianNotSet",
+    "JointCouplingTask",
+    "LinearHolonomicTask",
+    "PostureTask",
+    "Task",
+]
