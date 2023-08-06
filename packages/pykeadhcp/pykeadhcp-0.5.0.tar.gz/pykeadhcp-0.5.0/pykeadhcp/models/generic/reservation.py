8bcc721455b4585b5d2258274011312f1ae99421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:02 +0000

    Add package pykeadhcp/0.5.0/pykeadhcp-0.5.0.tar.gz

---
+++ packages/pykeadhcp/pykeadhcp-0.5.0.tar.gz/pykeadhcp-0.5.0/pykeadhcp/models/generic/reservation.py
@@ -0,0 +1,12 @@
+from typing import Optional, List
+from pykeadhcp.models.generic.base import KeaModel
+from pykeadhcp.models.generic.option_data import OptionData
+
+
+class Reservation(KeaModel):
+    duid: Optional[str]
+    client_classes: Optional[List[str]] = []
+    flex_id: Optional[str]
+    hw_address: Optional[str]
+    hostname: Optional[str]
+    option_data: Optional[List[OptionData]] = []
