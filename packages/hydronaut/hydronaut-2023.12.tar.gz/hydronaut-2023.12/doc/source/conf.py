29ae348d12a53fdeff90b21ba39d951f6dda53fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:19 +0000

    Add package hydronaut/2023.12/hydronaut-2023.12.tar.gz

---
+++ packages/hydronaut/hydronaut-2023.12.tar.gz/hydronaut-2023.12/doc/source/conf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+'''
+Sphinx configuration file.
+'''
+
+# pylint: disable=invalid-name,redefined-builtin
+
+import importlib
+import pathlib
+import sys
+
+source_code = '../../src'
+git_url = 'https://gitlab.inria.fr/jrye/hydronaut'
+
+this_path = pathlib.Path(__file__).resolve()
+sys.path.insert(0, str((this_path.parent / source_code).resolve()))
+
+author = 'Jan-Michael Rye'
+copyright = '2022, Inria'
+project = 'Hydronaut'
+html_theme = 'sphinx_rtd_theme'
+
+autodoc_mock_imports = [
+    'hydra',
+    'mlflow',
+    'omegaconf',
+    'pytorch_lightning',
+    'torch'
+]
+extensions = [
+  'sphinx.ext.autodoc',
+  'sphinx.ext.napoleon',
+  'sphinx.ext.linkcode',
+  'sphinx.ext.todo',
+  'myst_parser'
+]
+
+
+def linkcode_resolve(domain, info):
+    '''Get source links for the linkcode extension.'''
+    module = info['module']
+    if domain != 'py' or not module:
+        return None
+    top_mod = importlib.import_module(module.split('.')[0])
+    mod = importlib.import_module(module)
+    top_mod_path = pathlib.Path(top_mod.__file__)
+    mod_path = pathlib.Path(mod.__file__)
+    subpath = str(mod_path.relative_to(top_mod_path.parent.parent))
+    return f'{git_url}/-/blob/main/src/{subpath}'
---
+++ packages/hydronaut/hydronaut-2023.12.tar.gz/hydronaut-2023.12/examples/pytorch/lighting-mnist/src/experiment.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+'''
+MNIST experiment.
+'''
+
+import logging
+
+from omegaconf import DictConfig
+
+from hydronaut.torch.lightning.experiment import PLExperiment
+
+# pylint: disable=wrong-import-order
+from model import MNISTClassifier
+from data import MNISTDataModule
+
+LOGGER = logging.getLogger(__name__)
+
+
+class MNISTExperiment(PLExperiment):  # pylint: disable=too-few-public-methods
+    '''
+    MNIST experiment with PyTorch Lightning.
+    '''
+
+    # The PyTorch Lighting Experiment class only requires the user to define a
+    # model and a dataloader.
+    def __init__(self, config: DictConfig):
+        super().__init__(
+            config,
+            MNISTClassifier,
+            MNISTDataModule
+        )
+
+    def get_objective_value(self):
+        # Return the value logged by the LightningModule instance in the
+        # test_step method.
+        return self.results['test'][-1]['test_loss']
