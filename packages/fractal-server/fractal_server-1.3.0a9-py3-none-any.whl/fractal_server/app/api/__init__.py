3f7ad2c194aac330e125580fdad15b145750169f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:35 +0000

    Add package fractal-server/1.3.0a9/fractal_server-1.3.0a9-py3-none-any.whl

---
+++ packages/fractal-server/fractal_server-1.3.0a9-py3-none-any.whl/fractal_server/app/api/__init__.py
@@ -0,0 +1,32 @@
+"""
+`api` module
+"""
+from fastapi import APIRouter
+
+from ...config import get_settings
+from ...syringe import Inject
+from .v1.dataset import router as dataset_router
+from .v1.job import router as job_router
+from .v1.project import router as project_router
+from .v1.task import router as task_router
+from .v1.workflow import router as workflow_router
+
+
+router_default = APIRouter()
+router_v1 = APIRouter()
+
+router_v1.include_router(project_router, prefix="/project", tags=["Projects"])
+router_v1.include_router(task_router, prefix="/task", tags=["Tasks"])
+router_v1.include_router(dataset_router, tags=["Datasets"])
+router_v1.include_router(workflow_router, tags=["Workflows"])
+router_v1.include_router(job_router, tags=["Jobs"])
+
+
+@router_default.get("/alive/")
+async def alive():
+    settings = Inject(get_settings)
+    return dict(
+        alive=True,
+        deployment_type=settings.DEPLOYMENT_TYPE,
+        version=settings.PROJECT_VERSION,
+    )
