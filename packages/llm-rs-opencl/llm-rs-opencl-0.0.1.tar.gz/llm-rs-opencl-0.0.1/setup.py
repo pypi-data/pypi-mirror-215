b3ba952555a443d1ed1b00f86e4892a9c2121034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package llm-rs-opencl/0.0.1/llm-rs-opencl-0.0.1.tar.gz

---
+++ packages/llm-rs-opencl/llm-rs-opencl-0.0.1.tar.gz/llm-rs-opencl-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from setuptools_rust import Binding, RustExtension
+
+packages = find_packages()
+extended_packages = {package: ["py.typed", ".pyi", "**/.pyi"] for package in packages}
+
+
+setup(
+    name="llm-rs-cuda",
+    version="1.0.0",
+    rust_extensions=[RustExtension("llm_rs.llm_rs", binding=Binding.PyO3, features=["clblast"])],
+    packages=extended_packages,
+    package_data={"llm_rs": ["*.pyi", "*.typed"]},
+    # rust extensions are not zip safe, just like C-extensions.
+    zip_safe=False,
+)
