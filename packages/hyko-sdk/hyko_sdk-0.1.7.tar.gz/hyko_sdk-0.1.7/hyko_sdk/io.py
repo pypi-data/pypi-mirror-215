3c3577fb52eaaf831ff908551ccfee06cdcd45a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:34 +0000

    Add package hyko-sdk/0.1.7/hyko_sdk-0.1.7.tar.gz

---
+++ packages/hyko-sdk/hyko_sdk-0.1.7.tar.gz/hyko_sdk-0.1.7/hyko_sdk/io.py
@@ -0,0 +1,126 @@
+from enum import Enum
+from typing import Any, Union, Optional, Callable, Dict, Generator
+
+from pydantic import BaseModel
+from pydantic.fields import ModelField
+
+
+class Number(float):
+
+    @classmethod
+    def __get_validators__(cls) -> Generator[Callable, None, None]:
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value: str, values, config, field):
+        return cls(value)
+
+    @classmethod
+    def __modify_schema__(
+        cls,
+        field_schema: Dict[str, Any],
+        field: Optional[ModelField],
+    ):
+        if field:
+            field_schema["type"] = "number"
+
+class Integer(int):
+
+    @classmethod
+    def __get_validators__(cls) -> Generator[Callable, None, None]:
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value: str, values, config, field):
+        return cls(value)
+
+    @classmethod
+    def __modify_schema__(
+        cls,
+        field_schema: Dict[str, Any],
+        field: Optional[ModelField],
+    ):
+        if field:
+            field_schema["type"] = "integer"
+
+class String(str):
+
+    @classmethod
+    def __get_validators__(cls) -> Generator[Callable, None, None]:
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value: str, values, config, field):
+        return cls(value)
+
+    @classmethod
+    def __modify_schema__(
+        cls,
+        field_schema: Dict[str, Any],
+        field: Optional[ModelField],
+    ):
+        if field:
+            field_schema["type"] = "string"
+
+
+class Image(str):
+
+    @classmethod
+    def __get_validators__(cls) -> Generator[Callable, None, None]:
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value: str, values, config, field):
+        return value
+
+    @classmethod
+    def __modify_schema__(
+        cls,
+        field_schema: Dict[str, Any],
+        field: Optional[ModelField],
+    ):
+        if field:
+            field_schema["type"] = "string"
+            field_schema["format"] = "image"
+
+
+class Audio(str):
+
+    @classmethod
+    def __get_validators__(cls) -> Generator[Callable, None, None]:
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value: str, values, config, field):
+        return value
+
+    @classmethod
+    def __modify_schema__(
+        cls,
+        field_schema: Dict[str, Any],
+        field: Optional[ModelField],
+    ):
+        if field:
+            field_schema["type"] = "string"
+            field_schema["format"] = "audio"
+
+
+# Keep the same
+class IOPortType(str, Enum):
+    NUMBER = "number"
+    INTEGER = "integer"
+    STRING = "string"
+    IMAGE = "image"
+    AUDIO = "audio"
+    ARRAY_NUMBER = "array[number]"
+    ARRAY_INTEGER = "array[integer]"
+    ARRAY_STRING = "array[string]"
+    ARRAY_IMAGE = "array[image]"
+    ARRAY_AUDIO = "array[audio]"
+
+class IOPort(BaseModel):
+    name: str
+    description: Optional[str]
+    type: IOPortType
+    required: bool
+    default: Optional[Union[float, int, str]]
