3751eb59af889fc1196355b1e547fe1455a148a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:47 +0000

    Add package WB-Custom-Logger/0.0.3/WB%20Custom%20Logger-0.0.3.tar.gz

---
+++ packages/WB-Custom-Logger/WB%20Custom%20Logger-0.0.3.tar.gz/WB Custom Logger-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='WB Custom Logger',
+    version='0.0.3',
+    description='',
+    url='https://wavebridge.com',
+    author='Tom Choi',
+    author_email='tom.choi@wavebridge.com',
+    license='(c) WaveBridge',
+    packages=find_packages(exclude=["test"]),
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+    ],
+    install_requires=[
+    ],
+    zip_safe=False
+)
