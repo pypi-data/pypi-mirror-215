1adfae7bda4ae0d5f66c329545376a36ab6628c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:10 +0000

    Add package Brian2/2.5.2/Brian2-2.5.2-cp310-cp310-win_amd64.whl

---
+++ packages/Brian2/Brian2-2.5.2-cp310-cp310-win_amd64.whl/brian2/tests/features/__init__.py
@@ -0,0 +1,10 @@
+__all__ = [
+    "FeatureTest",
+    "SpeedTest",
+    "InaccuracyError",
+    "Configuration",
+    "run_feature_tests",
+]
+
+from . import input, monitors, neurongroup, speed, synapses
+from .base import *
