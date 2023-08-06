e884e9e9c5cab905fea35541f0ffbbe6ff5529d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:48 +0000

    Add package pyzscaler/1.5.0/pyzscaler-1.5.0.tar.gz

---
+++ packages/pyzscaler/pyzscaler-1.5.0.tar.gz/pyzscaler-1.5.0/pyzscaler/__init__.py
@@ -0,0 +1,12 @@
+__author__ = "Mitch Kelly"
+__license__ = "MIT"
+__contributors__ = [
+    "Dax Mickelson",
+    "Jacob Gårder",
+]
+__version__ = "1.5.0"
+
+from pyzscaler.zcc import ZCC  # noqa
+from pyzscaler.zdx import ZDX  # noqa
+from pyzscaler.zia import ZIA  # noqa
+from pyzscaler.zpa import ZPA  # noqa
