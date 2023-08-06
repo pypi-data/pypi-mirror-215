db8b11d9ff3de7873addd34ba05c8413d6346dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:41 +0000

    Add package pretext/1.6.1.dev20230622/pretext-1.6.1.dev20230622.tar.gz

---
+++ packages/pretext/pretext-1.6.1.dev20230622.tar.gz/pretext-1.6.1.dev20230622/pretext/templates/__init__.py
@@ -0,0 +1,19 @@
+from contextlib import AbstractContextManager
+import importlib.resources as ir
+
+
+def resource_path(filename: str) -> AbstractContextManager:
+    """
+    Returns resource manager
+    Usage:
+    with resource_path('foo.bar') as filepath:
+        # do things
+    """
+    from . import resources
+
+    # Try except here is to use newer importlib function,
+    # supported starting with 3.9, and required with 3.11
+    try:
+        return ir.as_file(ir.files(resources).joinpath(filename))
+    except AttributeError:
+        return ir.path(resources, filename)
