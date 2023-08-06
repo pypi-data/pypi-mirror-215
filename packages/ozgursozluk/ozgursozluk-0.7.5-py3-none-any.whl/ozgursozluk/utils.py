faa7c5111d202dc342adb0ac8ae2fdf54996ea56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:44 +0000

    Add package ozgursozluk/0.7.5/ozgursozluk-0.7.5-py3-none-any.whl

---
+++ packages/ozgursozluk/ozgursozluk-0.7.5-py3-none-any.whl/ozgursozluk/utils.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+
+import requests
+
+
+def expires() -> datetime:
+    """One year later."""
+
+    return datetime.now() + timedelta(days=365)
+
+
+def last_commit() -> str:
+    """Return the last commit ID."""
+
+    request = requests.get("https://api.github.com/repos/beucismis/ozgursozluk/commits")
+
+    return request.json()[0]["sha"]
+
+
+def contributors() -> list:
+    """Get GitHub contributors."""
+
+    request = requests.get(
+        "https://api.github.com/repos/beucismis/ozgursozluk/contributors"
+    )
+
+    for contributor in request.json():
+        yield {
+            "username": contributor["login"],
+            "total-commit": contributor["contributions"],
+        }
