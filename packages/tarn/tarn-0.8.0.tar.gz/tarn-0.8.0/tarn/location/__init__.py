39e0249ff93217f66b06e14f166a1da9602ca292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:38 +0000

    Add package tarn/0.8.0/tarn-0.8.0.tar.gz

---
+++ packages/tarn/tarn-0.8.0.tar.gz/tarn-0.8.0/tarn/location/__init__.py
@@ -0,0 +1,7 @@
+from .disk_dict import DiskDict
+from .fanout import Fanout
+from .interface import Location, Writable
+from .levels import Level, Levels
+from .nginx import Nginx
+from .s3 import S3
+from .scp import SCP
