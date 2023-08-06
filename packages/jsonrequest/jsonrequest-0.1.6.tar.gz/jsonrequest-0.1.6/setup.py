2f60244b5cf012e524bc63b09ec1cbc691ba924d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:00 +0000

    Add package jsonrequest/0.1.6/jsonrequest-0.1.6.tar.gz

---
+++ packages/jsonrequest/jsonrequest-0.1.6.tar.gz/jsonrequest-0.1.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='jsonrequest',
+    version='0.1.6',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pydantic',
+    ],
+    author='Justin Wong',
+    description='Lightweight wrapper to make http(s) requests. POST, PATCH, and DELETE requests only support json content.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='Apache 2.0', # license type
+    license_files=('LICENSE') # path to LICENSE file
+)
