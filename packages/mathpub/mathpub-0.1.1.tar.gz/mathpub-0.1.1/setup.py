62d99606a00506eb797225bfff230502d4097351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:47 +0000

    Add package mathpub/0.1.1/mathpub-0.1.1.tar.gz

---
+++ packages/mathpub/mathpub-0.1.1.tar.gz/mathpub-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mathpub",
+    description="small library for quick formatting of math calculations",
+    packages=["mathpub"],
+    author_email="timur20041229@gmail.com",
+    requires=["IPython", "ipywidgets"],
+    zip_safe=False,
+    version="0.1.1"
+)
