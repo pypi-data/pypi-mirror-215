530552edb70abdd49c336c3f9b0b183d795bdf97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:28 +0000

    Add package hi-cli-jash/1.0.3/hi-cli-jash-1.0.3.tar.gz

---
+++ packages/hi-cli-jash/hi-cli-jash-1.0.3.tar.gz/hi-cli-jash-1.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='hi-cli-jash',
+    version='1.0.3',
+    description='A pip module that prints customized greetings.',
+    author='Your Name',
+    author_email='your@email.com',
+    py_modules=['hi_module'],
+)
