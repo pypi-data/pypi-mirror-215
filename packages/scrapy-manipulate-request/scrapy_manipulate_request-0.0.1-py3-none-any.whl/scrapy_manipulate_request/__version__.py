8d626d952ee0144bc483b217ae6b5443b90ce65b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:36 +0000

    Add package scrapy-manipulate-request/0.0.1/scrapy_manipulate_request-0.0.1-py3-none-any.whl

---
+++ packages/scrapy-manipulate-request/scrapy_manipulate_request-0.0.1-py3-none-any.whl/scrapy_manipulate_request/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+VERSION = (0, 0, '1')
+
+__title__ = "scrapy-manipulate-request"
+__description__ = "An async scrapy request downloader middleware, support random request and response manipulation."
+__version__ = '.'.join(map(str, VERSION))
+__author__ = "Dylan Chen"
+__email__ = 'dylankeep@163.com'
+__url__ = 'https://github.com/dylankeepon/ScrapyManipulateRequestMiddleware'
+__license__ = "MIT"
