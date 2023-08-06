b03753897ce073f40d073094d7b9d3988eebf882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:16 +0000

    Add package gptdjmme/0.0.6/gptdjmme-0.0.6.tar.gz

---
+++ packages/gptdjmme/gptdjmme-0.0.6.tar.gz/gptdjmme-0.0.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, 'gptdjmme/README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+VERSION = '0.0.6'
+DESCRIPTION = 'A python package to clone yourself with GPT'
+LONG_DESCRIPTION = 'A python package to clone yourself with GPT'
+
+# Setting up
+setup(
+    name="gptdjmme",
+    version=VERSION,
+    author="Daniel J. McDonald",
+    author_email="<daniel@danielmcdonald.dev>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['openai', 'python-dotenv'],
+    keywords=['python', 'gpt', 'openai', 'ai', 'chatbot', 'clone', 'me'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
+
