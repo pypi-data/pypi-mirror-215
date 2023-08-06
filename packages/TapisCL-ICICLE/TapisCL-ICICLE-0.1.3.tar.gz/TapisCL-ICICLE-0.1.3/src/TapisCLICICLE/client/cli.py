408a2aafec8adbcb20bbec3a5d7349432ee29c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:46 +0000

    Add package TapisCL-ICICLE/0.1.3/TapisCL-ICICLE-0.1.3.tar.gz

---
+++ packages/TapisCL-ICICLE/TapisCL-ICICLE-0.1.3.tar.gz/TapisCL-ICICLE-0.1.3/src/TapisCLICICLE/client/cli.py
@@ -0,0 +1,210 @@
+#!/usr/bin/python3
+import socket
+import argparse
+import sys
+import os
+import time
+import traceback
+
+import pyfiglet
+from blessed import Terminal
+
+if __name__ != "__main__":
+    from . import handlers
+    from ..socketopts import socketOpts, schemas
+    from ..commands import decorators
+    from ..utilities import killableThread
+
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(f"{__file__}")))
+server_path = os.path.join(__location__, r'../serverRun.py')
+    
+
+class ClientSideConnection(socketOpts.ClientSocketOpts, handlers.Handlers):
+    def __init__(self, connection, debug=False):
+        super().__init__("client", debug=debug)
+        self.connection = connection
+    
+    def close(self):
+        self.connection.close()
+
+
+class CLI(handlers.Handlers):
+    debug=False
+    """
+    Receive user input, either direct from bash environment or from the custom interface, then parse these commands and send them to the server to be executed. 
+    """
+    def __init__(self, IP: str, PORT: int):
+        super().__init__()
+
+        self.term = Terminal()
+        self.ip, self.port = IP, PORT
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+        # set up argparse
+        self.parser = None
+
+        setup_message = schemas.ResponseData(request_content={'setup_success':True})
+        try:
+            self.username, self.url = self.connect()
+            self.connection.send(setup_message)
+        except (KeyboardInterrupt, Exception) as e:
+            error_str = traceback.format_exc()
+            if self.debug:
+                print(error_str)
+            print(e)
+            setup_message.error = str(e)
+            setup_message.request_content['setup_success'] = False
+            self.connection.send(setup_message)
+            os._exit(0)
+
+        self.pwd = ""
+        self.current_system = ""
+
+    def parser_error(self, args):
+        print(f"Ignoring unrecognized arguments: {args}")
+    
+    def configure_parser(self, arguments):
+        parser = argparse.ArgumentParser(description="Command Line Argument Parser", exit_on_error=False, usage=argparse.SUPPRESS, add_help=False, conflict_handler='resolve')
+        parser.add_argument('command_selection')
+        parser.add_argument('positionals', nargs='*', default='default1')
+        parser.error = self.parser_error
+
+        for arg_name, arg in arguments.items():
+            parser.add_argument(f"-{arg['truncated_arg']}", arg['full_arg'],
+                                action=arg['action'])
+        return parser
+
+    def initialize_server(self): 
+        """
+        detect client operating system. The local server intitialization is different between unix and windows based systems
+        """
+        if 'win' in sys.platform:
+            os.system(rf"pythonw {server_path}")
+        else: # unix based
+            os.system(f"python {server_path} &")
+
+    #@decorators.AnimatedLoading
+    def connection_initialization(self):
+        """
+        start the local server through the client
+        """
+        startup_flag = False
+        timeout_time = time.time() + 30 # server setup timeout. If expires, there is a problem!
+        while True:
+            if time.time() > timeout_time: # connection timeout condition
+                sys.stdout.write("\r[-] Connection timeout")
+                os._exit(0)
+            try:
+                self.connection.connect((self.ip, self.port)) 
+                self.connection = ClientSideConnection(self.connection, debug=self.debug)
+                if startup_flag:
+                    startup.kill()
+                break
+            except Exception as e:
+                if not startup_flag:
+                    startup = killableThread.KillableThread(target=self.initialize_server) # run the server setup on a separate thread
+                    startup.start() 
+                    startup_flag = True # set the flag to true so the thread runs only once
+                    continue
+
+    def auth(self):
+        while True:
+            server_auth_request: schemas.AuthRequest = self.connection.receive()
+            if not server_auth_request.message and not server_auth_request.request_content:
+                self.print_response(server_auth_request.error)
+                sys.exit(0)
+            elif server_auth_request.auth_request_type == "success":
+                self.print_response(server_auth_request.message)
+                return server_auth_request.message['username'], server_auth_request.message['url']
+            form_response, repeat = self.universal_message_handler(server_auth_request, self.term)
+            if not form_response:
+                break
+            client_response_request = schemas.AuthRequest(auth_request_type=server_auth_request.auth_request_type, request_content=form_response)
+            self.connection.send(client_response_request)
+        
+    def connect(self):
+        """
+        connect to the local server
+        """
+        self.connection_initialization() 
+        #self.connection.connect((self.ip, self.port)) # enable me for debugging. Requires manual server start
+        connection_info: schemas.StartupData = self.connection.receive() # receive info from the server whether it is a first time connection
+        if connection_info.initial: # if the server is receiving its first connection for the session\
+            username, url = self.auth()
+        else:
+            username, url = connection_info.username, connection_info.url
+        arguments: schemas.ResponseData = self.connection.receive()
+        self.parser = self.configure_parser(arguments.request_content)
+
+        return username, url # return the username and url
+
+    def interface(self, kwargs):
+        if not kwargs['command_selection']:
+            return
+        command_request = schemas.CommandData(request_content=kwargs)
+        self.connection.send(command_request)
+        while True:
+            command_response = self.connection.receive()
+            if isinstance(command_response, schemas.ResponseData):
+                self.url, self.username = command_response.url, command_response.active_username
+                self.pwd, self.current_system = command_response.pwd, command_response.system
+                if command_response.exit_status:
+                    print("Exit initiated")
+                    os._exit(0)
+            handled_response, repeat = self.universal_message_handler(command_response, self.term)
+            if not handled_response:
+                break
+            handled_response = schemas.FormResponse(request_content=handled_response)
+            self.connection.send(handled_response)
+
+    def terminal_cli(self):
+        try:
+            kwargs = self.parser.parse_args()
+        except:
+            print("Invalid Arguments")
+            os._exit(0)
+        kwargs = vars(kwargs)
+        self.interface(kwargs)
+        os._exit(0)
+
+    def cli_window(self):
+        title = pyfiglet.figlet_format("-----------\nTapisCLICICLE\n-----------", font="slant") # print the title when CLI is accessed
+        print(title)
+        print(r"""If you find any issues, please create a new issue here: https://github.com/sdsc-hpc-training-org/hello_icicle_auth_clients/issues
+                Enter 'exit' to exit the client
+                Enter 'shutdown' to shut down the client and server
+                Enter 'Help' to show command options""")
+        while True:
+            try:
+                time.sleep(0.01)
+                kwargs: dict = vars(self.parser.parse_args(str(input(f"[{self.username}@{self.url}][{self.current_system}]{self.pwd} ")).split(" ")))
+                self.interface(kwargs)
+            except KeyboardInterrupt:
+                continue
+            except (ConnectionAbortedError, ConnectionResetError):
+                print("Server shutdown, exiting")
+                os._exit(0)
+            except (TypeError, argparse.ArgumentError, argparse.ArgumentTypeError) as e:
+                print(e)
+                error_str = traceback.format_exc()
+                print(error_str)
+                print("Invalid command")
+            except Exception as e:
+                error_str = traceback.format_exc()
+                if self.debug:
+                    print(error_str)
+                error_message = schemas.ResponseData(error=str(e))
+                self.connection.send(error_message)
+        
+
+    def main(self):
+        if len(sys.argv) > 1: # checks if any command line arguments were provided. Does not open CLI
+            self.terminal_cli()
+        self.cli_window()
+
+
+if __name__ == "__main__":
+    client = CLI(socket.gethostbyname(socket.gethostname()), 30000)
+    client.main()
---
+++ packages/TapisCL-ICICLE/TapisCL-ICICLE-0.1.3.tar.gz/TapisCL-ICICLE-0.1.3/src/TapisCLICICLE/commands/commandMap.py
@@ -0,0 +1,208 @@
+if __name__ != "__main__":
+    from . import systemCommands, volumeCommands, serverCommands, appCommands, podCommands, fileCommands, dataFormatters, baseCommand
+    from .query import postgres, neo4j
+    from utilities import exceptions
+    from commands.arguments.argument import Argument
+    from commands.commandOpts import CHECK_EXPLICIT_ID
+
+
+class Systems(baseCommand.BaseCommandMap):
+    """
+    @help: run operations on Tapis systems
+    """
+    data_formatter = dataFormatters.DataFormatters.system_formatter
+    command_map = {
+        'get_systems':systemCommands.get_systems(), # since initialization of commands is separate from __init__, you dont need to specify these as classes anymore
+        'get_system_info':systemCommands.get_system_info(),
+        'get_scheduler_profiles':systemCommands.get_scheduler_profiles(),
+        'verify_pki_keys':systemCommands.verify_pki_keys(),
+        'create_child_system':systemCommands.create_child_system(),
+        'create_system':systemCommands.create_system(),
+        'update_system':systemCommands.update_system(),
+        'is_enabled':systemCommands.is_enabled(),
+        'enable_system':systemCommands.enable_system(),
+        'disable_system':systemCommands.disable_system(),
+        'system':systemCommands.system(),
+        'exit_system':systemCommands.exit_system(),
+        'delete_system':systemCommands.delete_system(),
+        'undelete_system':systemCommands.undelete_system()
+    }
+
+
+class Server(baseCommand.BaseCommandMap):
+    """
+    @help: run config operations on the 
+    """
+    data_formatter = dataFormatters.DataFormatters.server_formatter
+    command_map = {
+        'whoami':serverCommands.whoami(),
+        'whereami':serverCommands.whereami(),
+        'exit':serverCommands.exit(),
+        'shutdown':serverCommands.shutdown(),
+        "get_args":serverCommands.get_args(),
+        'switch_service':serverCommands.switch_service()
+    }
+
+
+class Pods(baseCommand.BaseCommandMap):
+    """
+    @help: run operations on tapis pods
+    """
+    data_formatter = dataFormatters.DataFormatters.pod_formatter
+    command_map = {
+        'get_pods':podCommands.get_pods(),
+        'get_pod':podCommands.get_pod(),
+        'create_pod':podCommands.create_pod(),
+        'update_pod':podCommands.update_pod(),
+        'start_pod':podCommands.start_pod(),
+        'restart_pod':podCommands.restart_pod(),
+        'delete_pod':podCommands.delete_pod(),
+        'set_pod_perms':podCommands.set_pod_perms(),
+        'stop_pod':podCommands.stop_pod(),
+        'delete_pod_perms':podCommands.delete_pod_perms(),
+        'get_perms':podCommands.get_perms(),
+        'copy_pod_password':podCommands.copy_pod_password(),
+        'get_pod_logs':podCommands.get_pod_logs(),
+    }
+
+
+class Volumes(baseCommand.BaseCommandMap):
+    """
+    @help: run operations on tapis volumes and snapshots
+    """
+    command_map = {
+        'get_volumes':volumeCommands.get_volumes(),
+        'create_volume':volumeCommands.create_volume(),
+        'get_volume':volumeCommands.get_volume(),
+        'volume':volumeCommands.volume(),
+        'update_volume':volumeCommands.update_volume(),
+        'delete_volume':volumeCommands.delete_volume(),
+        'dir':volumeCommands.dir(),
+        'upload_volume':volumeCommands.upload_volume(),
+        'set_volume_permission':volumeCommands.set_volume_permission(),
+        'get_volume_permissions':volumeCommands.get_volume_permissions(),
+        'delete_volume_permission':volumeCommands.delete_volume_permission(),
+        'get_snapshots':volumeCommands.get_snapshots(),
+        'create_snapshot':volumeCommands.create_snapshot(),
+        'get_snapshot':volumeCommands.get_snapshot(),
+        'update_snapshot':volumeCommands.update_snapshot(),
+        'delete_snapshot':volumeCommands.delete_snapshot(),
+        'list_snapshot_files':volumeCommands.list_snapshot_files()
+    }
+
+
+class Files(baseCommand.BaseCommandMap):
+    """
+    @help: run operations on tapis files
+    """
+    command_map = {
+        'ls':fileCommands.ls(),
+        'cd':fileCommands.cd(),
+        'showme':fileCommands.showme(),
+        'cat':fileCommands.cat(),
+        'mkdir':fileCommands.mkdir(),
+        'mv':fileCommands.mv(),
+        'cp':fileCommands.cp(),
+        'rm':fileCommands.rm(),
+        'get_recent_transfers':fileCommands.get_recent_transfers(),
+        'create_postit':fileCommands.create_postit(),
+        'list_postits':fileCommands.list_postits(),
+        'get_postit':fileCommands.get_postit(),
+        'delete_postit':fileCommands.delete_postit(),
+        'redeem_postit':fileCommands.redeem_postit(),
+        'upload':fileCommands.upload(),
+        'download':fileCommands.download(),
+    }
+
+
+class Apps(baseCommand.BaseCommandMap):
+    """
+    @help: Run operations on tapis apps
+    """
+    data_formatter = dataFormatters.DataFormatters.app_formatter
+    command_map = {
+        'create_app':appCommands.create_app(),
+        'get_apps':appCommands.get_apps(),
+        'delete_app':appCommands.delete_app(),
+        'get_app':appCommands.get_app(),
+        'run_job':appCommands.run_job(),
+        'get_job_status':appCommands.get_job_status(),
+        'download_job_output':appCommands.download_job_output(),
+    }
+
+
+class Query(baseCommand.BaseCommandMap):
+    """
+    @help: run integrated query CLIs
+    """
+    command_map = {
+        'postgres': postgres.postgres(),
+        'neo4j': neo4j.neo4j()
+    }
+
+
+class ArgsGenerator:
+    def generate_truncated_arguments(self, arg_dict: dict) -> dict:
+        truncation_dict = dict()
+        for argument_name in arg_dict.keys():
+            truncated_argument = self.__generate_truncated_argument(argument_name, truncation_dict)
+            truncation_dict[argument_name] = truncated_argument
+        return truncation_dict
+            
+    def __generate_truncated_argument(self, argument, truncated_arguments_dict, attempt=1):
+        if argument[attempt-1] in ('_', '-'):
+            attempt += 1
+        truncated_argument = argument[:attempt]
+        if truncated_argument in list(truncated_arguments_dict.values()):
+            return self.__generate_truncated_argument(argument, truncated_arguments_dict, attempt=attempt+1)
+        return truncated_argument
+    
+
+class AggregateCommandMap(baseCommand.CommandContainer, ArgsGenerator):
+    groups = {
+        'Systems': Systems(),
+        'Server': Server(),
+        'Pods': Pods(),
+        'Files': Files(),
+        #'Apps': Apps(),
+        'Query': Query(),
+        'Volumes':Volumes()
+    }
+    def __init__(self):
+        truncated_arguments = self.generate_truncated_arguments(self.arguments)
+        for command in self.aggregate_command_map.values():
+            command.update_args_with_truncated(truncated_arguments)
+        for name, argument in self.arguments.items():
+            argument.truncated_arg = truncated_arguments[name]
+        self.help = self.__general_help()
+
+    def __general_help(self):
+        general_help = list()
+        for group in self.groups.values():
+            general_help.append(group.brief_help)
+        return general_help
+
+    def update_credentials(self, t, username, password):
+        for command_name, command in self.aggregate_command_map.items():
+            command.set_t_and_creds(t, username, password, self)
+
+    async def run_command(self, connection, command_data: dict):
+        """
+        process and run command based on received kwargs
+        """
+        command_name = command_data['command_selection']
+
+        if command_name in list(self.aggregate_command_map.keys()):
+            command = self.aggregate_command_map[command_name]
+            if command.supports_config_file and 'file' in list(command_data.keys()) and command_data['file']:
+                command_data = {'file':command_data['file']}
+
+            command_data['connection'] = connection
+            command_data['server'] = self
+            return await command(**command_data)
+        elif command_name in list(self.groups.keys()):
+            return self.groups[command_name]()
+        elif command_name == "help":
+            return self.help
+        else:
+            raise exceptions.CommandNotFoundError(command_name)
