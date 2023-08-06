df1dd6840ce7bd19828fb4800283a9fcb739da28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:51 +0000

    Add package datamazing/0.0.5/datamazing-0.0.5.tar.gz

---
+++ packages/datamazing/datamazing-0.0.5.tar.gz/datamazing-0.0.5/datamazing/pandas/__init__.py
@@ -0,0 +1,5 @@
+from . import testing
+from .transformations.basic import latest, merge, shift_time, unique_one
+from .transformations.grouping import Grouper, GrouperResampler, group
+from .transformations.resampling import Resampler, resample
+from .types import TimeInterval
