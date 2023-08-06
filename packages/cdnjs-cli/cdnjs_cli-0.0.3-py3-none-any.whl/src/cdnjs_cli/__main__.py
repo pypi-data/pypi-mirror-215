a67edfcface31cc87b5b94ddfb7f8fb7cf9be750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:40 +0000

    Add package cdnjs-cli/0.0.3/cdnjs_cli-0.0.3-py3-none-any.whl

---
+++ packages/cdnjs-cli/cdnjs_cli-0.0.3-py3-none-any.whl/src/cdnjs_cli/__main__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2023-present woidzero <woidzeroo@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+import sys
+from .cli import cdnjs
+
+
+def main():
+    sys.exit(cdnjs())
