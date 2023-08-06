36a2243daf711b31e4c4489d90d97e49b45e5d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:52 +0000

    Add package sum-test-test/0.2/sum_test_test-0.2.tar.gz

---
+++ packages/sum-test-test/sum_test_test-0.2.tar.gz/sum_test_test-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="sum_test_test",
+    version="0.2",
+    description="A publishing test",
+    author = "Test",
+    url="https://github.com/mohamed-fadl-hanalytics/package_test.git",
+    packages=["sum"]
+)
