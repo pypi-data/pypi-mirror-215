3afd8a4296ae9ac3412a41bcf3b2260dbfeff7e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:05 +0000

    Add package mflag/1.6.6/mflag-1.6.6-py3-none-any.whl

---
+++ packages/mflag/mflag-1.6.6-py3-none-any.whl/mflag/__init__.py
@@ -0,0 +1,4 @@
+from mflag.src.flag import Flag, JobManager
+from mflag.src.rcmng.server import run as RMServer
+from mflag.src.rcmng.client import Client as RMClient
+from mflag.src.rcmng.submit import Submit as RMSubmit
