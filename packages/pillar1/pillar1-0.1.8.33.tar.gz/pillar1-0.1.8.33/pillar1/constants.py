e13fe37b276c82f99329c04553ade2a956f7dce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:50 +0000

    Add package pillar1/0.1.8.33/pillar1-0.1.8.33.tar.gz

---
+++ packages/pillar1/pillar1-0.1.8.33.tar.gz/pillar1-0.1.8.33/pillar1/constants.py
@@ -0,0 +1,35 @@
+STARCODER_BETA = """
+
+col_name,data_type,comment
+Source,string,
+NetworkID,int,
+DiagCode,string,
+DiagDesc,string,
+CPTDesc,string,
+IncurredDate,date,
+SubmittedDate,date,
+PaidDate,date,
+SubmittedAmount,int,
+PaidAmount,int,
+ClaimStatus,string,
+ServicingProviderNPI,bigint,
+ServicingProviderName,string,
+BillingProviderNPI,bigint,
+BillingProviderName,string,
+PlaceofService,string,
+DrugName,string,
+DrugClass,string,
+GPI,bigint,
+NDC,string,
+DaysSupplied,int,
+LengthOfStay,int,
+PatientFirstName,string,
+PatientLastName,string,
+PatientGender,string,
+PatientDateOfBirth,date,
+PatientAddress,string,
+PatientState,string,
+PatientZIP,int,
+PatientID,string.
+"""
+STARCODER_BETA += "When answering the request use the information listed above only, do not make up column names and do not place additional conditions"
