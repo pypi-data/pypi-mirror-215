b5efa030fdb10f79b64dd4542fff9b4c87688684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:50 +0000

    Add package cryptoowl/0.0.6/cryptoowl-0.0.6.tar.gz

---
+++ packages/cryptoowl/cryptoowl-0.0.6.tar.gz/cryptoowl-0.0.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cryptoowl",
+    version="0.0.6",
+    author="Cryptoowl",
+    author_email="cryptoowl.app@gmail.com",
+    description="A library, where you store common codes for different modules for cryptoowl",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["pytz"],
+)
