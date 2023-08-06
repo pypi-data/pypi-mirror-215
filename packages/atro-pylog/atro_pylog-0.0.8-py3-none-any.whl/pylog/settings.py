cced7d6ce278b3cf67826b60a491e0f57706d477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:10 +0000

    Add package atro-pylog/0.0.8/atro_pylog-0.0.8-py3-none-any.whl

---
+++ packages/atro-pylog/atro_pylog-0.0.8-py3-none-any.whl/pylog/settings.py
@@ -0,0 +1,28 @@
+from pydantic import BaseSettings
+import logging
+
+from pylog.logger_type import LoggerType
+
+
+class BaseLoggerSettings(BaseSettings):
+    name: str = "pylog"
+    type: LoggerType  = LoggerType.RICH
+    level: int | str  = logging.DEBUG
+    msg_format: str = "%(message)s"
+    date_format: str = "%X"
+    
+    class Config:
+        env_prefix = "ATRO_PYLOG_"
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+class OpenTelemetryLoggerSettings(BaseSettings):
+    service_name: str = "pylog"
+    instance_id: str = "pylog"
+    endpoint: str | None = None
+
+    class Config:
+        env_prefix = "ATRO_PYLOG_"
+        env_file = ".env"
+        env_file_encoding = "utf-8"
