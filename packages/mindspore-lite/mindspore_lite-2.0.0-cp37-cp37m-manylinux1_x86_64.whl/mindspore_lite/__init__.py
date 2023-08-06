c850e1bb0d60f64c154131c270340d49418062e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:05 +0000

    Add package mindspore-lite/2.0.0/mindspore_lite-2.0.0-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/mindspore-lite/mindspore_lite-2.0.0-cp37-cp37m-manylinux1_x86_64.whl/mindspore_lite/__init__.py
@@ -0,0 +1,46 @@
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""
+MindSpore Lite Python API.
+"""
+from __future__ import absolute_import
+import os
+import logging
+
+from mindspore_lite.version import __version__
+from mindspore_lite.context import Context
+from mindspore_lite.converter import FmkType, Converter
+from mindspore_lite.model import ModelType, Model, ModelParallelRunner
+from mindspore_lite.tensor import DataType, Format, Tensor
+
+
+def install_custom_kernels():
+    """install  mindspore lite custom kernels"""
+    custom_kernel_path = os.path.join(__path__[0], "custom_kernels/")
+    if os.path.exists(custom_kernel_path):
+        ascend_custom_kernel_path = custom_kernel_path + "ascend/"
+        install_script_path = ascend_custom_kernel_path + "install.sh"
+        cmd_str = "bash " + install_script_path
+        out = os.popen(cmd_str).read()
+        logging.info(out)
+    else:
+        logging.error("no custom kernel %s", custom_kernel_path)
+
+__all__ = []
+__all__.extend(__version__)
+__all__.extend(context.__all__)
+__all__.extend(converter.__all__)
+__all__.extend(model.__all__)
+__all__.extend(tensor.__all__)
