70c21e77f7d83c72dc0600304536cd6ba04042af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:49 +0000

    Add package biopipen/0.14.1/biopipen-0.14.1.tar.gz

---
+++ packages/biopipen/biopipen-0.14.1.tar.gz/biopipen-0.14.1/biopipen/scripts/scrna/CellTypeAnnotate-direct.R
@@ -0,0 +1,26 @@
+library(Seurat)
+
+sobjfile = {{in.sobjfile | r}}
+outfile = {{out.outfile | r}}
+celltypes = {{envs.cell_types | r}}
+
+sobj = readRDS(sobjfile)
+idents = as.character(unique(Idents(sobj)))
+idents = idents[order(as.numeric(idents))]
+
+if (!is.list(celltypes)) {
+    celltypes = celltypes[1:length(idents)]
+    celltypes = as.list(celltypes)
+    names(celltypes) = idents
+}
+
+if (length(celltypes) == 0) {
+    warning("No cell types are given!")
+} else {
+    celltypes$object = sobj
+    sobj = do_call(RenameIdents, celltypes)
+    sobj$seurat_clusters = Idents(sobj)
+
+}
+
+saveRDS(sobj, outfile)
