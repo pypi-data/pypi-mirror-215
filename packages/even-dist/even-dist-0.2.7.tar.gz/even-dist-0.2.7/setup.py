daf2968312f7edb3557995a09b51b5d56c0094d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:36 +0000

    Add package even-dist/0.2.7/even-dist-0.2.7.tar.gz

---
+++ packages/even-dist/even-dist-0.2.7.tar.gz/even-dist-0.2.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="even-dist",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="src", include=["*"]),
+    package_dir={"": "src"},
+    include_package_data=True,
+    version="0.2.7",
+    description="My first Python library",
+    install_requires=["numpy", "matplotlib"],
+    author="kirp",
+    author_email="kirp@umich.edu",
+    license="MIT",
+    setup_requires=["pytest-runner"],
+    test_suite="tests",
+    url="https://github.com/tic-top/even_dist/",
+)
