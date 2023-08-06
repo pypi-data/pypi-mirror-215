d3a9cef59866582c61636dad914cc08a43685a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:40 +0000

    Add package packageint/0.1/packageint-0.1.tar.gz

---
+++ packages/packageint/packageint-0.1.tar.gz/packageint-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="packageint",
+version="0.1",
+description="This is code",
+long_description="This is a very long description",
+author="Int-209",
+packages=['packageint'],
+install_requires=[])
