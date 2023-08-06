6d437b5d3571395354723318b29487be46b7d4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:32 +0000

    Add package censius/1.7.3/censius-1.7.3.tar.gz

---
+++ packages/censius/censius-1.7.3.tar.gz/censius-1.7.3/src/censius/endpoint.py
@@ -0,0 +1,5 @@
+
+""" This file was generated automatically using `generate.py` in "PROD" mode, do not edit it manually.
+	Generated at: 2023-06-22 09:14:04 UTC """
+
+CENSIUS_ENDPOINT = "https://gateway.censius.ai/"
