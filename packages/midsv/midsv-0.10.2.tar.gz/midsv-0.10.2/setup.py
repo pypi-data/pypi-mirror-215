2ce4b9a8b23ea7d4b5fe5dda461fb7549aff4f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:25 +0000

    Add package midsv/0.10.2/midsv-0.10.2.tar.gz

---
+++ packages/midsv/midsv-0.10.2.tar.gz/midsv-0.10.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="midsv",
+    version="0.10.2",
+    author="Akihiro Kuno",
+    author_email="akuno@md.tsukuba.ac.jp",
+    description="Python module to convert SAM to MIDSV format.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akikuno/midsv",
+    packages=setuptools.find_packages(
+        where="src",
+    ),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
