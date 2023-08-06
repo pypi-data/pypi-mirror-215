b364b96345bb1a29777cc9471fd2f7a754863c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:45 +0000

    Add package dbt-copilot-tools/0.1.3/dbt_copilot_tools-0.1.3.tar.gz

---
+++ packages/dbt-copilot-tools/dbt_copilot_tools-0.1.3.tar.gz/dbt_copilot_tools-0.1.3/copilot_helper.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from importlib.metadata import version
+
+import click
+
+from commands.bootstrap_cli import bootstrap as bootstrap_commands
+from commands.check_cloudformation import \
+    check_cloudformation as check_cloudformation_command
+from commands.codebuild_cli import codebuild as codebuild_commands
+from commands.copilot_cli import copilot as copilot_commands
+from commands.dns_cli import domain as domain_commands
+
+
+@click.group()
+@click.version_option(
+    version=version("dbt-copilot-tools"),
+    message=f"dbt-copilot-tools %(version)s",
+)
+def cli():
+    pass
+
+
+cli.add_command(bootstrap_commands)
+cli.add_command(check_cloudformation_command)
+cli.add_command(copilot_commands)
+cli.add_command(codebuild_commands)
+cli.add_command(domain_commands)
+
+if __name__ == "__main__":
+    cli()
