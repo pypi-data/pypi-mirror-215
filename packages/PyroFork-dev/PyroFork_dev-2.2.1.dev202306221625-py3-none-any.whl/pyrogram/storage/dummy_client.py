4d4a87a79e1644d1dcfc21794ab5e2600f08858c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:45 +0000

    Add package PyroFork-dev/2.2.1.dev202306221625/PyroFork_dev-2.2.1.dev202306221625-py3-none-any.whl

---
+++ packages/PyroFork-dev/PyroFork_dev-2.2.1.dev202306221625-py3-none-any.whl/pyrogram/storage/dummy_client.py
@@ -0,0 +1,45 @@
+from pymongo.client_session import TransactionOptions
+from bson.codec_options import CodecOptions
+from pymongo.read_concern import ReadConcern
+from pymongo.read_preferences import (
+    Nearest,
+    Primary,
+    PrimaryPreferred,
+    Secondary,
+    SecondaryPreferred,
+)
+from pymongo.write_concern import WriteConcern
+from typing import Any, Optional, Union
+from typing import runtime_checkable
+
+try:
+    from typing import Protocol
+except ImportError:
+    from typing_extensions import Protocol
+
+ReadPreferences = Union[Primary, PrimaryPreferred, Secondary, SecondaryPreferred, Nearest]
+
+@runtime_checkable
+class DummyMongoClient(Protocol):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        raise NotImplementedError
+
+    def get_database(
+        self,
+        name: Optional[str] = None,
+        *,
+        codec_options: Optional[CodecOptions] = None,
+        read_preference: Optional[ReadPreferences] = None,
+        write_concern: Optional[WriteConcern] = None,
+        read_concern: Optional[ReadConcern] = None,
+    ):
+        raise NotImplementedError
+    
+    async def start_session(
+        self,
+        *,
+        causal_consistency: Optional[bool] = None,
+        default_transaction_options: Optional[TransactionOptions] = None,
+        snapshot: bool = False,
+    ):
+        raise NotImplementedError
