b4585732eca61f39ca22df801229828f6a6fbcd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:20 +0000

    Add package aiotieba/3.4.5/aiotieba-3.4.5-cp310-cp310-musllinux_1_1_x86_64.whl

---
+++ packages/aiotieba/aiotieba-3.4.5-cp310-cp310-musllinux_1_1_x86_64.whl/aiotieba/const.py
@@ -0,0 +1,10 @@
+MAIN_VERSION = "12.43.1.0"
+POST_VERSION = "9.1.0.0"
+
+APP_SECURE_SCHEME = "https"
+APP_INSECURE_SCHEME = "http"
+
+APP_BASE_HOST = "tiebac.baidu.com"
+WEB_BASE_HOST = "tieba.baidu.com"
+
+CHECK_URL_PERFIX = "http://tieba.baidu.com/mo/q/checkurl?url="
