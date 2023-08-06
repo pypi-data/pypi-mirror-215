9d153365784e664b44b8fb1b67c2c8bbfad0cb42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:10 +0000

    Add package Auto-Vermietung/0.0.7/Auto_Vermietung-0.0.7.tar.gz

---
+++ packages/Auto-Vermietung/Auto_Vermietung-0.0.7.tar.gz/Auto_Vermietung-0.0.7/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Auto_Vermietung",
+    version="0.0.7",
+    author="Yann Macro",
+    author_email="",
+    description="My first Python Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    project_urls={
+        "Bug Tracker": "https://github.com/Xytoup/Auto_Vermietung_Macromedia",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    package_dir={'':"src"},
+    packages=find_packages("src"),
+    python_requires=">=3.6",
+)
