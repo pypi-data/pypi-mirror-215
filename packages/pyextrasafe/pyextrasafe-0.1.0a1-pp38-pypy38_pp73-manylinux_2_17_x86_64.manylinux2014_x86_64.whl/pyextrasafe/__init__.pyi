229f81a2c790a8f59c17ba2518b373755b217de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:26 +0000

    Add package pyextrasafe/0.1.0a1/pyextrasafe-0.1.0a1-pp38-pypy38_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyextrasafe/pyextrasafe-0.1.0a1-pp38-pypy38_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyextrasafe/__init__.pyi
@@ -0,0 +1,11 @@
+"""
+PyExtraSafe is a library that makes it easy to improve your program’s security by selectively
+allowing the syscalls it can perform via the Linux kernel’s seccomp facilities.
+"""
+
+from pyextrasafe._pyextrasafe import *
+
+__all__: list[str]
+__author__: str
+__license__: str
+__version__: str
