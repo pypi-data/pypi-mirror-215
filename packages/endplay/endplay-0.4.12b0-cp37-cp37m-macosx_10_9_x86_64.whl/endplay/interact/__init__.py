da3ed7a47fb990adcf9bb0131ebd76a0cc58bfca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:33 +0000

    Add package endplay/0.4.12b0/endplay-0.4.12b0-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/endplay/endplay-0.4.12b0-cp37-cp37m-macosx_10_9_x86_64.whl/endplay/interact/__init__.py
@@ -0,0 +1,8 @@
+"""
+Provides the `InteractiveDeal` class which keeps an undo stack whenever its 
+state is modified, making it easier to interact with the deal. 
+"""
+
+__all__ = ["InteractiveDeal"]
+
+from endplay.interact.interactivedeal import InteractiveDeal
