efb57255fb2d6b8ba3f7a9584cf0cb7428bfc52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:52 +0000

    Add package spacecutter-torch/1.0.0/spacecutter_torch-1.0.0.tar.gz

---
+++ packages/spacecutter-torch/spacecutter_torch-1.0.0.tar.gz/spacecutter_torch-1.0.0/spacecutter/__init__.py
@@ -0,0 +1,9 @@
+from .losses import CumulativeLinkLoss
+from .models import OrdinalLogisticHead
+from .callbacks import AscensionCallback
+
+__all__ = [
+    "CumulativeLinkLoss",
+    "OrdinalLogisticHead",
+    "AscensionCallback",
+]
