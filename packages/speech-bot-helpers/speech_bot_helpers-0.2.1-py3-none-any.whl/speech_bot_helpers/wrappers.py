75c1661622720a5e30986fc3df1c400c6ea22e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:51 +0000

    Add package speech-bot-helpers/0.2.1/speech_bot_helpers-0.2.1-py3-none-any.whl

---
+++ packages/speech-bot-helpers/speech_bot_helpers-0.2.1-py3-none-any.whl/speech_bot_helpers/wrappers.py
@@ -0,0 +1,11 @@
+from exceptions import S3ClientException
+
+
+def catch(func):
+    async def wrapper(*args, **kwargs):
+        res = await func(*args, **kwargs)
+        if not res.get("ResponseMetadata", {}).get("HTTPStatusCode") == 200:
+            raise S3ClientException
+        return res
+
+    return wrapper
