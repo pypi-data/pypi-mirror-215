d59ec90c9e0e852f005a579407cf6a741391c234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:23 +0000

    Add package irregular-chars/1.1.0/irregular-chars-1.1.0.tar.gz

---
+++ packages/irregular-chars/irregular-chars-1.1.0.tar.gz/irregular-chars-1.1.0/README.md
@@ -0,0 +1,21 @@
+# irregular-chars
+
+irregular-chars is a library for cleaning text, such as removing zero-width characters or converting full-width characters to half-width.
+
+## Installation
+
+You can install the package via pip:
+```bash
+pip install irregular-chars
+```
+
+## Usage
+
+Here is a simple example:
+```py
+from irregular_chars.zero_width import remove_zero_width
+
+text = "Hello\u200BWorld"
+clean_text = remove_zero_width(text)
+print(clean_text)  # Outputs: HelloWorld
+```
---
+++ packages/irregular-chars/irregular-chars-1.1.0.tar.gz/irregular-chars-1.1.0/tests/test_zero_width_spaces.py
@@ -0,0 +1,29 @@
+import pytest
+
+from irregular_chars import remove_zero_width
+from irregular_chars.zero_width import ZERO_WIDTH_SPACES
+
+
+@pytest.mark.parametrize("name, char", ZERO_WIDTH_SPACES.items())
+def test_remove_zero_width(name, char):
+    test_str = f"Hello{char}World"
+    expected_str = "HelloWorld"
+    assert remove_zero_width(test_str) == expected_str
+
+
+def test_remove_zero_width_multiple_and_mixed():
+    test_str = f"Hello{ZERO_WIDTH_SPACES['ZERO WIDTH SPACE']}W{ZERO_WIDTH_SPACES['ZERO WIDTH JOINER']}orld"
+    expected_str = "HelloWorld"
+    assert remove_zero_width(test_str) == expected_str
+
+
+def test_remove_zero_width_no_change():
+    test_str = "Hello World"
+    expected_str = "Hello World"
+    assert remove_zero_width(test_str) == expected_str
+
+
+def test_remove_zero_width_use_quotations():
+    origin = '"​"""⁡""­"'
+    expect = '"""""""'
+    assert expect == remove_zero_width(origin)
