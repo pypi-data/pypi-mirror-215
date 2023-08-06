3c254d35ccc5be5b196c28ea7f97a250775518c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:59 +0000

    Add package hi-cli-jash/0.1.0/hi-cli-jash-0.1.0.tar.gz

---
+++ packages/hi-cli-jash/hi-cli-jash-0.1.0.tar.gz/hi-cli-jash-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='hi-cli-jash',
+    version='0.1.0',
+    description='A simple CLI tool that prints "Hi"',
+    py_modules=['hijash'],
+    entry_points={
+        'console_scripts': [
+            'hi=hi:say_hi',
+        ],
+    },
+    author='jashwanth peddisetty',
+    author_email='jashwanth0712@gmail.com',
+)
