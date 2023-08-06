82b76a928009865823495bf42457b97d5d304e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:11 +0000

    Add package ai-embedder/1.0.1/ai_embedder-1.0.1.tar.gz

---
+++ packages/ai-embedder/ai_embedder-1.0.1.tar.gz/ai_embedder-1.0.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as readme_file:
+    README = readme_file.read()
+
+
+def _get_version():
+    version = os.getenv("RELEASE_VERSION", default="1.0")
+    if version.startswith("v"):
+        version = version[1:]
+    return version
+
+
+setup(name='ai_embedder',
+      version=_get_version(),
+      description='This script takes .ai files and save them with linked files embedded.',
+      author='mdobosz',
+      author_email='doboszsite@gmail.com',
+      packages=find_packages(),
+      long_description_content_type="text/markdown",
+      long_description=README,
+      install_requires=[
+            "altgraph==0.17.3",
+           " pefile==2023.2.7",
+            "pywin32==306",
+            "pywin32-ctypes==0.2.1"
+      ]
+      )
