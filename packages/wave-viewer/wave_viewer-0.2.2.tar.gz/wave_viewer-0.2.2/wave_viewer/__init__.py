f792f5e96c7e4b611133bfe133e1784da43684d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:39 +0000

    Add package wave-viewer/0.2.2/wave_viewer-0.2.2.tar.gz

---
+++ packages/wave-viewer/wave_viewer-0.2.2.tar.gz/wave_viewer-0.2.2/wave_viewer/__init__.py
@@ -0,0 +1,28 @@
+"""
+Wave Viewer
+===========
+
+A simple GUI for viewing waveforms. It plots the waveform with vispy in a
+separate process. The GUI is built with PySide6.
+
+The `WaveViewer` class is the main interface for the wave_viewer package. It
+provides methods for adding and removing lines from the plot, clearing the plot,
+and closing the GUI.
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata
+else:
+    import importlib_metadata as metadata
+
+from ._controller import WaveViewer
+
+__all__ = ["WaveViewer"]
+
+# from setuptools-scm docs
+try:
+    __version__ = metadata.version("wave-viewer")
+except metadata.PackageNotFoundError:
+    # package is not installed
+    pass
