5088fc551e24d4cd29631133584cf21d61fd3883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:13 +0000

    Add package calcipy/1.3.0/calcipy-1.3.0-py3-none-any.whl

---
+++ packages/calcipy/calcipy-1.3.0-py3-none-any.whl/calcipy/tasks/types.py
@@ -0,0 +1,27 @@
+"""Types CLI."""
+
+from beartype import beartype
+from corallium.file_helpers import read_package_name
+from invoke.context import Context
+
+from ..cli import task
+from ..invoke_helpers import run
+
+
+@beartype
+def _inner_task(ctx: Context, *, cli_args: str, command: str) -> None:
+    """Shared task logic."""
+    pkg_name = read_package_name()
+    run(ctx, f'poetry run {command} {pkg_name}{cli_args}')
+
+
+@task()
+def pyright(ctx: Context) -> None:
+    """Run pyright."""
+    _inner_task(ctx, cli_args='', command='pyright')
+
+
+@task()
+def mypy(ctx: Context) -> None:
+    """Run mypy."""
+    _inner_task(ctx, cli_args='', command='python -m mypy')
