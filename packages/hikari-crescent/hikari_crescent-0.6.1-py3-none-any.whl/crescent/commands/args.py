632703e873ce4f9c5965ae009bb828b2456dbe66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:42 +0000

    Add package hikari-crescent/0.6.1/hikari_crescent-0.6.1-py3-none-any.whl

---
+++ packages/hikari-crescent/hikari_crescent-0.6.1-py3-none-any.whl/crescent/commands/args.py
@@ -0,0 +1,116 @@
+from __future__ import annotations
+
+from abc import ABC
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Generic
+
+from hikari import ChannelType, CommandChoice
+
+from crescent.locale import LocaleBuilder
+from crescent.typedefs import AutocompleteValueT
+
+if TYPE_CHECKING:
+    from typing import Any, Sequence
+
+    from crescent.typedefs import AutocompleteCallbackT
+
+__all__: Sequence[str] = (
+    "Description",
+    "Name",
+    "Choices",
+    "ChannelTypes",
+    "MaxValue",
+    "MinValue",
+    "Autocomplete",
+)
+
+
+class Arg(ABC):
+    @property
+    def payload(self) -> Any:
+        """Returns the data for this object"""
+        ...
+
+    def __hash__(self) -> int:
+        return super().__hash__() ^ hash(self.payload)
+
+
+@dataclass(frozen=True)
+class Description(Arg):
+    description: str | LocaleBuilder
+
+    @property
+    def payload(self) -> str | LocaleBuilder:
+        return self.description
+
+
+@dataclass(frozen=True)
+class Name(Arg):
+    name: str | LocaleBuilder
+
+    @property
+    def payload(self) -> str | LocaleBuilder:
+        return self.name
+
+
+class Choices(Arg):
+    def __init__(self, *choices: CommandChoice) -> None:
+        self.choices = choices
+
+    @property
+    def payload(self) -> Sequence[CommandChoice]:
+        return self.choices
+
+
+class ChannelTypes(Arg):
+    def __init__(self, *channel_types: ChannelType) -> None:
+        self.channel_types = channel_types
+
+    @property
+    def payload(self) -> Sequence[ChannelType]:
+        return self.channel_types
+
+
+@dataclass(frozen=True)
+class MaxValue(Arg):
+    max_value: int
+
+    @property
+    def payload(self) -> int:
+        return self.max_value
+
+
+@dataclass(frozen=True)
+class MinValue(Arg):
+    min_value: int
+
+    @property
+    def payload(self) -> int:
+        return self.min_value
+
+
+@dataclass(frozen=True)
+class MinLength(Arg):
+    min_length: int
+
+    @property
+    def payload(self) -> int:
+        return self.min_length
+
+
+@dataclass(frozen=True)
+class MaxLength(Arg):
+    max_length: int
+
+    @property
+    def payload(self) -> int:
+        return self.max_length
+
+
+@dataclass(frozen=True)
+class Autocomplete(Arg, Generic[AutocompleteValueT]):
+    callback: AutocompleteCallbackT[AutocompleteValueT]
+
+    @property
+    def payload(self) -> AutocompleteCallbackT[AutocompleteValueT]:
+        return self.callback
---
+++ packages/hikari-crescent/hikari_crescent-0.6.1-py3-none-any.whl/crescent/context/context.py
@@ -0,0 +1,270 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, overload
+
+from hikari import (
+    UNDEFINED,
+    GatewayGuild,
+    GuildThreadChannel,
+    MessageFlag,
+    PermissibleGuildChannel,
+    ResponseType,
+)
+from hikari.traits import CacheAware
+
+from crescent.context.base_context import BaseContext
+
+if TYPE_CHECKING:
+    from typing import Any, Literal, Sequence
+
+    from hikari import (
+        Attachment,
+        CommandInteraction,
+        Embed,
+        Message,
+        PartialRole,
+        PartialUser,
+        Resourceish,
+        SnowflakeishSequence,
+        UndefinedNoneOr,
+        UndefinedOr,
+        UndefinedType,
+    )
+    from hikari.api import ComponentBuilder
+
+
+__all__: Sequence[str] = ("Context",)
+
+
+class Context(BaseContext):
+    """Represents the context for command interactions"""
+
+    interaction: CommandInteraction
+
+    @property
+    def channel(self) -> PermissibleGuildChannel | GuildThreadChannel | None:
+        """Get this context's guild channel or thread from the cache.
+
+        > 📝 This will always be `None` for interactions triggered in a DM channel.
+        """
+        if isinstance(self.app, CacheAware):
+            return self.app.cache.get_guild_channel(self.channel_id) or self.app.cache.get_thread(
+                self.channel_id
+            )
+        return None
+
+    @property
+    def guild(self) -> GatewayGuild | None:
+        """Get this context's guild from the cache."""
+        return self.interaction.get_guild()
+
+    async def defer(self, ephemeral: bool = False) -> None:
+        """
+        Defer this interaction response, allowing you to respond within the next 15
+        minutes.
+        """
+
+        if future := self._unset_future:
+            future.set_result(self.interaction.build_deferred_response())
+        else:
+            await self.app.rest.create_interaction_response(
+                interaction=self.id,
+                token=self.token,
+                flags=MessageFlag.EPHEMERAL if ephemeral else UNDEFINED,
+                response_type=ResponseType.DEFERRED_MESSAGE_CREATE,
+            )
+        self._has_deferred_response = True
+
+    @overload
+    async def respond(
+        self,
+        content: UndefinedOr[Any] = UNDEFINED,
+        *,
+        ensure_message: Literal[True],
+        ephemeral: bool = False,
+        flags: int | MessageFlag | UndefinedType = UNDEFINED,
+        tts: UndefinedOr[bool] = UNDEFINED,
+        attachment: UndefinedOr[Resourceish] = UNDEFINED,
+        attachments: UndefinedOr[Sequence[Resourceish]] = UNDEFINED,
+        component: UndefinedOr[ComponentBuilder] = UNDEFINED,
+        components: UndefinedOr[Sequence[ComponentBuilder]] = UNDEFINED,
+        embed: UndefinedOr[Embed] = UNDEFINED,
+        embeds: UndefinedOr[Sequence[Embed]] = UNDEFINED,
+        mentions_everyone: UndefinedOr[bool] = UNDEFINED,
+        user_mentions: UndefinedOr[SnowflakeishSequence[PartialUser] | bool] = UNDEFINED,
+        role_mentions: UndefinedOr[SnowflakeishSequence[PartialRole] | bool] = UNDEFINED,
+    ) -> Message:
+        ...
+
+    @overload
+    async def respond(
+        self,
+        content: UndefinedOr[Any] = UNDEFINED,
+        *,
+        ephemeral: bool = False,
+        flags: int | MessageFlag | UndefinedType = UNDEFINED,
+        tts: UndefinedOr[bool] = UNDEFINED,
+        attachment: UndefinedOr[Resourceish] = UNDEFINED,
+        attachments: UndefinedOr[Sequence[Resourceish]] = UNDEFINED,
+        component: UndefinedOr[ComponentBuilder] = UNDEFINED,
+        components: UndefinedOr[Sequence[ComponentBuilder]] = UNDEFINED,
+        embed: UndefinedOr[Embed] = UNDEFINED,
+        embeds: UndefinedOr[Sequence[Embed]] = UNDEFINED,
+        mentions_everyone: UndefinedOr[bool] = UNDEFINED,
+        user_mentions: UndefinedOr[SnowflakeishSequence[PartialUser] | bool] = UNDEFINED,
+        role_mentions: UndefinedOr[SnowflakeishSequence[PartialRole] | bool] = UNDEFINED,
+        ensure_message: Literal[False] = ...,
+    ) -> Message | None:
+        ...
+
+    async def respond(
+        self,
+        content: UndefinedOr[Any] = UNDEFINED,
+        *,
+        ephemeral: bool = False,
+        flags: int | MessageFlag | UndefinedType = UNDEFINED,
+        tts: UndefinedOr[bool] = UNDEFINED,
+        attachment: UndefinedOr[Resourceish] = UNDEFINED,
+        attachments: UndefinedOr[Sequence[Resourceish]] = UNDEFINED,
+        component: UndefinedOr[ComponentBuilder] = UNDEFINED,
+        components: UndefinedOr[Sequence[ComponentBuilder]] = UNDEFINED,
+        embed: UndefinedOr[Embed] = UNDEFINED,
+        embeds: UndefinedOr[Sequence[Embed]] = UNDEFINED,
+        mentions_everyone: UndefinedOr[bool] = UNDEFINED,
+        user_mentions: UndefinedOr[SnowflakeishSequence[PartialUser] | bool] = UNDEFINED,
+        role_mentions: UndefinedOr[SnowflakeishSequence[PartialRole] | bool] = UNDEFINED,
+        ensure_message: bool = False,
+    ) -> Message | None:
+        if ephemeral:
+            if flags is UNDEFINED:
+                flags = MessageFlag.EPHEMERAL
+            else:
+                flags |= MessageFlag.EPHEMERAL
+
+        kwargs: dict[str, Any] = dict(
+            content=content,
+            attachment=attachment,
+            attachments=attachments,
+            component=component,
+            components=components,
+            embed=embed,
+            embeds=embeds,
+            mentions_everyone=mentions_everyone,
+            user_mentions=user_mentions,
+            role_mentions=role_mentions,
+        )
+
+        if not (self._has_deferred_response or self._has_created_message):
+            if future := self._unset_future:
+                resp = (
+                    self.interaction.build_response()
+                    .set_content(content)
+                    .set_user_mentions(user_mentions)
+                    .set_role_mentions(role_mentions)
+                    .set_mentions_everyone(mentions_everyone)
+                    .set_flags(flags)
+                    .set_tts(tts)
+                )
+                if attachments:
+                    for a in attachments:
+                        resp = resp.add_attachment(a)
+                if attachment:
+                    resp = resp.add_attachment(attachment)
+                if components:
+                    for c in components:
+                        resp = resp.add_component(c)
+                if component:
+                    resp = resp.add_component(component)
+                if embeds:
+                    for e in embeds:
+                        resp = resp.add_embed(e)
+                if embed:
+                    resp = resp.add_embed(embed)
+
+                future.set_result(resp)
+            else:
+                await self.app.rest.create_interaction_response(
+                    **kwargs,
+                    tts=tts,
+                    flags=flags,
+                    interaction=self.id,
+                    token=self.token,
+                    response_type=ResponseType.MESSAGE_CREATE,
+                )
+
+            self._has_created_message = True
+
+            if not ensure_message:
+                return None
+
+            return await self.app.rest.fetch_interaction_response(self.application_id, self.token)
+
+        if self._has_deferred_response and not self._has_created_message:
+            res = await self.edit(**kwargs)
+            self._has_created_message = True
+            return res
+
+        return await self.followup(**kwargs)
+
+    async def edit(
+        self,
+        content: UndefinedNoneOr[Any] = UNDEFINED,
+        *,
+        attachment: UndefinedNoneOr[Resourceish | Attachment] = UNDEFINED,
+        attachments: UndefinedNoneOr[Sequence[Resourceish | Attachment]] = UNDEFINED,
+        component: UndefinedNoneOr[ComponentBuilder] = UNDEFINED,
+        components: UndefinedNoneOr[Sequence[ComponentBuilder]] = UNDEFINED,
+        embed: UndefinedNoneOr[Embed] = UNDEFINED,
+        embeds: UndefinedNoneOr[Sequence[Embed]] = UNDEFINED,
+        mentions_everyone: UndefinedOr[bool] = UNDEFINED,
+        user_mentions: UndefinedOr[SnowflakeishSequence[PartialUser] | bool] = UNDEFINED,
+        role_mentions: UndefinedOr[SnowflakeishSequence[PartialRole] | bool] = UNDEFINED,
+    ) -> Message:
+        return await self.app.rest.edit_interaction_response(
+            application=self.application_id,
+            token=self.token,
+            content=content,
+            attachment=attachment,
+            attachments=attachments,
+            component=component,
+            components=components,
+            embed=embed,
+            embeds=embeds,
+            mentions_everyone=mentions_everyone,
+            user_mentions=user_mentions,
+            role_mentions=role_mentions,
+        )
+
+    async def followup(
+        self,
+        content: UndefinedNoneOr[Any] = UNDEFINED,
+        *,
+        attachment: UndefinedOr[Resourceish] = UNDEFINED,
+        attachments: UndefinedOr[Sequence[Resourceish]] = UNDEFINED,
+        component: UndefinedOr[ComponentBuilder] = UNDEFINED,
+        components: UndefinedOr[Sequence[ComponentBuilder]] = UNDEFINED,
+        embed: UndefinedOr[Embed] = UNDEFINED,
+        embeds: UndefinedOr[Sequence[Embed]] = UNDEFINED,
+        mentions_everyone: UndefinedOr[bool] = UNDEFINED,
+        user_mentions: UndefinedOr[SnowflakeishSequence[PartialUser] | bool] = UNDEFINED,
+        role_mentions: UndefinedOr[SnowflakeishSequence[PartialRole] | bool] = UNDEFINED,
+    ) -> Message:
+        return await self.app.rest.execute_webhook(
+            webhook=self.application_id,
+            token=self.token,
+            content=content,
+            attachment=attachment,
+            attachments=attachments,
+            component=component,
+            components=components,
+            embed=embed,
+            embeds=embeds,
+            mentions_everyone=mentions_everyone,
+            user_mentions=user_mentions,
+            role_mentions=role_mentions,
+        )
+
+    async def delete(self) -> None:
+        await self.app.rest.delete_interaction_response(
+            application=self.application_id, token=self.token
+        )
