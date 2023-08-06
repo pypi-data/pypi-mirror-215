e9abb2c8205402f7fdfe7fedb744972fe51e91da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:37 +0000

    Add package ctd-python/0.1.0/ctd-python-0.1.0.tar.gz

---
+++ packages/ctd-python/ctd-python-0.1.0.tar.gz/ctd-python-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='ctd-python',
+    version='0.1.0',
+    description='Python interface to access data from The Comparative Toxicogenomics Database (CTD)',
+    packages=['ctd'],
+    install_requires=[
+        'requests',
+        'pandas',
+    ],
+    package_data={'ctd': ['zipped_data/*', 'unzipped_data/*']},
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
