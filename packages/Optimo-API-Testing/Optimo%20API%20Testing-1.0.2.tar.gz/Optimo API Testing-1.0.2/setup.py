e03fc1be7d9b8e42b36cc94b0ba48a897afd72d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:32 +0000

    Add package Optimo-API-Testing/1.0.2/Optimo%20API%20Testing-1.0.2.tar.gz

---
+++ packages/Optimo-API-Testing/Optimo%20API%20Testing-1.0.2.tar.gz/Optimo API Testing-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='Optimo API Testing',
+    version='1.0.2',
+    author='Bacho Labadze',
+    author_email='bachukilabadze@gmail.com',
+    description='Package for testing optimo API',
+    packages=['OptimoTests','Optimo_Instance_Manager'],
+    install_requires=requirements
+)
