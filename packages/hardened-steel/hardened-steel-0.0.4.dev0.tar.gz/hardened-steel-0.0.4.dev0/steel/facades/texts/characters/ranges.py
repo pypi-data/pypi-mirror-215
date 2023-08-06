b2872129cbdf6c06b8d43ced0b5f364d804d7323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:16 +0000

    Add package hardened-steel/0.0.4.dev0/hardened-steel-0.0.4.dev0.tar.gz

---
+++ packages/hardened-steel/hardened-steel-0.0.4.dev0.tar.gz/hardened-steel-0.0.4.dev0/steel/facades/texts/characters/ranges.py
@@ -0,0 +1,59 @@
+from steel.facades.texts.characters \
+    import get_system_random
+
+ascii_lowercase_begin:  int = ord('a')
+ascii_lowercase_end:    int = ord('z')
+
+ascii_uppercase_begin:  int = ord('A')
+ascii_uppercase_end:    int = ord('Z')
+
+
+def random_letter() -> chr:
+    random = get_system_random()
+
+    choose_uppercase: bool = bool(
+        random.getrandbits(1)
+    )
+
+    if choose_uppercase:
+        return random_uppercase_letter()
+    else:
+        return random_lowercase_letter()
+
+
+def random_uppercase_letter() -> chr:
+    global                          \
+        ascii_uppercase_begin,      \
+        ascii_uppercase_end
+
+    return character_in_range(
+        ascii_uppercase_begin,
+        ascii_uppercase_end
+    )
+
+
+def random_lowercase_letter() -> chr:
+    global                          \
+        ascii_lowercase_begin,      \
+        ascii_lowercase_end
+
+    return character_in_range(
+        ascii_lowercase_begin,
+        ascii_lowercase_end
+    )
+
+
+def character_in_range(
+        begin: int,
+        end: int
+) -> chr:
+    random = get_system_random()
+
+    random_number: int = random.randint(
+        begin,
+        end
+    )
+
+    return chr(
+        random_number
+    )
