169aeed0c1341c1167871e35eb08da1ddfbb3e23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:36 +0000

    Add package packageten/0.1/packageten-0.1.tar.gz

---
+++ packages/packageten/packageten-0.1.tar.gz/packageten-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name="packageten",
+version="0.1",
+description="this code can be used",
+long_description="this is long",
+author="souj",
+packages=['packageten'],
+install_requires=[]
+)
