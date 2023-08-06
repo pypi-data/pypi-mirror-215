0b0583a293cce72a0194afd787c32258dd37f8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:23 +0000

    Add package haupt/2.0.0rc21/haupt-2.0.0rc21.tar.gz

---
+++ packages/haupt/haupt-2.0.0rc21.tar.gz/haupt-2.0.0rc21/haupt/common/commands/management/commands/tables.py
@@ -0,0 +1,37 @@
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--app",
+            dest="app",
+            default="coredb",
+        )
+
+    @staticmethod
+    def update_migrations(cursor, app):
+        query = "UPDATE django_migrations SET app = 'db' where app='{}'".format(app)
+        cursor.execute(query)
+
+    @staticmethod
+    def update_table(cursor):
+        check_query = (
+            "select * from information_schema.tables where table_name='{}'".format(
+                "auth_user"
+            )
+        )
+        cursor.execute(check_query)
+        if bool(cursor.fetchall()):
+            alter_query = "ALTER TABLE auth_user RENAME TO db_user"
+            cursor.execute(alter_query)
+
+    def handle(self, *args, **options):
+        from django.conf import settings
+        from django.db import connection
+
+        if settings.DB_ENGINE_NAME == "sqlite":
+            return
+
+        with connection.cursor() as cursor:
+            self.update_migrations(cursor, options["app"])
