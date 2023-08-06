bb2a4f86ac4df89181a7acd448cfc989dcfaa35b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:28 +0000

    Add package newcaclculator/1.0.0/newcaclculator-1.0.0.tar.gz

---
+++ packages/newcaclculator/newcaclculator-1.0.0.tar.gz/newcaclculator-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='newcaclculator',
+    version='1.0.0',
+    author='soujanya',
+    author_email='soujanyaacharya2001@gmail.com',
+    description='A description of pytoolboxtwo',
+    url='https://github.com/SoujanyaRAcharya/newcalculator',
+    packages=['newcalculator'],
+    install_requires=[
+        'numpy',
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
