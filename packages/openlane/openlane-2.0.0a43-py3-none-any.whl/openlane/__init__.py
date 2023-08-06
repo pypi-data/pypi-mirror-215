7eed2283d3d49e862234ee4a9107317ef30c854b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:37 +0000

    Add package openlane/2.0.0a43/openlane-2.0.0a43-py3-none-any.whl

---
+++ packages/openlane/openlane-2.0.0a43-py3-none-any.whl/openlane/__init__.py
@@ -0,0 +1,33 @@
+# Copyright 2023 Efabless Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# flake8: noqa
+"""
+The OpenLane API
+
+These modules serve as the infrastructure for OpenLane-based flows.
+Using these functions, users may either utilize built-in flows, or build custom
+Flows and/or custom Steps for the design of more sophisticated chips.
+
+..
+    no-imported-members
+"""
+
+from .config import Variable, Config, InvalidConfig
+from .flows import Flow, SequentialFlow
+from .state import State, DesignFormat
+from .steps import Step
+from .common import *
+from .plugins import discovered_plugins
+from .__version__ import __version__
