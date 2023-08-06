07e2e0cc410b4b5eab6e329a0ec548f255d9171f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:05 +0000

    Add package monopoly-simulator-test/0.0.2/monopoly_simulator_test-0.0.2.tar.gz

---
+++ packages/monopoly-simulator-test/monopoly_simulator_test-0.0.2.tar.gz/monopoly_simulator_test-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+import setuptools
+
+setup(name='monopoly_simulator_test',
+      version='0.0.2',
+      description='USC/ISI monopoly simulator',
+      author='Min-Hsueh Chiu',
+      author_email='minhsueh@isi.edu',
+      url = "https://github.com/mayankkejriwal/GNOME-p3/tree/phase3_simulator_v1",
+      #packages=['monopoly'],
+      #package_dir={'monopoly': 'monopoly/monopoly_simulator'},
+      #package_data={'monopoly': ['./*.json']},
+      packages=setuptools.find_packages(),
+      data_files=[
+            ('', ['monopoly_simulator/monopoly_game_schema_v1-2.json']),
+            ('', ['monopoly_simulator/phase3_novelty_config.json'])
+      ],
+      include_package_data=True,
+      install_requires=['numpy', 'xlsxwriter']
+     )
