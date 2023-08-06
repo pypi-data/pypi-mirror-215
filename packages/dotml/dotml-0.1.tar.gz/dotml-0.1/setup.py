5a9828c7a76f7741c354408fca30f0a9e0249894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:45 +0000

    Add package dotml/0.1/dotml-0.1.tar.gz

---
+++ packages/dotml/dotml-0.1.tar.gz/dotml-0.1/setup.py
@@ -0,0 +1,24 @@
+import os
+
+from setuptools import setup, find_packages
+
+# Get the long description from the README file
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='dotml',
+    version='0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'dotml = dotML.cli:app',
+        ],
+    },
+    install_requires=[
+        'pyyaml',
+        'typer'
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
