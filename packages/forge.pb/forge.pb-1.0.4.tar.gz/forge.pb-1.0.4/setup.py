3df6abce6b1e4095a6c360f22ca9337ed4253cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:26 +0000

    Add package forge.pb/1.0.4/forge.pb-1.0.4.tar.gz

---
+++ packages/forge.pb/forge.pb-1.0.4.tar.gz/forge.pb-1.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
+setup(
+    name="forge.pb",
+    version="1.0.4",
+    url='https://github.com/provenance-io/forge.pb',
+    author='Wyatt Baker',
+    author_email='wbaker@figure.com',
+    packages=['forgepb','forgepb.cmd'],
+    install_requires = [requirements],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'forge=forgepb.command_line:start'
+        ]
+    },
+    keyword="provenance, node, bootstrap",
+    description="Node manager for the Provenance Blockchain",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
