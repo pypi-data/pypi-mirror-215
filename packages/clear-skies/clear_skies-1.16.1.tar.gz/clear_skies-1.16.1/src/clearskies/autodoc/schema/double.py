455175b1be63a3a53a8b0649604d35a63ea3c013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:42 +0000

    Add package clear-skies/1.16.1/clear_skies-1.16.1.tar.gz

---
+++ packages/clear-skies/clear_skies-1.16.1.tar.gz/clear_skies-1.16.1/src/clearskies/autodoc/schema/double.py
@@ -0,0 +1,6 @@
+from .number import Number
+class Double(Number):
+    _format = 'double'
+
+    def __init__(self, name, example=None, value=None):
+        super().__init__(name, example=example, value=value)
