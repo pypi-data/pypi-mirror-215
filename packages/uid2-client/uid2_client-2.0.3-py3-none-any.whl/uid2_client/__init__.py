452f6ae6315d5418899a35facbe9c272ac0e37ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:04 +0000

    Add package uid2-client/2.0.3/uid2_client-2.0.3-py3-none-any.whl

---
+++ packages/uid2-client/uid2_client-2.0.3-py3-none-any.whl/uid2_client/__init__.py
@@ -0,0 +1,16 @@
+"""Client implementation and helper functions for integrating with the UID2 services.
+
+Classes:
+    Uid2Client: main API for interacting with a UID service
+
+Functions:
+    decrypt_token: decrypt and advertising token to extract advertising ID from it
+"""
+
+
+from .auto_refresh import *
+from .client import *
+from .encryption import *
+from .keys import *
+
+
