595f655ecbb0b1c688dc841d7e6d4b28af0ad01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:03 +0000

    Add package FluidCubeGame/1.0.6/FluidCubeGame-1.0.6-py3-none-any.whl

---
+++ packages/FluidCubeGame/FluidCubeGame-1.0.6-py3-none-any.whl/src/FluidCubeGame/main.py
@@ -0,0 +1,39 @@
+import FluidCubeGame.FluidCubeGame as fc
+import pygame
+import sys
+import cv2
+
+N = fc.N
+SCALE = fc.SCALE
+       
+
+def run():
+    pygame.init()
+    screen = pygame.display.set_mode((N*SCALE, N*SCALE))
+    pygame.display.set_caption('FluidCubeGame')
+    fluid = fc.FluidCube(1e-9 / 2, 1e-7, 1e-2)
+    clock = pygame.time.Clock()
+    lx, ly, x, y = 0, 0, 0, 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        clock.tick(100)
+        screen.fill(0)
+        fluid.FluidCubeStep()
+        lx, ly = x, y 
+        x, y = pygame.mouse.get_pos()
+        fluid.FluidCubeAddDensity(x//SCALE, y//SCALE, 300)
+        amtX = float(x - lx) 
+        amtY = float(y - ly) 
+        fluid.FluidCubeAddVelocity(x//SCALE, y//SCALE, amtX, amtY)
+        fluid.fadeD()
+        fluid.renderD()
+        pygame.display.flip()
+
+def main():
+    run()
+
+if __name__ == '__main__':
+    main()
