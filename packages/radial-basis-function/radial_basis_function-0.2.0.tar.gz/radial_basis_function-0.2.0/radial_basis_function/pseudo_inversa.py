74f175a62fd268ca772d26050f0b036dce0f96e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:39 +0000

    Add package radial-basis-function/0.2.0/radial_basis_function-0.2.0.tar.gz

---
+++ packages/radial-basis-function/radial_basis_function-0.2.0.tar.gz/radial_basis_function-0.2.0/radial_basis_function/pseudo_inversa.py
@@ -0,0 +1,58 @@
+"""
+Classe e métodos para implementar a Regressão por multiplicação de Matriz Pseudo-Inversa.
+
+Original Author: Victor Venites
+"""
+
+import numpy as np
+import pandas as pd
+import time
+
+class PseudoInversa:
+    """Multiplicacao de Matriz Pseudo-Inversa
+    Original Author: Victor Venites
+    """
+    def __init__(self, bias = 1):
+        self.weights = None
+        self.bias = bias
+        self.feature_names_in_ = None
+        self.n_feature_in_ = None
+        self.n_linhas_in_ = None
+        self.time = None
+
+    def fit(self, X, y):
+        try:
+            self.feature_names_in_ = X.columns
+            self.n_feature_in_ = X.shape[1]
+            self.n_linhas_in_ = X.shape[0]
+        except:
+            self.n_feature_in_ = len(X[0])
+            self.n_linhas_in_ = len(X)
+        Matriz_X = pd.DataFrame(X)
+        if self.bias != 0:
+            Matriz_X["Bias_Vies_Intercept"] = self.bias
+        tempo_Inicial = time.time()
+        # Base_T * Base
+        Matriz_Quadrada = np.dot(Matriz_X.T, Matriz_X)
+        # Matriz Inversa
+        try:
+            Matriz_Inversa = np.linalg.inv(Matriz_Quadrada)
+        except np.linalg.LinAlgError as err:
+            if 'Singular matrix' in str(err):
+                Matriz_Regulada = Matriz_Quadrada + np.identity(len(Matriz_Quadrada))
+                Matriz_Inversa = np.linalg.inv(Matriz_Regulada)
+        # Inversa * Base_Dados_T
+        Matriz_Pseudo_Inversa = pd.DataFrame(np.dot(Matriz_Inversa, Matriz_X.T))
+        # Isola o peso final
+        self.weights = np.dot(Matriz_Pseudo_Inversa, y)
+        self.time = time.time() - tempo_Inicial
+
+    def predict(self, X):
+        Matriz_X = pd.DataFrame(X)
+        if self.bias != 0:
+            Matriz_X["Bias_Vies_Intercept"] = self.bias
+        return np.dot(Matriz_X, self.weights)
+
+    def score(self, x, y):
+        # TODO: adicionar métricas do próprio Sklearn
+        pass
