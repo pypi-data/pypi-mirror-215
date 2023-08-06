2ca51163f1b2a6736e2297b402464d8443b6c84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:50 +0000

    Add package ngdistributionsudacity/0.1/ngdistributionsudacity-0.1.tar.gz

---
+++ packages/ngdistributionsudacity/ngdistributionsudacity-0.1.tar.gz/ngdistributionsudacity-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ngdistributionsudacity',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['ngdistributionsudacity'],
+      author = 'Idriss Nguepi',
+      author_email = 'inguepi@aimsammi.org',
+      zip_safe=False)
