2d73b3b5224af439a4418a823f00b180a508611c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:10 +0000

    Add package NSMap/1.0/NSMap-1.0-py3-none-any.whl

---
+++ packages/NSMap/NSMap-1.0-py3-none-any.whl/NS-MapToolkit/__init__.py
@@ -0,0 +1,2 @@
+from modelSystems import Logistic, LogisticP, FoodChain, FoodChainP, generateTimeSeriesContinuous, generateTimeSeriesDiscrete, generateLogisticMapProcessNoise
+from NSMapToolkit import 
