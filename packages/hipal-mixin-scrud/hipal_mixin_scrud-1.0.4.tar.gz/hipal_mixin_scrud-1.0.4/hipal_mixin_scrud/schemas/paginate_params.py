76a9962f13a645f6ccd599b679cd1f08dc34bbdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:29 +0000

    Add package hipal-mixin-scrud/1.0.4/hipal_mixin_scrud-1.0.4.tar.gz

---
+++ packages/hipal-mixin-scrud/hipal_mixin_scrud-1.0.4.tar.gz/hipal_mixin_scrud-1.0.4/hipal_mixin_scrud/schemas/paginate_params.py
@@ -0,0 +1,13 @@
+from typing import Optional
+from pydantic import BaseModel
+
+from hipal_mixin_scrud.enums.operators import Operators
+from hipal_mixin_scrud.enums.sorts import Sort
+
+
+class PaginateParams(BaseModel):
+    offset: Optional[int] = 0
+    limit: Optional[int] = 10
+    search: Optional[str]
+    sort: Optional[Sort]
+    sort_field: Optional[str]
