32257c20e2843d321044e90c7681028c1148e8f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:53 +0000

    Add package PyQt-Fluent-Widgets/0.9.6/PyQt-Fluent-Widgets-0.9.6.tar.gz

---
+++ packages/PyQt-Fluent-Widgets/PyQt-Fluent-Widgets-0.9.6.tar.gz/PyQt-Fluent-Widgets-0.9.6/qfluentwidgets/components/navigation/__init__.py
@@ -0,0 +1,6 @@
+from .navigation_widget import (NavigationWidget, NavigationPushButton, NavigationSeparator, NavigationToolButton,
+                                NavigationTreeWidget, NavigationTreeWidgetBase)
+from .navigation_panel import NavigationPanel, NavigationItemPosition, NavigationDisplayMode
+from .navigation_interface import NavigationInterface
+from .pivot import Pivot, PivotItem
+from .segmented_widget import SegmentedItem, SegmentedWidget
