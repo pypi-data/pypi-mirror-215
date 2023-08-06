1969501b95bb94133385156451a5483f1737a740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:45 +0000

    Add package XPER/0.0.14/XPER-0.0.14-py3-none-any.whl

---
+++ packages/XPER/XPER-0.0.14-py3-none-any.whl/XPER/compute/__init__.py
@@ -0,0 +1,5 @@
+from XPER.compute.EM import * 
+from XPER.compute.Optimisation import OptimizationClass
+from XPER.compute.XGBoost_model import *
+from XPER.compute.Performance import *
+from XPER.compute.Load import xgboost
