75d02aea8f5e6b4b5ec95aaec4886bfe02313583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:20 +0000

    Add package GaussianBinomial-probability-dist03/0.3/GaussianBinomial_probability_dist03-0.3.tar.gz

---
+++ packages/GaussianBinomial-probability-dist03/GaussianBinomial_probability_dist03-0.3.tar.gz/GaussianBinomial_probability_dist03-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='GaussianBinomial_probability_dist03',
+      version='0.3',
+      description='Gaussian and Binomial distributions',
+      packages=['GaussianBinomial_probability_dist03'],
+      author = 'Ali Rezghi',
+      zip_safe=False)
